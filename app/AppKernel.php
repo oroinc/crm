3dc5c11178d28e67c3d1a58d7fc3115e8e5fa37d
Nicolas Dupont <nicolas@akeneo.com>
2012-12-18 18:06:55 +0100

    BAP-17 : add datamodel, product and manufacturer bundles, setup service manager, eav like impl for product, classic impl for manufacturer, setup unit tests, remove acme demo bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,10 +19,12 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Oro\Bundle\DataModelBundle\OroDataModelBundle(),
+            new Oro\Bundle\ProductBundle\OroProductBundle(),
+            new Oro\Bundle\ManufacturerBundle\OroManufacturerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
