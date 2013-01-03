2874e3e645381e37914f3e18559e96b247ac19f8
vladimir <vladimire@magecore.com>
2013-01-03 16:22:28 +0200

    add new structure

--- includes/head.php
+++ includes/head.php
@@ -5,7 +5,7 @@
     <!-- Bootstrap -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript"> localStorage.clear();</script>
-    <link rel="stylesheet/less" href="less/main.less" media="all" />
+    <link rel="stylesheet/less" href="css/less/main.less" media="all" />
     <script src="lib/less-1.3.1.min.js"></script>
     <link href="css/all.css" rel="stylesheet" media="all">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
