ce54c26f915da1be8bef88e5998a371b729232cb
yurio <yurio@ua.fm>
2013-01-11 16:33:04 +0200

    BAP-26: fixes

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oro\Bundle\UIBundle\Controller;
+namespace Oro\UIBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
