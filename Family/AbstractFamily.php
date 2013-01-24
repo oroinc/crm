2eeb90afce45111f9d21064ff9d01938a19a5db3
Romain Monceau <romain@akeneo.com>
2013-01-24 16:13:21 +0100

    BAP-38 : Refactoring : Rename Measure by Family for group of measures

---
+++ Family/AbstractFamily.php
@@ -0,0 +1,16 @@
+<?php
+namespace Oro\Bundle\MeasureBundle\Family;
+
+/**
+ * Abstract class for measures
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ * @abstract
+ */
+class AbstractFamily
+{
+
+}
