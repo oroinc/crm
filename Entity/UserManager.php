8f44e1bdf6eb947a16704ae4e185c75c4c2c72fa
Dan <danil@magecore.com>
2013-01-28 15:50:16 +0200

    BAP-49: Login and Forgot Password Forms

--- Entity/UserManager.php
+++ Entity/UserManager.php
@@ -3,8 +3,29 @@
 namespace Oro\Bundle\UserBundle\Entity;
 
 use FOS\UserBundle\Doctrine\UserManager as BaseUserManager;
+use FOS\UserBundle\Model\UserInterface;
 
 class UserManager extends BaseUserManager
 {
+    /**
+     * Finds a user by email
+     *
+     * @param string $email
+     * @return UserInterface
+     */
+    public function findUserByEmail($email)
+    {
+        return $this->findUserBy(array('email' => $this->canonicalizeEmail($email)));
+    }
 
+    /**
+     * Finds a user by username
+     *
+     * @param string $username
+     * @return UserInterface
+     */
+    public function findUserByUsername($username)
+    {
+        return $this->findUserBy(array('username' => $this->canonicalizeUsername($username)));
+    }
 }
---
+++ Security/UserProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Security;
+
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface as SecurityUserInterface;
+
+use FOS\UserBundle\Security\UserProvider as BaseUserProvider;
+
+use Oro\Bundle\UserBundle\Entity\User;
+
+class UserProvider extends BaseUserProvider
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function refreshUser(SecurityUserInterface $user)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Expected an instance of FOS\UserBundle\Model\User, but got "%s".', get_class($user)));
+        }
+
+        if (null === $reloadedUser = $this->userManager->findUserBy(array('id' => $user->getId()))) {
+            throw new UsernameNotFoundException(sprintf('User with ID "%d" could not be reloaded.', $user->getId()));
+        }
+
+        return $reloadedUser;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function findUser($username)
+    {
+        return $this->userManager->findUserByUsernameOrEmail($username);
+    }
+}
