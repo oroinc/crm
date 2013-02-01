5e68822160c412004872da0a952636baa79f19e2
yurio <yurio@ua.fm>
2013-02-01 19:30:53 +0200

    BAP-55: Check access

--- Aop/AclInterceptor.php
+++ Aop/AclInterceptor.php
@@ -9,36 +9,50 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use JMS\SecurityExtraBundle\Exception\RuntimeException;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class AclInterceptor implements MethodInterceptorInterface
 {
-    private $context;
+    /**
+     * @var \Symfony\Component\Security\Core\SecurityContextInterface
+     */
+    private $securityContext;
+
+    /**
+     * @var \Symfony\Component\HttpKernel\Log\LoggerInterface
+     */
     private $logger;
+
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
     private $container;
+
     private $em;
     private $reader;
+    private $accessDecisionManager;
 
     public function __construct(SecurityContextInterface $context, LoggerInterface $logger, ContainerInterface $container)
     {
-        $this->context = $context;
+        $this->securityContext = $context;
         $this->logger = $logger;
         $this->container = $container;
         $this->em = $container->get('doctrine')->getManager();
         $this->reader = $container->get('annotation_reader');
+        $this->accessDecisionManager = $container->get('security.access.decision_manager');
     }
 
-    public function intercept(MethodInvocation $invocation)
+    public function intercept(MethodInvocation $method)
     {
-        /*$this->logger->info(sprintf('User invoked class: "%s" method "%s".', $invocation->reflection->class, $invocation->reflection->name));
+        $this->logger->info(sprintf('User invoked class: "%s" method "%s".', $method->reflection->class, $method->reflection->name));
         //get acl method resource name
-        $aclAnnotation = $this->reader->getMethodAnnotation($invocation->reflection, 'Oro\Bundle\UserBundle\Annotation\Acl');
+        $aclAnnotation = $this->reader->getMethodAnnotation($method->reflection, 'Oro\Bundle\UserBundle\Annotation\Acl');
 
-        if (!$invocation->reflection->name == 'testAction') {
-
-        } else {
-            //throw new RuntimeException('yessss');
+        $token = $this->securityContext->getToken();
+        if (false === $this->accessDecisionManager->decide($token, array(ROLE_SUPER_ADMIN), $method)) {
+            throw new AccessDeniedException('Access denied.');
         }
-        return $invocation->proceed();*/
+
+        return $method->proceed();
     }
 }
