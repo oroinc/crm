9174360eaac7adb302ccbf4625304f5ab8b94258
Romain Monceau <romain@akeneo.com>
2013-02-05 18:49:51 +0100

    BAP-43 : Add chain directory

---
+++ Chain/ConnectorChain.php
@@ -0,0 +1,54 @@
+<?php
+namespace Oro\Bundle\DataFlowBundle\Chain;
+
+use Oro\Bundle\DataFlowBundle\Connector\ConnectorInterface;
+
+/**
+ * Chain to define all connectors
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ */
+class ConnectorChain
+{
+
+    /**
+     *
+     * @var multitype
+     */
+    protected $connectors;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->connectors = array();
+    }
+
+    /**
+     * Add a connector
+     * @param ConnectorInterface $connector
+     *
+     * @return ConnectorChain
+     */
+    public function addConnector(ConnectorInterface $connector)
+    {
+        $this->connectors[] = $connector;
+
+        return $this;
+    }
+
+    /**
+     * Get the list of connectors
+     *
+     * @return multitype:ConnectorInterface
+     */
+    public function getConnectors()
+    {
+        return $this->connectors;
+    }
+
+}
