ec708831ee84e91e5c2d854b6f262b4f5f50d77b
Romain Monceau <romain@akeneo.com>
2013-02-05 17:47:54 +0100

    BAP-43 : Move exceptions and make them extend DataFlowException

---
+++ Exception/CurlException.php
@@ -0,0 +1,16 @@
+<?php
+namespace Oro\Bundle\DataFlowBundle\Source\Exception;
+
+/**
+ * Curl exception.
+ * Generated if problems encountered using curl library
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ */
+class CurlException extends SourceException
+{
+
+}
