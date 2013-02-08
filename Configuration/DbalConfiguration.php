885177c7d5ef165b27999db2d7341dab8a13270e
Romain Monceau <romain@akeneo.com>
2013-02-08 16:34:53 +0100

    BAP-43 : Add DbalConfiguration class

---
+++ Configuration/DbalConfiguration.php
@@ -0,0 +1,66 @@
+<?php
+namespace Oro\Bundle\DataFlowBundle\Configuration;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+/**
+ * Define DBAL configuration
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ */
+class DbalConfiguration extends AbstractConfiguration
+{
+
+    /**
+     * Label of root node configuration
+     * @staticvar string
+     */
+    const ROOT_NODE = 'database';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root(static::ROOT_NODE);
+
+        $rootNode
+            ->children()
+                ->enumNode('driver')
+                    ->cannotBeEmpty()
+                    ->isRequired()
+                    ->values(array('pdo_mysql', 'pdo_sqlite', 'pdo_pgsql', 'pdo_oci', 'pdo_sqlsrv', 'oci8'))
+                ->end()
+
+                ->scalarNode('username')
+                    ->cannotBeEmpty()
+                    ->isRequired()
+                ->end()
+
+                ->scalarNode('password')->isRequired()->end()
+
+                ->scalarNode('host')->end()
+
+                ->scalarNode('port')->end()
+
+                ->scalarNode('dbname')->end()
+
+                ->scalarNode('unix_socket')->end()
+
+                ->scalarNode('charset')->end()
+
+                ->scalarNode('path')->end()
+
+                ->booleanNode('memory')->end()
+
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+
+}
