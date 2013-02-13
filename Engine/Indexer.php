ee36a43e696060e93d9dbfb609326db20e73ec5f
yurio <yurio@ua.fm>
2013-02-13 16:30:57 +0200

    BAP-85: flexible test entity

--- Engine/Indexer.php
+++ Engine/Indexer.php
@@ -4,7 +4,6 @@ namespace Oro\Bundle\SearchBundle\Engine;
 
 use Doctrine\Common\Persistence\ObjectManager;
 
-use Oro\Bundle\SearchBundle\Entity\Queue;
 use Oro\Bundle\SearchBundle\Query\Query;
 
 class Indexer
@@ -85,83 +84,5 @@ class Indexer
         if ($query->getQuery() == Query::SELECT) {
             return $this->adapter->search($query);
         }
-    }
-
-    /**
-     * Delete record from search index
-     *
-     * @param \Oro\Bundle\SearchBundle\Entity\Queue $queue
-     *
-     * @return bool|array
-     */
-    public function delete(Queue $queue)
-    {
-        $result = $this->adapter->delete($queue->getEntity(), $queue->getRecordId());
-        $this->em->remove($queue);
-        $this->em->flush();
-        if ($result) {
-            return array('recordId' => $result, 'action' => Queue::EVENT_DELETE);
-        }
-
-        return false;
-    }
-
-    /**
-     * Save record to search index
-     *
-     * @param \Oro\Bundle\SearchBundle\Entity\Queue $queue
-     *
-     * @return bool|array
-     */
-    public function save(Queue $queue)
-    {
-        $object = $this->em->getRepository($queue->getEntity())->find($queue->getRecordId());
-        if ($object) {
-            $result = $this->adapter->save($queue->getEntity(), $object);
-            $this->em->remove($queue);
-            $this->em->flush();
-
-            if ($result) {
-                return array('recordId' => $result, 'action' => Queue::EVENT_SAVE);
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Processing queue records
-     *
-     * @return array
-     */
-    public function runQueues()
-    {
-        $result = array();
-        $queues = $this->getQueueRepo()->findAll();
-        foreach ($queues as $queue) {
-            /** @var $queue \Oro\Bundle\SearchBundle\Entity\Queue */
-            if ($queue->getEvent() == Queue::EVENT_SAVE) {
-                if ($resultRecord = $this->save($queue)) {
-                    $result[] = $resultRecord;
-                }
-            } else {
-                if ($resultRecord = $this->delete($queue)) {
-                    $result[] = $resultRecord;
-                }
-            }
-
-        }
-
-        return $result;
-    }
-
-    /**
-     * Return Queue repository
-     *
-     * @return \Oro\Bundle\SearchBundle\Entity\Repository\QueueRepository
-     */
-    protected function getQueueRepo()
-    {
-        return $this->em->getRepository('OroSearchBundle:Queue');
     }
 }
