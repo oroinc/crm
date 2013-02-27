d86e318102bedb512162ea67edfb105ed6acc2cd
Nicolas Dupont <nicolas@akeneo.com>
2013-02-27 10:50:41 +0100

    BAP-15 : update phpdoc, fix problem with readonly when edit an attribute

--- Form/EventListener/AttributeTypeSubscriber.php
+++ Form/EventListener/AttributeTypeSubscriber.php
@@ -78,10 +78,10 @@ class AttributeTypeSubscriber implements EventSubscriberInterface
                         )
                     )
                 );
-
-                $this->disableField($form, 'code');
-                $this->disableField($form, 'attributeType');
             }
+
+            $this->disableField($form, 'code');
+            $this->disableField($form, 'attributeType');
         }
     }
 
