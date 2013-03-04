e03de83f35d46e06b5abe334c22018bed7716eec
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-03-04 13:40:10 +0200

    BAP-136 User Bundle functional tests coverage  - added REST API for Roles and Groups

--- Tests/Functional/API/SoapGroupsApi.php
+++ Tests/Functional/API/SoapGroupsApi.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Finder\Iterator;
 
-class SoapRolesApiTest extends WebTestCase
+class SoapGroupsApiTest extends WebTestCase
 {
     /** Default value for role label */
     const DEFAULT_VALUE = 'ROLE_LABEL';
@@ -31,15 +31,12 @@ class SoapRolesApiTest extends WebTestCase
      *
      * @dataProvider requestsApi
      */
-    public function testCreateRole($request, $response)
+    public function testCreateGroup($request, $response)
     {
-        if (is_null($request['role'])) {
-            $request['role'] ='';
-        }
-        if (is_null($request['label'])) {
-            $request['label'] = self::DEFAULT_VALUE;
-        }
-        $result = self::$clientSoap->createRole($request);
+        //if (is_null($request['roles'])) {
+        //    unset($request['role']);
+        //}
+        $result = self::$clientSoap->createGroup($request);
         $result = $this->classToArray($result);
         $this->assertEqualsResponse($response, $result);
     }
@@ -51,22 +48,16 @@ class SoapRolesApiTest extends WebTestCase
      * @dataProvider requestsApi
      * @depends testCreateRole
      */
-    public function testUpdateRole($request, $response)
+    public function testUpdateGroup($request, $response)
     {
-        if (is_null($request['role'])) {
-            $request['role'] ='';
-        }
-        if (is_null($request['label'])) {
-            $request['label'] = self::DEFAULT_VALUE;
-        }
-        $request['label'] .= '_Updated';
+        $request['name'] .= '_Updated';
         //get role id
-        $roleId = self::$clientSoap->getRoleByName($request['role']);
+        $roleId = self::$clientSoap->getGroupByName($request['role']);
         $roleId = $this->classToArray($roleId);
-        $result = self::$clientSoap->updateRole($roleId['id'], $request);
+        $result = self::$clientSoap->updateGroup($roleId['id'], $request);
         $result = $this->classToArray($result);
         $this->assertEqualsResponse($response, $result);
-        $role = self::$clientSoap->getRole($roleId['id']);
+        $role = self::$clientSoap->getGroup($roleId['id']);
         $role = $this->classToArray($role);
         $this->assertEquals($request['label'], $role['label']);
     }
@@ -77,7 +68,7 @@ class SoapRolesApiTest extends WebTestCase
     public function testGetRoles()
     {
         //get roles
-        $roles = self::$clientSoap->getRoles();
+        $roles = self::$clientSoap->getGroups();
         $roles = $this->classToArray($roles);
         $this->assertEquals(5, count($roles['item']));
         foreach ($roles['item'] as $role) {
@@ -91,14 +82,14 @@ class SoapRolesApiTest extends WebTestCase
     public function testDeleteRoles()
     {
         //get roles
-        $roles = self::$clientSoap->getRoles();
+        $roles = self::$clientSoap->getGroups();
         $roles = $this->classToArray($roles);
         $this->assertEquals(5, count($roles['item']));
         foreach ($roles['item'] as $role) {
-            $result = self::$clientSoap->deleteRole($role['id']);
+            $result = self::$clientSoap->deleteGroup($role['id']);
             $this->assertTrue($result);
         }
-        $roles = self::$clientSoap->getRoles();
+        $roles = self::$clientSoap->getGroups();
         $roles = $this->classToArray($roles);
         $this->assertEmpty($roles);
     }
