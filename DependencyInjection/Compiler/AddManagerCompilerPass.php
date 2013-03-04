f97e9ffbd7ff88a997335f5b6b141bb4db6ae5fd
Nicolas Dupont <nicolas@akeneo.com>
2013-03-04 11:20:32 +0100

    BAP-46 : fix typo

--- DependencyInjection/Compiler/AddManagerCompilerPass.php
+++ DependencyInjection/Compiler/AddManagerCompilerPass.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 /**
  * CompilerPass to add flexible manager to connector
  *
- * @author    Nicolas Dupont <romain@akeneo.com>
+ * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/MIT MIT
  *
