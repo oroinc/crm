7a77e5494e0c627838c39ef6623ec180bb9988c6
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2013-03-06 19:07:49 +0200

    BAP-135: Implement AJAX grid using backbons.js - add utils method into OroApp

--- Resources/public/js/backbone/app.js
+++ Resources/public/js/backbone/app.js
@@ -1,2 +1,71 @@
-// main Oro Application backbone.js namespace
-window.OroApp = {};
+/**
+ * Main Oro Application backbone.js namespace
+ */
+window.OroApp = {
+
+    /**
+     * Pack object to string
+     *
+     * Object {foo: 'x', 'bar': 'y'} will be converted to string "foo=x&bar=y".
+     *
+     * @param {Object} object
+     * @return {String}
+     */
+    packToQueryString: function(object) {
+        return $.param(object);
+    },
+
+    /**
+     * Unpack string to object. Reverse from packToQueryString.
+     *
+     * @param {String} string
+     * @return {Object}
+     */
+    unpackFromQueryString: function(string) {
+        var result = {};
+        var vars = string.split("&");
+        for (var i=0; i<vars.length; i++) {
+            var pair = vars[i].split("=");
+            pair[0] = decodeURIComponent(pair[0]);
+            pair[1] = decodeURIComponent(pair[1]);
+            // If first entry with this name
+            if (typeof result[pair[0]] === "undefined") {
+                result[pair[0]] = pair[1];
+                // If second entry with this name
+            } else if (typeof result[pair[0]] === "string") {
+                result[pair[0]] = [ result[pair[0]], pair[1] ];
+                // If third or later entry with this name
+            } else {
+                result[pair[0]].push(pair[1]);
+            }
+        }
+        return result;
+    },
+
+    /**
+     * Invert object keys.
+     *
+     * Example of usage:
+     *
+     * OroApp.mirrorKeys({foo: 'x', bar: 'y'}, {foo: 'f', bar: 'b'})
+     * will return {f: 'x', b: 'y'}
+     *
+     * @param {Object} object
+     * @param {Object} keys
+     * @return {Object}
+     */
+    invertKeys: function(object, keys) {
+        var result = _.extend({}, object);
+        for (key in keys) {
+            var mirrorKey, baseKey;
+            baseKey = key;
+            mirrorKey = keys[key];
+
+            if (baseKey in result) {
+                result[mirrorKey] = result[baseKey]
+                delete result[baseKey];
+            }
+        }
+        return result;
+    }
+};
