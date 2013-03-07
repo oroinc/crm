66ae4e2ecaf0479d0f9bbecd4bc400e7ade071f8
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2013-03-07 17:35:33 +0200

    BAP-135: Implement AJAX grid using backbons.js  - fixed bug related to records count

--- Datagrid/ORM/Pager.php
+++ Datagrid/ORM/Pager.php
@@ -14,25 +14,4 @@ class Pager extends BasePager implements PagerInterface
     {
         return intval(parent::getNbResults());
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function computeNbResult()
-    {
-        /** @var $countQuery \Doctrine\ORM\QueryBuilder */
-        $countQuery = clone $this->getQuery();
-
-        if (count($this->getParameters()) > 0) {
-            $countQuery->setParameters($this->getParameters());
-        }
-
-        $countQuery->resetDQLPart('orderBy');
-
-        $countQuery->select(
-            sprintf('%s.%s', $countQuery->getRootAlias(), current($this->getCountColumn()))
-        );
-
-        return count($countQuery->getQuery()->getResult());
-    }
 }
--- Datagrid/ORM/ProxyQuery.php
+++ Datagrid/ORM/ProxyQuery.php
@@ -43,18 +43,4 @@ class ProxyQuery extends BaseProxyQuery implements ProxyQueryInterface
     {
         return $this->queryBuilder;
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function execute(array $params = array(), $hydrationMode = null)
-    {
-        // always clone the original queryBuilder
-        $queryBuilder = clone $this->queryBuilder;
-        $queryBuilder->setMaxResults(null);
-        $queryBuilder->setFirstResult(null);
-
-        // there is no need to perform additional filtering here
-        return $queryBuilder->getQuery()->execute($params, $hydrationMode);
-    }
 }
