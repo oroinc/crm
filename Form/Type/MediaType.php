312f7762a56e94e01e2b00bed43588cd87d69085
Romain Monceau <romain@akeneo.com>
2013-03-14 18:01:29 +0100

    BAP-102 : Create Media entity and form type

---
+++ Form/Type/MediaType.php
@@ -0,0 +1,51 @@
+<?php
+namespace Oro\Bundle\FlexibleEntityBundle\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Form\AbstractType;
+
+/**
+ * Form type linked to Media entity
+ *
+ * @author    Romain Monceau <romain@akeneo.com>
+ * @copyright 2012 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ */
+class MediaType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('id', 'hidden');
+
+        $builder->add('file', 'file', array('required' => false));
+
+        $builder->add('filename', 'text', array('required' => false, 'disabled' => true));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => 'Oro\Bundle\FlexibleEntityBundle\Entity\Media'
+            )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'media';
+    }
+}
