84b5bc144062a3fc054ec7d8726e779a2d35bb34
Dan <danil@magecore.com>
2013-03-19 14:12:22 +0200

    Fixed multioption flexible form fields (attributes)

--- Entity/RequestFix.php
+++ Entity/RequestFix.php
@@ -70,8 +70,19 @@ class RequestFix
         foreach ($attrDef as $i => $attr) {
             /* @var $attr \Oro\Bundle\FlexibleEntityBundle\Entity\Mapping\AbstractEntityAttribute */
             if ($attr->getBackendType() == 'options') {
-                $type    = 'option';
-                $default = $attr->getOptions()->offsetGet(0)->getId();
+                if (in_array(
+                    $attr->getAttributeType(),
+                    array(
+                        'Oro\Bundle\FlexibleEntityBundle\Model\AttributeType\OptionMultiSelectType',
+                        'Oro\Bundle\FlexibleEntityBundle\Model\AttributeType\OptionMultiCheckboxType',
+                    ))
+                ) {
+                    $type    = 'options';
+                    $default = array($attr->getOptions()->offsetGet(0)->getId());
+                } else {
+                    $type    = 'option';
+                    $default = $attr->getOptions()->offsetGet(0)->getId();
+                }
             } else {
                 $type    = 'data';
                 $default = null;
