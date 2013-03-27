24185643e910c269962720618f78c02ce1dc81c9
Romain Monceau <romain@akeneo.com>
2013-03-27 15:08:23 +0100

    BAP-117 : First implementation for i18n

---
+++ Entity/Article.php
@@ -0,0 +1,65 @@
+<?php
+namespace Oro\Bundle\SegmentationTreeBundle\Entity;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Translatable\Translatable;
+
+/**
+ * @ORM\Table(name="articles")
+ * @ORM\Entity
+ * @Gedmo\TranslationEntity(class="Oro\Bundle\SegmentationTreeBundle\Entity\ArticleTranslation")
+ */
+class Article implements Translatable
+{
+    /** @ORM\Id @ORM\GeneratedValue @ORM\Column(type="integer") */
+    private $id;
+
+    /**
+     * @Gedmo\Translatable
+     * @ORM\Column(name="title", type="string", length=128)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @Gedmo\Locale
+     * Used locale to override Translation listener`s locale
+     * this is not a mapped field of entity metadata, just a simple property
+     */
+    private $locale;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function setTranslatableLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+}
