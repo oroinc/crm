4b7b34251fbbac96725bd1fc8e5a6e0a3393710f
Dmitry Khrysev <dkhrysev@magecore.com>
2013-03-27 18:01:35 +0200

    BAP-245: URL identified by route

--- Resources/public/js/collections/state.js
+++ Resources/public/js/collections/state.js
@@ -2,7 +2,6 @@ var Oro = Oro || {};
 Oro.widget = Oro.widget || {};
 
 Oro.widget.StateCollection = Backbone.Collection.extend({
-    url: '/app_dev.php/api/rest/latest/windows',
     model: Oro.widget.StateModel
 });
 
--- Resources/public/js/models/state.js
+++ Resources/public/js/models/state.js
@@ -2,5 +2,4 @@ var Oro = Oro || {};
 Oro.widget = Oro.widget || {};
 
 Oro.widget.StateModel = Backbone.Model.extend({
-    urlRoot: '/app_dev.php/api/rest/latest/windows'
 });
