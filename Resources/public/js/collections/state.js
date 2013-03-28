184de404c8b6907dca7541878c4b6573499dccee
Dmitry Khrysev <dkhrysev@magecore.com>
2013-03-28 14:10:51 +0200

    BAP-245: state collection use state model url

--- Resources/public/js/collections/state.js
+++ Resources/public/js/collections/state.js
@@ -2,7 +2,11 @@ var Oro = Oro || {};
 Oro.widget = Oro.widget || {};
 
 Oro.widget.StateCollection = Backbone.Collection.extend({
-    model: Oro.widget.StateModel
+    model: Oro.widget.StateModel,
+
+    url: function() {
+        return this.model.prototype.urlRoot;
+    }
 });
 
 Oro.widget.States = new Oro.widget.StateCollection();
