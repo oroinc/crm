cb20cbe08c0e52613124e18ec8991a216949a11b
yurio <yurio@ua.fm>
2013-03-29 14:10:46 +0200

    BAP-251: Add design to email collection

--- Resources/public/js/emails.js
+++ Resources/public/js/emails.js
@@ -1,17 +1,17 @@
-jQuery(document).ready(function () {
-    jQuery('#add-another-email').click(function () {
-        var emailList = jQuery('#email-fields-list');
+$(document).ready(function () {
+    $('#add-another-email').click(function () {
+        var emailList = $('#email-fields-list');
         var newWidget = emailList.attr('data-prototype');
         newWidget = newWidget.replace(/__name__/g, emailCount);
         emailCount++;
-        var newDiv = jQuery('<div></div>').html(newWidget);
-        newDiv.appendTo(jQuery('#email-fields-list'));
+        var newDiv = $('<div></div>').html(newWidget);
+        newDiv.appendTo($('#email-fields-list'));
         return false;
     });
 
-    jQuery('.removeRow').live('click', function (event) {
+    $(document).on('click', '.removeRow', function (event) {
         var name = $(this).attr('data-related');
-        jQuery('*[data-content="' + name + '"]').remove();
+        $('*[data-content="' + name + '"]').remove();
         return false;
     });
 });
