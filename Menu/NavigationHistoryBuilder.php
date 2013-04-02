8d2af00589efc52605d35d9b40705170dc8e3dc7
Aleksandr Smaga <asmaga@magecore.com>
2013-04-02 19:00:16 +0200

    BAP-333 history
    Code reviews fixes

--- Menu/NavigationHistoryBuilder.php
+++ Menu/NavigationHistoryBuilder.php
@@ -15,6 +15,11 @@ class NavigationHistoryBuilder extends NavigationItemBuilder
     private $matcher;
 
     /**
+     * @var Marcher
+     */
+    private $matcher;
+
+    /**
      * Modify menu by adding, removing or editing items.
      *
      * @param \Knp\Menu\ItemInterface $menu
@@ -38,6 +43,19 @@ class NavigationHistoryBuilder extends NavigationItemBuilder
         }
 
         $menu->slice(0, $maxItems);
+    }
+
+    /**
+     * Setter for matcher service
+     *
+     * @param \Knp\Menu\Matcher\Matcher $matcher
+     * @return $this
+     */
+    public function setMatcher(Matcher $matcher)
+    {
+        $this->matcher = $matcher;
+
+        return $this;
     }
 
     /**
