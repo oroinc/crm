e45de4c60aadbb897366fb04e88de0a9dd240ec4
Aleksandr Smaga <asmaga@magecore.com>
2013-04-05 12:42:21 +0200

    BAP-376 Code review fixes

--- Menu/Matcher/Voter/RequestVoter.php
+++ Menu/Matcher/Voter/RequestVoter.php
@@ -3,23 +3,22 @@
 namespace Oro\Bundle\NavigationBundle\Menu\Matcher\Voter;
 
 use Knp\Menu\Matcher\Voter\UriVoter;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class RequestVoter extends UriVoter
 {
     /**
-     * Constructor
-     * @param ContainerInterface $container
+     * @var Request
      */
-    public function __construct(ContainerInterface $container)
+    private $request;
+
+    public function setRequest(Request $request)
     {
-        $uri = null;
+        $this->request = $request;
 
-        // TODO BAP-430 do correct processing of scope to avoid problems with CLI environment
-        if ($container->isScopeActive('request')) {
-            $container->get('request')->getRequestUri();
-        }
+        $this->setUri($request->getRequestUri());
 
-        parent::__construct($uri);
+        return $this;
     }
 }
