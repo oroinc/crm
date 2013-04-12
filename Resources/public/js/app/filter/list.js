64bc5d98a24b9c34e9c639a4464c54f58f3b219b
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2013-04-12 14:07:45 +0300

    BAP-509: Extract Filter List entity from Datagrid Filters List  - refactored filter list

--- Resources/public/js/app/filter/list.js
+++ Resources/public/js/app/filter/list.js
@@ -6,6 +6,10 @@ OroApp.Filter = OroApp.Filter || {};
  *
  * @class   OroApp.Filter.List
  * @extends Backbone.View
+ *
+ * @event updateList    on update of filter list
+ * @event updateFilter  on update data of specific filter
+ * @event disableFilter on disable specific filter
  */
 OroApp.Filter.List = Backbone.View.extend({
     /**
@@ -67,30 +71,24 @@ OroApp.Filter.List = Backbone.View.extend({
      * Initialize filter list options
      *
      * @param {Object} options
-     * @param {OroApp.PageableCollection} [options.collection]
      * @param {Object} [options.filters]
      * @param {String} [options.addButtonHint]
      */
     initialize: function(options)
     {
-        this.collection = options.collection;
-
         if (options.filters) {
             this.filters = options.filters;
         }
+
+        _.each(this.filters, function(filter) {
+            this.listenTo(filter, "update", this._onFilterUpdated);
+            this.listenTo(filter, "disable", this._onFilterDisabled);
+        }, this);
 
         if (options.addButtonHint) {
             this.addButtonHint = options.addButtonHint;
         }
 
-        _.each(this.filters, function(filter, name) {
-            this.filters[name] = new filter();
-            this.listenTo(this.filters[name], "update", this._onFilterUpdated);
-            this.listenTo(this.filters[name], "disable", this._onFilterDisabled);
-        }, this);
-
-        this.collection.on('beforeFetch', this._beforeCollectionFetch, this);
-        this.collection.on('updateState', this._onUpdateCollectionState, this);
         Backbone.View.prototype.initialize.apply(this, arguments);
     },
 
@@ -101,11 +99,7 @@ OroApp.Filter.List = Backbone.View.extend({
      * @protected
      */
     _onFilterUpdated: function(filter) {
-        if (this.ignoreFiltersUpdateEvents) {
-            return;
-        }
-        this.collection.state.currentPage = 1;
-        this.collection.fetch();
+        this.trigger('updateFilter', filter);
     },
 
     /**
@@ -115,82 +109,13 @@ OroApp.Filter.List = Backbone.View.extend({
      * @protected
      */
     _onFilterDisabled: function(filter) {
+        this.trigger('disableFilter', filter);
         this.disableFilter(filter);
     },
 
-    /**
-     * Triggers before collection fetch it's data
-     *
-     * @protected
-     */
-    _beforeCollectionFetch: function(collection) {
-        collection.state.filters = this._createState();
-    },
+    getValues: function() {},
 
-    /**
-     * Triggers when collection state is updated
-     *
-     * @param {OroApp.PageableCollection} collection
-     */
-    _onUpdateCollectionState: function(collection) {
-        this.ignoreFiltersUpdateEvents = true;
-        this._applyState(collection.state.filters || {});
-        this.ignoreFiltersUpdateEvents = false;
-    },
-
-    /**
-     * Create state according to filters parameters
-     *
-     * @return {Object}
-     * @protected
-     */
-    _createState: function() {
-        var state = {};
-        _.each(this.filters, function(filter, name) {
-            var shortName = '__' + name;
-            if (filter.enabled) {
-                if (!filter.isEmpty()) {
-                    state[name] = filter.getValue();
-                } else if (!filter.defaultEnabled) {
-                    state[shortName] = 1;
-                }
-            } else if (filter.defaultEnabled) {
-                state[shortName] = 0;
-            }
-        }, this);
-
-        return state;
-    },
-
-    /**
-     * Apply filter values from state
-     *
-     * @param {Object} state
-     * @protected
-     * @return {*}
-     */
-    _applyState: function(state) {
-        _.each(this.filters, function(filter, name) {
-            var shortName = '__' + name;
-            if (_.has(state, name)) {
-                var filterState = state[name];
-                if (!_.isObject(filterState)) {
-                    filterState = {
-                        value: filterState
-                    }
-                }
-                this.enableFilter(filter.setValue(filterState));
-            } else if (_.has(state, shortName)) {
-                if (Number(state[shortName])) {
-                    this.enableFilter(filter.reset());
-                } else {
-                    this.disableFilter(filter.reset());
-                }
-            }
-        }, this);
-
-        return this;
-    },
+    setValues: function() {},
 
     /**
      * Triggers when filter select is changed
@@ -198,6 +123,7 @@ OroApp.Filter.List = Backbone.View.extend({
      * @protected
      */
     _onChangeFilterSelect: function() {
+        this.trigger('updateList', this);
         this._processFilterStatus();
     },
 
