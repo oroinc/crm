f2262459a996407e5a94a4bb1fb16e42714d8e2f
bogdan <btsyganovskiy@magecore.com>
2013-04-12 18:46:11 +0300

    BAP-316: Js update

--- Resources/public/js/views/dot.menu.js
+++ Resources/public/js/views/dot.menu.js
@@ -19,6 +19,7 @@ navigation.dotMenu.MainView = Backbone.View.extend({
     initialize: function() {
         this.$tabsContainer = this.$('.nav-tabs');
         this.$tabsContent = this.$('.tab-content')
+        this.$tabsContent.find('.menu-close').click(_.bind(this.close, this));
     },
 
     addTab: function(options) {
