e6795f1f92bb97706cc28e1f831b4ae29a063bbf
Ivan Shakuta <ishakuta@magecore.com>
2013-04-18 17:31:37 +0300

    BAP-542 address type entity manager

--- Entity/Repository/AddressTypeRepository.php
+++ Entity/Repository/AddressTypeRepository.php
@@ -4,6 +4,10 @@ namespace Oro\Bundle\AddressBundle\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * Class AddressTypeRepository
+ * @package Oro\Bundle\AddressBundle\Entity\Repository
+ */
 class AddressTypeRepository extends EntityRepository
 {
 }
