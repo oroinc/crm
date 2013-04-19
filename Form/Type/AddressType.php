d33274fb8f14e4ca0834418e0edf22b0a19c3ab0
Aleksandr Smaga <asmaga@magecore.com>
2013-04-19 15:32:39 +0200

    BAP-542 CRUD Addresses BAP-544 Create entities and implement basic CRUD

--- Form/Type/AddressType.php
+++ Form/Type/AddressType.php
@@ -16,9 +16,19 @@ class AddressType extends FlexibleType
         // add default flexible fields
         parent::addEntityFields($builder);
 
+        $required =  array(
+            'required' => true,
+        );
+
         // address fields
         $builder
-            ->add('street', 'text');
+            ->add('street', 'text', $required)
+            ->add('street2', 'text')
+            ->add('city', 'text', $required)
+            ->add('state', 'text', $required)
+            ->add('postal_code', 'text', $required)
+            ->add('country', 'text', $required)
+            ->add('mark', 'text');
     }
 
     /**
@@ -26,11 +36,13 @@ class AddressType extends FlexibleType
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(array(
-            'data_class'           => $this->flexibleClass,
-            'intention'            => 'address',
-            'extra_fields_message' => 'This form should not contain extra fields: "{{ extra_fields }}"',
-        ));
+        $resolver->setDefaults(
+            array(
+                'data_class'           => $this->flexibleClass,
+                'intention'            => 'address',
+                'extra_fields_message' => 'This form should not contain extra fields: "{{ extra_fields }}"',
+            )
+        );
     }
 
     /**
