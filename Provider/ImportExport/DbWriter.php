6bcbd41837915e243943eddb4fa78ad225c99eb1
Ivan Shakuta <ishakuta@magecore.com>
2013-04-24 18:52:48 +0300

    BAP-574 import export dicts

---
+++ Provider/ImportExport/DbWriter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Oro\Bundle\AddressBundle\Provider\ImportExport;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+class DbWriter implements WriterInterface
+{
+    /**
+     * @var ObjectManager
+     */
+    protected $om;
+
+    public function __construct(ObjectManager $om)
+    {
+        $this->om = $om;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function writeBatch($data)
+    {
+        if (empty($data) || !is_array($data)) {
+            return false;
+        }
+
+        foreach ($data as $entry) {
+            if (is_object($entry)) {
+                $this->om->persist($entry);
+            } else {
+                throw new Exception("Entry passed to writer is not an object");
+            }
+        }
+
+        $this->om->flush();
+    }
+}
---
+++ Provider/ImportExport/Reader.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Oro\Bundle\AddressBundle\Provider\ImportExport;
+
+class Reader implements ReaderInterface
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function readBatch()
+    {
+
+    }
+}
