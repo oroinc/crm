164f0fd71fcc81a2dd48013ee9841d75debef66c
Aleksandr Smaga <asmaga@magecore.com>
2013-04-24 18:09:00 +0200

    BAP-542 CRUD Addresses BAP-573 Create dictionary storage

---
+++ Entity/Country.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Oro\Bundle\AddressBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Country
+ *
+ * @ORM\Table("oro_dictionary_country")
+ * @ORM\Entity
+ */
+class Country
+{
+    /**
+     * @var string
+     *
+     * @ORM\Id
+     * @ORM\Column(name="iso2_code", type="string", length=2)
+     */
+    private $iso2Code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="iso3_code", type="string", length=3)
+     */
+    private $iso3Code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\OneToMany(targetEntity="Region", mappedBy="country", cascade={"persist", "remove"}, orphanRemoval=true, fetch="EXTRA_LAZY")
+     */
+    private $regions;
+
+    /**
+     * @param \Doctrine\Common\Collections\ArrayCollection $regions
+     * @return Country
+     */
+    public function setRegions($regions)
+    {
+        $this->regions = $regions;
+
+        return $this;
+    }
+
+    /**
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getRegions()
+    {
+        return $this->regions;
+    }
+
+    /**
+     * Set iso2_code
+     *
+     * @param string $iso2Code
+     * @return Country
+     */
+    public function setIso2Code($iso2Code)
+    {
+        $this->iso2Code = $iso2Code;
+    
+        return $this;
+    }
+
+    /**
+     * Get iso2_code
+     *
+     * @return string 
+     */
+    public function getIso2Code()
+    {
+        return $this->iso2Code;
+    }
+
+    /**
+     * Set iso3_code
+     *
+     * @param string $iso3Code
+     * @return Country
+     */
+    public function setIso3Code($iso3Code)
+    {
+        $this->iso3Code = $iso3Code;
+    
+        return $this;
+    }
+
+    /**
+     * Get iso3_code
+     *
+     * @return string 
+     */
+    public function getIso3Code()
+    {
+        return $this->iso3Code;
+    }
+
+    /**
+     * Set country name
+     *
+     * @param string $name
+     * @return Country
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get country name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
---
+++ Entity/Region.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Oro\Bundle\AddressBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+use Oro\Bundle\AddressBundle\Entity\Country;
+
+/**
+ * Region
+ *
+ * @ORM\Table("oro_dictionary_region")
+ * @ORM\Entity
+ * @Gedmo\TranslationEntity(class="Oro\Bundle\AddressBundle\Entity\RegionLocalized")
+ */
+class Region
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\ManyToOne(targetEntity="Country", inversedBy="regions")\
+     * @ORM\JoinColumn(name="country_id", referencedColumnName="iso2_code", onDelete="CASCADE")
+     *
+     */
+    private $country;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=32)
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     * @Gedmo\Translatable
+     */
+    private $name;
+
+    /**
+     * @Gedmo\Locale
+     */
+    private $locale;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set country
+     *
+     * @param Country $country
+     * @return Region
+     */
+    public function setCountry(Country $country)
+    {
+        $this->country = $country;
+    
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return Country
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return Region
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Region
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set locale
+     *
+     * @param string $locale
+     * @return $this
+     */
+    public function setLocale($locale = 'en_US')
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+}
