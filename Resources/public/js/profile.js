2fa9de1bc9063263da9cbe15a5f7aae7712d3f3f
bogdan <btsyganovskiy@magecore.com>
2013-04-27 13:35:23 +0300

    BAP-559: Edit user form fixes

--- Resources/public/js/profile.js
+++ Resources/public/js/profile.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('#btn-apigen').on('click', function(e) {
+    $(document).on('click', '#btn-apigen', function (e) {
         el = $(this);
 
         $.get(el.attr('href'), function (data) {
@@ -9,11 +9,8 @@ $(function() {
         return false;
     });
 
-    $('#roles-list input')
-        .on('click', function() {
-            inputs = $(this).closest('.controls');
-
-            inputs.find(':checkbox').attr('required', inputs.find(':checked').length > 0 ? null : 'required');
-        })
-        .triggerHandler('click');
+    $(document).on('click', '#roles-list input', function (e) {
+        inputs = $(this).closest('.controls');
+        inputs.find(':checkbox').attr('required', inputs.find(':checked').length > 0 ? null : 'required');
+    });
 });
