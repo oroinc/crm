2da658d98b04be88b04d46766d3cb8657df217ec
Alexander Nezdoiminoga <alex@magecore.com>
2013-04-29 19:13:27 +0300

    initial commit

---
+++ Entity/Log.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Oro\Bundle\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Loggable\Entity\MappedSuperclass\AbstractLogEntry;
+
+use Oro\Bundle\UserBundle\Entity\User;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="oro_log")
+ */
+class Log extends AbstractLogEntry
+{
+    /**
+     * @var User $user
+     *
+     * @ORM\ManyToOne(targetEntity="Oro\Bundle\UserBundle\Entity\User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id", nullable=false, onDelete="CASCADE")
+     */
+    protected $user;
+
+    /**
+     * Set user
+     *
+     * @param  User      $user
+     * @return Log
+     */
+    public function setUser(User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
---
+++ OroLogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oro\Bundle\LogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OroLogBundle extends Bundle
+{
+}
