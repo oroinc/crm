f0fef3f5ab79bcfc8da7b2a18b79d23e1f86cdcf
yurio <yurio@ua.fm>
2013-05-14 18:56:32 +0300

    BAP-749: Redirect to login page if session is expired in hash navigation

--- Event/ResponseHashnavListener.php
+++ Event/ResponseHashnavListener.php
@@ -4,9 +4,20 @@ namespace Oro\Bundle\NavigationBundle\Event;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class ResponseHashnavListener
 {
+    /**
+     * @var \Symfony\Component\Security\Core\SecurityContextInterface
+     */
+    protected $security;
+
+    public function __construct(SecurityContextInterface $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * @param FilterResponseEvent $event
      */
@@ -14,9 +25,16 @@ class ResponseHashnavListener
     {
         $request = $event->getRequest();
         $response = $event->getResponse();
+
         if (($request->get('x-oro-hash-navigation') || $request->headers->get('x-oro-hash-navigation'))
             && $response->getStatusCode() == 302) {
-            $response = '<div id="redirect">' . $response->headers->get('location') . '</div>';
+
+            $documentRedirect = '';
+            if (!$this->security->getToken()) {
+                $documentRedirect = 'data-redirect=true';
+            }
+
+            $response = '<div id="redirect" ' . $documentRedirect . '>' . $response->headers->get('location') . '</div>';
             $event->setResponse(new Response($response));
         }
     }
