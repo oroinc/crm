e6f77b45ca9bf3004602af59ac9ec7a6e1acaf8a
Aleksandr Smaga <asmaga@magecore.com>
2013-05-16 15:38:13 +0200

    BAP-564 Create emails collection flexible attribute type

--- Resources/public/js/emails.js
+++ Resources/public/js/emails.js
@@ -1,8 +1,8 @@
 $(function () {
-    $(document).on('click', '#add-another-email', function (event) {
-        var cList  = $('#email-fields-list'),
-        cCount = cList.children().length;
-        widget = cList.attr('data-prototype').replace(/__name__/g, cCount++);
+    $(document).on('click', '.add-list-item', function (event) {
+        var cList  = $(this).siblings('.collection-fields-list'),
+            cCount = cList.children().length;
+            widget = cList.attr('data-prototype').replace(/__name__/g, cCount++);
 
         $('<div></div>').html(widget).appendTo(cList);
 
