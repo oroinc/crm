495819721696a6bc733ff7b4d125f439f3103cf0
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-05-16 15:04:43 +0300

    BAP-762 Create annotation @db_isolation for functional tests  - fixed tests from Acme  - removed unexpected files from tests

--- DependencyInjection/OroTestFrameworkExtension.php
+++ DependencyInjection/OroTestFrameworkExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class AcmeTestsExtension extends Extension
+class OroTestFrameworkExtension extends Extension
 {
     /**
      * {@inheritDoc}
