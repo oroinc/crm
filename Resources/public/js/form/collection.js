9569053aaff9fedacbd2e98833913c4519352136
Aleksandr Smaga <asmaga@magecore.com>
2013-05-17 12:50:04 +0200

    BAP-564 Create emails collection flexible attribute type, Fix js remove item functional

--- Resources/public/js/form/collection.js
+++ Resources/public/js/form/collection.js
@@ -1,19 +1,15 @@
 $(function () {
     $(document).on('click', '.add-list-item', function (event) {
+        event.preventDefault();
         var cList  = $(this).siblings('.collection-fields-list'),
             cCount = cList.children().length;
             widget = cList.attr('data-prototype').replace(/__name__/g, cCount++);
 
         $('<div></div>').html(widget).appendTo(cList);
-
-        return false;
     });
 
     $(document).on('click', '.removeRow', function (event) {
-        name = $(this).attr('data-related');
-
-        $('*[data-content="' + name + '"]').remove();
-
-        return false;
+        event.preventDefault();
+        $(this).parents('*[data-content]').remove();
     });
 });
