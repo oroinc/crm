a117611b23aa47c016179402c08787a2d5d67632
Ivan Shakuta <ishakuta@magecore.com>
2013-05-21 19:43:04 +0300

    BAP-721 CR fixes

--- Controller/Api/Soap/RoleController.php
+++ Controller/Api/Soap/RoleController.php
@@ -5,7 +5,6 @@ namespace Oro\Bundle\UserBundle\Controller\Api\Soap;
 use BeSimple\SoapBundle\ServiceDefinition\Annotation as Soap;
 
 use Oro\Bundle\SoapBundle\Controller\Api\Soap\SoapController;
-use Oro\Bundle\SoapBundle\Form\Handler\ApiFormHandler;
 use Oro\Bundle\UserBundle\Entity\Role;
 use Oro\Bundle\UserBundle\Annotation\AclAncestor;
 
