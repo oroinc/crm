6c8c43da7bc1195d3bf7890b198f8886e6a6dc68
Dmitry Khrysev <dkhrysev@magecore.com>
2013-05-22 19:57:45 +0300

    BAP-562: flexible value refatoring

--- Form/EventListener/FlexibleValueSubscriber.php
+++ Form/EventListener/FlexibleValueSubscriber.php
@@ -5,7 +5,8 @@ use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Oro\Bundle\FlexibleEntityBundle\Manager\FlexibleManager;
+use Oro\Bundle\FlexibleEntityBundle\Manager\FlexibleManagerRegistry;
+use Oro\Bundle\FlexibleEntityBundle\AttributeType\AttributeTypeFactory;
 
 /**
  * Add a relevant form for each flexible entity value
@@ -23,20 +24,27 @@ class FlexibleValueSubscriber implements EventSubscriberInterface
     protected $factory;
 
     /**
-     * @var FlexibleManager
+     * @var AttributeTypeFactory
      */
-    protected $flexibleManager;
+    protected $attributeTypeFactory;
+
+    /**
+     * @var FlexibleManagerRegistry
+     */
+    protected $flexibleManagerRegistry;
 
     /**
      * Constructor
      *
      * @param FormFactoryInterface $factory         the form factory
-     * @param FlexibleManager      $flexibleManager the flexible manager
+     * @param AttributeTypeFactory $attributeTypeFactory
+     * @param FlexibleManagerRegistry $flexibleManagerRegistry
      */
-    public function __construct(FormFactoryInterface $factory, FlexibleManager $flexibleManager)
+    public function __construct(FormFactoryInterface $factory, AttributeTypeFactory $attributeTypeFactory, FlexibleManagerRegistry $flexibleManagerRegistry)
     {
-        $this->factory         = $factory;
-        $this->flexibleManager = $flexibleManager;
+        $this->factory = $factory;
+        $this->attributeTypeFactory = $attributeTypeFactory;
+        $this->flexibleManagerRegistry = $flexibleManagerRegistry;
     }
 
     /**
@@ -67,8 +75,16 @@ class FlexibleValueSubscriber implements EventSubscriberInterface
         }
 
         $attributeTypeAlias = $value->getAttribute()->getAttributeType();
-        $attributeType      = $this->flexibleManager->getAttributeTypeFactory()->get($attributeTypeAlias);
-        $valueForm          = $attributeType->buildValueFormType($this->factory, $value);
+        $attributeType = $this->attributeTypeFactory->get($attributeTypeAlias);
+        $valueForm = $attributeType->buildValueFormType($this->factory, $value);
+
+        // Initialize subforms which connected to flexible entities
+        $dataClass = $valueForm->getConfig()->getDataClass();
+        if (is_subclass_of($dataClass, 'Oro\Bundle\FlexibleEntityBundle\Model\FlexibleInterface')) {
+            $flexibleManager = $this->flexibleManagerRegistry->getManager($dataClass);
+            $entity = $flexibleManager->createFlexible();
+            $valueForm->setData($entity);
+        }
 
         $form->add($valueForm);
     }
