5fec288e60270b2aec1f4b50c3a4ea498fde3be5
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2013-05-27 17:33:00 +0300

    CRM-89: Rename Oro to OroCRM - rename Oro to OroCRM

--- DependencyInjection/OroCRMDashboardExtension.php
+++ DependencyInjection/OroCRMDashboardExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oro\Bundle\DashboardBundle\DependencyInjection;
+namespace OroCRM\Bundle\DashboardBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class OroDashboardExtension extends Extension
+class OroCRMDashboardExtension extends Extension
 {
     /**
      * {@inheritDoc}
