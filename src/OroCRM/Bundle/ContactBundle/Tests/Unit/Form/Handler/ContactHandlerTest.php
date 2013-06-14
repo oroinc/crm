980cfe21c81380574bbdb7e7a6b1564b5226228c
Ivan Klymenko <iklymenko@magecore.com>
2013-06-14 20:55:32 +0300

    CRM-138: Upgrade Symfony to version 2.3 - removed usage of Symfony\Component\Form\Exception\FormException - refactored usage of Symfony\Component\Form\Util\PropertyPath - renamed FormInterface::bind to FormInterface::submit - removed usage of deprecated Form::getAttribute - removed usage of deprecated Form::getChildren, Form::hasChildren, Form::getClientData

--- src/OroCRM/Bundle/ContactBundle/Tests/Unit/Form/Handler/ContactHandlerTest.php
+++ src/OroCRM/Bundle/ContactBundle/Tests/Unit/Form/Handler/ContactHandlerTest.php
@@ -59,7 +59,7 @@ class ContactHandlerTest extends \PHPUnit_Framework_TestCase
             ->with($this->entity);
 
         $this->form->expects($this->never())
-            ->method('bind');
+            ->method('submit');
 
         $this->assertFalse($this->handler->process($this->entity));
     }
@@ -77,7 +77,7 @@ class ContactHandlerTest extends \PHPUnit_Framework_TestCase
         $this->request->setMethod($method);
 
         $this->form->expects($this->once())
-            ->method('bind')
+            ->method('submit')
             ->with($this->request);
 
         $this->assertFalse($this->handler->process($this->entity));
@@ -108,7 +108,7 @@ class ContactHandlerTest extends \PHPUnit_Framework_TestCase
             ->with($this->entity);
 
         $this->form->expects($this->once())
-            ->method('bind')
+            ->method('submit')
             ->with($this->request);
 
         $this->form->expects($this->once())
