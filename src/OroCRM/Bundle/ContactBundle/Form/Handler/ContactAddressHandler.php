cda39e68dca955895485a50276450ee64d635d2a
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2013-08-09 12:01:42 +0300

    CRM-333: Address add/edit form widget - fix contact address handler

--- src/OroCRM/Bundle/ContactBundle/Form/Handler/ContactAddressHandler.php
+++ src/OroCRM/Bundle/ContactBundle/Form/Handler/ContactAddressHandler.php
@@ -2,19 +2,30 @@
 
 namespace OroCRM\Bundle\ContactBundle\Form\Handler;
 
-use OroCRM\Bundle\ContactBundle\Entity\Contact;
-use OroCRM\Bundle\ContactBundle\Entity\ContactAddress;
+use Oro\Bundle\AddressBundle\Entity\AbstractAddress;
 use Oro\Bundle\AddressBundle\Form\Handler\AddressHandler;
+use OroCRM\Bundle\ContactBundle\Entity\ContactAddress;
 
 class ContactAddressHandler extends AddressHandler
 {
     /**
      * "Success" form handler
      *
-     * @param ContactAddress $address
+     * @param AbstractAddress $address
+     * @throws \InvalidArgumentException When argument is not ContactAddress
      */
-    protected function onSuccess(ContactAddress $address)
+    protected function onSuccess(AbstractAddress $address)
     {
+        if (!$address instanceof ContactAddress) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'Argument expect to be an instance of %s, %s given',
+                    'OroCRM\Bundle\ContactBundle\Entity\ContactAddress',
+                    get_class($address)
+                )
+            );
+        }
+
         $contact = $address->getOwner();
 
         if ($address->isPrimary()) {
