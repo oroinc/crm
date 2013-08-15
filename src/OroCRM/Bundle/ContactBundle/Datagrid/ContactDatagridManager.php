c3f4c5fa1b5681127f2e1ae1c54cd9fa5b06ca2a
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2013-08-15 15:31:56 +0300

    BAP-1292: Mass delete action  - added test mass actions for redirect and window

--- src/OroCRM/Bundle/ContactBundle/Datagrid/ContactDatagridManager.php
+++ src/OroCRM/Bundle/ContactBundle/Datagrid/ContactDatagridManager.php
@@ -16,7 +16,9 @@ use Oro\Bundle\GridBundle\Property\UrlProperty;
 use Oro\Bundle\GridBundle\Property\FixedProperty;
 use Oro\Bundle\GridBundle\Datagrid\ProxyQueryInterface;
 use Oro\Bundle\GridBundle\Sorter\SorterInterface;
-use Oro\Bundle\GridBundle\Action\MassAction\DeleteMassAction;
+use Oro\Bundle\GridBundle\Action\MassAction\Ajax\DeleteMassAction;
+use Oro\Bundle\GridBundle\Action\MassAction\Redirect\RedirectMassAction;
+use Oro\Bundle\GridBundle\Action\MassAction\Widget\WindowMassAction;
 
 class ContactDatagridManager extends DatagridManager
 {
@@ -301,13 +303,34 @@ class ContactDatagridManager extends DatagridManager
     {
         $deleteMassAction = new DeleteMassAction(
             array(
+                'name'         => 'delete',
                 'acl_resource' => 'orocrm_contact_delete',
                 'label'        => $this->translate('orocrm.contact.datagrid.delete'),
                 'icon'         => 'trash',
             )
         );
 
-        return array($deleteMassAction);
+        // TODO Remove this mass action after development
+        $redirectMassAction = new RedirectMassAction(
+            array(
+                'name'             => 'redirect',
+                'label'            => 'Redirect',
+                'route'            => 'oro_user_view',
+                'route_parameters' => array('id' => 1)
+            )
+        );
+
+        // TODO Remove this mass action after development
+        $windowMassAction = new WindowMassAction(
+            array(
+                'name'             => 'window',
+                'label'            => 'Window',
+                'route'            => 'oro_user_view',
+                'route_parameters' => array('id' => 1),
+            )
+        );
+
+        return array($deleteMassAction, $redirectMassAction, $windowMassAction);
     }
 
     /**
