b26026efc21096d788748114edabf48af8d584fd
Michael Banin <mbanin@magecore.com>
2013-08-21 13:04:09 +0300

    CRM-371 - Add Contacts controllers tests - Added tests

---
+++ src/OroCRM/Bundle/ContactBundle/Tests/Functional/ControllersTest.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace OroCrRM\Bundle\ContactBundle\Tests\Functional;
+
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Oro\Bundle\TestFrameworkBundle\Test\ToolsAPI;
+use Oro\Bundle\TestFrameworkBundle\Test\Client;
+use Symfony\Component\DomCrawler\Form;
+use Symfony\Component\DomCrawler\Field\ChoiceFormField;
+
+/**
+ * @outputBuffering enabled
+ * @db_isolation
+ */
+class ControllersTest extends WebTestCase
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    public function setUp()
+    {
+        $this->client = static::createClient(
+            array(),
+            array_merge(ToolsAPI::generateBasicHeader(), array('HTTP_X-CSRF-Header' => 1))
+        );
+    }
+
+    public function testIndex()
+    {
+        $this->client->request('GET', $this->client->generate('orocrm_contact_index'));
+        $result = $this->client->getResponse();
+        ToolsAPI::assertJsonResponse($result, 200, 'text/html; charset=UTF-8');
+    }
+
+    public function testCreate()
+    {
+        $crawler = $this->client->request('GET', $this->client->generate('orocrm_contact_create'));
+        /** @var Form $form */
+        $form = $crawler->selectButton('Save and Close')->form();
+        $form['orocrm_contact_form[firstName]'] = 'Contact_fname';
+        $form['orocrm_contact_form[lastName]'] = 'Contact_lname';
+
+        $this->client->followRedirects(true);
+        $crawler = $this->client->submit($form);
+
+        $result = $this->client->getResponse();
+        ToolsAPI::assertJsonResponse($result, 200, 'text/html; charset=UTF-8');
+        $this->assertContains("Contact successfully saved", $crawler->html());
+    }
+
+    public function testUpdate()
+    {
+        $this->client->request(
+            'GET',
+            $this->client->generate('orocrm_contact_index', array('_format' =>'json')),
+            array(
+                'contacts[_filter][first_name][value]' => 'Contact_fname',
+                'contacts[_pager][_per_page]' => '10',
+                'contacts[_sort_by][first_name]' => 'ASC',
+                'contacts[_sort_by][last_name]' => 'ASC',
+            )
+        );
+
+        $result = $this->client->getResponse();
+        ToolsAPI::assertJsonResponse($result, 200);
+
+        $result = ToolsAPI::jsonToArray($result->getContent());
+        $result = reset($result['data']);
+
+        $crawler = $this->client->request(
+            'GET',
+            $this->client->generate('orocrm_contact_update', array('id' => $result['id']))
+        );
+        /** @var Form $form */
+        $form = $crawler->selectButton('Save and Close')->form();
+        $form['orocrm_contact_form[firstName]'] = 'Contact_fname';
+        $form['orocrm_contact_form[lastName]'] = 'Contact_lname';
+
+        $this->client->followRedirects(true);
+        $crawler = $this->client->submit($form);
+
+        $result = $this->client->getResponse();
+        ToolsAPI::assertJsonResponse($result, 200, 'text/html; charset=UTF-8');
+        $this->assertContains("Contact successfully saved", $crawler->html());
+    }
+
+    public function testView()
+    {
+        $this->client->request(
+            'GET',
+            $this->client->generate('orocrm_contact_index', array('_format' =>'json')),
+            array(
+                'contacts[_filter][first_name][value]' => 'Contact_fname',
+                'contacts[_pager][_per_page]' => '10',
+                'contacts[_sort_by][first_name]' => 'ASC',
+                'contacts[_sort_by][last_name]' => 'ASC',
+            )
+        );
+
+        $result = $this->client->getResponse();
+        ToolsAPI::assertJsonResponse($result, 200);
+
+        $result = ToolsAPI::jsonToArray($result->getContent());
+        $result = reset($result['data']);
+
+        $crawler = $this->client->request(
+            'GET',
+            $this->client->generate('orocrm_contact_view', array('id' => $result['id']))
+        );
+
+        $result = $this->client->getResponse();
+        ToolsAPI::assertJsonResponse($result, 200, 'text/html; charset=UTF-8');
+        $this->assertContains("Contact_fname Contact_lname - Contacts - Customers", $crawler->html());
+    }
+
+    public function testDelete()
+    {
+        $this->client->request(
+            'GET',
+            $this->client->generate('orocrm_contact_index', array('_format' =>'json')),
+            array(
+                'contacts[_filter][first_name][value]' => 'Contact_fname',
+                'contacts[_pager][_per_page]' => '10',
+                'contacts[_sort_by][first_name]' => 'ASC',
+                'contacts[_sort_by][last_name]' => 'ASC',
+            )
+        );
+
+        $result = $this->client->getResponse();
+        ToolsAPI::assertJsonResponse($result, 200);
+
+        $result = ToolsAPI::jsonToArray($result->getContent());
+        $result = reset($result['data']);
+
+        $this->client->request(
+            'DELETE',
+            $this->client->generate('oro_api_delete_contact', array('id' => $result['id']))
+        );
+
+        $result = $this->client->getResponse();
+        ToolsAPI::assertJsonResponse($result, 204);
+    }
+}
