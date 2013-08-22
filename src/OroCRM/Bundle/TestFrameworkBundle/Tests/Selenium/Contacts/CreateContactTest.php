b5a3c5c0dfec1012478bc2bea02d017f1ba073c7
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-08-22 16:01:01 +0300

    BAP-1397: Adding owner field  - Fixing selenium tests

--- src/OroCRM/Bundle/TestFrameworkBundle/Tests/Selenium/Contacts/CreateContactTest.php
+++ src/OroCRM/Bundle/TestFrameworkBundle/Tests/Selenium/Contacts/CreateContactTest.php
@@ -38,6 +38,8 @@ class CreateContactTest extends \PHPUnit_Extensions_Selenium2TestCase
         $this->setPort(intval(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_PORT));
         $this->setBrowser(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM2_BROWSER);
         $this->setBrowserUrl(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_TESTS_URL);
+        $this->prepareSession();
+        $this->url('/');
     }
 
     protected function tearDown()
@@ -62,6 +64,7 @@ class CreateContactTest extends \PHPUnit_Extensions_Selenium2TestCase
             ->add()
             ->setFirstName($contactname . '_first')
             ->setLastName($contactname . '_last')
+            ->setOwner('admin')
             ->setEmail($contactname . '@mail.com')
             ->setAddress($this->addressPrimary)
             ->setAddress($this->addressSecondary, 1)
@@ -96,6 +99,7 @@ class CreateContactTest extends \PHPUnit_Extensions_Selenium2TestCase
             ->add()
             ->setFirstName($contactname . '_first_autocomplete')
             ->setLastName($contactname . '_last_autocomplete')
+            ->setOwner('admin')
             ->setAssignedTo('admin')
             ->setReportsTo($contactname)
             ->setAddressStreet('Street')
