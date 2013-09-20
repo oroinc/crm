347a033475ac40187a5b7d48beea995d44ad46f5
Aleksandr Smaga <asmaga@magecore.com>
2013-09-20 17:28:33 +0200

    CRM-502: Account Life time value    - Report configuration

--- src/OroCRM/Bundle/DemoDataBundle/DataFixtures/LoadAccountOpportunitiesData.php
+++ src/OroCRM/Bundle/DemoDataBundle/DataFixtures/LoadAccountOpportunitiesData.php
@@ -4,12 +4,22 @@ namespace OroCRM\Bundle\DemoDataBundle\DataFixtures;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\Doctrine;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class LoadAccountOpportunitiesData extends AbstractFixture implements ContainerAwareInterface, OrderedFixtureInterface
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 400;
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -23,11 +33,12 @@ class LoadAccountOpportunitiesData extends AbstractFixture implements ContainerA
      */
     public function load(ObjectManager $om)
     {
-        $accounts = $om->getRepository('OroCRMAccountBundle:Account')->findAll();
+        $accounts      = $om->getRepository('OroCRMAccountBundle:Account')->findAll();
         $opportunities = $om->getRepository('OroCRMSalesBundle:Opportunity')->findAll();
 
         foreach ($opportunities as $opportunity) {
-            $opportunity->setAccount(array_rand($accounts));
+            $index = array_rand($accounts);
+            $opportunity->setAccount($accounts[$index]);
             $om->persist($opportunity);
         }
         $om->flush();
