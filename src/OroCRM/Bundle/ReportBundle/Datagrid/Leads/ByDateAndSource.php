bf388246e39e826f4229b546f9c813499d1b21a5
Aleksandr Smaga <asmaga@magecore.com>
2013-09-20 19:25:29 +0200

    CRM-504: Leads: Number Leads by date and source     - Added basic files

---
+++ src/OroCRM/Bundle/ReportBundle/Datagrid/Leads/ByDateAndSource.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace OroCRM\Bundle\ReportBundle\Datagrid\Leads;
+
+use OroCRM\Bundle\ReportBundle\Datagrid\ReportGridManagerAbstract;
+
+class ByDateAndSource extends ReportGridManagerAbstract
+{
+
+}
