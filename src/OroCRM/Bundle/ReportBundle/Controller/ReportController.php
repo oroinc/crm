c8d326019fb218a53ae871f97605782feb248612
Aleksandr Smaga <asmaga@magecore.com>
2013-09-20 14:26:26 +0200

    CRM-502: Account Life time value    - basic implementation

--- src/OroCRM/Bundle/ReportBundle/Controller/ReportController.php
+++ src/OroCRM/Bundle/ReportBundle/Controller/ReportController.php
@@ -5,7 +5,10 @@ namespace OroCRM\Bundle\ReportBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Symfony\Component\Yaml\Yaml;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use OroCRM\Bundle\ReportBundle\Datagrid\ReportGridManagerAbstract;
 
 class ReportController extends Controller
 {
@@ -13,22 +16,37 @@ class ReportController extends Controller
      * @Route(
      *      "/{reportGroupName}/{reportName}/{_format}",
      *      name="orocrm_report_index",
-     *      requirements={"reportGroupName"="\w+", "reportGroupName"="\w+", "_format"="html|json"},
+     *      requirements={"reportGroupName"="\w+", "reportName"="\w+", "_format"="html|json"},
      *      defaults={"_format" = "html"}
      * )
      * @Template()
      */
     public function indexAction($reportGroupName, $reportName)
     {
+        $input = Yaml::parse(file_get_contents(__DIR__ . '/../Resources/config/reports.yml'));
+        /** @var ReportGridManagerAbstract $datagridManager */
         $datagridManager = $this->get('orocrm_report.datagrid.' . implode('.', array($reportGroupName, $reportName)));
-        $datagridView    = $datagridManager->getDatagrid()->createView();
+
+        if (isset($input['reports'][$reportGroupName][$reportName])) {
+            $definition = $input['reports'][$reportGroupName][$reportName];
+            $datagridManager->setReportDefinitionArray($definition);
+            $datagridManager->getRouteGenerator()->setRouteParameters(
+                array(
+                    'reportGroupName' => $reportGroupName,
+                    'reportName'      => $reportName
+                )
+            );
+
+            $pageTitle = $definition['name'];
+        }
+        $datagridView = $datagridManager->getDatagrid()->createView();
 
         if ('json' == $this->getRequest()->getRequestFormat()) {
             return $this->get('oro_grid.renderer')->renderResultsJsonResponse($datagridView);
         }
 
         return array(
-            'pageTitle' => '',
+            'pageTitle' => $pageTitle,
             'datagrid'  => $datagridView
         );
     }
