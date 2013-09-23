8d10b6e098ebe2e2a8aed0b4ae316f7d40af6205
Aleksandr Smaga <asmaga@magecore.com>
2013-09-23 13:48:30 +0200

    CRM-516: Won Opportunities by date period     - added dynamic column

--- src/OroCRM/Bundle/ReportBundle/Datagrid/Opportunities/WonByPeriodManager.php
+++ src/OroCRM/Bundle/ReportBundle/Datagrid/Opportunities/WonByPeriodManager.php
@@ -2,18 +2,22 @@
 
 namespace OroCRM\Bundle\ReportBundle\Datagrid\Opportunities;
 
+use Oro\Bundle\GridBundle\Datagrid\DatagridInterface;
 use Oro\Bundle\GridBundle\Datagrid\ResultRecord;
 use Oro\Bundle\GridBundle\Filter\FilterInterface;
 use Oro\Bundle\GridBundle\Field\FieldDescription;
 use Oro\Bundle\GridBundle\Field\FieldDescriptionCollection;
 use Oro\Bundle\GridBundle\Field\FieldDescriptionInterface;
-
 use Oro\Bundle\GridBundle\Property\CallbackProperty;
-use OroCRM\Bundle\ReportBundle\Datagrid\ReportGridManagerAbstract;
+
 use OroCRM\Bundle\ReportBundle\Filter\PeriodFilter;
+use OroCRM\Bundle\ReportBundle\Datagrid\ReportGridManagerAbstract;
 
 class WonByPeriodManager extends ReportGridManagerAbstract
 {
+    const PERIOD_COLUMN_NAME          = 'period';
+    const PERIOD_FILTER_DEFAULT_VALUE = 'monthPeriod';
+
     /**
      * {@inheritDoc}
      */
@@ -21,32 +25,24 @@ class WonByPeriodManager extends ReportGridManagerAbstract
     {
         $field = new FieldDescription();
 
-        $periodName = 'yearPeriod';
-        $field->setName('period');
+        $field->setName(self::PERIOD_COLUMN_NAME);
         $field->setOptions(
             array(
-                'type'        => FieldDescriptionInterface::TYPE_TEXT,
-                'label'       => $this->translate('orocrm.report.datagrid.columns.period'),
-                'expression'  => 'period',
-                'required'    => true,
-                'sortable'    => true,
-                'show_column' => true,
-                'filter_type' => PeriodFilter::SERVICE_NAME,
-                'filterable'  => true,
-                'show_filter' => true,
-                'choices'     => array(
+                'type'             => FieldDescriptionInterface::TYPE_TEXT,
+                'label'            => $this->translate('orocrm.report.datagrid.columns.period'),
+                'expression'       => self::PERIOD_COLUMN_NAME,
+                'required'         => true,
+                'sortable'         => true,
+                'show_column'      => true,
+                'filter_type'      => PeriodFilter::SERVICE_NAME,
+                'filterable'       => true,
+                'show_filter'      => true,
+                'choices'          => array(
                     'monthPeriod'   => 'Monthly',
                     'quarterPeriod' => 'Quarterly',
                     'yearPeriod'    => 'Yearly'
-                )
-            )
-        );
-        $field->setProperty(
-            new CallbackProperty(
-                'period',
-                function (ResultRecord $record) use ($periodName) {
-                    return $record->getValue($periodName);
-                }
+                ),
+                'populate_default' => false
             )
         );
 
@@ -134,9 +130,45 @@ class WonByPeriodManager extends ReportGridManagerAbstract
     protected function getDefaultFilters()
     {
         return array(
-            'period' => array(
-                'value' => 'monthPeriod'
+            self::PERIOD_COLUMN_NAME => array(
+                'value' => self::PERIOD_FILTER_DEFAULT_VALUE
             )
         );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function configureDatagrid(DatagridInterface $datagrid)
+    {
+        parent::configureDatagrid($datagrid);
+
+        $periodName = $datagrid->getForm()->get(self::PERIOD_COLUMN_NAME)->get('value')->getNormData()
+            ? : self::PERIOD_FILTER_DEFAULT_VALUE;
+
+        // dynamically get needed column value
+        $datagrid->addProperty(
+            new CallbackProperty(
+                self::PERIOD_COLUMN_NAME,
+                function (ResultRecord $record) use ($periodName) {
+                    return $record->getValue($periodName);
+                }
+            )
+        );
+
+        /** @var FieldDescriptionInterface $field */
+        $originalField = $this->getFieldDescriptionCollection()->get(self::PERIOD_COLUMN_NAME);
+        $field         = clone $originalField;
+        $field->setName($periodName);
+        $fieldMapping = array(
+            'fieldName'       => $periodName,
+            'fieldExpression' => $periodName
+        );
+        $field->setFieldMapping($fieldMapping);
+
+        // change sorter field name
+        $sorter = $datagrid->getSorter(self::PERIOD_COLUMN_NAME);
+        $sorter->initialize($field, $sorter->getDirection());
+        $datagrid->addSorter($sorter);
     }
 }
