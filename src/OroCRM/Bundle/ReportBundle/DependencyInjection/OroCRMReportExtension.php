ff28681d43380de7108adf4db40a385162fc0d9b
Aleksandr Smaga <asmaga@magecore.com>
2013-09-24 13:55:34 +0200

    CRM-346: CRM Entities reports    - fix small issues

--- src/OroCRM/Bundle/ReportBundle/DependencyInjection/OroCRMReportExtension.php
+++ src/OroCRM/Bundle/ReportBundle/DependencyInjection/OroCRMReportExtension.php
@@ -23,7 +23,6 @@ class OroCRMReportExtension extends Extension
         $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
         $loader->load('orm_filter_types.yml');
         $loader->load('datagrid.yml');
     }
