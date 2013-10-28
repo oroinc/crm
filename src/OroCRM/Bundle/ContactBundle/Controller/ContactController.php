5da532adb98d18eb3f793938150300df73106eb5
Alexander Nezdoiminoga <alex@magecore.com>
2013-10-28 20:05:54 +0200

    contact view: relations

--- src/OroCRM/Bundle/ContactBundle/Controller/ContactController.php
+++ src/OroCRM/Bundle/ContactBundle/Controller/ContactController.php
@@ -3,6 +3,8 @@
 namespace OroCRM\Bundle\ContactBundle\Controller;
 
 use Doctrine\Common\Inflector\Inflector;
+use Doctrine\ORM\PersistentCollection;
+use Oro\Bundle\EntityConfigBundle\Metadata\EntityMetadata;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
@@ -62,6 +64,9 @@ class ContactController extends Controller
      */
     public function infoAction(Contact $contact)
     {
+        /** @var \Oro\Bundle\EntityConfigBundle\Config\ConfigManager $configManager */
+        //$configManager  = $this->get('oro_entity_config.config_manager');
+
         $extendProvider = $this->get('oro_entity_config.provider.extend');
         $entityProvider = $this->get('oro_entity_config.provider.entity');
         $viewProvider   = $this->get('oro_entity_config.provider.view');
@@ -78,10 +83,75 @@ class ContactController extends Controller
         );
 
         $dynamicRow = array();
-        foreach ($fields as $field) {
-            $label = $entityProvider->getConfigById($field->getId())->get('label') ? : $field->getId()->getFieldName();
 
-            $dynamicRow[$label] = $contact->{'get' . ucfirst(Inflector::camelize($field->getId()->getFieldName()))}();
+        foreach ($fields as $field) {
+            //$fieldName = $field->getId()->getFieldName();
+            //$value = $contact->{'get' . ucfirst(Inflector::camelize($fieldName))}();
+
+            /**
+             * Prepare DateTime field type
+             */
+            /*
+            if ($value instanceof \DateTime) {
+                $configFormat = $this->get('oro_config.global')->get('oro_locale.date_format') ? : 'Y-m-d';
+                $value        = $value->format($configFormat);
+            }*/
+
+            /**
+             * Prepare Relation field type
+             */
+            /*
+            if ($value instanceof PersistentCollection) {
+                $collection     = $value;
+                $extendConfig   = $extendProvider->getConfigById($field->getId());
+                $titleFieldName = $extendConfig->get('target_title');
+
+                /**
+                 * generate link for related entities collection
+                 * /
+                $route       = false;
+                $routeParams = false;
+
+                if (class_exists($extendConfig->get('target_entity'))) {
+                    /** @var EntityMetadata $metadata * /
+                    $metadata = $configManager->getEntityMetadata($extendConfig->get('target_entity'));
+                    if ($metadata && $metadata->routeView) {
+                        $route       = $metadata->routeView;
+                        $routeParams = array(
+                            'id' => null
+                        );
+                    }
+
+                    $relationExtendConfig = $extendProvider->getConfig($extendConfig->get('target_entity'));
+                    if ($relationExtendConfig->is('owner', ExtendManager::OWNER_CUSTOM)) {
+                        $route       = 'oro_entity_view';
+                        $routeParams = array(
+                            'entity_id' => str_replace('\\', '_', $extendConfig->get('target_entity')),
+                            'id'        => null
+                        );
+                    }
+                }
+
+                $value = array(
+                    'title'        => $titleFieldName,
+                    'route'        => $route,
+                    'route_params' => $routeParams,
+                    'values'       => array()
+                );
+
+                foreach ($collection as $item) {
+                    $routeParams['id'] = $item->getId();
+                    $value['values'][] = array(
+                        'id'    => $item->getId(),
+                        'link'  => $route ? $this->generateUrl($route, $routeParams) : false,
+                        'title' => $item->{Inflector::camelize('get_' . $titleFieldName)}()
+                    );
+                }
+            }*/
+
+            $fieldName = $field->getId()->getFieldName();
+            $dynamicRow[$entityProvider->getConfigById($field->getId())->get('label') ? : $fieldName]
+                = $contact->{'get' . ucfirst(Inflector::camelize($fieldName))}();
         }
 
         return array(
