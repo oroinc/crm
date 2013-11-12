ffa20e1c995ae0e63d7b6373e5780dbef9b30c8b
Ivan Shakuta <ishakuta@gmail.com>
2013-11-12 10:43:20 +0000

    CRM-577: Magento customer data import - integration bundle

--- src/OroCRM/Bundle/IntegrationBundle/DependencyInjection/Configuration.php
+++ src/OroCRM/Bundle/IntegrationBundle/DependencyInjection/Configuration.php
@@ -1,16 +1,10 @@
 <?php
 
-namespace OroCRM\Bundle\AccountBundle\DependencyInjection;
+namespace OroCRM\Bundle\IntegrationBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see
- * {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
 class Configuration implements ConfigurationInterface
 {
     /**
@@ -19,7 +13,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('orocrm_account');
+        $rootNode = $treeBuilder->root('orocrm_integration');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
---
+++ src/OroCRM/Bundle/IntegrationBundle/OroCRMIntegrationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OroCRM\Bundle\IntegrationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OroCRMIntegrationBundle extends Bundle
+{
+}
