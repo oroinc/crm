a467b34ebfa6fe800ddcd4bac8c74a06dcbc781c
Ivan Shakuta <ishakuta@gmail.com>
2013-11-13 11:43:48 +0000

    CRM-577: Magento customer import - db transport and connector changes

---
+++ src/OroCRM/Bundle/IntegrationBundle/Provider/Magento/MageDBTransport.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace OroCRM\Bundle\IntegrationBundle\Provider\Magento;
+
+use Doctrine\Bundle\DoctrineBundle\ConnectionFactory;
+use Doctrine\DBAL\Connection;
+
+use OroCRM\Bundle\IntegrationBundle\Provider\TransportInterface;
+
+/**
+ * Magento DB transport
+ * used to fetch and pull data to/from Magento instance
+ * with sessionId param using direct DB access
+ *
+ * @package OroCRM\Bundle\IntegrationBundle\Provider\Magento
+ */
+class MageDBTransport implements TransportInterface
+{
+    /** @var ConnectionFactory */
+    protected $connFactory;
+
+    /** @var Connection */
+    protected $conn;
+
+    /**
+     * @param ConnectionFactory $connFactory
+     */
+    public function __construct(ConnectionFactory $connFactory)
+    {
+        $this->connFactory = $connFactory;
+    }
+
+    /**
+     * @param array $settings
+     * @return bool|mixed
+     */
+    public function init(array $settings)
+    {
+        $this->conn = $this->connFactory->createConnection($settings);
+
+        return false;
+    }
+
+    /**
+     * @param $action
+     * @param $params
+     * @return mixed
+     */
+    public function call($action, $params = [])
+    {
+        // TODO: based on action create query and return array data
+        switch ($action) {
+            case 'customerCustomerList':
+                $qb = $this->conn->createQueryBuilder();
+                // query configuration, etc
+                break;
+            case 'customerCustomerInfo':
+                break;
+            case 'customerAddressList':
+                break;
+            case 'customerGroupList':
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ src/OroCRM/Bundle/IntegrationBundle/Provider/Magento/MagentoCustomerConnectorInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace OroCRM\Bundle\IntegrationBundle\Provider\Magento;
+
+interface MagentoCustomerConnectorInterface
+{
+    /**
+     * Get customer list
+     *
+     * @param array $filters
+     * @return array
+     */
+    public function getCustomersList($filters = []);
+
+    /**
+     * @param $id
+     * @param bool $isAddressesIncluded
+     * @param bool $isGroupsIncluded
+     * @param array $onlyAttributes
+     * @return mixed
+     */
+    public function getCustomerData($id, $isAddressesIncluded = false, $isGroupsIncluded = false, $onlyAttributes = []);
+
+    /**
+     * @param $customerId
+     * @return mixed
+     */
+    public function getCustomerAddressData($customerId);
+
+    /**
+     * Return customer groups assoc array
+     * with magento group ids as keys and codes as values
+     *
+     * @param null $groupId if specified, only data for this group will be returned
+     * @return mixed
+     */
+    public function getCustomerGroups($groupId = null);
+
+    public function saveCustomerData();
+
+    public function saveCustomerAddress();
+}
