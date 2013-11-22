3b3c20f3a96d131389cdd702744832880949912e
Vova Soroka <vsoroka@magecore.com>
2013-11-22 08:49:46 +0200

    BAP-2130: Table reports creation wizard (simplified UI)

--- src/OroCRM/Bundle/ReportBundle/Form/Type/ReportType.php
+++ src/OroCRM/Bundle/ReportBundle/Form/Type/ReportType.php
@@ -7,9 +7,10 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormEvent;
+use Oro\Bundle\QueryDesignerBundle\Form\Type\AbstractQueryDesignerType;
 use OroCRM\Bundle\ReportBundle\Entity\Report;
 
-class ReportType extends AbstractType
+class ReportType extends AbstractQueryDesignerType
 {
     /**
      * {@inheritdoc}
@@ -29,45 +30,9 @@ class ReportType extends AbstractType
                     'empty_value' => 'orocrm.report.form.choose_report_type'
                 )
             )
-            ->add('description', 'textarea', array('required' => false))
-            ->add('definition', 'hidden', array('required' => false));
+            ->add('description', 'textarea', array('required' => false));
 
-        $factory = $builder->getFormFactory();
-        $builder->addEventListener(
-            FormEvents::PRE_SET_DATA,
-            function (FormEvent $event) use ($factory) {
-                $form = $event->getForm();
-                /** @var Report $data */
-                $data = $event->getData();
-
-                $form->add(
-                    $factory->createNamed(
-                        'column',
-                        'oro_query_designer_column',
-                        null,
-                        array(
-                            'mapped'             => false,
-                            'column_choice_type' => 'orocrm_report_entity_field_choice',
-                            'entity'             => $data ? $data->getEntity() : null,
-                            'auto_initialize'    => false
-                        )
-                    )
-                );
-                $form->add(
-                    $factory->createNamed(
-                        'filter',
-                        'oro_query_designer_filter',
-                        null,
-                        array(
-                            'mapped'             => false,
-                            'column_choice_type' => 'orocrm_report_entity_field_choice',
-                            'entity'             => $data ? $data->getEntity() : null,
-                            'auto_initialize'    => false
-                        )
-                    )
-                );
-            }
-        );
+        parent::buildForm($builder, $options);
     }
 
     /**
@@ -75,13 +40,18 @@ class ReportType extends AbstractType
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(
+        $options = array_merge(
+            $this->getDefaultOptions(),
             array(
-                'data_class'         => 'OroCRM\Bundle\ReportBundle\Entity\Report',
-                'intention'          => 'report',
-                'cascade_validation' => true,
+                'data_class'                => 'OroCRM\Bundle\ReportBundle\Entity\Report',
+                'intention'                 => 'report',
+                'cascade_validation'        => true,
+                'column_column_choice_type' => 'orocrm_report_entity_field_choice',
+                'filter_column_choice_type' => 'orocrm_report_entity_field_choice'
             )
         );
+
+        $resolver->setDefaults($options);
     }
 
     /**
