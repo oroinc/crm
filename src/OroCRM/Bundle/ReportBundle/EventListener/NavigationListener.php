020c7831dac059132a747a2a7341779fd56a8f85
yurio <yurio@ua.fm>
2013-11-29 13:30:17 +0200

    BAP-2130: Groups eports in menu

--- src/OroCRM/Bundle/ReportBundle/EventListener/NavigationListener.php
+++ src/OroCRM/Bundle/ReportBundle/EventListener/NavigationListener.php
@@ -3,7 +3,9 @@
 namespace OroCRM\Bundle\ReportBundle\EventListener;
 
 use Doctrine\ORM\EntityManager;
+use Knp\Menu\ItemInterface;
 
+use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
 use Oro\Bundle\NavigationBundle\Event\ConfigureMenuEvent;
 
 class NavigationListener
@@ -14,12 +16,20 @@ class NavigationListener
     protected $em;
 
     /**
-     * @param EntityManager  $entityManager
+     * @var ConfigProvider
+     */
+    protected $entityConfigProvider = null;
+
+    /**
+     * @param EntityManager $entityManager
+     * @param ConfigProvider $entityConfigProvider
      */
     public function __construct(
-        EntityManager $entityManager
+        EntityManager $entityManager,
+        ConfigProvider $entityConfigProvider
     ) {
         $this->em = $entityManager;
+        $this->entityConfigProvider = $entityConfigProvider;
     }
 
     /**
@@ -27,13 +37,14 @@ class NavigationListener
      */
     public function onNavigationConfigure(ConfigureMenuEvent $event)
     {
+        /** @var ItemInterface $reportsMenuItem */
         $reportsMenuItem = $event->getMenu()->getChild('reports_tab');
         if ($reportsMenuItem) {
-            $reports = $this->em->getRepository('OroCRM\Bundle\ReportBundle\Entity\Report')->findAll();
+            $reports = $this->em->getRepository('OroCRM\Bundle\ReportBundle\Entity\Report')->findBy([], ['entity' => 'ASC', 'name' => 'ASC']);
             //todo: Add ACL Access level protection
             if (!empty($reports)) {
                 foreach ($reports as $report) {
-                    $reportsMenuItem->addChild(
+                    $this->getEntityMenuItem($reportsMenuItem, $report->getEntity())->addChild(
                         $report->getName(),
                         [
                             'label' => $report->getName(),
@@ -51,5 +62,32 @@ class NavigationListener
                 }
             }
         }
+    }
+
+    /**
+     * Get entity menu item for retprt item
+     *
+     * @param ItemInterface $reportItem
+     * @param $entityClass
+     * @return ItemInterface
+     */
+    protected function getEntityMenuItem(ItemInterface $reportItem, $entityClass)
+    {
+        $config = $this->entityConfigProvider->getConfig($entityClass);
+        $entityLabel = $config->get('label');
+        $entityItemName = $entityLabel . '_report_tab';
+        $entityItem = $reportItem->getChild($entityItemName);
+        if (!$entityItem) {
+            $reportItem->addChild(
+                $entityItemName,
+                [
+                    'label' => $entityLabel,
+                    'uri' => '#',
+                ]
+            );
+            $entityItem = $reportItem->getChild($entityItemName);
+        }
+
+        return $entityItem;
     }
 }
