85305ac60e0d20ebb8e6306b896e1fffb1895c44
Alexander Nezdoiminoga <alex@magecore.com>
2013-12-05 20:21:43 +0200

    contact translations

--- src/OroCRM/Bundle/ContactBundle/Entity/Contact.php
+++ src/OroCRM/Bundle/ContactBundle/Entity/Contact.php
@@ -36,7 +36,10 @@ use OroCRM\Bundle\AccountBundle\Entity\Account;
  *  routeName="orocrm_contact_index",
  *  routeView="orocrm_contact_view",
  *  defaultValues={
- *      "entity"={"label"="Contact", "plural_label"="Contacts"},
+ *      "entity"={
+ *          "label"="Contact",
+ *          "plural_label"="Contacts"
+ *      },
  *      "ownership"={
  *          "owner_type"="USER",
  *          "owner_field_name"="owner",
