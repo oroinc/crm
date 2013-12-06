b6fcb7b58de485f84923bc85df784f9a30f745fd
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2013-12-06 19:03:30 +0200

    BAP-2498: UI improvements  - added workflow attribute view for calls and emails  - small UI fixes

--- src/OroCRM/Bundle/B2CMockBundle/OroCRMB2CMockBundle.php
+++ src/OroCRM/Bundle/B2CMockBundle/OroCRMB2CMockBundle.php
@@ -3,7 +3,19 @@
 namespace OroCRM\Bundle\B2CMockBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Oro\Bundle\WorkflowBundle\DependencyInjection\Compiler\AddViewAttributeTwigTemplateCompilerPass;
 
 class OroCRMB2CMockBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(
+            new AddViewAttributeTwigTemplateCompilerPass('OroCRMB2CMockBundle:Workflow:view_attributes.html.twig')
+        );
+    }
 }
