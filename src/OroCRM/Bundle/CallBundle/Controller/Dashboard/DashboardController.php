c0b8d61bcdb3ffdba4ff73eeaea0eb4732ad4efd
yurio <yurio@ua.fm>
2013-12-12 13:20:49 +0200

    BAP-2525: Recent Calls widget

---
+++ src/OroCRM/Bundle/CallBundle/Controller/Dashboard/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace OroCRM\Bundle\CallBundle\Controller\Dashboard;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DashboardController extends Controller
+{
+    /**
+     * @Route(
+     *      "/recent_calls/{_format}",
+     *      name="orocrm_call_dashboard_recentcalls",
+     *      requirements={"_format"="html|json"},
+     *      defaults={"_format" = "html"}
+     * )
+     * @Template("OroCRMCallBundle:Dashboard:recentCalls.html.twig")
+     */
+    public function recentCallsAction()
+    {
+        return ['currentUserId' => $this->getUser()->getId()];
+    }
+}
