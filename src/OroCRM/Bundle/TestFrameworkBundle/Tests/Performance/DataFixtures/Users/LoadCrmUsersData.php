b076583527460f32cb01a81f44356c31dd747463
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-12-12 18:49:52 +0200

    BAP-498: Refactoring code style

--- src/OroCRM/Bundle/TestFrameworkBundle/Tests/Performance/DataFixtures/Users/LoadCrmUsersData.php
+++ src/OroCRM/Bundle/TestFrameworkBundle/Tests/Performance/DataFixtures/Users/LoadCrmUsersData.php
@@ -13,11 +13,6 @@ use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Oro\Bundle\UserBundle\Entity\UserManager;
 use Oro\Bundle\UserBundle\Entity\User;
 
-/**
- * @SuppressWarnings(PHPMD.TooManyMethods)
- * @SuppressWarnings(PHPMD.ExcessiveClassComplexity)
- * TODO: This class should be refactored (BAP-975)
- */
 class LoadCrmUsersData extends AbstractFixture implements ContainerAwareInterface
 {
     const USERS_NUMBER  = 200;
@@ -102,8 +97,6 @@ class LoadCrmUsersData extends AbstractFixture implements ContainerAwareInterfac
      * @param string $middleName
      * @param \DateTime $birthday
      * @return User
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
-     * TODO: This method should be refactored (BAP-975)
      */
     private function createUser(
         $username,
