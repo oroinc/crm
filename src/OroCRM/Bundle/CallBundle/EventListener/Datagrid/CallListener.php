caeed19cab901470a2015c9510158d3e425fb779
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2013-12-24 17:09:25 +0200

    BAP-2652: Call log grid on Contact view  - fixed call datagrid configuration  - removed extra grid column templates  - updated dashboard calls grid  - updated call listener and unit test

--- src/OroCRM/Bundle/CallBundle/EventListener/Datagrid/CallListener.php
+++ src/OroCRM/Bundle/CallBundle/EventListener/Datagrid/CallListener.php
@@ -4,33 +4,59 @@ namespace OroCRM\Bundle\CallBundle\EventListener\Datagrid;
 
 use Doctrine\ORM\EntityManager;
 
-use Oro\Bundle\DataGridBundle\Datagrid\RequestParameters;
 use Oro\Bundle\DataGridBundle\Datasource\Orm\OrmDatasource;
 use Oro\Bundle\DataGridBundle\Event\BuildAfter;
+use Oro\Bundle\DataGridBundle\Event\BuildBefore;
+use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 
 class CallListener
 {
-    /**
-     * @var RequestParameters
-     */
-    protected $requestParameters;
-
     /**
      * @var EntityManager
      */
     protected $entityManager;
 
     /**
-     * @param RequestParameters $requestParameters
      * @param EntityManager $entityManager
      */
-    public function __construct(RequestParameters $requestParameters, EntityManager $entityManager)
+    public function __construct(EntityManager $entityManager)
     {
-        $this->requestParameters = $requestParameters;
         $this->entityManager = $entityManager;
     }
 
     /**
+     * Remove useless fields in case of filtering
+     *
+     * @param BuildBefore $event
+     */
+    public function onBuildBefore(BuildBefore $event)
+    {
+        $config = $event->getConfig();
+        $parameters = $event->getParameters();
+
+        if (!empty($parameters['contactId'])) {
+            $this->removeColumn($config, 'contactName');
+        }
+
+        if (!empty($parameters['accountId'])) {
+            $this->removeColumn($config, 'accountName');
+        }
+    }
+
+    /**
+     * @param DatagridConfiguration $config
+     * @param string $fieldName
+     */
+    protected function removeColumn(DatagridConfiguration $config, $fieldName)
+    {
+        $config->offsetUnsetByPath(sprintf('[columns][%s]', $fieldName));
+        $config->offsetUnsetByPath(sprintf('[filters][columns][%s]', $fieldName));
+        $config->offsetUnsetByPath(sprintf('[sorters][columns][%s]', $fieldName));
+    }
+
+    /**
+     * Add required filters
+     *
      * @param BuildAfter $event
      */
     public function onBuildAfter(BuildAfter $event)
@@ -38,26 +64,24 @@ class CallListener
         /** @var OrmDatasource $ormDataSource */
         $ormDataSource = $event->getDatagrid()->getDatasource();
         $queryBuilder = $ormDataSource->getQueryBuilder();
+        $parameters = $event->getParameters();
 
-        $userId = $this->requestParameters->get('userId', null);
-        if ($userId) {
-            $user = $this->entityManager->find('OroUserBundle:User', $userId);
+        if (!empty($parameters['userId'])) {
+            $user = $this->entityManager->find('OroUserBundle:User', $parameters['userId']);
             $queryBuilder
                 ->andWhere('call.owner = :user')
                 ->setParameter('user', $user);
         }
 
-        $contactId = $this->requestParameters->get('contactId', null);
-        if ($contactId) {
-            $contact = $this->entityManager->find('OroCRMContactBundle:Contact', $contactId);
+        if (!empty($parameters['contactId'])) {
+            $contact = $this->entityManager->find('OroCRMContactBundle:Contact', $parameters['contactId']);
             $queryBuilder
                 ->andWhere('call.relatedContact = :contact')
                 ->setParameter('contact', $contact);
         }
 
-        $accountId = $this->requestParameters->get('accountId', null);
-        if ($accountId) {
-            $account = $this->entityManager->find('OroCRMAccountBundle:Account', $accountId);
+        if (!empty($parameters['accountId'])) {
+            $account = $this->entityManager->find('OroCRMAccountBundle:Account', $parameters['accountId']);
             $queryBuilder
                 ->andWhere('(call.relatedAccount = :account OR :account MEMBER OF contact.accounts)')
                 ->setParameter('account', $account);
