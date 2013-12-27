24cb37afa882d8088e87e9143b858d8af27e2e17
yurio <yurio@ua.fm>
2013-12-27 18:29:31 +0200

    Add OROCrm installer script

--- install.php
+++ install.php
@@ -5,7 +5,5 @@
 
 /** @var $container \Symfony\Component\DependencyInjection\ContainerInterface */
 // Temporary fix till EmailAddress will be moved to the cache folder
-$cacheDir = $container->getParameter('oro_email.entity.cache_dir');
-
-$container->get('oro_email.entity.cache.clearer')->force($cacheDir);
-$container->get('oro_email.entity.cache.warmer')->warmUp($cacheDir);
+$container->get('oro_email.entity.cache.clearer')->forceClear();
+$container->get('oro_email.entity.cache.warmer')->warmUp('');
