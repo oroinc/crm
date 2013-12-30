94171ab7a22644f332c4a27c3f45793f142b0d99
Roman Grebenchuk <rgrebenchuk@magecore.com>
2013-12-30 21:47:42 +0200

    BAP-2640: Run a script  - added process isolation

--- install.php
+++ install.php
@@ -9,5 +9,5 @@
 $container->get('oro_email.entity.cache.clearer')->forceClear();
 $container->get('oro_email.entity.cache.warmer')->warmUp('');
 $commandExecutor
-    ->runCommand('doctrine:schema:update', array('--force', '--process-isolation'))
+    ->runCommand('doctrine:schema:update', array('--force' => true, '--process-isolation' => true))
     ->runCommand('oro:search:create-index');
