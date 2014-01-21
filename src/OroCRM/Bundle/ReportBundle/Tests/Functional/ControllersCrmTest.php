de52cf17a81b9a9720666e08a1f8233b45040186
Roman Grebenchuk <rgrebenchuk@magecore.com>
2014-01-21 18:44:58 +0200

    BAP-2828: Manage Custom Reports functional tests  - added CR changes

--- src/OroCRM/Bundle/ReportBundle/Tests/Functional/ControllersCrmTest.php
+++ src/OroCRM/Bundle/ReportBundle/Tests/Functional/ControllersCrmTest.php
@@ -22,10 +22,6 @@ class ControllersCrmTest extends BaseControllersTest
             array(),
             array_merge(ToolsAPI::generateBasicHeader(), array('HTTP_X-CSRF-Header' => 1))
         );
-        if (!self::$fixturesLoaded) {
-            $this->client->appendFixtures(__DIR__ . DIRECTORY_SEPARATOR . 'fixtures', array('LoadLead'));
-            self::$fixturesLoaded = true;
-        }
     }
 
     /**
