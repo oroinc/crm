be6a362fb30eae30d811ffc305e7c0942b6dcb30
yurio <yurio@ua.fm>
2014-01-29 19:21:15 +0200

    BAP-2878: Fix Fixtures Dependencies

--- src/OroCRM/Bundle/DemoDataBundle/Migrations/DataFixtures/Demo/ORM/v1_0/LoadLeadsData.php
+++ src/OroCRM/Bundle/DemoDataBundle/Migrations/DataFixtures/Demo/ORM/v1_0/LoadLeadsData.php
@@ -64,7 +64,7 @@ class LoadLeadsData extends AbstractFixture implements ContainerAwareInterface, 
         return [
             'OroCRM\Bundle\DemoDataBundle\Migrations\DataFixtures\Demo\ORM\v1_0\LoadUsersData',
             'OroCRM\Bundle\DemoDataBundle\Migrations\DataFixtures\Demo\ORM\v1_0\LoadLeadSourceData',
-            'OroCRM\Bundle\DemoDataBundle\Migrations\DataFixtures\Demo\ORM\v1_0\LoadAclData'
+            'OroCRM\Bundle\DemoDataBundle\Migrations\DataFixtures\Demo\ORM\v1_0\LoadAccountData'
         ];
     }
 
