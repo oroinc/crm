2ad0217a30dfe2e19b5ecc64d736a05a62408788
Ruslan Polutsygan <rpolytsygan@magecore.com>
2014-01-29 16:09:25 +0200

    ticket/BAP-2755: embedded forms  - added EmbeddedFormTypeInterface  - dynamic loading of default css and default  success message  - watching form state and showing confirm box if css and message differ from default

--- src/OroCRM/Bundle/ContactUsBundle/Form/Type/ContactRequestType.php
+++ src/OroCRM/Bundle/ContactUsBundle/Form/Type/ContactRequestType.php
@@ -1,11 +1,12 @@
 <?php
 namespace OroCRM\Bundle\ContactUsBundle\Form\Type;
 
+use Oro\Bundle\EmbeddedFormBundle\Form\Type\EmbeddedFormInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class ContactRequestType extends AbstractType
+class ContactRequestType extends AbstractType implements EmbeddedFormInterface
 {
     /**
      * {@inheritdoc}
@@ -40,4 +41,57 @@ class ContactRequestType extends AbstractType
             ]
         );
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultCss()
+    {
+        return <<<CSS
+form {
+  font-family: "Helvetica Neue", Arial, Helvetica, sans-serif;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+  cursor: pointer;
+}
+
+label, input, button, select, textarea {
+font-size: 13px;
+font-weight: normal;
+line-height: 20px;
+}
+
+textarea, input[type="text"] {
+background-color: #fff;
+border: 1px solid #ccc;
+}
+
+label.validation-error {
+  color: #C81717 !important;
+}
+
+.validation-error .error {
+  border: 1px solid #e9322d;
+  outline: 0;
+  outline: thin dotted \9;
+  -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,0.075), 0 0 8px rgba(211,33,33,0.6);
+  -moz-box-shadow: inset 0 1px 1px rgba(0,0,0,0.075), 0 0 8px rgba(211,33,33,0.6);
+  box-shadow: inset 0 1px 1px rgba(0,0,0,0.075), 0 0 8px rgba(211,33,33,0.6);
+  color: #555;
+}
+CSS;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultSuccessMessage()
+    {
+        return '<h3>Form has been submitted successfully</h3>{back_link}';
+    }
+
+
 }
