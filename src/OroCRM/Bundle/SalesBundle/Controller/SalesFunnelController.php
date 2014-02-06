dbf2f7b220b8bf1b9c8e783d2ddf491eadf4eae3
Dmitry Khrysev <x86demon@gmail.com>
2014-02-06 20:17:58 +0200

    BAP-3074: Create DB entity SalesFunnel with grid and view pages

---
+++ src/OroCRM/Bundle/SalesBundle/Controller/SalesFunnelController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace OroCRM\Bundle\SalesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Oro\Bundle\SecurityBundle\Annotation\Acl;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+
+use OroCRM\Bundle\SalesBundle\Entity\SalesFunnel;
+
+/**
+ * @Route("/salesfunnel")
+ */
+class SalesFunnelController extends Controller
+{
+    /**
+     * @Route(
+     *      "/{_format}",
+     *      name="orocrm_sales_funnel_index",
+     *      requirements={"_format"="html|json"},
+     *      defaults={"_format" = "html"}
+     * )
+     * @Template
+     * @AclAncestor("orocrm_sales_funnel_view")
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/view/{id}", name="orocrm_sales_funnel_view", requirements={"id"="\d+"})
+     * @Template
+     * @Acl(
+     *      id="orocrm_sales_funnel_view",
+     *      type="entity",
+     *      permission="VIEW",
+     *      class="OroCRMSalesBundle:SalesFunnel"
+     * )
+     */
+    public function viewAction(SalesFunnel $entity)
+    {
+        return array(
+            'entity' => $entity,
+        );
+    }
+
+    /**
+     * @Route("/info/{id}", name="orocrm_sales_funnel_info", requirements={"id"="\d+"})
+     * @Template
+     * @AclAncestor("orocrm_sales_funnel_view")
+     */
+    public function infoAction(SalesFunnel $entity)
+    {
+        return array(
+            'entity'  => $entity
+        );
+    }
+
+    /**
+     * @Route("/create", name="orocrm_sales_funnel_create")
+     * @Template("OroCRMSalesBundle:SalesFunnel:update.html.twig")
+     * @Acl(
+     *      id="orocrm_sales_funnel_create",
+     *      type="entity",
+     *      permission="CREATE",
+     *      class="OroCRMSalesBundle:SalesFunnel"
+     * )
+     */
+    public function createAction()
+    {
+        $entity = new SalesFunnel();
+        return $this->update($entity);
+    }
+
+    /**
+     * @Route("/update/{id}", name="orocrm_sales_funnel_update", requirements={"id"="\d+"}, defaults={"id"=0})
+     * @Template
+     * @Acl(
+     *      id="orocrm_sales_funnel_update",
+     *      type="entity",
+     *      permission="EDIT",
+     *      class="OroCRMSalesBundle:SalesFunnel"
+     * )
+     */
+    public function updateAction(SalesFunnel $entity)
+    {
+        return $this->update($entity);
+    }
+
+    /**
+     * @param SalesFunnel $entity
+     * @return array
+     */
+    protected function update(SalesFunnel $entity)
+    {
+        if ($this->get('orocrm_sales.funnel.form.handler')->process($entity)) {
+            $this->get('session')->getFlashBag()->add(
+                'success',
+                $this->get('translator')->trans('orocrm.sales.controller.sales_funnel.saved.message')
+            );
+
+            return $this->get('oro_ui.router')->redirectAfterSave(
+                array('route' => 'orocrm_sales_funnel_update', 'parameters' => array('id' => $entity->getId())),
+                array('route' => 'orocrm_sales_funnel_view', 'parameters' => array('id' => $entity->getId())),
+                $entity
+            );
+        }
+
+        return array(
+            'entity' => $entity,
+            'form' => $this->get('orocrm_sales.funnel.form')->createView(),
+        );
+    }
+}
---
+++ src/OroCRM/Bundle/SalesBundle/Form/Type/SalesFunnelType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace OroCRM\Bundle\SalesBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SalesFunnelType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text', array('required' => true, 'label' => 'orocrm.sales.funnel.name.label'))
+            ->add('startDate', 'date', array('required' => true, 'label' => 'orocrm.sales.funnel.start_date.label'));
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => 'OroCRM\Bundle\SalesBundle\Entity\SalesFunnel',
+                'cascade_validation' => false,
+            )
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'orocrm_sales_funnel';
+    }
+}
