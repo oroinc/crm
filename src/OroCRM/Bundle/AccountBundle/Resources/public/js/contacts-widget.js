810301ca39025c76a426939d0bb831cce1cec7b1
Alexandr <avreliy.89@gmail.com>
2014-02-21 19:44:50 +0200

    CRM-841: Contacts widget block - remove unused dependencies - modify css, js, twig

--- src/OroCRM/Bundle/AccountBundle/Resources/public/js/contacts-widget.js
+++ src/OroCRM/Bundle/AccountBundle/Resources/public/js/contacts-widget.js
@@ -1,22 +1,26 @@
-define(['jquery'], function($){
+/* global define */
+define(['jquery'], function ($) {
     return {
-
         /**
          * @desc Fire name link click
          * @callback
          */
-        squadClickHandler: function(onClickEven){
-            if($(onClickEven.target).hasClass('contact-squad-email-link')){
+        squadClickHandler: function (onClickEven) {
+            /**
+             * @desc if target item has class contact-box-link
+             * we does not click redirection link(name link)
+             */
+            if ($(onClickEven.target).hasClass('contact-box-link')) {
                 return;
             }
-            $(this).find('.contact-squad-name-link').click();
+            $(this).find('.contact-box-name-link').click();
         },
 
         /**
          * @constructs
          */
-        init: function(){
-            $('.contact-squad').click(this.squadClickHandler);
+        init: function () {
+            $('.contact-box').click(this.squadClickHandler);
         }
     };
 });
