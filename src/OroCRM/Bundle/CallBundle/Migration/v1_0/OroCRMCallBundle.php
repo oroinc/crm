b98f2f8112c742c09aaa2665e0687c0d090fb3fb
Alexander Nezdoiminoga <alex@magecore.com>
2014-02-24 13:41:01 +0200

    BAP-3159: OroCRMCallBundle migrations

--- src/OroCRM/Bundle/CallBundle/Migration/v1_0/OroCRMCallBundle.php
+++ src/OroCRM/Bundle/CallBundle/Migration/v1_0/OroCRMCallBundle.php
@@ -9,20 +9,61 @@ class OroCRMCallBundle implements Migration
 {
     /**
      * @inheritdoc
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     public function up(Schema $schema)
     {
-        return [
-            "CREATE TABLE orocrm_call (id INT AUTO_INCREMENT NOT NULL, call_direction_name VARCHAR(32) DEFAULT NULL, related_account_id INT DEFAULT NULL, related_contact_id INT DEFAULT NULL, call_status_name VARCHAR(32) DEFAULT NULL, owner_id INT DEFAULT NULL, contact_phone_id INT DEFAULT NULL, subject VARCHAR(255) NOT NULL, phone_number VARCHAR(255) DEFAULT NULL, notes LONGTEXT DEFAULT NULL, call_date_time DATETIME NOT NULL, duration TIME DEFAULT NULL, INDEX IDX_1FBD1A247E3C61F9 (owner_id), INDEX IDX_1FBD1A246D6C2DFA (related_contact_id), INDEX IDX_1FBD1A2411A6570A (related_account_id), INDEX IDX_1FBD1A24A156BF5C (contact_phone_id), INDEX IDX_1FBD1A2476DB3689 (call_status_name), INDEX IDX_1FBD1A249F3E257D (call_direction_name), PRIMARY KEY(id))",
-            "CREATE TABLE orocrm_call_direction (name VARCHAR(32) NOT NULL, `label` VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_D0EB34BAEA750E8 (`label`), PRIMARY KEY(name))",
-            "CREATE TABLE orocrm_call_status (name VARCHAR(32) NOT NULL, `label` VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_FBA13581EA750E8 (`label`), PRIMARY KEY(name))",
+        // @codingStandardsIgnoreStart
 
-            "ALTER TABLE orocrm_call ADD CONSTRAINT FK_1FBD1A249F3E257D FOREIGN KEY (call_direction_name) REFERENCES orocrm_call_direction (name) ON DELETE SET NULL",
-            "ALTER TABLE orocrm_call ADD CONSTRAINT FK_1FBD1A2411A6570A FOREIGN KEY (related_account_id) REFERENCES orocrm_account (id) ON DELETE SET NULL",
-            "ALTER TABLE orocrm_call ADD CONSTRAINT FK_1FBD1A246D6C2DFA FOREIGN KEY (related_contact_id) REFERENCES orocrm_contact (id) ON DELETE SET NULL",
-            "ALTER TABLE orocrm_call ADD CONSTRAINT FK_1FBD1A2476DB3689 FOREIGN KEY (call_status_name) REFERENCES orocrm_call_status (name) ON DELETE SET NULL",
-            "ALTER TABLE orocrm_call ADD CONSTRAINT FK_1FBD1A247E3C61F9 FOREIGN KEY (owner_id) REFERENCES oro_user (id) ON DELETE SET NULL",
-            "ALTER TABLE orocrm_call ADD CONSTRAINT FK_1FBD1A24A156BF5C FOREIGN KEY (contact_phone_id) REFERENCES orocrm_contact_phone (id) ON DELETE SET NULL"
-        ];
+        /** Generate table orocrm_call **/
+        $table = $schema->createTable('orocrm_call');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->addColumn('call_direction_name', 'string', ['default' => null, 'notnull' => false, 'length' => 32, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('related_account_id', 'integer', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('related_contact_id', 'integer', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('call_status_name', 'string', ['default' => null, 'notnull' => false, 'length' => 32, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('owner_id', 'integer', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('contact_phone_id', 'integer', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('subject', 'string', ['default' => null, 'notnull' => true, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('phone_number', 'string', ['default' => null, 'notnull' => false, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('notes', 'text', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('call_date_time', 'datetime', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('duration', 'time', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['owner_id'], 'IDX_1FBD1A247E3C61F9', []);
+        $table->addIndex(['related_contact_id'], 'IDX_1FBD1A246D6C2DFA', []);
+        $table->addIndex(['related_account_id'], 'IDX_1FBD1A2411A6570A', []);
+        $table->addIndex(['contact_phone_id'], 'IDX_1FBD1A24A156BF5C', []);
+        $table->addIndex(['call_status_name'], 'IDX_1FBD1A2476DB3689', []);
+        $table->addIndex(['call_direction_name'], 'IDX_1FBD1A249F3E257D', []);
+        /** End of generate table orocrm_call **/
+
+        /** Generate table orocrm_call_direction **/
+        $table = $schema->createTable('orocrm_call_direction');
+        $table->addColumn('name', 'string', ['default' => null, 'notnull' => true, 'length' => 32, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('label', 'string', ['default' => null, 'notnull' => true, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['name']);
+        $table->addUniqueIndex(['label'], 'UNIQ_D0EB34BAEA750E8');
+        /** End of generate table orocrm_call_direction **/
+
+        /** Generate table orocrm_call_status **/
+        $table = $schema->createTable('orocrm_call_status');
+        $table->addColumn('name', 'string', ['default' => null, 'notnull' => true, 'length' => 32, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('label', 'string', ['default' => null, 'notnull' => true, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['name']);
+        $table->addUniqueIndex(['label'], 'UNIQ_FBA13581EA750E8');
+        /** End of generate table orocrm_call_status **/
+
+        /** Generate foreign keys for table orocrm_call **/
+        $table = $schema->getTable('orocrm_call');
+        $table->addForeignKeyConstraint($schema->getTable('orocrm_call_direction'), ['call_direction_name'], ['name'], ['onDelete' => 'SET NULL', 'onUpdate' => null]);
+        $table->addForeignKeyConstraint($schema->getTable('orocrm_account'), ['related_account_id'], ['id'], ['onDelete' => 'SET NULL', 'onUpdate' => null]);
+        $table->addForeignKeyConstraint($schema->getTable('orocrm_contact'), ['related_contact_id'], ['id'], ['onDelete' => 'SET NULL', 'onUpdate' => null]);
+        $table->addForeignKeyConstraint($schema->getTable('orocrm_call_status'), ['call_status_name'], ['name'], ['onDelete' => 'SET NULL', 'onUpdate' => null]);
+        $table->addForeignKeyConstraint($schema->getTable('oro_user'), ['owner_id'], ['id'], ['onDelete' => 'SET NULL', 'onUpdate' => null]);
+        $table->addForeignKeyConstraint($schema->getTable('orocrm_contact_phone'), ['contact_phone_id'], ['id'], ['onDelete' => 'SET NULL', 'onUpdate' => null]);
+        /** End of generate foreign keys for table orocrm_call **/
+
+        // @codingStandardsIgnoreEnd
     }
 }
