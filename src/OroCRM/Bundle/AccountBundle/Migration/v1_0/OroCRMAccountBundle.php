2d9e3a52cf7f1cbf22d400004ea776ce44777429
Alexander Nezdoiminoga <alex@magecore.com>
2014-02-24 13:40:49 +0200

    BAP-3159: OroCRMAccountBundle migrations

--- src/OroCRM/Bundle/AccountBundle/Migration/v1_0/OroCRMAccountBundle.php
+++ src/OroCRM/Bundle/AccountBundle/Migration/v1_0/OroCRMAccountBundle.php
@@ -9,19 +9,53 @@ class OroCRMAccountBundle implements Migration
 {
     /**
      * @inheritdoc
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     public function up(Schema $schema)
     {
-        return [
-            "CREATE TABLE orocrm_account (id INT AUTO_INCREMENT NOT NULL, shipping_address_id INT DEFAULT NULL, billing_address_id INT DEFAULT NULL, user_owner_id INT DEFAULT NULL, default_contact_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, createdAt DATETIME NOT NULL, updatedAt DATETIME NOT NULL, INDEX IDX_7166D3719EB185F9 (user_owner_id), INDEX IDX_7166D3714D4CFF2B (shipping_address_id), INDEX IDX_7166D37179D0C0E4 (billing_address_id), INDEX IDX_7166D371AF827129 (default_contact_id), INDEX account_name_idx (name), PRIMARY KEY(id))",
-            "CREATE TABLE orocrm_account_to_contact (account_id INT NOT NULL, contact_id INT NOT NULL, INDEX IDX_65B8FBEC9B6B5FBA (account_id), INDEX IDX_65B8FBECE7A1254A (contact_id), PRIMARY KEY(account_id, contact_id))",
+        // @codingStandardsIgnoreStart
 
-            "ALTER TABLE orocrm_account ADD CONSTRAINT FK_7166D3714D4CFF2B FOREIGN KEY (shipping_address_id) REFERENCES oro_address (id) ON DELETE SET NULL",
-            "ALTER TABLE orocrm_account ADD CONSTRAINT FK_7166D37179D0C0E4 FOREIGN KEY (billing_address_id) REFERENCES oro_address (id) ON DELETE SET NULL",
-            "ALTER TABLE orocrm_account ADD CONSTRAINT FK_7166D3719EB185F9 FOREIGN KEY (user_owner_id) REFERENCES oro_user (id) ON DELETE SET NULL",
-            "ALTER TABLE orocrm_account ADD CONSTRAINT FK_7166D371AF827129 FOREIGN KEY (default_contact_id) REFERENCES orocrm_contact (id) ON DELETE SET NULL",
-            "ALTER TABLE orocrm_account_to_contact ADD CONSTRAINT FK_65B8FBECE7A1254A FOREIGN KEY (contact_id) REFERENCES orocrm_contact (id) ON DELETE CASCADE",
-            "ALTER TABLE orocrm_account_to_contact ADD CONSTRAINT FK_65B8FBEC9B6B5FBA FOREIGN KEY (account_id) REFERENCES orocrm_account (id) ON DELETE CASCADE"
-        ];
+        /** Generate table orocrm_account **/
+        $table = $schema->createTable('orocrm_account');
+        $table->addColumn('id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => true, 'comment' => '']);
+        $table->addColumn('default_contact_id', 'integer', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('shipping_address_id', 'integer', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('billing_address_id', 'integer', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('user_owner_id', 'integer', ['default' => null, 'notnull' => false, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('name', 'string', ['default' => null, 'notnull' => true, 'length' => 255, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('createdAt', 'datetime', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('updatedAt', 'datetime', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['user_owner_id'], 'IDX_7166D3719EB185F9', []);
+        $table->addIndex(['shipping_address_id'], 'IDX_7166D3714D4CFF2B', []);
+        $table->addIndex(['billing_address_id'], 'IDX_7166D37179D0C0E4', []);
+        $table->addIndex(['default_contact_id'], 'IDX_7166D371AF827129', []);
+        $table->addIndex(['name'], 'account_name_idx', []);
+        /** End of generate table orocrm_account **/
+
+        /** Generate table orocrm_account_to_contact **/
+        $table = $schema->createTable('orocrm_account_to_contact');
+        $table->addColumn('account_id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->addColumn('contact_id', 'integer', ['default' => null, 'notnull' => true, 'length' => null, 'precision' => 10, 'scale' => 0, 'fixed' => false, 'unsigned' => false, 'autoincrement' => false, 'comment' => '']);
+        $table->setPrimaryKey(['account_id', 'contact_id']);
+        $table->addIndex(['account_id'], 'IDX_65B8FBEC9B6B5FBA', []);
+        $table->addIndex(['contact_id'], 'IDX_65B8FBECE7A1254A', []);
+        /** End of generate table orocrm_account_to_contact **/
+
+        /** Generate foreign keys for table orocrm_account **/
+        $table = $schema->getTable('orocrm_account');
+        $table->addForeignKeyConstraint($schema->getTable('orocrm_contact'), ['default_contact_id'], ['id'], ['onDelete' => 'SET NULL', 'onUpdate' => null]);
+        $table->addForeignKeyConstraint($schema->getTable('oro_address'), ['shipping_address_id'], ['id'], ['onDelete' => 'SET NULL', 'onUpdate' => null]);
+        $table->addForeignKeyConstraint($schema->getTable('oro_address'), ['billing_address_id'], ['id'], ['onDelete' => 'SET NULL', 'onUpdate' => null]);
+        $table->addForeignKeyConstraint($schema->getTable('oro_user'), ['user_owner_id'], ['id'], ['onDelete' => 'SET NULL', 'onUpdate' => null]);
+        /** End of generate foreign keys for table orocrm_account **/
+
+        /** Generate foreign keys for table orocrm_account_to_contact **/
+        $table = $schema->getTable('orocrm_account_to_contact');
+        $table->addForeignKeyConstraint($schema->getTable('orocrm_contact'), ['contact_id'], ['id'], ['onDelete' => 'CASCADE', 'onUpdate' => null]);
+        $table->addForeignKeyConstraint($schema->getTable('orocrm_account'), ['account_id'], ['id'], ['onDelete' => 'CASCADE', 'onUpdate' => null]);
+        /** End of generate foreign keys for table orocrm_account_to_contact **/
+
+        // @codingStandardsIgnoreEnd
     }
 }
