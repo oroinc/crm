56d80242de64417063da22b9fb3191ae3b6bbec8
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2014-02-26 16:00:38 +0200

    CRM-841: Contacts widget block - CR changes

--- src/OroCRM/Bundle/AccountBundle/Resources/public/js/contacts-widget.js
+++ src/OroCRM/Bundle/AccountBundle/Resources/public/js/contacts-widget.js
@@ -9,12 +9,12 @@ define(['jquery'], function ($) {
          * @desc Fire name link click
          * @callback
          */
-        boxClickHandler: function (onClickEven) {
+        boxClickHandler: function (even) {
             /**
              * @desc if target item has class contact-box-link
              * we does not click redirection link(name link)
              */
-            if ($(onClickEven.target).hasClass('contact-box-link')) {
+            if ($(even.target).hasClass('contact-box-link')) {
                 return;
             }
             $(this).find('.contact-box-name-link').click();
