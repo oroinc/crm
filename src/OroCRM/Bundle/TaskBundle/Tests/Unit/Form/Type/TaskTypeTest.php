eeab875cd23eaa6030060c82d5a4818c50aa2fa0
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-03-04 20:20:32 +0200

    BAP-3124: Add Grid and CRUD for task - add form type test

--- src/OroCRM/Bundle/TaskBundle/Tests/Unit/Form/Type/TaskTypeTest.php
+++ src/OroCRM/Bundle/TaskBundle/Tests/Unit/Form/Type/TaskTypeTest.php
@@ -2,7 +2,6 @@
 
 namespace OroCRM\Bundle\TaskBundle\Tests\Unit\Form\Type;
 
-
 use OroCRM\Bundle\TaskBundle\Form\Type\TaskType;
 
 class TaskTypeTest extends \PHPUnit_Framework_TestCase
@@ -15,6 +14,49 @@ class TaskTypeTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->formType = new TaskType();
+    }
+
+    /**
+     * @param $widgets
+     *
+     * @dataProvider widgetProvider
+     */
+    public function testBuildForm($widgets)
+    {
+        $builder = $this->getMockBuilder('Symfony\Component\Form\FormBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $builder->expects($this->exactly(8))
+            ->method('add')
+            ->will($this->returnSelf());
+
+        foreach ($widgets as $key => $widget) {
+            $builder->expects($this->at($key))
+                ->method('add')
+                ->with($this->equalTo($widget))
+                ->will($this->returnSelf());
+        }
+
+        $this->formType->buildForm($builder, []);
+    }
+
+    public function widgetProvider()
+    {
+        return [
+            'all' => [
+                'widgets' => [
+                    'subject',
+                    'description',
+                    'dueDate',
+                    'priority',
+                    'assignedTo',
+                    'relatedAccount',
+                    'relatedContact',
+                    'owner',
+                ]
+            ]
+        ];
     }
 
     public function testGetName()
