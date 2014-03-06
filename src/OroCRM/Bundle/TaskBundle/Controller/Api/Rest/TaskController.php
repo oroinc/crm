ca0aa600d8f88a3dd1047be318b5b72a07f356b7
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-03-06 17:25:38 +0200

    BAP-3125: Add REST and SOAP API for get list, create, update - transform entity field

--- src/OroCRM/Bundle/TaskBundle/Controller/Api/Rest/TaskController.php
+++ src/OroCRM/Bundle/TaskBundle/Controller/Api/Rest/TaskController.php
@@ -150,4 +150,50 @@ class TaskController extends RestController implements ClassResourceInterface
     {
         return $this->get('orocrm_task.form.handler.task_api');
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function transformEntityField($field, &$value)
+    {
+        switch ($field) {
+            case 'taskPriority':
+                if ($value) {
+                    $value = $value->getName();
+                }
+                break;
+            case 'assignedTo':
+                if ($value) {
+                    $value = $value->getId();
+                }
+                break;
+            case 'relatedAccount':
+                if ($value) {
+                    $value = $value->getId();
+                }
+                break;
+            case 'relatedContact':
+                if ($value) {
+                    $value = $value->getId();
+                }
+                break;
+            case 'owner':
+                if ($value) {
+                    $value = $value->getId();
+                }
+                break;
+            case 'workflowItem':
+                if ($value) {
+                    $value = $value->getId();
+                }
+                break;
+            case 'workflowStep':
+                if ($value) {
+                    $value = $value->getId();
+                }
+                break;
+            default:
+                parent::transformEntityField($field, $value);
+        }
+    }
 }
