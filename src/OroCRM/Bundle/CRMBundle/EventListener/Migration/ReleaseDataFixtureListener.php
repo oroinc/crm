eb76e763b5c1b5fd4d90320eab2cb698bb0e9c82
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-03-20 13:11:06 +0200

    BAP-3573: Impossible to upgrade exist instance   - added TODOs to listeners

--- src/OroCRM/Bundle/CRMBundle/EventListener/Migration/ReleaseDataFixtureListener.php
+++ src/OroCRM/Bundle/CRMBundle/EventListener/Migration/ReleaseDataFixtureListener.php
@@ -6,6 +6,10 @@ use Symfony\Component\Yaml\Yaml;
 
 use Oro\Bundle\MigrationBundle\EventListener\ReleaseDataFixtureListener as BaseListener;
 
+/**
+ * TODO: This listener is a temporary solution for migration of data fixtures.
+ * TODO: It should be removed in scope of https://magecore.atlassian.net/browse/BAP-3605
+ */
 class ReleaseDataFixtureListener extends BaseListener
 {
     /**
