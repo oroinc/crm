21bc8352fdfeeb22ed7a0110eafc12b31a71bb5d
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-03-28 20:35:48 +0200

    BAP-3296: Opportunity attributes must be Text, not String  - updated opportunity and migration  - updated form type  - updated info widget

--- src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_3/OroCRMSalesBundle.php
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_3/OroCRMSalesBundle.php
@@ -3,6 +3,8 @@
 namespace OroCRM\Bundle\SalesBundle\Migrations\Schema\v1_3;
 
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
@@ -21,6 +23,8 @@ class OroCRMSalesBundle implements Migration
         $leadTable->addIndex(array('createdAt'), 'lead_created_idx');
 
         $opportunityTable = $schema->getTable('orocrm_sales_opportunity');
+        $opportunityTable->getColumn('customer_need')->setType(Type::getType(Type::TEXT))->setLength(null);
+        $opportunityTable->getColumn('proposed_solution')->setType(Type::getType(Type::TEXT))->setLength(null);
         $opportunityTable->addIndex(array('created_at'), 'opportunity_created_idx');
     }
 }
