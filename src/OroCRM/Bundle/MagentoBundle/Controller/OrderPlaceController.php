bedc10f5da18b41ca257fbc706ebe5863dfa80e2
Dmytro Naidonov <dnaidonov@magecore.com>
2014-04-11 20:06:51 +0300

    CRM-949 Extend order completion flow

--- src/OroCRM/Bundle/MagentoBundle/Controller/OrderPlaceController.php
+++ src/OroCRM/Bundle/MagentoBundle/Controller/OrderPlaceController.php
@@ -144,6 +144,7 @@ class OrderPlaceController extends Controller
     {
         $em = $this->get('doctrine.orm.entity_manager');
         try {
+            throw new \LogicException('Unable to load order.');
             $orderConnector = $this->get('orocrm_magento.mage.order_connector');
             $processor      = $this->get('oro_integration.sync.processor');
             $processor->process(
