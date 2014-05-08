160ed6bf6ceace9143032fea4a355eebe66c443e
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-05-08 14:21:09 +0300

    CRM-1125: Incorrect calculation of Lifetime value column  - added migration to update customer lifetime

---
+++ src/OroCRM/Bundle/MagentoBundle/Migrations/Data/ORM/UpdateCustomerLifetime.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace OroCRM\Bundle\MagentoBundle\Migrations\Data\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+
+use Oro\Bundle\BatchBundle\ORM\Query\BufferedQueryResultIterator;
+
+class UpdateCustomerLifetime extends AbstractFixture
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        /** @var EntityManager $manager */
+        /** @var EntityRepository $repository */
+        $repository = $manager->getRepository('OroCRMMagentoBundle:Customer');
+        $queryBuilder = $repository->createQueryBuilder('customer')
+            ->select('customer.id, SUM(customerOrder.subtotalAmount) as lifetime')
+            ->leftJoin('customer.orders', 'customerOrder')
+            ->where('customerOrder.status != :status')->setParameter('status', 'canceled')
+            ->groupBy('customer.id')
+            ->orderBy('customer.id');
+
+        $updateQuery =
+            'UPDATE OroCRMMagentoBundle:Customer customer SET customer.lifetime = :lifetime WHERE customer.id = :id';
+
+        // update lifetime for all customers
+        $iterator = new BufferedQueryResultIterator($queryBuilder);
+        foreach ($iterator as $row) {
+            $customerId = $row['id'];
+            $lifetime = $row['lifetime'] ?: 0;
+            $manager->createQuery($updateQuery)
+                ->setParameter('id', $customerId)
+                ->setParameter('lifetime', $lifetime)
+                ->execute();
+        }
+    }
+}
