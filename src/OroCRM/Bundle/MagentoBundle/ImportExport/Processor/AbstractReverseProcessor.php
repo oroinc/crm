e714927549e54f275b3cb3c93d6218054fce9bb5
Dmytro Naidonov <dnaidonov@magecore.com>
2014-05-12 18:15:26 +0300

    CRM-1117 Implement change set calculation provider

---
+++ src/OroCRM/Bundle/MagentoBundle/ImportExport/Processor/AbstractReverseProcessor.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace OroCRM\Bundle\MagentoBundle\ImportExport\Processor;
+
+use Oro\Bundle\ImportExportBundle\Context\ContextAwareInterface;
+use Oro\Bundle\ImportExportBundle\Context\ContextInterface;
+use Oro\Bundle\ImportExportBundle\Processor\ProcessorInterface;
+use Oro\Bundle\ImportExportBundle\Exception\InvalidConfigurationException;
+
+abstract class AbstractReverseProcessor implements ProcessorInterface, ContextAwareInterface
+{
+    /** @var ContextInterface */
+    protected $context;
+
+    /** @var array */
+    protected $checkEntityClasses = [];
+
+    /**
+     * @param object $entity
+     *
+     * @return array
+     */
+    public function process($entity)
+    {
+        $result = [];
+
+        if ($entity->getChannel()) {
+
+            foreach ($this->checkEntityClasses as $classNames => $classMapConfig) {
+                $this->fieldPlaceholder(
+                    $entity,
+                    $classNames,
+                    $result,
+                    $classMapConfig['fields'],
+                    $classMapConfig['checking'],
+                    'object'
+                );
+
+                if (!empty($classMapConfig['relation'])) {
+                    foreach ($classMapConfig['relation'] as $relationName => $relationClassMapConfig) {
+                        $this->fieldPlaceholder(
+                            $entity->$relationClassMapConfig['method'](),
+                            $classNames,
+                            $result,
+                            $classMapConfig['fields'],
+                            $classMapConfig['checking'],
+                            'object'
+                        );
+                    }
+
+                }
+
+
+            }
+
+            if (!empty($result['object'])) {
+                $result['channel'] = $entity->getChannel();
+            }
+        }
+
+        return (object)$result;
+    }
+
+    /**
+     * @param ContextInterface $context
+     * @throws InvalidConfigurationException
+     */
+    public function setImportExportContext(ContextInterface $context)
+    {
+        $this->context = $context;
+    }
+
+    /**
+     * @param object $entity
+     * @param string $classNames
+     * @param array  $result
+     * @param array  $fields
+     * @param array  $checking
+     * @param string $arrayName
+     */
+    protected function fieldPlaceholder(
+        $entity,
+        $classNames,
+        array &$result,
+        array $fields,
+        array $checking,
+        $arrayName
+    ) {
+        if ($entity instanceof $classNames) {
+            foreach ($fields as $name => $methods) {
+                if ($this->isChanged($entity, $methods, $checking)) {
+                    $result[$arrayName][$name] = $this->getObjectMethodValue($entity, $methods);
+                }
+            }
+        }
+    }
+
+    /**
+     * @param object $entity
+     * @param array $methods
+     * @param array $checking
+     *
+     * @return bool
+     */
+    protected function isChanged($entity, array $methods, array $checking)
+    {
+        if (is_array($checking)) {
+            $checkingMethod = $checking['method'];
+            $checkingClass = $checking['class'];
+
+            if ($entity->$checkingMethod() instanceof $checkingClass) {
+                return $this->hasDistinction($entity, $checkingMethod, $methods);
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param object $entity
+     * @param string $checkingMethod
+     * @param string $methods
+     *
+     * @return mixed
+     */
+    protected function getCheckingMethodValue($entity, $checkingMethod, $methods)
+    {
+        if (!empty($methods[1])) {
+            return $entity->$checkingMethod()->$methods[1]();
+        }
+
+        return $entity->$checkingMethod()->$methods[0]();
+    }
+
+    /**
+     * @param object $entity
+     * @param string $methods
+     *
+     * @return mixed
+     */
+    protected function getObjectMethodValue($entity, $methods)
+    {
+        return $entity->$methods[0]();
+    }
+
+    /**
+     * @param object $entity
+     * @param string $checkingMethod
+     * @param string $methods
+     *
+     * @return bool
+     */
+    protected function hasDistinction($entity, $checkingMethod, $methods)
+    {
+        if (!empty($methods[1])) {
+            return (
+                $this->getObjectMethodValue($entity, $methods)
+                !== $this->getCheckingMethodValue($entity, $checkingMethod, $methods)
+            );
+        }
+
+        return (
+            $this->getObjectMethodValue($entity, $methods)
+            !== $this->getCheckingMethodValue($entity, $checkingMethod, $methods)
+        );
+    }
+}
--- src/OroCRM/Bundle/MagentoBundle/ImportExport/Processor/CustomerReverseProcessor.php
+++ src/OroCRM/Bundle/MagentoBundle/ImportExport/Processor/CustomerReverseProcessor.php
@@ -2,157 +2,37 @@
 
 namespace OroCRM\Bundle\MagentoBundle\ImportExport\Processor;
 
-use Oro\Bundle\ImportExportBundle\Context\ContextAwareInterface;
-use Oro\Bundle\ImportExportBundle\Context\ContextInterface;
-use Oro\Bundle\ImportExportBundle\Processor\ProcessorInterface;
-use Oro\Bundle\ImportExportBundle\Exception\InvalidConfigurationException;
-
-use OroCRM\Bundle\MagentoBundle\Entity\Customer;
-
-class CustomerReverseProcessor implements ProcessorInterface, ContextAwareInterface
+class CustomerReverseProcessor extends AbstractReverseProcessor
 {
-    /**
-     * @var ContextInterface
-     */
-    protected $context;
+    /** {@inheritdoc} */
+    protected $checkEntityClasses = [
+        'OroCRM\Bundle\MagentoBundle\Entity\Customer'=> [
+            'fields' => [
+                'email'      => ['getEmail', 'getPrimaryEmail'],
+                'firstname'  => ['getFirstName'],
+                'lastname'   => ['getLastName'],
+                'prefix'     => ['getNamePrefix'],
+                'suffix'     => ['getNameSuffix'],
+                'dob'        => ['getBirthday'],
+                'gender'     => ['getGender'],
+                'middlename' => ['getMiddleName'],
+            ],
+            'checking' => [
+                'method' => 'getContact',
+                'class'  => 'OroCRM\Bundle\ContactBundle\Entity\Contact',
+            ],
+            'relation' => [
+                'addresses' => [
+                    'method' => 'getAddresses',
+                    'checking' => [
+                        'method' => 'getContact',
+                        'class'  => 'OroCRM\Bundle\MagentoBundle\Entity\Address',
+                    ],
+                    'fields' => [
 
-    /**
-     * @param Customer $customer
-     *
-     * @return array
-     */
-    public function process($customer)
-    {
-        $result = [];
-
-        if ($customer->getChannel()) {
-            $result['channel'] = $customer->getChannel();
-        }
-
-        if ($customer->getContact()) {
-
-            if ($this->isEmailChanged($customer)) {
-                $result['object']['email'] = $customer->getEmail();
-            }
-
-            if ($this->isFirstNameChanged($customer)) {
-                $result['object']['firstname'] = $customer->getEmail();
-            }
-
-            if ($this->isLastNameChanged($customer)) {
-                $result['object']['lastname'] = $customer->getEmail();
-            }
-
-            if ($this->isNamePreffixChanged($customer)) {
-                $result['object']['prefix'] = $customer->getNamePrefix();
-            }
-
-            if ($this->isNameSuffixChanged($customer)) {
-                $result['object']['prefix'] = $customer->getNameSuffix();
-            }
-
-            if ($this->isBirthdayChanged($customer)) {
-                $result['object']['dob'] = $customer->getBirthday();
-            }
-
-            if ($this->isGenderChanged($customer)) {
-                $result['object']['gender'] = $customer->getGender();
-            }
-
-            if ($this->isMiddleNameChanged($customer)) {
-                $result['object']['middlename'] = $customer->getMiddleName();
-            }
-        }
-
-        return (object)$result;
-    }
-
-    /**
-     * @param ContextInterface $context
-     * @throws InvalidConfigurationException
-     */
-    public function setImportExportContext(ContextInterface $context)
-    {
-        $this->context = $context;
-    }
-
-    /**
-     * @param $customer
-     *
-     * @return bool
-     */
-    protected function isEmailChanged(Customer $customer)
-    {
-        return $customer->getEmail() !== $customer->getContact()->getPrimaryEmail();
-    }
-
-    /**
-     * @param Customer $customer
-     *
-     * @return bool
-     */
-    protected function isFirstNameChanged(Customer $customer)
-    {
-        return $customer->getFirstName() !== $customer->getContact()->getFirstName();
-    }
-
-    /**
-     * @param Customer $customer
-     *
-     * @return bool
-     */
-    protected function isLastNameChanged(Customer $customer)
-    {
-        return $customer->getLastName() !== $customer->getContact()->getLastName();
-    }
-
-    /**
-     * @param Customer $customer
-     *
-     * @return bool
-     */
-    protected function isNamePreffixChanged(Customer $customer)
-    {
-        return $customer->getNamePrefix() === $customer->getContact()->getNamePrefix();
-    }
-
-    /**
-     * @param Customer $customer
-     *
-     * @return bool
-     */
-    protected function isNameSuffixChanged(Customer $customer)
-    {
-        return $customer->getNameSuffix() === $customer->getContact()->getNameSuffix();
-    }
-
-    /**
-     * @param Customer $customer
-     *
-     * @return bool
-     */
-    protected function isBirthdayChanged(Customer $customer)
-    {
-        return $customer->getBirthday() === $customer->getContact()->getBirthday();
-    }
-
-    /**
-     * @param Customer $customer
-     *
-     * @return bool
-     */
-    protected function isGenderChanged(Customer $customer)
-    {
-        return $customer->getGender() === $customer->getContact()->getGender();
-    }
-
-    /**
-     * @param Customer $customer
-     *
-     * @return bool
-     */
-    protected function isMiddleNameChanged(Customer $customer)
-    {
-        return $customer->getMiddleName() === $customer->getContact()->getMiddleName();
-    }
+                    ]
+                ],
+            ]
+        ]
+    ];
 }
