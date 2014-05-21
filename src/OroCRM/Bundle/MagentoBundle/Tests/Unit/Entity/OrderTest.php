3c345f1a93130229a3084c1613bc20740c66a355
Dmytro Naidonov <dnaidonov@magecore.com>
2014-05-21 16:18:58 +0300

    CRM-1163 Magento Sync. Guest orders do not contain emails

--- src/OroCRM/Bundle/MagentoBundle/Tests/Unit/Entity/OrderTest.php
+++ src/OroCRM/Bundle/MagentoBundle/Tests/Unit/Entity/OrderTest.php
@@ -35,50 +35,50 @@ class OrderTest extends AbstractEntityTestCase
     public function getSetDataProvider()
     {
         $customer = new Customer();
-        $store = new Store();
-        $cart = new Cart();
-        $items = new ArrayCollection();
+        $store    = new Store();
+        $cart     = new Cart();
+        $items    = new ArrayCollection();
         $items->add(new OrderItem());
         $workFlowItem = new WorkflowItem();
         $workflowStep = new WorkflowStep();
-        $createdAt = new \DateTime('now');
-        $updatedAt = new \DateTime('now');
-        $channel = new Channel();
+        $createdAt    = new \DateTime('now');
+        $updatedAt    = new \DateTime('now');
+        $channel      = new Channel();
 
         return [
-            'incrementId' => ['incrementId', 1, 1],
-            'customer' => ['customer', $customer, $customer],
-            'store' => ['store', $store, $store],
-            'isVirtual' => ['isVirtual', true, true],
-            'isGuest' => ['isGuest', true, true],
-            'giftMessage' => ['giftMessage', 'some message', 'some message'],
-            'remoteIp' => ['remoteIp', 'remoteIp', 'remoteIp'],
-            'storeName' => ['storeName', 'store name', 'store name'],
-            'totalPaidAmount' => ['totalPaidAmount', 100.14, 100.14],
+            'incrementId'         => ['incrementId', 1, 1],
+            'customer'            => ['customer', $customer, $customer],
+            'store'               => ['store', $store, $store],
+            'isVirtual'           => ['isVirtual', true, true],
+            'isGuest'             => ['isGuest', true, true],
+            'giftMessage'         => ['giftMessage', 'some message', 'some message'],
+            'remoteIp'            => ['remoteIp', 'remoteIp', 'remoteIp'],
+            'storeName'           => ['storeName', 'store name', 'store name'],
+            'totalPaidAmount'     => ['totalPaidAmount', 100.14, 100.14],
             'totalInvoicedAmount' => ['totalInvoicedAmount', 90.55, 90.55],
             'totalRefundedAmount' => ['totalRefundedAmount', 87.82, 87.82],
             'totalCanceledAmount' => ['totalCanceledAmount', 94.47, 94.47],
-            'cart' => ['cart', $cart, $cart],
-            'items' => ['items', $items, $items],
-            'notes' => ['notes', 'notes', 'notes'],
-            'feedback' => ['feedback', 'positive', 'positive'],
-            'workflowItem' => ['workflowItem', $workFlowItem, $workFlowItem],
-            'workflowStep' => ['workflowStep', $workflowStep, $workflowStep],
-            'customerEmail' => ['customerEmail', 'email@email.com', 'email@email.com'],
-            'currency' => ['currency', 'uah', 'uah'],
-            'paymentMethod' => ['paymentMethod', 'payment method', 'payment method'],
-            'paymentDetails' => ['paymentDetails', 'payment details', 'payment details'],
-            'subtotalAmount' => ['subtotalAmount', 0.12, 0.12],
-            'shippingAmount' => ['shippingAmount', 0.12, 0.12],
-            'shippingMethod' => ['shippingMethod', 'shipping method', 'shipping method'],
-            'taxAmount' => ['taxAmount', 10.92, 10.92],
-            'discountAmount' => ['discountAmount', 90.92, 90.92],
-            'discountPercent' => ['discountPercent', 2, 2],
-            'totalAmount' => ['totalAmount', 5.22, 5.22],
-            'status' => ['status', 'status', 'status'],
-            'createdAt' => ['createdAt', $createdAt, $createdAt],
-            'updatedAt' => ['updatedAt', $updatedAt, $updatedAt],
-            'channel' => ['channel', $channel, $channel],
+            'cart'                => ['cart', $cart, $cart],
+            'items'               => ['items', $items, $items],
+            'notes'               => ['notes', 'notes', 'notes'],
+            'feedback'            => ['feedback', 'positive', 'positive'],
+            'workflowItem'        => ['workflowItem', $workFlowItem, $workFlowItem],
+            'workflowStep'        => ['workflowStep', $workflowStep, $workflowStep],
+            'customerEmail'       => ['customerEmail', 'email@email.com', 'email@email.com'],
+            'currency'            => ['currency', 'uah', 'uah'],
+            'paymentMethod'       => ['paymentMethod', 'payment method', 'payment method'],
+            'paymentDetails'      => ['paymentDetails', 'payment details', 'payment details'],
+            'subtotalAmount'      => ['subtotalAmount', 0.12, 0.12],
+            'shippingAmount'      => ['shippingAmount', 0.12, 0.12],
+            'shippingMethod'      => ['shippingMethod', 'shipping method', 'shipping method'],
+            'taxAmount'           => ['taxAmount', 10.92, 10.92],
+            'discountAmount'      => ['discountAmount', 90.92, 90.92],
+            'discountPercent'     => ['discountPercent', 2, 2],
+            'totalAmount'         => ['totalAmount', 5.22, 5.22],
+            'status'              => ['status', 'status', 'status'],
+            'createdAt'           => ['createdAt', $createdAt, $createdAt],
+            'updatedAt'           => ['updatedAt', $updatedAt, $updatedAt],
+            'channel'             => ['channel', $channel, $channel],
         ];
     }
 }
