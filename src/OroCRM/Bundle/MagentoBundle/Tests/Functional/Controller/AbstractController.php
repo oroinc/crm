0d8dbf6a9eda895735cc695d7299cc328ea3c356
Alexandr Smaga <asmaga@magecore.com>
2014-05-31 13:25:45 +0000

    CRM-1227: Add set owner for existing data    - minor fixes

--- src/OroCRM/Bundle/MagentoBundle/Tests/Functional/Controller/AbstractController.php
+++ src/OroCRM/Bundle/MagentoBundle/Tests/Functional/Controller/AbstractController.php
@@ -42,16 +42,12 @@ abstract class AbstractController extends WebTestCase
     public function testGrid($filters)
     {
         if (isset($filters['gridParameters']['id'])) {
-            $gridId
-                                                =
-                $filters['gridParameters']['gridName'] . '[' . $filters['gridParameters']['id'] . ']';
+            $gridId = $filters['gridParameters']['gridName'] . '[' . $filters['gridParameters']['id'] . ']';
             $filters['gridParameters'][$gridId] = $this->getMainEntityId();
         }
 
         if (isset($filters['gridParameters']['channel'])) {
-            $gridChannel
-                                                     =
-                $filters['gridParameters']['gridName'] . '[' . $filters['gridParameters']['channel'] . ']';
+            $gridChannel = $filters['gridParameters']['gridName'] . '[' . $filters['gridParameters']['channel'] . ']';
             $filters['gridParameters'][$gridChannel] = $this->getMainEntityId();
         }
 
