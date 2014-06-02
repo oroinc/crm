ea926e0ac18f3fbc67c0c8e384a4f58fa6ccb138
AlexandrDmitriev <admitriev@magecore.com>
2014-06-02 14:30:54 +0300

    CRM-1253: Cases list page with grid

--- src/OroCRM/Bundle/CaseBundle/Controller/CaseController.php
+++ src/OroCRM/Bundle/CaseBundle/Controller/CaseController.php
@@ -15,6 +15,16 @@ class CaseController extends Controller
     /**
      * @Route(name="orocrm_case_index")
      * @Template
+     * @AclAncestor("orocrm_account_view")
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/view/{id}", name="orocrm_case_view", requirements={"id"="\d+"})
+     * @Template
      * @Acl(
      *      id="orocrm_case_view",
      *      type="entity",
@@ -22,7 +32,39 @@ class CaseController extends Controller
      *      class="OroCRMCaseBundle:CaseEntity"
      * )
      */
-    public function indexAction()
+    public function viewAction()
+    {
+        return array();
+    }
+
+    /**
+     * Create account form
+     *
+     * @Route("/create", name="orocrm_case_create")
+     * @Acl(
+     *      id="orocrm_case_create",
+     *      type="entity",
+     *      permission="CREATE",
+     *      class="OroCRMCaseBundle:CaseEntity"
+     * )
+     * @Template("OroCRMCaseBundle:Case:update.html.twig")
+     */
+    public function createAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/update/{id}", name="orocrm_case_update", requirements={"id"="\d+"})
+     * @Template
+     * @Acl(
+     *      id="orocrm_case_update",
+     *      type="entity",
+     *      permission="EDIT",
+     *      class="OroCRMCaseBundle:CaseEntity"
+     * )
+     */
+    public function updateAction()
     {
         return array();
     }
