4fd5ffef2ac4b975769f6e6a836360b16a161007
ignat <ignat@ignat-laptop.(none)>
2014-06-02 19:56:57 +0300

    CRM-1223: Create OroCRMCase Bundle - CR changes

--- src/OroCRM/Bundle/CaseBundle/Entity/CaseEntity.php
+++ src/OroCRM/Bundle/CaseBundle/Entity/CaseEntity.php
@@ -2,7 +2,6 @@
 
 namespace OroCRM\Bundle\CaseBundle\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
