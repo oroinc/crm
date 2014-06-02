2e412b8b561df79b9bbebde106fbfada35fccaf0
Dmitrij Najdenov <dnaidonov@magecore.com>
2014-06-02 17:20:37 +0300

    CRM-1230 add custom form type for campaign code add validation add unittest remove remarks

--- src/OroCRM/Bundle/CampaignBundle/Form/Type/CodeType.php
+++ src/OroCRM/Bundle/CampaignBundle/Form/Type/CodeType.php
@@ -6,11 +6,17 @@ use Symfony\Component\Form\AbstractType;
 
 class CodeType extends AbstractType
 {
+    /**
+     * {@inheritdoc}
+     */
     public function getParent()
     {
         return 'text';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return 'orocrm_campaign_code_type';
