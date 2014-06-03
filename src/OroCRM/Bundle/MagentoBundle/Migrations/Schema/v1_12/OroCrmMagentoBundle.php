d6a21d136ee2f6b50bc5f159766c76f69b575f26
Alexandr Smaga <asmaga@magecore.com>
2014-06-03 12:43:07 +0300

    CRM-1266: Add owner for all magento entities    - fix key names

--- src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_12/OroCrmMagentoBundle.php
+++ src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_12/OroCrmMagentoBundle.php
@@ -1,7 +1,6 @@
 <?php
 namespace OroCRM\Bundle\MagentoBundle\Migrations\Schema\v1_12;
 
-use Doctrine\DBAL\Schema\Table;
 use Doctrine\DBAL\Schema\Schema;
 
 use Oro\Bundle\MigrationBundle\Migration\Migration;
@@ -15,29 +14,36 @@ class OroCrmMagentoBundle implements Migration
     public function up(Schema $schema, QueryBag $queries)
     {
         $cart = $schema->getTable('orocrm_magento_cart');
-        $this->addOwnerField($schema, $cart);
+        $cart->addColumn('user_owner_id', 'integer', ['notnull' => false]);
+        $cart->addIndex(['user_owner_id'], 'IDX_96661A809EB185F9', []);
+        $cart->addForeignKeyConstraint(
+            $schema->getTable('oro_user'),
+            ['user_owner_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null],
+            'FK_96661A809EB185F9'
+        );
 
         $customer = $schema->getTable('orocrm_magento_customer');
-        $this->addOwnerField($schema, $customer);
-
-        $order = $schema->getTable('orocrm_magento_order');
-        $this->addOwnerField($schema, $order);
-    }
-
-    /**
-     * @param Schema $schema
-     * @param Table  $table
-     */
-    protected function addOwnerField(Schema $schema, Table $table)
-    {
-        $table->addColumn('user_owner_id', 'integer', ['notnull' => false]);
-        $table->addIndex(['user_owner_id'], 'IDX_2A61EE7D9EB185F9', []);
-        $table->addForeignKeyConstraint(
+        $customer->addColumn('user_owner_id', 'integer', ['notnull' => false]);
+        $customer->addIndex(['user_owner_id'], 'IDX_2A61EE7D9EB185F9', []);
+        $customer->addForeignKeyConstraint(
             $schema->getTable('oro_user'),
             ['user_owner_id'],
             ['id'],
             ['onDelete' => 'SET NULL', 'onUpdate' => null],
             'FK_2A61EE7D9EB185F9'
+        );
+
+        $order = $schema->getTable('orocrm_magento_order');
+        $order->addColumn('user_owner_id', 'integer', ['notnull' => false]);
+        $order->addIndex(['user_owner_id'], 'IDX_4D09F3059EB185F9', []);
+        $order->addForeignKeyConstraint(
+            $schema->getTable('oro_user'),
+            ['user_owner_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null],
+            'FK_4D09F3059EB185F9'
         );
     }
 }
