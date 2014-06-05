4ed066914a47b0a9380883b708f08a7c742a4e0f
Alexander Nezdoiminoga <alex@magecore.com>
2014-06-05 17:32:25 +0300

    BAP-4301: migrations

--- src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_5/OroCRMSalesBundle.php
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_5/OroCRMSalesBundle.php
@@ -4,25 +4,23 @@ namespace OroCRM\Bundle\SalesBundle\Migrations\Schema\v1_5;
 
 use Doctrine\DBAL\Schema\Schema;
 
-use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
-use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
-use Oro\Bundle\EntityExtendBundle\Migration\Schema\ExtendColumn;
-use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
-
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-class OroCRMSalesBundle implements Migration, ExtendExtensionAwareInterface
+use Oro\Bundle\NoteBundle\Migration\Extension\NoteExtension;
+use Oro\Bundle\NoteBundle\Migration\Extension\NoteExtensionAwareInterface;
+
+class OroCRMSalesBundle implements Migration, NoteExtensionAwareInterface
 {
-    /** @var  ExtendExtension */
-    protected $extendExtension;
+    /** @var NoteExtension */
+    protected $noteExtension;
 
     /**
      * {@inheritdoc}
      */
-    public function setExtendExtension(ExtendExtension $extendExtension)
+    public function setNoteExtension(NoteExtension $noteExtension)
     {
-        $this->extendExtension = $extendExtension;
+        $this->noteExtension = $noteExtension;
     }
 
     /**
@@ -30,48 +28,18 @@ class OroCRMSalesBundle implements Migration, ExtendExtensionAwareInterface
      */
     public function up(Schema $schema, QueryBag $queries)
     {
-        self::addNoteAssociations($schema, $this->extendExtension);
+        self::addNoteAssociations($schema, $this->noteExtension);
     }
 
     /**
      * Enable notes for Lead and Opportunity entities
      *
-     * @param Schema          $schema
-     * @param ExtendExtension $extendExtension
+     * @param Schema        $schema
+     * @param NoteExtension $noteExtension
      */
-    public static function addNoteAssociations(Schema $schema, ExtendExtension $extendExtension)
+    public static function addNoteAssociations(Schema $schema, NoteExtension $noteExtension)
     {
-        $noteTable        = $schema->getTable('oro_note');
-        $leadTable        = $schema->getTable('orocrm_sales_lead');
-        $opportunityTable = $schema->getTable('orocrm_sales_opportunity');
-
-        $options['note']['enabled'] = true;
-
-        $leadTable->addOption(ExtendColumn::ORO_OPTIONS_NAME, $options);
-        $opportunityTable->addOption(ExtendColumn::ORO_OPTIONS_NAME, $options);
-
-        $leadAssociationName = ExtendHelper::buildAssociationName(
-            $extendExtension->getEntityClassByTableName('orocrm_sales_lead')
-        );
-        $opportunityAssociationName = ExtendHelper::buildAssociationName(
-            $extendExtension->getEntityClassByTableName('orocrm_sales_opportunity')
-        );
-
-        $extendExtension->addManyToOneRelation(
-            $schema,
-            $noteTable,
-            $leadAssociationName,
-            $leadTable,
-            'name',
-            ['extend' => ['owner' => 'Custom', 'is_extend' => true]]
-        );
-        $extendExtension->addManyToOneRelation(
-            $schema,
-            $noteTable,
-            $opportunityAssociationName,
-            $opportunityTable,
-            'name',
-            ['extend' => ['owner' => 'Custom', 'is_extend' => true]]
-        );
+        $noteExtension->addNoteAssociation($schema, 'orocrm_sales_lead', 'name');
+        $noteExtension->addNoteAssociation($schema, 'orocrm_sales_opportunity', 'name');
     }
 }
