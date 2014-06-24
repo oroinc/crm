0ca255c6260b22371ddb637496f2454f3a6fb2d6
Vova Soroka <vsoroka@magecore.com>
2014-06-24 04:07:15 +0300

    BAP-4209: Add EntityExtendBundle generator extension to generate activity association entities. Refactor and fix generator core

--- src/OroCRM/Bundle/CampaignBundle/Model/ExtendCampaign.php
+++ src/OroCRM/Bundle/CampaignBundle/Model/ExtendCampaign.php
@@ -4,4 +4,14 @@ namespace OroCRM\Bundle\CampaignBundle\Model;
 
 class ExtendCampaign
 {
+    /**
+     * Constructor
+     *
+     * The real implementation of this method is auto generated.
+     *
+     * IMPORTANT: If the derived class has own constructor it must call parent constructor.
+     */
+    public function __construct()
+    {
+    }
 }
--- src/OroCRM/Bundle/MagentoBundle/Model/ExtendCustomer.php
+++ src/OroCRM/Bundle/MagentoBundle/Model/ExtendCustomer.php
@@ -6,4 +6,14 @@ use Oro\Bundle\BusinessEntitiesBundle\Entity\BasePerson;
 
 class ExtendCustomer extends BasePerson
 {
+    /**
+     * Constructor
+     *
+     * The real implementation of this method is auto generated.
+     *
+     * IMPORTANT: If the derived class has own constructor it must call parent constructor.
+     */
+    public function __construct()
+    {
+    }
 }
