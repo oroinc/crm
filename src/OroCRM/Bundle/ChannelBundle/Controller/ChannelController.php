5794ebc163d1fa1ed923a8856f242269aeaf5f41
Dmitry Naydenov <dnaidonov@magecore.com>
2014-06-25 17:34:05 +0300

    CRM-1294 CRUD: Entity list page

--- src/OroCRM/Bundle/ChannelBundle/Controller/ChannelController.php
+++ src/OroCRM/Bundle/ChannelBundle/Controller/ChannelController.php
@@ -7,14 +7,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Oro\Bundle\SecurityBundle\Annotation\Acl;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+
+use OroCRM\Bundle\ChannelBundle\Entity\Channel;
+
 class ChannelController extends Controller
 {
     /**
      * @Route("/", name="orocrm_channel_index")
+     * @AclAncestor("orocrm_channel_view")
      * @Template
      */
     public function indexAction()
     {
         return [];
+    }
+
+    /**
+     * @Route("/view/{id}", name="orocrm_channel_view", requirements={"id"="\d+"}))
+     * @Acl(
+     *      id="orocrm_channel_view",
+     *      type="entity",
+     *      permission="VIEW",
+     *      class="OroCRMChannelBundle:Channel"
+     * )
+     * @Template
+     */
+    public function viewAction(Channel $cart)
+    {
+        return ['entity' => $cart];
     }
 }
