6f715f73f2cfbc8e151cfd58235b6f962f97fe73
Vova Soroka <vsoroka@magecore.com>
2014-06-25 03:21:23 +0300

    BAP-4208: Create migration to mark Account, Contact, Customer, Opportunity, Lead as have activities

---
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_6/OroCRMSalesBundle.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace OroCRM\Bundle\SalesBundle\Migrations\Schema\v1_6;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtension;
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtensionAwareInterface;
+
+class OroCRMSalesBundle implements Migration, ActivityExtensionAwareInterface
+{
+    /** @var ActivityExtension */
+    protected $activityExtension;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setActivityExtension(ActivityExtension $activityExtension)
+    {
+        $this->activityExtension = $activityExtension;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        self::addActivityAssociations($schema, $this->activityExtension);
+    }
+
+    /**
+     * Enable Email activity for Account entity
+     *
+     * @param Schema            $schema
+     * @param ActivityExtension $activityExtension
+     */
+    public static function addActivityAssociations(Schema $schema, ActivityExtension $activityExtension)
+    {
+        $activityExtension->addActivityAssociation($schema, 'oro_email', 'orocrm_sales_lead');
+        $activityExtension->addActivityAssociation($schema, 'oro_email', 'orocrm_sales_opportunity');
+    }
+}
