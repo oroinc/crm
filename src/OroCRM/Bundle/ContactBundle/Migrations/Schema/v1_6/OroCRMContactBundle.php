ae8854137ce123a77f696becbb6f7057bddd71b6
Ivan Shakuta <ishakuta@magecore.com>
2014-07-02 13:30:23 +0300

    BAP-4658: Email activity enabled for User by default - fix CR

--- src/OroCRM/Bundle/ContactBundle/Migrations/Schema/v1_6/OroCRMContactBundle.php
+++ src/OroCRM/Bundle/ContactBundle/Migrations/Schema/v1_6/OroCRMContactBundle.php
@@ -4,9 +4,6 @@ namespace OroCRM\Bundle\ContactBundle\Migrations\Schema\v1_6;
 
 use Doctrine\DBAL\Schema\Schema;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 use Oro\Bundle\MigrationBundle\Migration\Extension\NameGeneratorAwareInterface;
@@ -20,14 +17,11 @@ use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
 use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
 use Oro\Bundle\EntityExtendBundle\Tools\ExtendDbIdentifierNameGenerator;
 use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
-use Oro\Bundle\EmailBundle\DependencyInjection\Compiler\EmailOwnerConfigurationPass;
-use Oro\Bundle\EmailBundle\Entity\Provider\EmailOwnerProviderStorage;
 
 class OroCRMContactBundle implements
     Migration,
     NameGeneratorAwareInterface,
     ExtendExtensionAwareInterface,
-    ContainerAwareInterface,
     ActivityExtensionAwareInterface
 {
     /** @var ActivityExtension */
@@ -44,11 +38,6 @@ class OroCRMContactBundle implements
     protected $extendExtension;
 
     /**
-     * @var EmailOwnerProviderStorage
-     */
-    protected $ownerProviderStorage;
-
-    /**
      * @inheritdoc
      */
     public function setExtendExtension(ExtendExtension $extendExtension)
@@ -62,14 +51,6 @@ class OroCRMContactBundle implements
     public function setNameGenerator(DbIdentifierNameGenerator $nameGenerator)
     {
         $this->nameGenerator = $nameGenerator;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->ownerProviderStorage = $container->get(EmailOwnerConfigurationPass::SERVICE_KEY);
     }
 
     /**
