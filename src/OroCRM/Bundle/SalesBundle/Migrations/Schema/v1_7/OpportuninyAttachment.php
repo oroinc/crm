79909f9013c3b636832d5b8bb59e56f10e93ad5c
Yurii Muratov <yurio@ua.fm>
2014-07-08 11:58:55 +0300

    BAP-4360: Attachments management   - Add attachments to accounts and opportunity

---
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_7/OpportuninyAttachment.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace OroCRM\Bundle\SalesBundle\Migrations\Schema\v1_7;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\AttachmentBundle\Migration\Extension\AttachmentExtension;
+use Oro\Bundle\AttachmentBundle\Migration\Extension\AttachmentExtensionAwareInterface;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+
+class OpportuninyAttachment  implements Migration, AttachmentExtensionAwareInterface
+{
+    /** @var AttachmentExtension */
+    protected $attachmentExtension;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setAttachmentExtension(AttachmentExtension $attachmentExtension)
+    {
+        $this->attachmentExtension = $attachmentExtension;
+    }
+
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $this->attachmentExtension->addAttachmentAssociation(
+            $schema,
+            'orocrm_sales_opportunity',
+            [
+                'image/*',
+                'application/pdf',
+                'application/zip',
+                'application/x-gzip',
+                'application/msword',
+                'application/vnd.openxmlformats-officedocument.wordprocessingml.docume',
+                'application/vnd.ms-excel',
+                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                'application/vnd.ms-powerpoint',
+                'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+            ],
+            2
+        );
+    }
+}
