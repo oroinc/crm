db955fb8fa2c52bd6810506e90bc6dd58b2b8cd2
Vova Soroka <vsoroka@magecore.com>
2014-07-17 03:03:04 +0300

    BAP-4791: Fill in To field in emails send in the context of an entity (e.g. Case) from the related Contact

---
+++ src/OroCRM/Bundle/ContactBundle/DependencyInjection/Compiler/EmailHolderHelperConfigPass.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace OroCRM\Bundle\ContactBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class EmailHolderHelperConfigPass implements CompilerPassInterface
+{
+    const SERVICE_KEY = 'oro_email.email.holder.helper';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition(self::SERVICE_KEY)) {
+            return;
+        }
+
+        $serviceDef = $container->getDefinition(self::SERVICE_KEY);
+        $serviceDef->addMethodCall(
+            'addTargetEntity',
+            [$container->getParameter('orocrm_contact.entity.class')]
+        );
+    }
+}
