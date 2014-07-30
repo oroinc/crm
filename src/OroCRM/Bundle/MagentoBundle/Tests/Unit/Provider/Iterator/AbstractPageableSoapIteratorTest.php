f6cf1c57dc6b4b4b71b3026b73f93da499c18795
Alexandr Smaga <asmaga@magecore.com>
2014-07-30 14:39:35 +0300

    CRM-1704 Magento Import: Do not use own date for last sync date, use dat    - minor fixes

--- src/OroCRM/Bundle/MagentoBundle/Tests/Unit/Provider/Iterator/AbstractPageableSoapIteratorTest.php
+++ src/OroCRM/Bundle/MagentoBundle/Tests/Unit/Provider/Iterator/AbstractPageableSoapIteratorTest.php
@@ -6,9 +6,6 @@ use OroCRM\Bundle\MagentoBundle\Provider\Iterator\AbstractPageableSoapIterator;
 
 class AbstractPageableSoapIteratorTest extends BaseIteratorTestCase
 {
-    /** @var \PHPUnit_Framework_MockObject_MockObject|AbstractPageableSoapIterator */
-    protected $iterator;
-
     public function testIterator()
     {
         $date     = new \DateTime('now');
@@ -17,6 +14,7 @@ class AbstractPageableSoapIteratorTest extends BaseIteratorTestCase
             'start_sync_date' => $date
         ];
 
+        /** @var \PHPUnit_Framework_MockObject_MockObject|AbstractPageableSoapIterator $iterator */
         $iterator = $this->getMockForAbstractClass(
             'OroCRM\Bundle\MagentoBundle\Provider\Iterator\AbstractPageableSoapIterator',
             [$this->transport, $settings]
