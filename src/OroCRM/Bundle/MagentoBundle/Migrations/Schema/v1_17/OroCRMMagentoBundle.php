0530035d193a42f54824ad935010d022fcf4d3bf
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-08-01 11:32:52 +0300

    CRM-1610: Create migrations for incompatible initial migrations - cleanup migrations

--- src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_17/OroCRMMagentoBundle.php
+++ src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_17/OroCRMMagentoBundle.php
@@ -31,29 +31,12 @@ class OroCRMMagentoBundle implements Migration, DatabasePlatformAwareInterface
      */
     public function up(Schema $schema, QueryBag $queries)
     {
-        /** placed here to load in last order */
         $queries->addQuery(
             new MigrateTypesQuery($this->platform, $schema, 'orocrm_contactus_contact_rsn', 'id', Type::INTEGER)
         );
         $queries->addQuery(
             new MigrateTypesQuery($this->platform, $schema, 'orocrm_contact_group', 'id', Type::INTEGER)
         );
-
-        /** override platform queries */
-        $queries->addQuery(
-            new MigrateTypesQuery($this->platform, $schema, 'oro_integration_channel', 'id', Type::INTEGER)
-        );
-        $queries->addQuery(
-            new MigrateTypesQuery($this->platform, $schema, 'oro_integration_channel_status', 'id', Type::INTEGER)
-        );
-        $queries->addQuery(
-            new MigrateTypesQuery($this->platform, $schema, 'oro_integration_transport', 'id', Type::INTEGER)
-        );
-
-        $queries->addQuery(new MigrateTypesQuery($this->platform, $schema, 'oro_access_group', 'id', Type::INTEGER));
-        $queries->addQuery(new MigrateTypesQuery($this->platform, $schema, 'oro_access_role', 'id', Type::INTEGER));
-        $queries->addQuery(new MigrateTypesQuery($this->platform, $schema, 'oro_user_email', 'id', Type::INTEGER));
-        $queries->addQuery(new MigrateTypesQuery($this->platform, $schema, 'oro_user_status', 'id', Type::INTEGER));
 
         $table = $schema->getTable('orocrm_magento_customer');
         $table->changeColumn('vat', ['type' => Type::getType('float'), 'notnull' => true]);
