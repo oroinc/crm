88ce9f7fe0b88e39d0a40a584ed850f1b6f840a0
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-08-01 03:48:09 +0300

    CRM-1610: Create migrations for incompatible initial migrations - add MigrateTypesQuery

--- src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_17/OroCRMMagentoBundle.php
+++ src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_17/OroCRMMagentoBundle.php
@@ -2,33 +2,58 @@
 
 namespace OroCRM\Bundle\MagentoBundle\Migrations\Schema\v1_17;
 
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Types\Type;
 
 use Oro\Bundle\EntityBundle\Migrations\MigrateTypesQuery;
+use Oro\Bundle\MigrationBundle\Migration\Extension\DatabasePlatformAwareInterface;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-class OroCRMMagentoBundle implements Migration
+class OroCRMMagentoBundle implements Migration, DatabasePlatformAwareInterface
 {
+    /**
+     * @var AbstractPlatform
+     */
+    protected $platform;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDatabasePlatform(AbstractPlatform $platform)
+    {
+        $this->platform = $platform;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function up(Schema $schema, QueryBag $queries)
     {
         /** placed here to load in last order */
-        $queries->addQuery(new MigrateTypesQuery($schema, 'orocrm_contactus_contact_rsn', 'id', Type::INTEGER));
-        $queries->addQuery(new MigrateTypesQuery($schema, 'orocrm_contact_group', 'id', Type::INTEGER));
+        $queries->addQuery(
+            new MigrateTypesQuery($this->platform, $schema, 'orocrm_contactus_contact_rsn', 'id', Type::INTEGER)
+        );
+        $queries->addQuery(
+            new MigrateTypesQuery($this->platform, $schema, 'orocrm_contact_group', 'id', Type::INTEGER)
+        );
 
         /** override platform queries */
-        $queries->addQuery(new MigrateTypesQuery($schema, 'oro_integration_channel', 'id', Type::INTEGER));
-        $queries->addQuery(new MigrateTypesQuery($schema, 'oro_integration_channel_status', 'id', Type::INTEGER));
-        $queries->addQuery(new MigrateTypesQuery($schema, 'oro_integration_transport', 'id', Type::INTEGER));
+        $queries->addQuery(
+            new MigrateTypesQuery($this->platform, $schema, 'oro_integration_channel', 'id', Type::INTEGER)
+        );
+        $queries->addQuery(
+            new MigrateTypesQuery($this->platform, $schema, 'oro_integration_channel_status', 'id', Type::INTEGER)
+        );
+        $queries->addQuery(
+            new MigrateTypesQuery($this->platform, $schema, 'oro_integration_transport', 'id', Type::INTEGER)
+        );
 
-        $queries->addQuery(new MigrateTypesQuery($schema, 'oro_access_group', 'id', Type::INTEGER));
-        $queries->addQuery(new MigrateTypesQuery($schema, 'oro_access_role', 'id', Type::INTEGER));
-        $queries->addQuery(new MigrateTypesQuery($schema, 'oro_user_email', 'id', Type::INTEGER));
-        $queries->addQuery(new MigrateTypesQuery($schema, 'oro_user_status', 'id', Type::INTEGER));
+        $queries->addQuery(new MigrateTypesQuery($this->platform, $schema, 'oro_access_group', 'id', Type::INTEGER));
+        $queries->addQuery(new MigrateTypesQuery($this->platform, $schema, 'oro_access_role', 'id', Type::INTEGER));
+        $queries->addQuery(new MigrateTypesQuery($this->platform, $schema, 'oro_user_email', 'id', Type::INTEGER));
+        $queries->addQuery(new MigrateTypesQuery($this->platform, $schema, 'oro_user_status', 'id', Type::INTEGER));
 
         $table = $schema->getTable('orocrm_magento_customer');
         $table->changeColumn('vat', ['type' => Type::getType('float'), 'notnull' => true]);
