669b5bb8064dd507b94684a7f0f81911a39b4807
Dmitry Khrysev <dkhrysev@magecore.com>
2014-08-08 19:13:45 +0400

    CRM-1201: Marketing lists CRUD

---
+++ src/OroCRM/Bundle/MarketingListBundle/Entity/MarketingListRemovedItem.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace OroCRM\Bundle\MarketingListBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Marketing list removed items.
+ *
+ * @ORM\Table(name="orocrm_ml_item_rm",  uniqueConstraints={
+ *      @ORM\UniqueConstraint(columns={"entity_id", "marketing_list_id"}, name="orocrm_ml_list_ent_rm_unq")
+ * })
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class MarketingListRemovedItem
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer", name="id")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="entity_id", type="string", nullable=false)
+     */
+    protected $entityId;
+
+
+    /**
+     * @var MarketingList
+     *
+     * @ORM\ManyToOne(targetEntity="OroCRM\Bundle\CampaignBundle\Entity\MarketingList")
+     * @ORM\JoinColumn(name="segment_id", referencedColumnName="id", onDelete="CASCADE", nullable=false)
+     */
+    protected $marketingList;
+
+    /**
+     * @var \Datetime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    protected $createdAt;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $entityId
+     * @return MarketingListRemovedItem
+     */
+    public function setEntityId($entityId)
+    {
+        $this->entityId = $entityId;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEntityId()
+    {
+        return $this->entityId;
+    }
+
+    /**
+     * @param MarketingList $marketingList
+     * @return MarketingListRemovedItem
+     */
+    public function setMarketingList(MarketingList $marketingList)
+    {
+        $this->marketingList = $marketingList;
+
+        return $this;
+    }
+
+    /**
+     * @return MarketingList
+     */
+    public function getMarketingList()
+    {
+        return $this->marketingList;
+    }
+
+    /**
+     * @param \Datetime $createdAt
+     * @return MarketingListItem
+     */
+    public function setCreatedAt(\Datetime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return \Datetime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Pre persist event listener
+     *
+     * @ORM\PrePersist
+     */
+    public function beforeSave()
+    {
+        $this->createdAt = new \DateTime('now', new \DateTimeZone('UTC'));
+    }
+}
---
+++ src/OroCRM/Bundle/MarketingListBundle/OroCRMMarketingListBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OroCRM\Bundle\CampaignBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OroCRMMarketingListBundle extends Bundle
+{
+}
