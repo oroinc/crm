c7c5d8a9c19f9740b00b5d97352e50f4d5ee4631
Alexander Nezdoiminoga <alex@magecore.com>
2014-08-13 14:39:16 +0300

    OEE-26: merge with master fixes

--- src/OroCRM/Bundle/CampaignBundle/Migrations/Schema/v1_3/OroCRMCampaignBundle.php
+++ src/OroCRM/Bundle/CampaignBundle/Migrations/Schema/v1_3/OroCRMCampaignBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OroCRM\Bundle\CampaignBundle\Migrations\Schema\v1_2;
+namespace OroCRM\Bundle\CampaignBundle\Migrations\Schema\v1_3;
 
 use Doctrine\DBAL\Schema\Schema;
 
--- src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_17/OroCRMMagentoBundle.php
+++ src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_17/OroCRMMagentoBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OroCRM\Bundle\MagentoBundle\Migrations\Schema\v1_16;
+namespace OroCRM\Bundle\MagentoBundle\Migrations\Schema\v1_17;
 
 use Doctrine\DBAL\Schema\Schema;
 
