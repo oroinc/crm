769c2f7bfe56da276f86b50a8db13fee164fd625
Konstantin Popov <kpopov@magecore.com>
2014-08-15 17:53:41 +0300

    ticket/CRM-1719: Implement channel view page - fix rewiew issues

--- src/OroCRM/Bundle/ChannelBundle/Controller/ChannelController.php
+++ src/OroCRM/Bundle/ChannelBundle/Controller/ChannelController.php
@@ -102,8 +102,11 @@ class ChannelController extends Controller
 
     /**
      * @Route(
-     *  "/status/change/{id}/{backRoute}", requirements={"id"="\d+"}, defaults={"backRoute"="orocrm_channel_view"},
-     *  name="orocrm_channel_change_status")
+     *  "/status/change/{id}/{backRoute}",
+     *  requirements={"id"="\d+"},
+     *  defaults={"backRoute"="orocrm_channel_view"},
+     *  name="orocrm_channel_change_status"
+     *  )
      * @AclAncestor("orocrm_channel_update")
      */
     public function changeStatusAction(Channel $channel, $backRoute)
