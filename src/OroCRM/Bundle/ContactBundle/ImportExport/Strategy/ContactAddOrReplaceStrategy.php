13bb42e908d09237412931ddfc3bb7b3bfde570f
Viktor Linkin <vlinkin@magecore.com>
2014-08-15 19:26:19 +0300

    CRM-1825: Failed import Data Template  - fixed code style

--- src/OroCRM/Bundle/ContactBundle/ImportExport/Strategy/ContactAddOrReplaceStrategy.php
+++ src/OroCRM/Bundle/ContactBundle/ImportExport/Strategy/ContactAddOrReplaceStrategy.php
@@ -163,32 +163,37 @@ class ContactAddOrReplaceStrategy extends ConfigurableAddOrReplaceStrategy
     protected function afterProcessEntity($entity)
     {
         // there can be only one primary entity
-        foreach (array('address', 'email', 'phone') as $contactName) {
-            /** @var Collection $contacts */
-            $contactGetter  = $this->generateGetter($entity, $contactName);
-            $contactSetter  = 'setPrimary' . ucfirst($contactName);
-            $contacts       = $entity->$contactGetter();
-            $primaryContact = $this->getPrimaryEntity($contacts);
+        $addresses = $entity->getAddresses();
+        $primaryAddress = $this->getPrimaryEntity($addresses);
 
-            if ($primaryContact) {
-                $entity->$contactSetter($primaryContact);
-            } elseif ($contacts->count() > 0) {
-                $entity->$contactSetter($contacts->first());
-            }
+        if ($primaryAddress) {
+            /** @var \OroCRM\Bundle\ContactBundle\Entity\ContactAddress $primaryAddress */
+            $entity->setPrimaryAddress($primaryAddress);
+        } elseif ($addresses->count() > 0) {
+                $entity->setPrimaryAddress($addresses->first());
+        }
+
+        $emails = $entity->getEmails();
+        $primaryEmail = $this->getPrimaryEntity($emails);
+
+        if ($primaryEmail) {
+            /** @var \OroCRM\Bundle\ContactBundle\Entity\ContactEmail $primaryEmail */
+            $entity->setPrimaryEmail($primaryEmail);
+        } elseif ($emails->count() > 0) {
+            $entity->setPrimaryEmail($emails->first());
+        }
+
+        $phones = $entity->getPhones();
+        $primaryPhone = $this->getPrimaryEntity($phones);
+
+        if ($primaryPhone) {
+            /** @var \OroCRM\Bundle\ContactBundle\Entity\ContactPhone $primaryPhone */
+            $entity->setPrimaryPhone($primaryPhone);
+        } elseif ($phones->count() > 0) {
+            $entity->setPrimaryPhone($phones->first());
         }
 
         return $entity;
-    }
-
-    /**
-     * @param object $entity
-     * @param string $neededName
-     * @return string
-     */
-    protected function generateGetter($entity, $neededName)
-    {
-        $getter = 'get' . ucfirst($neededName);
-        return method_exists($entity, $getter . 's') ? $getter . 's' : $getter . 'es';
     }
 
     /**
