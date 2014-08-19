3691b492a42f87f258a51f6f33fec50459fb4ddd
Konstantin Popov <kpopov@magecore.com>
2014-08-19 15:21:40 +0300

    ticket/CRM-1833: Demo fixes - fix test

--- src/OroCRM/Bundle/ChannelBundle/Tests/Unit/EventListener/ChangeChannelStatusListenerTest.php
+++ src/OroCRM/Bundle/ChannelBundle/Tests/Unit/EventListener/ChangeChannelStatusListenerTest.php
@@ -6,6 +6,7 @@ use Oro\Bundle\IntegrationBundle\Entity\Channel as Integration;
 
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
+use OroCRM\Bundle\ChannelBundle\Event\AbstractEvent;
 use OroCRM\Bundle\ChannelBundle\EventListener\ChangeChannelStatusListener;
 use OroCRM\Bundle\ChannelBundle\Entity\Channel;
 use OroCRM\Bundle\ChannelBundle\Event\ChannelChangeStatusEvent;
@@ -24,6 +25,9 @@ class ChangeChannelStatusListenerTest extends \PHPUnit_Framework_TestCase
     /** @var ChannelChangeStatusEvent */
     protected $event;
 
+    /** @var AbstractEvent */
+    protected $eventAbstract;
+
     /** @var Integration */
     protected $integration;
 
@@ -34,6 +38,10 @@ class ChangeChannelStatusListenerTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()->getMock();
         $this->event       = $this->getMockBuilder('OroCRM\Bundle\ChannelBundle\Event\ChannelChangeStatusEvent')
             ->disableOriginalConstructor()->getMock();
+
+        $this->eventAbstract = $this->getMockBuilder('OroCRM\Bundle\ChannelBundle\Event\AbstractEvent')
+            ->disableOriginalConstructor()->getMock();
+
         $this->entity      = new Channel();
         $this->integration = new Integration();
         $this->entity->setDataSource($this->integration);
@@ -52,16 +60,22 @@ class ChangeChannelStatusListenerTest extends \PHPUnit_Framework_TestCase
         $this->entity->setStatus($status);
 
         $this->registry->expects($this->any())->method('getManager')->will($this->returnValue($this->em));
-        $this->em->expects($this->once())->method('persist')->with($this->integration);
-        $this->em->expects($this->once())->method('flush');
+        $this->em->expects($this->exactly(2))->method('persist')->with($this->integration);
+        $this->em->expects($this->exactly(2))->method('flush');
 
-        $this->event->expects($this->once())
+        $this->event->expects($this->exactly(1))
+            ->method('getChannel')
+            ->will($this->returnValue($this->entity));
+
+        $this->eventAbstract->expects($this->exactly(1))
             ->method('getChannel')
             ->will($this->returnValue($this->entity));
 
         $listener = new ChangeChannelStatusListener($this->registry);
         $listener->onChannelStatusChange($this->event);
+        $this->assertEquals($this->integration->getEnabled(), $isEnabled);
 
+        $listener->onChannelStatusChange($this->eventAbstract);
         $this->assertEquals($this->integration->getEnabled(), $isEnabled);
     }
 
