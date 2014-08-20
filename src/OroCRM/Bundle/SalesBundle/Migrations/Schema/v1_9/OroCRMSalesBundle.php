88243b3e9d7937541ec6d0a2b55c981994fc24ed
Dmitry Naydenov <dnaidonov@magecore.com1~>
2014-08-20 19:35:44 +0300

    CRM-1855 Modify opportunity and lead add migration

--- src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_9/OroCRMSalesBundle.php
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_9/OroCRMSalesBundle.php
@@ -11,7 +11,6 @@ use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
 class OroCRMSalesBundle implements Migration, ExtendExtensionAwareInterface
 {
-
     /**
      * @var ExtendExtension
      */
@@ -41,22 +40,29 @@ class OroCRMSalesBundle implements Migration, ExtendExtensionAwareInterface
     /**
      * @param Schema $schema
      */
+    protected static function orocrmB2BCustomerTable(Schema $schema)
+    {
+        $b2bCustomerTable = $schema->createTable('orocrm_sales_b2b_customer');
+        $b2bCustomerTable->addColumn('id', 'integer', ['autoincrement' => true]);
+        $b2bCustomerTable->addColumn('account_id', 'integer', ['notnull' => false]);
+
+        $b2bCustomerTable->setPrimaryKey(['id']);
+        $b2bCustomerTable->addIndex(['account_id'], 'IDX_94CC12929B6B5FBA', []);
+    }
+
+    /**
+     * @param Schema $schema
+     */
     protected static function orocrmLeadTable(Schema $schema)
     {
         $leadTable = $schema->getTable('orocrm_sales_lead');
-        /*$leadTable->removeForeignKey('FK_73DB46339B6B5FBA');
+
+        $leadTable->removeForeignKey('FK_73DB46339B6B5FBA');
         $leadTable->dropIndex('IDX_73DB46339B6B5FBA');
-        $leadTable->dropColumn('account_id');*/
+        $leadTable->dropColumn('account_id');
 
         $leadTable->addColumn('b2bcustomer_id', 'integer', ['notnull' => false]);
         $leadTable->addIndex(['b2bcustomer_id']);
-
-        $leadTable->addForeignKeyConstraint(
-            $schema->getTable('orocrm_sales_b2b_customer'),
-            ['b2bcustomer_id'],
-            ['id'],
-            ['onDelete' => null, 'onUpdate' => null]
-        );
     }
 
     /**
@@ -65,31 +71,13 @@ class OroCRMSalesBundle implements Migration, ExtendExtensionAwareInterface
     protected static function orocrmOpportunityTable(Schema $schema)
     {
         $opportunityTable = $schema->getTable('orocrm_sales_opportunity');
-        /*$opportunityTable->removeForeignKey('FK_C0FE4AAC9B6B5FBA');
+
+        $opportunityTable->removeForeignKey('FK_C0FE4AAC9B6B5FBA');
         $opportunityTable->dropIndex('IDX_C0FE4AAC9B6B5FBA');
-        $opportunityTable->dropColumn('account_id');*/
+        $opportunityTable->dropColumn('account_id');
 
         $opportunityTable->addColumn('b2bcustomer_id', 'integer', ['notnull' => false]);
         $opportunityTable->addIndex(['b2bcustomer_id']);
-
-        $opportunityTable->addForeignKeyConstraint(
-            $schema->getTable('orocrm_sales_b2b_customer'),
-            ['b2bcustomer_id'],
-            ['id'],
-            ['onDelete' => null, 'onUpdate' => null]
-        );
-    }
-
-    /**
-     * @param Schema $schema
-     */
-    protected static function orocrmB2BCustomerTable(Schema $schema)
-    {
-        $b2bCustomerTable = $schema->createTable('orocrm_sales_b2b_customer');
-        $b2bCustomerTable->addColumn('id', 'integer', ['autoincrement' => true]);
-        $b2bCustomerTable->addColumn('account_id', 'integer', ['notnull' => false]);
-        $b2bCustomerTable->setPrimaryKey(['id']);
-        $b2bCustomerTable->addIndex(['account_id'], 'IDX_94CC12929B6B5FBA', []);
     }
 
     /**
@@ -103,6 +91,22 @@ class OroCRMSalesBundle implements Migration, ExtendExtensionAwareInterface
             ['account_id'],
             ['id'],
             ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
+
+        $opportunityTable = $schema->getTable('orocrm_sales_opportunity');
+        $opportunityTable->addForeignKeyConstraint(
+            $schema->getTable('orocrm_sales_b2b_customer'),
+            ['b2bcustomer_id'],
+            ['id'],
+            ['onDelete' => null, 'onUpdate' => null]
+        );
+
+        $leadTable = $schema->getTable('orocrm_sales_lead');
+        $leadTable->addForeignKeyConstraint(
+            $schema->getTable('orocrm_sales_b2b_customer'),
+            ['b2bcustomer_id'],
+            ['id'],
+            ['onDelete' => null, 'onUpdate' => null]
         );
     }
 }
---
+++ src/OroCRM/Bundle/SalesBundle/Tests/Unit/Entity/B2BCustomerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace OroCRM\Bundle\SalesBundle\Tests\Unit\Entity;
+
+use OroCRM\Bundle\SalesBundle\Entity\B2BCustomer;
+
+class B2BCustomerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getSetDataProvider
+     */
+    public function testGetSet($property, $value, $expected)
+    {
+        $obj = new B2BCustomer();
+
+        call_user_func_array(array($obj, 'set' . ucfirst($property)), array($value));
+        $this->assertEquals($expected, call_user_func_array(array($obj, 'get' . ucfirst($property)), array()));
+    }
+
+    /**
+     * @return array
+     */
+    public function getSetDataProvider()
+    {
+        $account = $this->getMockBuilder('OroCRM\Bundle\AccountBundle\Entity\Account')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return [
+            'account' => ['account', $account, $account],
+        ];
+    }
+}
