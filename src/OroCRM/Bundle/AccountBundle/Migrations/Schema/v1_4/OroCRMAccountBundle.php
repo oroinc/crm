3fef97c2243356585e9427e0f9e5cfe252cd38ca
Dmitry Naydenov <dnaidonov@magecore.com1~>
2014-08-21 14:56:34 +0300

    CRM-1858 Modify 'account entity' - remove field that

---
+++ src/OroCRM/Bundle/AccountBundle/Migrations/Schema/v1_4/OroCRMAccountBundle.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace OroCRM\Bundle\AccountBundle\Migrations\Schema\v1_4;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class OroCRMAccountBundle implements Migration
+{
+    /**
+     * @inheritdoc
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $table = $schema->getTable('orocrm_account');
+
+        $table->dropColumn('extend_website');
+        $table->dropColumn('extend_employees');
+        $table->dropColumn('extend_ownership');
+        $table->dropColumn('extend_ticker_symbol');
+        $table->dropColumn('extend_rating');
+        $table->dropColumn('extend_description');
+    }
+}
