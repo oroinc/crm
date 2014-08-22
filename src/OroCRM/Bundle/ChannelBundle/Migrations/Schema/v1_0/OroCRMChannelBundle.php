49335c6376c8897f8d2ce527139219f2f134be81
Alexandr Smaga <asmaga@magecore.com>
2014-08-22 14:37:05 +0300

    CRM-1856: Create "B2B customer" entity in sales bundle     - fix installer code style

--- src/OroCRM/Bundle/ChannelBundle/Migrations/Schema/v1_0/OroCRMChannelBundle.php
+++ src/OroCRM/Bundle/ChannelBundle/Migrations/Schema/v1_0/OroCRMChannelBundle.php
@@ -7,73 +7,94 @@ use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-class OroCRMChannelBundle implements Migration
+class OroCRMChannelBundleInstaller implements Migration
 {
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function up(Schema $schema, QueryBag $queries)
     {
-        self::addChannelTable($schema);
-        self::addChannelEntityNameTable($schema);
+        /** Tables generation **/
+        $this->createOrocrmChannelTable($schema);
+        $this->createOrocrmChannelEntityNameTable($schema);
+
+        /** Foreign keys generation **/
+        $this->addOrocrmChannelForeignKeys($schema);
+        $this->addOrocrmChannelEntityNameForeignKeys($schema);
     }
 
     /**
+     * Create orocrm_channel table
+     *
      * @param Schema $schema
      */
-    public function addChannelTable(Schema $schema)
+    protected function createOrocrmChannelTable(Schema $schema)
     {
         $table = $schema->createTable('orocrm_channel');
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
-        $table->addColumn('name', 'string', ['notnull' => true, 'length' => 255]);
-        $table->addColumn('organization_owner_id', 'integer', ['notnull' => false]);
         $table->addColumn('data_source_id', 'integer', ['notnull' => false]);
-        $table->addColumn('status', 'boolean', ['notnull' => true]);
-        $table->addColumn('channel_type', 'string', ['notnull' => true]);
-        $table->addColumn('customer_identity', 'string', ['notnull' => true]);
-        $table->addColumn('createdAt', 'datetime', ['notnull' => true]);
+        $table->addColumn('organization_owner_id', 'integer', ['notnull' => false]);
+        $table->addColumn('name', 'string', ['length' => 255]);
+        $table->addColumn('status', 'boolean', []);
+        $table->addColumn('channel_type', 'string', ['length' => 255]);
+        $table->addColumn('customer_identity', 'string', ['length' => 255]);
+        $table->addColumn('createdAt', 'datetime', []);
         $table->addColumn('updatedAt', 'datetime', ['notnull' => false]);
-
         $table->setPrimaryKey(['id']);
-        $table->addIndex(['organization_owner_id'], 'IDX_AEA90B929124A35B', []);
         $table->addUniqueIndex(['data_source_id'], 'UNIQ_AEA90B921A935C57');
-
-        $table->addForeignKeyConstraint(
-            $schema->getTable('oro_organization'),
-            ['organization_owner_id'],
-            ['id'],
-            ['onDelete' => 'SET NULL', 'onUpdate' => null],
-            'FK_AEA90B929124A35B'
-        );
-
-        $table->addForeignKeyConstraint(
-            $schema->getTable('oro_integration_channel'),
-            ['data_source_id'],
-            ['id'],
-            ['onDelete' => 'SET NULL', 'onUpdate' => null],
-            'FK_AEA90B921A935C57'
-        );
+        $table->addIndex(['organization_owner_id'], 'IDX_AEA90B929124A35B', []);
     }
 
     /**
+     * Create orocrm_channel_entity_name table
+     *
      * @param Schema $schema
      */
-    public function addChannelEntityNameTable(Schema $schema)
+    protected function createOrocrmChannelEntityNameTable(Schema $schema)
     {
         $table = $schema->createTable('orocrm_channel_entity_name');
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
         $table->addColumn('channel_id', 'integer', ['notnull' => false]);
         $table->addColumn('name', 'string', ['length' => 255]);
-
         $table->setPrimaryKey(['id']);
         $table->addIndex(['channel_id'], 'IDX_92BC967172F5A1AA', []);
+    }
 
+    /**
+     * Add orocrm_channel foreign keys.
+     *
+     * @param Schema $schema
+     */
+    protected function addOrocrmChannelForeignKeys(Schema $schema)
+    {
+        $table = $schema->getTable('orocrm_channel');
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_integration_channel'),
+            ['data_source_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_organization'),
+            ['organization_owner_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
+    }
+
+    /**
+     * Add orocrm_channel_entity_name foreign keys.
+     *
+     * @param Schema $schema
+     */
+    protected function addOrocrmChannelEntityNameForeignKeys(Schema $schema)
+    {
+        $table = $schema->getTable('orocrm_channel_entity_name');
         $table->addForeignKeyConstraint(
             $schema->getTable('orocrm_channel'),
             ['channel_id'],
             ['id'],
-            ['onDelete' => 'CASCADE', 'onUpdate' => null],
-            'FK_92BC967172F5A1AA'
+            ['onDelete' => 'CASCADE', 'onUpdate' => null]
         );
     }
 }
