d2414156cb88dd01f9bc3df6d952b7e12f407163
Dmitry Naydenov <dnaidonov@magecore.com1~>
2014-08-27 17:53:47 +0300

    CRM-1862 Create view for b2b customer fix menu item visibility add b2b customer block to the entity_data block

--- src/OroCRM/Bundle/SalesBundle/Controller/B2bCustomerController.php
+++ src/OroCRM/Bundle/SalesBundle/Controller/B2bCustomerController.php
@@ -59,7 +59,7 @@ class B2bCustomerController extends Controller
     }
 
     /**
-     * @Route("/widget/b2bcustomer-leads/{id}", name="orocrm_sales_widget_b2bcustomer_leads", requirements={"id"="\d+"})
+     * @Route("/widget/b2bcustomer-leads/{id}", name="orocrm_sales_b2bcustomer_widget_leads", requirements={"id"="\d+"})
      * @AclAncestor("orocrm_sales_lead_view")
      * @Template
      */
@@ -73,7 +73,7 @@ class B2bCustomerController extends Controller
     /**
      * @Route(
      *      "/widget/b2bcustomer-opportunities/{id}",
-     *      name="orocrm_sales_widget_b2bcustomer_opportunities",
+     *      name="orocrm_sales_b2bcustomer_widget_opportunities",
      *      requirements={"id"="\d+"}
      * )
      * @AclAncestor("orocrm_sales_opportunity_view")
