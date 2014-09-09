33596717ed301a4839ae64d3b9df005a0cbc15b6
Dmitry Naydenov <dnaidonov@magecore.com1~>
2014-09-09 18:40:51 +0300

    CRM-Extend embedded forms creation in order minor fix

--- src/OroCRM/Bundle/ChannelBundle/Tests/Unit/EventListener/EmbeddedFormListenerTest.php
+++ src/OroCRM/Bundle/ChannelBundle/Tests/Unit/EventListener/EmbeddedFormListenerTest.php
@@ -8,30 +8,41 @@ class EmbeddedFormListenerTest extends \PHPUnit_Framework_TestCase
 {
     public function testAddDataChannelField()
     {
-        $env = $this->getMockBuilder('Twig_Environment')
+        $env      = $this->getMockBuilder('Twig_Environment')
             ->disableOriginalConstructor()
             ->getMock();
-
         $newField = "<input>";
-        $env->expects($this->once())->method('render')->will($this->returnValue($newField));
-        $formView = $this->getMockBuilder('Symfony\Component\Form\FormView')
+
+        $env->expects($this->once())
+            ->method('render')
+            ->will($this->returnValue($newField));
+
+        $formView        = $this->getMockBuilder('Symfony\Component\Form\FormView')
             ->disableOriginalConstructor()
             ->getMock();
         $currentFormData = 'someHTML';
-        $formData = array('dataBlocks' => array(
-            array(
-                'subblocks' => array(
-                    array('data' => array($currentFormData))
-                )
-            )
-        ));
+        $formData        = [
+            'dataBlocks' => [
+                [
+                    'subblocks' => [
+                        ['data' => [$currentFormData]]
+                    ]
+                ]
+            ]
+        ];
 
         $event = $this->getMockBuilder('Oro\Bundle\UIBundle\Event\BeforeFormRenderEvent')
             ->disableOriginalConstructor()
             ->getMock();
-        $event->expects($this->once())->method('getTwigEnvironment')->will($this->returnValue($env));
-        $event->expects($this->once())->method('getFormData')->will($this->returnValue($formData));
-        $event->expects($this->once())->method('getForm')->will($this->returnValue($formView));
+        $event->expects($this->once())
+            ->method('getTwigEnvironment')
+            ->will($this->returnValue($env));
+        $event->expects($this->once())
+            ->method('getFormData')
+            ->will($this->returnValue($formData));
+        $event->expects($this->once())
+            ->method('getForm')
+            ->will($this->returnValue($formView));
 
         $formData['dataBlocks'][0]['subblocks'][0]['data'][] = $newField;
         $event->expects($this->once())->method('setFormData')->with($formData);
@@ -42,6 +53,7 @@ class EmbeddedFormListenerTest extends \PHPUnit_Framework_TestCase
         $provider->expects($this->any())
             ->method('hasConfig')
             ->will($this->returnValue(false));
+
         $configManager = $this->getMockBuilder('Oro\Bundle\EntityConfigBundle\Config\ConfigManager')
             ->disableOriginalConstructor()
             ->getMock();
@@ -50,7 +62,6 @@ class EmbeddedFormListenerTest extends \PHPUnit_Framework_TestCase
             ->will($this->returnValue($provider));
 
         $listener = new EmbeddedFormListener($configManager);
-
         $listener->addDataChannelField($event);
     }
 }
