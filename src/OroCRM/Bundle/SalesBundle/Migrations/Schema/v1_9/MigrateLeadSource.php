a46640d251fcea08b887b91eadb42c03c07b6561
Ivan Shakuta <ishakuta@magecore.com>
2014-09-10 15:34:52 +0300

    BAP-3939: New Option Sets implementation. Fix sorting of options on view page and grid - fix CR

--- src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_9/MigrateLeadSource.php
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_9/MigrateLeadSource.php
@@ -4,6 +4,7 @@ namespace OroCRM\Bundle\SalesBundle\Migrations\Schema\v1_9;
 
 use Doctrine\DBAL\Schema\Schema;
 
+use Oro\Bundle\EntityConfigBundle\Config\ConfigModelManager;
 use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
 use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
 use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
@@ -36,6 +37,14 @@ class MigrateLeadSource implements Migration, ExtendExtensionAwareInterface
             false,
             false,
             ['extend' => ['owner' => ExtendScope::OWNER_CUSTOM]]
+        );
+
+        $queries->addPostQuery(
+            sprintf(
+                'UPDATE oro_entity_config_field SET mode=\'%s\' WHERE field_name=\'%s\'',
+                ConfigModelManager::MODE_HIDDEN,
+                'extend_source'
+            )
         );
     }
 }
