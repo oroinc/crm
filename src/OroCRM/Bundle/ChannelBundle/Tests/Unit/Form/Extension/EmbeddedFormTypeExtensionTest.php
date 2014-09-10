b6a0d85c624144a475990de1b50b276fd1f9c76e
Dmitry Naydenov <dnaidonov@magecore.com1~>
2014-09-10 12:19:07 +0300

    CRM-1915 Extend embedded forms creation in order minor fix

---
+++ src/OroCRM/Bundle/ChannelBundle/Tests/Unit/Form/Extension/EmbeddedFormTypeExtensionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace OroCRM\Bundle\ChannelBundle\Tests\Unit\Form\Extension;
+
+use OroCRM\Bundle\ChannelBundle\Form\Extension\EmbeddedFormTypeExtension;
+
+use Symfony\Component\Form\Test\FormIntegrationTestCase;
+
+class EmbeddedFormTypeExtensionTest extends FormIntegrationTestCase
+{
+    /** @var EmbeddedFormTypeExtension */
+    protected $extension;
+
+    protected function setUp()
+    {
+        $this->extension = new EmbeddedFormTypeExtension();
+        parent::setUp();
+    }
+
+    public function testGetExtendedType()
+    {
+        $this->assertEquals(
+            $this->extension->getExtendedType(),
+            'embedded_form'
+        );
+    }
+
+    public function testBuildForm()
+    {
+        $builder      = $this->factory->createNamedBuilder('root');
+        $builderInner = $this->factory->createNamedBuilder('additional');
+        $builderInner->add('dataChannel', 'text');
+        $builder->add($builderInner);
+
+        $form = $builder->getForm();
+
+        $this->extension->buildForm($builder, []);
+
+        $this->assertTrue($form->get('additional')->get('dataChannel')->getConfig()->getOption('required'));
+        $this->assertFalse($form->get('additional')->get('dataChannel')->getConfig()->getOption('error_bubbling'));
+    }
+}
