08c446bbc7bb0e0baa91bd092326b4b9d1e5df96
Alexandr Smaga <asmaga@magecore.com>
2014-09-10 12:50:56 +0300

    CRM-1915 Extend embedded forms creation in order    - fix unit test

--- src/OroCRM/Bundle/ChannelBundle/Tests/Unit/Form/Extension/EmbeddedFormTypeExtensionTest.php
+++ src/OroCRM/Bundle/ChannelBundle/Tests/Unit/Form/Extension/EmbeddedFormTypeExtensionTest.php
@@ -4,7 +4,9 @@ namespace OroCRM\Bundle\ChannelBundle\Tests\Unit\Form\Extension;
 
 use OroCRM\Bundle\ChannelBundle\Form\Extension\EmbeddedFormTypeExtension;
 
+use Symfony\Component\Form\PreloadedExtension;
 use Symfony\Component\Form\Test\FormIntegrationTestCase;
+use Symfony\Component\Form\Extension\Validator\Type\FormTypeValidatorExtension;
 
 class EmbeddedFormTypeExtensionTest extends FormIntegrationTestCase
 {
@@ -23,6 +25,24 @@ class EmbeddedFormTypeExtensionTest extends FormIntegrationTestCase
         parent::tearDown();
     }
 
+    /**
+     * @return array
+     */
+    protected function getExtensions()
+    {
+        return [
+            new PreloadedExtension(
+                [],
+                [
+                    'form' => [
+                        new FormTypeValidatorExtension($this->getMock('Symfony\Component\Validator\ValidatorInterface'))
+                    ]
+                ]
+            )
+        ];
+    }
+
+
     public function testGetExtendedType()
     {
         $this->assertEquals(
@@ -35,14 +55,15 @@ class EmbeddedFormTypeExtensionTest extends FormIntegrationTestCase
     {
         $builder      = $this->factory->createNamedBuilder('root');
         $builderInner = $this->factory->createNamedBuilder('additional');
-        $builderInner->add('dataChannel', 'text');
+        $builderInner->add('dataChannel', 'text', ['required' => false, 'constraints' => []]);
         $builder->add($builderInner);
 
         $form = $builder->getForm();
 
         $this->extension->buildForm($builder, []);
+        $form->setData([]);
 
         $this->assertTrue($form->get('additional')->get('dataChannel')->getConfig()->getOption('required'));
-        $this->assertFalse($form->get('additional')->get('dataChannel')->getConfig()->getOption('error_bubbling'));
+        $this->assertNotEmpty($form->get('additional')->get('dataChannel')->getConfig()->getOption('constraints'));
     }
 }
