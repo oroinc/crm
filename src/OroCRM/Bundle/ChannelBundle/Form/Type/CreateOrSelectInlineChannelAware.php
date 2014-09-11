f56bcc1d95ae80b6c77fd50132b252b5cc2cde53
Alexandr Smaga <asmaga@magecore.com>
2014-09-11 13:47:43 +0300

    CRM-1920: Filter assignment grids by current channel    - added field renderer    - added form theme to twig config

--- src/OroCRM/Bundle/ChannelBundle/Form/Type/CreateOrSelectInlineChannelAware.php
+++ src/OroCRM/Bundle/ChannelBundle/Form/Type/CreateOrSelectInlineChannelAware.php
@@ -2,12 +2,14 @@
 
 namespace OroCRM\Bundle\ChannelBundle\Form\Type;
 
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class CreateOrSelectInlineChannelAware extends AbstractType
 {
-    const NAME = 'orocrm_channel_create_or_select_inline_channel_aware';
+    const NAME = 'oro_entity_create_or_select_inline_channel_aware';
 
     /**
      * {@inheritdoc}
@@ -30,6 +32,42 @@ class CreateOrSelectInlineChannelAware extends AbstractType
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setRequired(['data_channel_field']);
+        $resolver->setOptional(['channel_id']);
+        $resolver->setDefaults(['channel_field' => 'dataChannel',]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['channel_id'] = isset($options['channel_id']) ? $options['channel_id'] : null;
+
+        $view->vars['channel_field_selector'] = isset($view->parent[$options['channel_field']])
+            ? $view->parent[$options['channel_field']]->vars['id']
+            : $this->suggestSelector($options['channel_field'], $view);
+
+        $options['configs']['extra_config'] = 'channel_aware';
+
+        $view->vars = array_replace_recursive($view->vars, ['configs' => $options['configs']]);
+    }
+
+    /**
+     * Suggest field ID based on parent view
+     *
+     * @param string   $name
+     * @param FormView $view
+     *
+     * @return string
+     */
+    protected function suggestSelector($name, FormView $view)
+    {
+        $id = $name;
+
+        if ('' !== ($parentFullName = $view->parent->vars['full_name'])) {
+            $id = sprintf('%s_%s', $view->parent->vars['id'], $name);
+        }
+
+        return $id;
     }
 }
