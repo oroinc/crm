ff034a2d2a688b38d0ec7305a98b4d36277a451a
Konstantin Popov <kpopov@magecore.com>
2014-09-11 11:54:08 +0300

    ticket/CRM-1916: Teach embedded form to preset channel to each entity - fix review issues

---
+++ src/OroCRM/Bundle/ChannelBundle/Tests/Unit/Stubs/Entity/EmbeddedFormStub.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OroCRM\Bundle\ChannelBundle\Tests\Unit\Stubs\Entity;
+
+use Oro\Bundle\EmbeddedFormBundle\Entity\EmbeddedForm;
+
+class EmbeddedFormStub extends EmbeddedForm
+{
+    public function getDataChannel()
+    {
+
+    }
+}
