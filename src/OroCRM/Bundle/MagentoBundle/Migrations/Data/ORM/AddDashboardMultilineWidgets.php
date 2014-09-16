96a1c90dee983ccca0a3b8eedc3a50a5061ccdf9
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-09-16 18:57:53 +0300

    CRM-1940: Average order amount widget  - added raw implementation of dashboard widget

---
+++ src/OroCRM/Bundle/MagentoBundle/Migrations/Data/ORM/AddDashboardMultilineWidgets.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace OroCRM\Bundle\MagentoBundle\Migrations\Data\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+use Oro\Bundle\DashboardBundle\Migrations\Data\ORM\AbstractDashboardFixture;
+
+class AddDashboardMultilineWidgets extends AbstractDashboardFixture implements DependentFixtureInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getDependencies()
+    {
+        return ['Oro\Bundle\DashboardBundle\Migrations\Data\ORM\LoadDashboardData'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $mainDashboard = $this->findAdminDashboardModel($manager, 'main');
+
+        if ($mainDashboard) {
+            $mainDashboard->addWidget($this->createWidgetModel('average_order_amount_by_customer_chart', [1, 200]));
+
+            $manager->flush();
+        }
+    }
+}
