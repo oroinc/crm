c34e596f9dd6628752a75974d0e65ee8305a16e0
Roman Grebenchuk <rgrebenchuk@magecore.com>
2014-09-18 15:05:14 +0300

    BAP-498: Investigate failed builds  -  CR changes

--- src/OroCRM/Bundle/TestFrameworkBundle/Tests/Selenium/NotFoundTest.php
+++ src/OroCRM/Bundle/TestFrameworkBundle/Tests/Selenium/NotFoundTest.php
@@ -3,12 +3,14 @@
 namespace OroCRM\Bundle\TestsBundle\Tests\Selenium;
 
 use Oro\Bundle\TestFrameworkBundle\Test\Selenium2TestCase;
+use Oro\Bundle\SecurityBundle\Tests\Selenium\Pages\AclCheck;
 
-class Test404 extends Selenium2TestCase
+class NotFoundTest404 extends Selenium2TestCase
 {
     public function test404()
     {
         $login = $this->login();
+        /* @var AclCheck $login */
         $login->openAclCheck('Oro\Bundle\SecurityBundle')
             ->assertAcl('404', '404 - Not Found')
             ->assertElementPresent(
