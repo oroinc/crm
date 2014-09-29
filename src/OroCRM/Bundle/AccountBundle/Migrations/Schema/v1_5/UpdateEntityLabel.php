db77eb669f65f14dc01be762d9b482d27fc81464
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-09-29 14:26:00 +0300

    BAP-5065: Not all parts of the system has translations  - converted fixtures to migrations

---
+++ src/OroCRM/Bundle/AccountBundle/Migrations/Schema/v1_5/UpdateEntityLabel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace OroCRM\Bundle\AccountBundle\Migrations\Schema\v1_5;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\EntityConfigBundle\Migration\UpdateEntityConfigEntityValueQuery;
+
+class UpdateEntityLabel implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $queries->addQuery(
+            new UpdateEntityConfigEntityValueQuery(
+                'OroCRM\Bundle\AccountBundle\Entity\Account',
+                'entity',
+                'label',
+                'orocrm.account.entity_label'
+            )
+        );
+    }
+}
