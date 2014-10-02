a883aa39c365d7df878549769d66a84cba35ba01
Roman Grebenchuk <rgrebenchuk@magecore.com>
2014-10-02 12:29:28 +0300

    BAP-498: Investigate failed builds  - B2B changes

--- src/OroCRM/Bundle/SalesBundle/Tests/Selenium/WorkflowTest.php
+++ src/OroCRM/Bundle/SalesBundle/Tests/Selenium/WorkflowTest.php
@@ -5,6 +5,7 @@ namespace OroCRM\Bundle\SalesBundle\Tests\Selenium\Sales;
 use Oro\Bundle\TestFrameworkBundle\Test\Selenium2TestCase;
 use Oro\Bundle\UserBundle\Tests\Selenium\Pages\Login;
 use OroCRM\Bundle\AccountBundle\Tests\Selenium\Pages\Accounts;
+use OroCRM\Bundle\SalesBundle\Tests\Selenium\Pages\B2BCustomers;
 use OroCRM\Bundle\SalesBundle\Tests\Selenium\Pages\Leads;
 use OroCRM\Bundle\SalesBundle\Tests\Selenium\Pages\Opportunities;
 use OroCRM\Bundle\SalesBundle\Tests\Selenium\Pages\SalesFunnels;
@@ -97,8 +98,9 @@ class WorkflowTest extends Selenium2TestCase
             ->closeAsLost()
             ->setCloseReason('Cancelled')
             ->submit()
-            ->checkStep('Lost Opportunity')
-            ->openOpportunities('OroCRM\Bundle\SalesBundle')
+            ->checkStep('Lost Opportunity');
+        /** @var  Opportunities $login */
+        $login->openOpportunities('OroCRM\Bundle\SalesBundle')
             ->filterBy('Opportunity name', $leadName)
             ->open(array($leadName))
             ->checkStatus('Lost');
@@ -165,8 +167,9 @@ class WorkflowTest extends Selenium2TestCase
             ->closeAsWon()
             ->setCloseRevenue('100')
             ->submit()
-            ->checkStep('Won Opportunity')
-            ->openOpportunities('OroCRM\Bundle\SalesBundle')
+            ->checkStep('Won Opportunity');
+        /** @var  Opportunities $login */
+        $login->openOpportunities('OroCRM\Bundle\SalesBundle')
             ->filterBy('Opportunity name', $opportunityName)
             ->open(array($opportunityName))
             ->checkStatus('Won');
@@ -261,12 +264,12 @@ class WorkflowTest extends Selenium2TestCase
     {
         $opportunityName = 'Opportunity_'.mt_rand();
         $accountName = $this->createAccount($login);
-
+        $customer = $this->createB2BCustomer($login, $accountName);
         /** @var Opportunities $login */
         $login->openOpportunities('OroCRM\Bundle\SalesBundle')
             ->add()
             ->setName($opportunityName)
-            ->setAccount($accountName)
+            ->setB2BCustomer($customer)
             ->setProbability('50')
             ->seBudget('100')
             ->setCustomerNeed('50')
@@ -298,5 +301,24 @@ class WorkflowTest extends Selenium2TestCase
             ->save();
 
         return $accountName;
+    }
+
+    /**
+     * @param Login  $login
+     * @param string $account
+     * @return string
+     */
+    protected function createB2BCustomer(Login $login, $account)
+    {
+        $name = 'B2BCustomer_'.mt_rand();
+        /** @var B2BCustomers $login */
+        $login->openB2BCustomers('OroCRM\Bundle\SalesBundle')
+            ->add()
+            ->setName($name)
+            ->setOwner('admin')
+            ->setAccount($account)
+            ->save();
+
+        return $name;
     }
 }
