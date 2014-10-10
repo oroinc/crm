940c92a9214af52c74c34b3308e774c36c0abcaa
Dmitry Khrysev <x86demon@gmail.com>
2014-10-10 12:31:17 +0300

    CRM-2039: Improve marketing lists with Manual type - marketing list edit page support for manual type

--- src/OroCRM/Bundle/MarketingListBundle/Form/Type/MarketingListType.php
+++ src/OroCRM/Bundle/MarketingListBundle/Form/Type/MarketingListType.php
@@ -2,10 +2,15 @@
 
 namespace OroCRM\Bundle\MarketingListBundle\Form\Type;
 
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 use Oro\Bundle\QueryDesignerBundle\Form\Type\AbstractQueryDesignerType;
+use OroCRM\Bundle\MarketingListBundle\Entity\MarketingList;
+use OroCRM\Bundle\MarketingListBundle\Entity\MarketingListType as MarketingListTypeEntity;
 
 class MarketingListType extends AbstractQueryDesignerType
 {
@@ -17,17 +22,42 @@ class MarketingListType extends AbstractQueryDesignerType
         $builder
             ->add('name', 'text', ['required' => true])
             ->add('entity', 'orocrm_marketing_list_contact_information_entity_choice', ['required' => true])
-            ->add(
-                'type',
-                'entity',
-                [
-                    'class'       => 'OroCRMMarketingListBundle:MarketingListType',
-                    'property'    => 'label',
-                    'required'    => true,
-                    'empty_value' => 'orocrm.marketinglist.form.choose_marketing_list_type'
-                ]
-            )
             ->add('description', 'textarea', ['required' => false]);
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) {
+                /** @var MarketingList $marketingList */
+                $marketingList = $event->getData();
+                $form = $event->getForm();
+                if ($marketingList && $marketingList->getId() && $marketingList->isManual()) {
+                    $qb = function (EntityRepository $er) {
+                        return $er->createQueryBuilder('mlt')
+                            ->andWhere('mlt.name = :manualTypeName')
+                            ->setParameter('manualTypeName', MarketingListTypeEntity::TYPE_MANUAL);
+                    };
+                } else {
+                    $qb = function (EntityRepository $er) {
+                        return $er->createQueryBuilder('mlt')
+                            ->andWhere('mlt.name != :manualTypeName')
+                            ->setParameter('manualTypeName', MarketingListTypeEntity::TYPE_MANUAL)
+                            ->addOrderBy('mlt.name', 'ASC');
+                    };
+                }
+
+                $form->add(
+                    'type',
+                    'entity',
+                    [
+                        'class' => 'OroCRMMarketingListBundle:MarketingListType',
+                        'property' => 'label',
+                        'required' => true,
+                        'empty_value' => 'orocrm.marketinglist.form.choose_marketing_list_type',
+                        'query_builder' => $qb
+                    ]
+                );
+            }
+        );
 
         parent::buildForm($builder, $options);
     }
@@ -53,8 +83,8 @@ class MarketingListType extends AbstractQueryDesignerType
         $options = array_merge(
             $this->getDefaultOptions(),
             [
-                'data_class'         => 'OroCRM\Bundle\MarketingListBundle\Entity\MarketingList',
-                'intention'          => 'marketing_list',
+                'data_class' => 'OroCRM\Bundle\MarketingListBundle\Entity\MarketingList',
+                'intention' => 'marketing_list',
                 'cascade_validation' => true
             ]
         );
