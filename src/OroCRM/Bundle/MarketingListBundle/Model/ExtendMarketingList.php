4c298c7a3414b5d49e65bb054caa4d9be6cf1a4a
Dmitry Khrysev <x86demon@gmail.com>
2014-10-27 19:41:32 +0200

    CRM-2121: Create Workflow for Marketing List to connect it with MailChimp

---
+++ src/OroCRM/Bundle/MarketingListBundle/Model/ExtendMarketingList.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OroCRM\Bundle\MarketingListBundle\Model;
+
+class ExtendMarketingList
+{
+    /**
+     * Constructor
+     *
+     * The real implementation of this method is auto generated.
+     *
+     * IMPORTANT: If the derived class has own constructor it must call parent constructor.
+     */
+    public function __construct()
+    {
+    }
+}
