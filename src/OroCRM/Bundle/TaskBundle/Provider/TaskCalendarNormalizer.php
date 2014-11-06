f1d94f43b05b35e7cb3e460e3a20b12036b602a9
Valentine Velikiy <vvelykyi@magecore.com>
2014-11-06 16:16:54 +0200

    BAP-5840: Add provider for My Tasks calendar -  variable definition

--- src/OroCRM/Bundle/TaskBundle/Provider/TaskCalendarNormalizer.php
+++ src/OroCRM/Bundle/TaskBundle/Provider/TaskCalendarNormalizer.php
@@ -13,6 +13,7 @@ class TaskCalendarNormalizer
      */
     public function getTasks($calendarId, QueryBuilder $qb)
     {
+        $result = [];
         $items = $qb->getQuery()->getArrayResult();
         foreach ($items as $item) {
             $resultItem = [];
