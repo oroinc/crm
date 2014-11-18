c91d962d0abae574c158fba7c355e35ee7410766
kmuenze <kmuenze@gmail.com>
2014-11-18 10:11:48 -0800

    Create OroCRMContactBundle.php
    Add index for all phone numbers in support of issue #82.

---
+++ src/OroCRM/Bundle/ContactBundle/Migrations/Schema/v1_11/OroCRMContactBundle.php
@@ -0,0 +1,16 @@
+<?php
+namespace OroCRM\Bundle\ContactBundle\Migrations\Schema\v1_11;
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+class OroCRMContactBundle implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $table = $schema->getTable('orocrm_contact');
+        $table->addIndex(array('phoneNumbers'), 'phoneNumbers_idx');
+    }
+}
