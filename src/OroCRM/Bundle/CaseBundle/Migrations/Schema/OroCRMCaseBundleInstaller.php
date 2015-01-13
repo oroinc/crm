1468d56be92db4372d820d3299b4709a2c66b47b
Makar Sichevoy <sichevoy@gmail.com>
2015-01-13 13:10:32 +0200

    BAP-6774: Fix migration

--- src/OroCRM/Bundle/CaseBundle/Migrations/Schema/OroCRMCaseBundleInstaller.php
+++ src/OroCRM/Bundle/CaseBundle/Migrations/Schema/OroCRMCaseBundleInstaller.php
@@ -12,6 +12,7 @@ use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 use OroCRM\Bundle\CaseBundle\Migrations\Schema\v1_0\OroCRMCaseBundle;
 use OroCRM\Bundle\CaseBundle\Migrations\Schema\v1_1\OroCRMCaseBundle as OroCRMCaseBundle11;
 use OroCRM\Bundle\CaseBundle\Migrations\Schema\v1_2\OroCRMCaseBundle as OroCRMCaseBundle12;
+use OroCRM\Bundle\CaseBundle\Migrations\Schema\v1_4\OroCRMCaseBundle as OroCRMCaseBundle14;
 
 class OroCRMCaseBundleInstaller implements
     Installation,
@@ -46,6 +47,9 @@ class OroCRMCaseBundleInstaller implements
 
         $migration11 = new OroCRMCaseBundle11();
         $migration11->up($schema, $queries);
+
+        $migration14 = new OroCRMCaseBundle14();
+        $migration14->up($schema, $queries);
 
         OroCRMCaseBundle12::addOrganization($schema);
 
