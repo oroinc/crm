537890edb8bd639f361361e893a58f519f6b8f9a
Hryhorii Hrebiniuk <hhrebiniuk@magecore.com>
2015-01-30 15:51:40 +0200

    Reverted accidental merge

--- src/OroCRM/Bundle/TaskBundle/Tests/Selenium/Pages/Task.php
+++ src/OroCRM/Bundle/TaskBundle/Tests/Selenium/Pages/Task.php
@@ -20,8 +20,6 @@ class Task extends AbstractPageEntity
     /** @var  \PHPUnit_Extensions_Selenium2TestCase_Element */
     protected $dueDate;
     /** @var  \PHPUnit_Extensions_Selenium2TestCase_Element */
-    protected $dueTime;
-    /** @var  \PHPUnit_Extensions_Selenium2TestCase_Element */
     protected $prioty;
     /** @var  \PHPUnit_Extensions_Selenium2TestCase_Element */
     protected $assignedTo;
@@ -41,8 +39,7 @@ class Task extends AbstractPageEntity
     {
         $this->subject = $this->test->byId('orocrm_task_subject');
         $this->description = $this->test->byId('orocrm_task_description');
-        $this->dueDate = $this->test->byId('date_selector_orocrm_task_dueDate');
-        $this->dueTime = $this->test->byId('time_selector_orocrm_task_dueDate');
+        $this->dueDate = $this->test->byId('datetime_selector_orocrm_task_dueDate');
 
         return $this;
     }
@@ -74,17 +71,13 @@ class Task extends AbstractPageEntity
     public function setDueDate($dueDate)
     {
         $this->dueDate->clear();
-        $this->dueTime->clear();
-        if (preg_match('/^(.+)\s(\d{2}\:\d{2}\s\w{2})$/', $dueDate, $dueDate)) {
-            $this->dueDate->value($dueDate[1]);
-            $this->dueTime->value($dueDate[2]);
-        }
+        $this->dueDate->value($dueDate);
         return $this;
     }
 
     public function getDueDate()
     {
-        return $this->dueDate->value() . ' ' . $this->dueTime->value();
+        return $this->dueDate->value();
     }
 
     public function delete()
