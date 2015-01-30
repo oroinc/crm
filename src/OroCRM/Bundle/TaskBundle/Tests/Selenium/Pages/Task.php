a87d2ae7a8869adf5fe4edadfca6d758a97ccc84
Hryhorii Hrebiniuk <hhrebiniuk@magecore.com>
2015-01-30 10:55:09 +0200

    BAP-7096: Check selenium tests are not failed

--- src/OroCRM/Bundle/TaskBundle/Tests/Selenium/Pages/Task.php
+++ src/OroCRM/Bundle/TaskBundle/Tests/Selenium/Pages/Task.php
@@ -20,6 +20,8 @@ class Task extends AbstractPageEntity
     /** @var  \PHPUnit_Extensions_Selenium2TestCase_Element */
     protected $dueDate;
     /** @var  \PHPUnit_Extensions_Selenium2TestCase_Element */
+    protected $dueTime;
+    /** @var  \PHPUnit_Extensions_Selenium2TestCase_Element */
     protected $prioty;
     /** @var  \PHPUnit_Extensions_Selenium2TestCase_Element */
     protected $assignedTo;
@@ -39,7 +41,8 @@ class Task extends AbstractPageEntity
     {
         $this->subject = $this->test->byId('orocrm_task_subject');
         $this->description = $this->test->byId('orocrm_task_description');
-        $this->dueDate = $this->test->byId('datetime_selector_orocrm_task_dueDate');
+        $this->dueDate = $this->test->byId('date_selector_orocrm_task_dueDate');
+        $this->dueTime = $this->test->byId('time_selector_orocrm_task_dueDate');
 
         return $this;
     }
@@ -71,13 +74,17 @@ class Task extends AbstractPageEntity
     public function setDueDate($dueDate)
     {
         $this->dueDate->clear();
-        $this->dueDate->value($dueDate);
+        $this->dueTime->clear();
+        if (preg_match('/^(.+)\s(\d{2}\:\d{2}\s\w{2})$/', $dueDate, $dueDate)) {
+            $this->dueDate->value($dueDate[1]);
+            $this->dueTime->value($dueDate[2]);
+        }
         return $this;
     }
 
     public function getDueDate()
     {
-        return $this->dueDate->value();
+        return $this->dueDate->value() . ' ' . $this->dueTime->value();
     }
 
     public function delete()
