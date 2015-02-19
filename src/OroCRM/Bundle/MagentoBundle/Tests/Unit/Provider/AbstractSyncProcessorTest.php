6a4956bea08a02224f328e99b73322c375e4eea4
Dmitry Khrysev <x86demon@gmail.com>
2015-02-19 13:24:11 +0200

    CRM-2552: Add InitialSyncProcessor - tests

---
+++ src/OroCRM/Bundle/MagentoBundle/Tests/Unit/Provider/AbstractSyncProcessorTest.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace OroCRM\Bundle\MagentoBundle\Tests\Unit\Provider;
+
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+use Oro\Bundle\ImportExportBundle\Job\JobResult;
+use Oro\Bundle\IntegrationBundle\Provider\AbstractSyncProcessor;
+use Oro\Bundle\IntegrationBundle\Tests\Unit\Fixture\TestConnector;
+use Oro\Bundle\IntegrationBundle\Tests\Unit\Fixture\TestContext;
+use Oro\Bundle\ImportExportBundle\Processor\ProcessorRegistry;
+use Oro\Bundle\DataGridBundle\Common\Object;
+use OroCRM\Bundle\MagentoBundle\Provider\AbstractInitialProcessor;
+
+abstract class AbstractSyncProcessorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var AbstractSyncProcessor
+     */
+    protected $processor;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $registry;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $processorRegistry;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $jobExecutor;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $typesRegistry;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $eventDispatcher;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $logger;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $em;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $repository;
+
+    protected function setUp()
+    {
+        $this->registry = $this->getMockForAbstractClass('Doctrine\Common\Persistence\ManagerRegistry');
+        $this->processorRegistry = $this->getMockBuilder('Oro\Bundle\ImportExportBundle\Processor\ProcessorRegistry')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->jobExecutor = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\ImportExport\Job\Executor')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->typesRegistry = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\Manager\TypesRegistry')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->eventDispatcher = $this->getMockBuilder('Symfony\Component\EventDispatcher\EventDispatcherInterface')
+            ->getMockForAbstractClass();
+        $this->logger = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\Logger\LoggerStrategy')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->repository = $this
+            ->getMockBuilder('Oro\Bundle\IntegrationBundle\Entity\Repository\ChannelRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->registry->expects($this->any())
+            ->method('getManager')
+            ->will($this->returnValue($this->em));
+        $this->registry->expects($this->any())
+            ->method('getRepository')
+            ->will($this->returnValue($this->repository));
+    }
+
+    protected function tearDown()
+    {
+        unset(
+            $this->em,
+            $this->repository,
+            $this->registry,
+            $this->processorRegistry,
+            $this->jobExecutor,
+            $this->typesRegistry,
+            $this->eventDispatcher,
+            $this->logger,
+            $this->processor
+        );
+    }
+
+    /**
+     * @param \PHPUnit_Framework_MockObject_MockObject $integration
+     * @param array $settings
+     * @return \Oro\Bundle\DataGridBundle\Common\Object
+     */
+    public function assertIntegrationSettingsCall($integration, array $settings = [])
+    {
+        $syncSettings = Object::create($settings);
+        $integration->expects($this->any())
+            ->method('getSynchronizationSettings')
+            ->will($this->returnValue($syncSettings));
+
+        return $syncSettings;
+    }
+
+    public function assertProcessCalls()
+    {
+        $this->logger->expects($this->never())
+            ->method('critical');
+
+        $this->processorRegistry->expects($this->once())
+            ->method('getProcessorAliasesByEntity')
+            ->will($this->returnValue([]));
+    }
+
+    /**
+     * @param array $expectedConfig
+     */
+    public function assertExecuteJob(array $expectedConfig = null)
+    {
+        $jobResult = new JobResult();
+        $jobResult->setContext(new TestContext());
+        $jobResult->setSuccessful(true);
+
+        if ($expectedConfig) {
+            $this->jobExecutor->expects($this->once())
+                ->method('executeJob')
+                ->with(
+                    ProcessorRegistry::TYPE_IMPORT,
+                    'test job',
+                    [ProcessorRegistry::TYPE_IMPORT => $expectedConfig]
+                )
+                ->will($this->returnValue($jobResult));
+        } else {
+            $this->jobExecutor->expects($this->once())
+                ->method('executeJob')
+                ->with(
+                    ProcessorRegistry::TYPE_IMPORT,
+                    'test job',
+                    $this->isType('array')
+                )
+                ->will($this->returnValue($jobResult));
+        }
+    }
+
+    /**
+     * @param array $connectors
+     * @param array $settings
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getIntegration(array $connectors = [], array $settings = [])
+    {
+        $integration = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\Entity\Channel')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $integration
+            ->expects($this->once())
+            ->method('getConnectors')
+            ->will($this->returnValue($connectors));
+
+        $integration
+            ->expects($this->any())
+            ->method('getId')
+            ->will($this->returnValue('testChannel'));
+
+        $integration
+            ->expects($this->atLeastOnce())
+            ->method('getType')
+            ->will($this->returnValue('testChannelType'));
+
+        $settingsBag = new ParameterBag($settings);
+        $transport = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\Entity\Transport')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        $transport->expects($this->any())
+            ->method('getSettingsBag')
+            ->will($this->returnValue($settingsBag));
+        $integration
+            ->expects($this->any())
+            ->method('getTransport')
+            ->will($this->returnValue($transport));
+
+        $integration
+            ->expects($this->any())
+            ->method('isEnabled')
+            ->will($this->returnValue(true));
+
+        $realConnector = new TestConnector();
+        $this->typesRegistry
+            ->expects($this->any())
+            ->method('getConnectorType')
+            ->will($this->returnValue($realConnector));
+
+        return $integration;
+    }
+}
--- src/OroCRM/Bundle/MagentoBundle/Tests/Unit/Provider/InitialSyncProcessorTest.php
+++ src/OroCRM/Bundle/MagentoBundle/Tests/Unit/Provider/InitialSyncProcessorTest.php
@@ -2,94 +2,14 @@
 
 namespace OroCRM\Bundle\MagentoBundle\Tests\Unit\Provider;
 
-use Symfony\Component\HttpFoundation\ParameterBag;
-
-use Oro\Bundle\DataGridBundle\Common\Object;
-use Oro\Bundle\ImportExportBundle\Job\JobResult;
-use Oro\Bundle\IntegrationBundle\Tests\Unit\Fixture\TestConnector;
-use Oro\Bundle\IntegrationBundle\Tests\Unit\Fixture\TestContext;
 use OroCRM\Bundle\MagentoBundle\Provider\AbstractInitialProcessor;
 use OroCRM\Bundle\MagentoBundle\Provider\InitialSyncProcessor;
-use Oro\Bundle\ImportExportBundle\Processor\ProcessorRegistry;
 
-class InitialSyncProcessorTest extends \PHPUnit_Framework_TestCase
+class InitialSyncProcessorTest extends AbstractSyncProcessorTest
 {
-    /**
-     * @var InitialSyncProcessor
-     */
-    protected $processor;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $registry;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $processorRegistry;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $jobExecutor;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $typesRegistry;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $eventDispatcher;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $logger;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $em;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $repository;
-
     protected function setUp()
     {
-        $this->registry = $this->getMockForAbstractClass('Doctrine\Common\Persistence\ManagerRegistry');
-        $this->processorRegistry = $this->getMockBuilder('Oro\Bundle\ImportExportBundle\Processor\ProcessorRegistry')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->jobExecutor = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\ImportExport\Job\Executor')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->typesRegistry = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\Manager\TypesRegistry')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->eventDispatcher = $this->getMockBuilder('Symfony\Component\EventDispatcher\EventDispatcherInterface')
-            ->getMockForAbstractClass();
-        $this->logger = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\Logger\LoggerStrategy')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $this->em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->repository = $this
-            ->getMockBuilder('Oro\Bundle\IntegrationBundle\Entity\Repository\ChannelRepository')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->registry->expects($this->any())
-            ->method('getManager')
-            ->will($this->returnValue($this->em));
-        $this->registry->expects($this->any())
-            ->method('getRepository')
-            ->will($this->returnValue($this->repository));
+        parent::setUp();
 
         $this->processor = new InitialSyncProcessor(
             $this->registry,
@@ -101,37 +21,15 @@ class InitialSyncProcessorTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-    protected function tearDown()
-    {
-        unset(
-            $this->em,
-            $this->repository,
-            $this->registry,
-            $this->processorRegistry,
-            $this->jobExecutor,
-            $this->typesRegistry,
-            $this->eventDispatcher,
-            $this->logger,
-            $this->processor
-        );
-    }
-
     public function testProcess()
     {
-        $this->logger->expects($this->never())
-            ->method('critical');
-
-        $this->processorRegistry->expects($this->once())
-            ->method('getProcessorAliasesByEntity')
-            ->will($this->returnValue([]));
-
         $connector  = 'testConnector';
         $connectors = [$connector];
         $syncStartDate = new \DateTime('2000-01-01 00:00:00', new \DateTimeZone('UTC'));
         $syncedTo = new \DateTime('2011-01-02 12:13:14', new \DateTimeZone('UTC'));
         $initialStartDate = new \DateTime('2011-01-03 12:13:14', new \DateTimeZone('UTC'));
 
-        $integration = $this->getIntegration($syncStartDate, $connectors);
+        $integration = $this->getIntegration($connectors, ['start_sync_date' => $syncStartDate]);
 
         $status = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\Entity\Status')
             ->disableOriginalConstructor()
@@ -145,148 +43,66 @@ class InitialSyncProcessorTest extends \PHPUnit_Framework_TestCase
                     ]
                 )
             );
-        $this->repository->expects($this->atLeastOnce())
-            ->method('getLastStatusForConnector')
-            ->with($integration, $connector)
-            ->will($this->returnValue($status));
 
-        $syncSettings = Object::create([
+        $this->assertConnectorStatusCall($integration, $connector, $status);
+        $settings = [
             AbstractInitialProcessor::INITIAL_SYNC_START_DATE => $initialStartDate->format(\DateTime::ISO8601)
-        ]);
-        $integration->expects($this->any())
-            ->method('getSynchronizationSettings')
-            ->will($this->returnValue($syncSettings));
-
-        $jobResult = new JobResult();
-        $jobResult->setContext(new TestContext());
-        $jobResult->setSuccessful(true);
-        $this->jobExecutor->expects($this->once())
-            ->method('executeJob')
-            ->with(
-                ProcessorRegistry::TYPE_IMPORT,
-                'test job',
-                [
-                    ProcessorRegistry::TYPE_IMPORT => [
-                        'processorAlias' => false,
-                        'entityName'     => 'testEntity',
-                        'channel'        => 'testChannel',
-                        'channelType'    => 'testChannelType',
-                        AbstractInitialProcessor::INITIAL_SYNCED_TO => $syncedTo,
-                        'start_sync_date' => $syncStartDate
-                    ]
-                ]
-            )
-            ->will($this->returnValue($jobResult));
-
-
+        ];
+        $this->assertIntegrationSettingsCall($integration, $settings);
+        $this->assertProcessCalls();
+        $this->assertExecuteJob(
+            [
+                'processorAlias' => false,
+                'entityName'     => 'testEntity',
+                'channel'        => 'testChannel',
+                'channelType'    => 'testChannelType',
+                AbstractInitialProcessor::INITIAL_SYNCED_TO => $syncedTo,
+                'start_sync_date' => $syncStartDate
+            ]
+        );
 
         $this->processor->process($integration);
     }
 
     public function testProcessFirst()
     {
-        $this->logger->expects($this->never())
-            ->method('critical');
-
-        $this->processorRegistry->expects($this->once())
-            ->method('getProcessorAliasesByEntity')
-            ->will($this->returnValue([]));
-
         $connector  = 'testConnector';
         $connectors = [$connector];
         $syncStartDate = new \DateTime('2000-01-01 00:00:00', new \DateTimeZone('UTC'));
         $initialStartDate = new \DateTime('2011-01-03 12:13:14', new \DateTimeZone('UTC'));
 
-        $integration = $this->getIntegration($syncStartDate, $connectors);
+        $integration = $this->getIntegration($connectors, ['start_sync_date' => $syncStartDate]);
 
-        $this->repository->expects($this->atLeastOnce())
-            ->method('getLastStatusForConnector')
-            ->with($integration, $connector)
-            ->will($this->returnValue(null));
-
-        $syncSettings = Object::create([
+        $this->assertConnectorStatusCall($integration, $connector);
+        $settings = [
             AbstractInitialProcessor::INITIAL_SYNC_START_DATE => $initialStartDate->format(\DateTime::ISO8601)
-        ]);
-        $integration->expects($this->any())
-            ->method('getSynchronizationSettings')
-            ->will($this->returnValue($syncSettings));
-
-        $jobResult = new JobResult();
-        $jobResult->setContext(new TestContext());
-        $jobResult->setSuccessful(true);
-        $this->jobExecutor->expects($this->once())
-            ->method('executeJob')
-            ->with(
-                ProcessorRegistry::TYPE_IMPORT,
-                'test job',
-                [
-                    ProcessorRegistry::TYPE_IMPORT => [
-                        'processorAlias' => false,
-                        'entityName'     => 'testEntity',
-                        'channel'        => 'testChannel',
-                        'channelType'    => 'testChannelType',
-                        AbstractInitialProcessor::INITIAL_SYNCED_TO => $initialStartDate,
-                        'start_sync_date' => $syncStartDate
-                    ]
-                ]
-            )
-            ->will($this->returnValue($jobResult));
-
-
+        ];
+        $this->assertIntegrationSettingsCall($integration, $settings);
+        $this->assertProcessCalls();
+        $this->assertExecuteJob(
+            [
+                'processorAlias' => false,
+                'entityName'     => 'testEntity',
+                'channel'        => 'testChannel',
+                'channelType'    => 'testChannelType',
+                AbstractInitialProcessor::INITIAL_SYNCED_TO => $initialStartDate,
+                'start_sync_date' => $syncStartDate
+            ]
+        );
 
         $this->processor->process($integration);
     }
 
     /**
-     * @param \DateTime $startSyncDate
-     * @param array $connectors
-     * @return \PHPUnit_Framework_MockObject_MockObject
+     * @param \PHPUnit_Framework_MockObject_MockObject $integration
+     * @param string $connector
+     * @param null|object $status
      */
-    protected function getIntegration(\DateTime $startSyncDate, array $connectors = [])
+    protected function assertConnectorStatusCall($integration, $connector, $status = null)
     {
-        $integration = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\Entity\Channel')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $integration
-            ->expects($this->once())
-            ->method('getConnectors')
-            ->will($this->returnValue($connectors));
-
-        $integration
-            ->expects($this->any())
-            ->method('getId')
-            ->will($this->returnValue('testChannel'));
-
-        $integration
-            ->expects($this->atLeastOnce())
-            ->method('getType')
-            ->will($this->returnValue('testChannelType'));
-
-        $settingsBag = new ParameterBag();
-        $settingsBag->set('start_sync_date', $startSyncDate);
-        $transport = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\Entity\Transport')
-            ->disableOriginalConstructor()
-            ->getMockForAbstractClass();
-        $transport->expects($this->any())
-            ->method('getSettingsBag')
-            ->will($this->returnValue($settingsBag));
-        $integration
-            ->expects($this->any())
-            ->method('getTransport')
-            ->will($this->returnValue($transport));
-
-        $integration
-            ->expects($this->any())
-            ->method('isEnabled')
-            ->will($this->returnValue(true));
-
-        $realConnector = new TestConnector();
-        $this->typesRegistry
-            ->expects($this->any())
-            ->method('getConnectorType')
-            ->will($this->returnValue($realConnector));
-
-        return $integration;
+        $this->repository->expects($this->atLeastOnce())
+            ->method('getLastStatusForConnector')
+            ->with($integration, $connector)
+            ->will($this->returnValue($status));
     }
 }
