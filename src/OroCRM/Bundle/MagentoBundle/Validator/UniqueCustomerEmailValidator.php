a4c82a5651d0c82709a97f8d6609b3d8e9b73e2f
Dmitry Khrysev <x86demon@gmail.com>
2015-03-13 20:20:40 +0200

    CRM-2726: Add process for export job once customer was created or updated - email valuidation

--- src/OroCRM/Bundle/MagentoBundle/Validator/UniqueCustomerEmailValidator.php
+++ src/OroCRM/Bundle/MagentoBundle/Validator/UniqueCustomerEmailValidator.php
@@ -7,6 +7,8 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 use Oro\Bundle\IntegrationBundle\Provider\TransportInterface;
 use OroCRM\Bundle\MagentoBundle\Entity\Customer;
+use OroCRM\Bundle\MagentoBundle\Provider\BatchFilterBag;
+use OroCRM\Bundle\MagentoBundle\Provider\Transport\SoapTransport;
 use OroCRM\Bundle\MagentoBundle\Provider\Transport\MagentoTransportInterface;
 use OroCRM\Bundle\MagentoBundle\Validator\Constraints\UniqueCustomerEmailConstraint;
 
@@ -31,14 +33,65 @@ class UniqueCustomerEmailValidator extends ConstraintValidator
      */
     public function validate($value, Constraint $constraint)
     {
-        if ($value instanceof Customer && !$value->getOriginId()) {
-            $this->transport->init($value->getChannel()->getTransport());
+        if ($value instanceof Customer) {
+            $customers = $this->getRemoteCustomers($value);
 
-            // TODO Filter customers by email, store and website. Remove current user
-            $customers = $this->transport->getCustomers($value->getEmail());
+            $customers = array_filter(
+                $customers,
+                function ($customerData) use ($value) {
+                    if (is_object($customerData)) {
+                        $customerData = (array)$customerData;
+                    }
+                    if ($customerData
+                        && array_key_exists('customer_id', $customerData)
+                        && $customerData['customer_id'] == $value->getOriginId()
+                    ) {
+                        return false;
+                    }
+
+                    return true;
+                }
+            );
+
             if (count($customers) > 0) {
                 $this->context->addViolationAt('name', $constraint->message);
             }
         }
+    }
+
+    /**
+     * @param Customer $value
+     * @return array
+     */
+    protected function getRemoteCustomers($value)
+    {
+        $this->transport->init($value->getChannel()->getTransport());
+
+        $filter = new BatchFilterBag();
+        $filter->addComplexFilter(
+            'email',
+            [
+                'key' => 'email',
+                'value' => [
+                    'key' => 'eq',
+                    'value' => $value->getEmail()
+                ]
+            ]
+        );
+        $filter->addComplexFilter(
+            'store_id',
+            [
+                'key' => 'store_id',
+                'value' => [
+                    'key' => 'eq',
+                    'value' => $value->getStore()->getOriginId()
+                ]
+            ]
+        );
+
+        $filters = $filter->getAppliedFilters();
+        $customers = $this->transport->call(SoapTransport::ACTION_CUSTOMER_LIST, $filters);
+
+        return (array)$customers;
     }
 }
