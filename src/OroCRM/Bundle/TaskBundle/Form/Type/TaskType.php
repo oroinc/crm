eaf39682e33b211633658e90d21c2a9c4eda6500
Vova Soroka <vsoroka@magecore.com>
2015-03-16 20:21:16 +0200

    CRM-2836: Task > Create Task: Past Due Dates can be entered

--- src/OroCRM/Bundle/TaskBundle/Form/Type/TaskType.php
+++ src/OroCRM/Bundle/TaskBundle/Form/Type/TaskType.php
@@ -4,14 +4,15 @@ namespace OroCRM\Bundle\TaskBundle\Form\Type;
 
 use Doctrine\ORM\EntityRepository;
 
-use Oro\Bundle\FormBundle\Utils\FormUtils;
-use OroCRM\Bundle\TaskBundle\Entity\Task;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+
+use Oro\Bundle\FormBundle\Utils\FormUtils;
+use OroCRM\Bundle\TaskBundle\Entity\Task;
 
 class TaskType extends AbstractType
 {
