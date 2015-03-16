d2c07512e6780ace756d254d5f5a10ebc5171410
Vova Soroka <vsoroka@magecore.com>
2015-03-16 20:10:46 +0200

    CRM-2836: Task > Create Task: Past Due Dates can be entered

--- src/OroCRM/Bundle/TaskBundle/Form/Type/TaskType.php
+++ src/OroCRM/Bundle/TaskBundle/Form/Type/TaskType.php
@@ -4,9 +4,14 @@ namespace OroCRM\Bundle\TaskBundle\Form\Type;
 
 use Doctrine\ORM\EntityRepository;
 
+use Oro\Bundle\FormBundle\Utils\FormUtils;
+use OroCRM\Bundle\TaskBundle\Entity\Task;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class TaskType extends AbstractType
 {
@@ -37,7 +42,10 @@ class TaskType extends AbstractType
                 'oro_datetime',
                 [
                     'required' => false,
-                    'label' => 'orocrm.task.due_date.label'
+                    'label' => 'orocrm.task.due_date.label',
+                    'constraints' => [
+                        $this->getDueDateValidationConstraint(new \DateTime('now', new \DateTimeZone('UTC')))
+                    ]
                 ]
             )
             ->add(
@@ -60,6 +68,29 @@ class TaskType extends AbstractType
                     'label' => 'oro.reminder.entity_plural_label'
                 ]
             );
+        $builder->addEventListener(FormEvents::POST_SET_DATA, [$this, 'postSetData']);
+    }
+
+    /**
+     * Post set data handler
+     *
+     * @param FormEvent $event
+     */
+    public function postSetData(FormEvent $event)
+    {
+        /** @var Task $data */
+        $data = $event->getData();
+        if ($data && $data->getCreatedAt()) {
+            FormUtils::replaceField(
+                $event->getForm(),
+                'dueDate',
+                [
+                    'constraints' => [
+                        $this->getDueDateValidationConstraint($data->getCreatedAt())
+                    ]
+                ]
+            );
+        }
     }
 
     /**
@@ -82,5 +113,20 @@ class TaskType extends AbstractType
     public function getName()
     {
         return 'orocrm_task';
+    }
+
+    /**
+     * @param \DateTime $startDate
+     *
+     * @return Assert\GreaterThanOrEqual
+     */
+    protected function getDueDateValidationConstraint(\DateTime $startDate)
+    {
+        return new Assert\GreaterThanOrEqual(
+            [
+                'value'   => $startDate,
+                'message' => 'Due date must not be in the past'
+            ]
+        );
     }
 }
