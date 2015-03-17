d8af5b023578e1a6b9c2bcf80bd0ac6d6e9ab5b0
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2015-03-17 13:09:15 +0200

    CRM-2891: Make MagentoSoapTransport not Configurable - CR fix

--- src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_28/DropTransportConfigurable.php
+++ src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_28/DropTransportConfigurable.php
@@ -15,7 +15,6 @@ class DropTransportConfigurable implements Migration
      */
     public function up(Schema $schema, QueryBag $queries)
     {
-
         $className = 'OroCRM\Bundle\MagentoBundle\Entity\MagentoSoapTransport';
 
         $dropFieldsSql = 'DELETE FROM oro_entity_config_field'
