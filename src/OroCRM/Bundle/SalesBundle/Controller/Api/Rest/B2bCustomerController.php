44f7fd369b108d6cb25782ade22dc8cf3ba90c4a
Vova Soroka <vsoroka@magecore.com>
2015-03-20 04:49:11 +0200

    CRM-2765: Implement API for B2B Customer entity

--- src/OroCRM/Bundle/SalesBundle/Controller/Api/Rest/B2bCustomerController.php
+++ src/OroCRM/Bundle/SalesBundle/Controller/Api/Rest/B2bCustomerController.php
@@ -4,9 +4,12 @@ namespace OroCRM\Bundle\SalesBundle\Controller\Api\Rest;
 
 use FOS\RestBundle\Routing\ClassResourceInterface;
 use FOS\RestBundle\Controller\Annotations\NamePrefix;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
 
+use Oro\Bundle\AddressBundle\Utils\AddressApiUtils;
 use Oro\Bundle\SecurityBundle\Annotation\Acl;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 use Oro\Bundle\SoapBundle\Entity\Manager\ApiEntityManager;
 use Oro\Bundle\SoapBundle\Controller\Api\Rest\RestController;
 
@@ -21,12 +24,89 @@ use Symfony\Component\HttpFoundation\Response;
 class B2bCustomerController extends RestController implements ClassResourceInterface
 {
     /**
-     * REST DELETE
+     * Get B2B customers.
+     *
+     * @QueryParam(
+     *      name="page",
+     *      requirements="\d+",
+     *      nullable=true,
+     *      description="Page number, starting from 1. Defaults to 1."
+     * )
+     * @QueryParam(
+     *      name="limit",
+     *      requirements="\d+", nullable=true,
+     *      description="Number of items per page. defaults to 10."
+     * )
+     * @ApiDoc(
+     *      description="Get B2B customers",
+     *      resource=true
+     * )
+     * @AclAncestor("orocrm_sales_b2bcustomer_view")
+     * @return Response
+     */
+    public function cgetAction()
+    {
+        $page = (int) $this->getRequest()->get('page', 1);
+        $limit = (int) $this->getRequest()->get('limit', self::ITEMS_PER_PAGE);
+
+        return $this->handleGetListRequest($page, $limit);
+    }
+
+    /**
+     * Get B2B customer.
+     *
+     * @param string $id
+     *
+     * @ApiDoc(
+     *      description="Get B2B customer",
+     *      resource=true
+     * )
+     * @AclAncestor("orocrm_sales_b2bcustomer_view")
+     * @return Response
+     */
+    public function getAction($id)
+    {
+        return $this->handleGetRequest($id);
+    }
+
+    /**
+     * Update B2B customer.
      *
      * @param int $id
      *
      * @ApiDoc(
-     *      description="Delete B2bCustomer",
+     *      description="Update B2B customer",
+     *      resource=true
+     * )
+     * @AclAncestor("orocrm_sales_b2bcustomer_update")
+     * @return Response
+     */
+    public function putAction($id)
+    {
+        return $this->handleUpdateRequest($id);
+    }
+
+    /**
+     * Create new B2B customer.
+     *
+     * @ApiDoc(
+     *      description="Create new B2B customer",
+     *      resource=true
+     * )
+     * @AclAncestor("orocrm_sales_b2bcustomer_create")
+     */
+    public function postAction()
+    {
+        return $this->handleCreateRequest();
+    }
+
+    /**
+     * Delete B2B customer.
+     *
+     * @param int $id
+     *
+     * @ApiDoc(
+     *      description="Delete B2B customer",
      *      resource=true
      * )
      * @Acl(
@@ -57,7 +137,7 @@ class B2bCustomerController extends RestController implements ClassResourceInter
      */
     public function getForm()
     {
-        throw new \BadMethodCallException('FormInterface is not available.');
+        return $this->get('orocrm_sales.b2bcustomer.form.api');
     }
 
     /**
@@ -65,6 +145,37 @@ class B2bCustomerController extends RestController implements ClassResourceInter
      */
     public function getFormHandler()
     {
-        throw new \BadMethodCallException('FormHandler is not available.');
+        return $this->get('orocrm_sales.b2bcustomer.form.handler.api');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function processForm($entity)
+    {
+        $this->fixRequest();
+        return parent::processForm($entity);
+    }
+
+    /**
+     */
+    protected function fixRequest()
+    {
+        $formAlias = 'b2bcustomer';
+        $customerData = $this->getRequest()->request->get($formAlias);
+
+        // @todo: just a temporary workaround until new API is implemented
+        // - convert country name to country code (as result we accept both the code and the name)
+        //   also it will be good to accept ISO3 code in future, need to be discussed with product owners
+        // - convert region name to region code (as result we accept the combined code, code and name)
+        // - move region name to region_text field for unknown region
+        if (array_key_exists('shippingAddress', $customerData)) {
+            AddressApiUtils::fixAddress($customerData['shippingAddress'], $this->get('doctrine.orm.entity_manager'));
+            $this->getRequest()->request->set($formAlias, $customerData);
+        }
+        if (array_key_exists('billingAddress', $customerData)) {
+            AddressApiUtils::fixAddress($customerData['billingAddress'], $this->get('doctrine.orm.entity_manager'));
+            $this->getRequest()->request->set($formAlias, $customerData);
+        }
     }
 }
