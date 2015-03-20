401613893fb0e7a499569df796feaf62923be661
Dmitry Khrysev <x86demon@gmail.com>
2015-03-20 20:38:57 +0200

    CRM-2411: Magento customer CU - export fixes

--- src/OroCRM/Bundle/MagentoBundle/Form/Handler/CustomerHandler.php
+++ src/OroCRM/Bundle/MagentoBundle/Form/Handler/CustomerHandler.php
@@ -5,11 +5,28 @@ namespace OroCRM\Bundle\MagentoBundle\Form\Handler;
 use Symfony\Component\Form\FormInterface;
 
 use Oro\Bundle\FormBundle\Model\UpdateHandler;
+use OroCRM\Bundle\MagentoBundle\Entity\Address;
 use OroCRM\Bundle\MagentoBundle\Entity\Customer;
-use OroCRM\Bundle\MagentoBundle\Service\StateManager;
+use OroCRM\Bundle\MagentoBundle\Service\CustomerStateHandler;
 
 class CustomerHandler extends UpdateHandler
 {
+    /**
+     * @var CustomerStateHandler
+     */
+    protected $stateHandler;
+
+    /**
+     * @param CustomerStateHandler $stateHandler
+     * @return CustomerHandler
+     */
+    public function setStateHandler($stateHandler)
+    {
+        $this->stateHandler = $stateHandler;
+
+        return $this;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -24,13 +41,30 @@ class CustomerHandler extends UpdateHandler
             $form->submit($this->request);
 
             if ($form->isValid()) {
-                $this->markForSync($entity);
+                $addressesToSync = [];
+                if ($entity->getId()) {
+                    $this->stateHandler->markCustomerForSync($entity);
+
+                    if (!$entity->getAddresses()->isEmpty()) {
+                        foreach ($entity->getAddresses() as $address) {
+                            if (!$address->getOriginId()) {
+                                $addressesToSync[] = $address;
+                            } else {
+                                $this->stateHandler->markAddressForSync($address);
+                            }
+                        }
+                    }
+                }
                 $this->saveEntity($entity);
 
                 // Process trigger listen for update, because create will trigger export during import
                 // This will schedule new entity for export
                 if (!$entity->getOriginId()) {
-                    $this->scheduleSyncToMagento($entity);
+                    $this->scheduleCustomerSyncToMagento($entity);
+                }
+
+                foreach ($addressesToSync as $address) {
+                    $this->scheduleAddressSyncToMagento($address);
                 }
 
                 return true;
@@ -41,9 +75,9 @@ class CustomerHandler extends UpdateHandler
     }
 
     /**
-     * @param Customer $entity
+     * @param object $entity
      */
-    protected function saveEntity(Customer $entity)
+    protected function saveEntity($entity)
     {
         $manager = $this->doctrineHelper->getEntityManager($entity);
         $manager->persist($entity);
@@ -53,20 +87,18 @@ class CustomerHandler extends UpdateHandler
     /**
      * @param Customer $entity
      */
-    protected function scheduleSyncToMagento($entity)
+    protected function scheduleCustomerSyncToMagento(Customer $entity)
     {
-        $this->markForSync($entity);
+        $this->stateHandler->markCustomerForSync($entity);
         $this->saveEntity($entity);
     }
 
     /**
-     * @param Customer $entity
+     * @param Address $entity
      */
-    protected function markForSync(Customer $entity)
+    protected function scheduleAddressSyncToMagento(Address $entity)
     {
-        $stateManager = new StateManager();
-        if (!$stateManager->isInState($entity->getSyncState(), Customer::MAGENTO_REMOVED)) {
-            $stateManager->addState($entity, 'syncState', Customer::SYNC_TO_MAGENTO);
-        }
+        $this->stateHandler->markAddressForSync($entity);
+        $this->saveEntity($entity);
     }
 }
