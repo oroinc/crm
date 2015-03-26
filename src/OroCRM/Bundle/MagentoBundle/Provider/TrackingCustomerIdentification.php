273c3f7ef4de93a15df05ad9c2480321ae645337
yurio <yurio@ua.fm>
2015-03-26 16:21:30 +0200

    CRM-2408: Fix cs

--- src/OroCRM/Bundle/MagentoBundle/Provider/TrackingCustomerIdentification.php
+++ src/OroCRM/Bundle/MagentoBundle/Provider/TrackingCustomerIdentification.php
@@ -17,12 +17,12 @@ use OroCRM\Bundle\ChannelBundle\Provider\SettingsProvider;
 class TrackingCustomerIdentification implements TrackingEventIdentifierInterface
 {
     const EVENT_REGISTRATION_FINISHED = 'registration';
-    const EVENT_CART_ITEM_ADDED = 'cart item added';
-    const EVENT_CHECKOUT_STARTED = 'user entered checkout';
-    const EVENT_ORDER_PLACE_SUCCESS = 'order successfully placed';
-    const EVENT_ORDER_PLACED = 'order placed';
-    const EVENT_CUSTOMER_LOGIN = 'user logged in';
-    const EVENT_CUSTOMER_LOGOUT = 'user logged out';
+    const EVENT_CART_ITEM_ADDED       = 'cart item added';
+    const EVENT_CHECKOUT_STARTED      = 'user entered checkout';
+    const EVENT_ORDER_PLACE_SUCCESS   = 'order successfully placed';
+    const EVENT_ORDER_PLACED          = 'order placed';
+    const EVENT_CUSTOMER_LOGIN        = 'user logged in';
+    const EVENT_CUSTOMER_LOGOUT       = 'user logged out';
 
     /** @var ObjectManager */
     protected $em;
