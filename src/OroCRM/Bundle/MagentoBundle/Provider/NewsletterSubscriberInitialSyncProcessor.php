8f1aa2dde87a282c39638ebcd942f74dc137c93e
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2015-03-31 19:45:28 +0300

    CRM-3062: Magento Newsletter Subscriber not created in channel with the same configuration - filter by integration

--- src/OroCRM/Bundle/MagentoBundle/Provider/NewsletterSubscriberInitialSyncProcessor.php
+++ src/OroCRM/Bundle/MagentoBundle/Provider/NewsletterSubscriberInitialSyncProcessor.php
@@ -17,7 +17,7 @@ class NewsletterSubscriberInitialSyncProcessor extends AbstractInitialProcessor
      */
     public function process(Integration $integration, $connector = null, array $parameters = [])
     {
-        $parameters['initial_id'] = $this->getSyncedToId();
+        $parameters['initial_id'] = $this->getSyncedToId($integration);
 
         return parent::process($integration, $connector, $parameters);
     }
@@ -34,9 +34,11 @@ class NewsletterSubscriberInitialSyncProcessor extends AbstractInitialProcessor
     }
 
     /**
+     * @param Integration $integration
+     *
      * @return int
      */
-    protected function getSyncedToId()
+    protected function getSyncedToId(Integration $integration)
     {
         if (!$this->subscriberClassName) {
             throw new \InvalidArgumentException('NewsletterSubscriber class name is missing');
@@ -45,7 +47,11 @@ class NewsletterSubscriberInitialSyncProcessor extends AbstractInitialProcessor
         /** @var EntityRepository $repository */
         $repository = $this->doctrineRegistry->getRepository($this->subscriberClassName);
         // API return newsletter subscribers sorted by DESC, this means that latest subscriber will have minimal id
-        $qb = $repository->createQueryBuilder('e')->select('MIN(e.id)');
+        $qb = $repository->createQueryBuilder('e');
+        $qb
+            ->select('MIN(e.id)')
+            ->where($qb->expr()->eq('e.channel', ':channel'))
+            ->setParameter('channel', $integration);
 
         return $qb->getQuery()->getSingleScalarResult();
     }
