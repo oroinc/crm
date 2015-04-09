a680c690167d75d614fdf99cb4a8bc33e93c3e95
Vova Soroka <vsoroka@magecore.com>
2015-04-09 06:22:19 +0300

    CRM-2968: Investigate magento Sync on Whitecloud
    Conflicts:
    	UPGRADE-1.7.md

---
+++ src/OroCRM/Bundle/SalesBundle/Command/RecalculateLifetimeCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace OroCRM\Bundle\SalesBundle\Command;
+
+use Doctrine\ORM\EntityManager;
+
+use OroCRM\Bundle\ChannelBundle\Command\RecalculateLifetimeCommand as AbstractRecalculateLifetimeCommand;
+use OroCRM\Bundle\SalesBundle\Entity\B2bCustomer;
+use OroCRM\Bundle\SalesBundle\Entity\Repository\B2bCustomerRepository;
+
+class RecalculateLifetimeCommand extends AbstractRecalculateLifetimeCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setName('oro:b2b:lifetime:recalculate')
+            ->setDescription('Perform re-calculation of lifetime values for B2B channel.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getChannelType()
+    {
+        return 'b2b';
+    }
+
+    /**
+     * @param EntityManager $em
+     * @param B2bCustomer   $customer
+     *
+     * @return float
+     */
+    protected function calculateCustomerLifetime(EntityManager $em, $customer)
+    {
+        /** @var B2bCustomerRepository $customerRepo */
+        $customerRepo  = $em->getRepository('OroCRMSalesBundle:B2bCustomer');
+        $lifetimeValue = $customerRepo->calculateLifetimeValue($customer);
+
+        return $lifetimeValue;
+    }
+}
