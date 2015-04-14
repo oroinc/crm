80ff4eab8079969b10a4e198310a81ae52174756
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2015-04-14 20:20:51 +0300

    OEE-592: Magento Customers from different Organizations are matched to one Account - fix organization condition

--- src/OroCRM/Bundle/MagentoBundle/Tests/Unit/Service/AutomaticDiscoveryTest.php
+++ src/OroCRM/Bundle/MagentoBundle/Tests/Unit/Service/AutomaticDiscoveryTest.php
@@ -3,8 +3,10 @@
 namespace OroCRM\Bundle\MagentoBundle\Tests\Unit\Service;
 
 use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\SecurityBundle\Owner\Metadata\OwnershipMetadataProvider;
 
 use OroCRM\Bundle\MagentoBundle\DependencyInjection\Configuration;
+use OroCRM\Bundle\MagentoBundle\Entity\Customer;
 use OroCRM\Bundle\MagentoBundle\Service\AutomaticDiscovery;
 use OroCRM\Bundle\MagentoBundle\Service\AutomaticDiscovery\DiscoveryStrategyInterface;
 
@@ -21,6 +23,11 @@ class AutomaticDiscoveryTest extends \PHPUnit_Framework_TestCase
     protected $defaultStrategy;
 
     /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|OwnershipMetadataProvider
+     */
+    protected $metadataProvider;
+
+    /**
      * @var string
      */
     protected $entityClass;
@@ -35,21 +42,30 @@ class AutomaticDiscoveryTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
         $this->defaultStrategy = $this
-            ->getMockBuilder('OroCRM\Bundle\MagentoBundle\Service\AutomaticDiscovery\DiscoveryStrategyInterface')
+            ->getMock('OroCRM\Bundle\MagentoBundle\Service\AutomaticDiscovery\DiscoveryStrategyInterface');
+        $this->metadataProvider = $this
+            ->getMockBuilder('Oro\Bundle\SecurityBundle\Owner\Metadata\OwnershipMetadataProvider')
+            ->disableOriginalConstructor()
             ->getMock();
-        $this->entityClass = '\stdClass';
+        $this->entityClass = 'OroCRM\Bundle\MagentoBundle\Entity\Customer';
 
-        return new AutomaticDiscovery($this->doctrineHelper, $this->defaultStrategy, $this->entityClass, $config);
+        return new AutomaticDiscovery(
+            $this->doctrineHelper,
+            $this->defaultStrategy,
+            $this->metadataProvider,
+            $this->entityClass,
+            $config
+        );
     }
 
     public function testDiscoverSimilarNoConfig()
     {
-        $this->assertNull($this->getDiscovery([])->discoverSimilar(new \stdClass()));
+        $this->assertNull($this->getDiscovery([])->discoverSimilar(new Customer()));
     }
 
     public function testDiscoverSimilarMatchAny()
     {
-        $entity = new \stdClass();
+        $entity = new Customer();
         $id = null;
         $config = [
             Configuration::DISCOVERY_NODE => [
@@ -79,7 +95,7 @@ class AutomaticDiscoveryTest extends \PHPUnit_Framework_TestCase
 
     public function testDiscoverSimilarMatchLatest()
     {
-        $entity = new \stdClass();
+        $entity = new Customer();
         $id = null;
         $config = [
             Configuration::DISCOVERY_NODE => [
@@ -112,7 +128,7 @@ class AutomaticDiscoveryTest extends \PHPUnit_Framework_TestCase
 
     public function testDiscoverSimilarMatchExisting()
     {
-        $entity = new \stdClass();
+        $entity = new Customer();
         $id = 1;
         $config = [
             Configuration::DISCOVERY_NODE => [
@@ -160,6 +176,13 @@ class AutomaticDiscoveryTest extends \PHPUnit_Framework_TestCase
      */
     protected function assertDiscoveryCalls($qb, $entity, $id, $config, AutomaticDiscovery $service)
     {
+        $metadata = $this->getMockBuilder('Oro\Bundle\SecurityBundle\Owner\Metadata\OwnershipMetadata')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->metadataProvider->expects($this->any())->method('getMetadata')->willReturn($metadata);
+        $metadata->expects($this->any())->method('getOrganizationFieldName')->willReturn('organization');
+
         $query = $this->getMockBuilder('Doctrine\ORM\AbstractQuery')
             ->disableOriginalConstructor()
             ->setMethods(['getOneOrNullResult'])
@@ -203,8 +226,7 @@ class AutomaticDiscoveryTest extends \PHPUnit_Framework_TestCase
 
         /** @var \PHPUnit_Framework_MockObject_MockObject|DiscoveryStrategyInterface $customStrategy */
         $customStrategy = $this
-            ->getMockBuilder('OroCRM\Bundle\MagentoBundle\Service\AutomaticDiscovery\DiscoveryStrategyInterface')
-            ->getMock();
+            ->getMock('OroCRM\Bundle\MagentoBundle\Service\AutomaticDiscovery\DiscoveryStrategyInterface');
         $customStrategy->expects($this->once())
             ->method('apply')
             ->with($qb, AutomaticDiscovery::ROOT_ALIAS, 'test2', $config[Configuration::DISCOVERY_NODE], $entity);
