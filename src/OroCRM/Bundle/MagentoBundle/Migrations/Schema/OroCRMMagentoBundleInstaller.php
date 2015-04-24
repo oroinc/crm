e5e15d8b5528f1f2a8e3c78ae89094f5e78ca453
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2015-04-24 20:05:08 +0300

    CRM-3233: Magento Orders grid. First /Last name is not displayed in grid for guest orders - drop custom items and addresses stategy logic - add origin ids - add channel relation

--- src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/OroCRMMagentoBundleInstaller.php
+++ src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/OroCRMMagentoBundleInstaller.php
@@ -80,7 +80,7 @@ class OroCRMMagentoBundleInstaller implements
      */
     public function getMigrationVersion()
     {
-        return 'v1_31';
+        return 'v1_32';
     }
 
     /**
@@ -446,6 +446,7 @@ class OroCRMMagentoBundleInstaller implements
         $table->addColumn('createdAt', 'datetime', ['precision' => 0]);
         $table->addColumn('updatedAt', 'datetime', ['precision' => 0]);
         $table->addColumn('origin_id', 'integer', ['notnull' => false, 'precision' => 0, 'unsigned' => true]);
+        $table->addColumn('channel_id', 'integer', ['notnull' => false]);
         $table->addIndex(['cart_id'], 'IDX_A73DC8621AD5CDBF', []);
         $table->setPrimaryKey(['id']);
         $table->addIndex(['origin_id'], 'magecartitem_origin_idx', []);
@@ -529,6 +530,8 @@ class OroCRMMagentoBundleInstaller implements
         $table->addColumn('region_text', 'string', ['notnull' => false, 'length' => 255, 'precision' => 0]);
         $table->addColumn('first_name', 'string', ['notnull' => false, 'length' => 255, 'precision' => 0]);
         $table->addColumn('last_name', 'string', ['notnull' => false, 'length' => 255, 'precision' => 0]);
+        $table->addColumn('channel_id', 'integer', ['notnull' => false]);
+        $table->addColumn('origin_id', 'integer', ['notnull' => false, 'precision' => 0, 'unsigned' => true]);
         $table->addIndex(['owner_id'], 'IDX_E31C6DEC7E3C61F9', []);
         $table->addIndex(['country_code'], 'IDX_E31C6DECF026BB7C', []);
         $table->addIndex(['region_code'], 'IDX_E31C6DECAEB327AF', []);
@@ -789,6 +792,7 @@ class OroCRMMagentoBundleInstaller implements
         );
         $table->addColumn('row_total', 'money', ['notnull' => false, 'precision' => 0, 'comment' => '(DC2Type:money)']);
         $table->addColumn('origin_id', 'integer', ['notnull' => false, 'precision' => 0, 'unsigned' => true]);
+        $table->addColumn('channel_id', 'integer', ['notnull' => false]);
         $table->addIndex(['order_id'], 'IDX_3135EFF68D9F6D38', []);
         $table->setPrimaryKey(['id']);
     }
@@ -1165,6 +1169,12 @@ class OroCRMMagentoBundleInstaller implements
             ['region_code'],
             ['combined_code'],
             []
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_integration_channel'),
+            ['channel_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL']
         );
     }
 
