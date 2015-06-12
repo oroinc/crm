4d9164c39a20d64b14c3a66bbc1ba587f4eb78ce
Makar Sichevoy <sichevoy@gmail.com>
2015-06-12 09:28:43 +0300

    CRM-3211: Connect guest orders placed using the same email under a single account

--- src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_34/AddCustomerIndices.php
+++ src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_34/AddCustomerIndices.php
@@ -7,7 +7,7 @@ use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-class AddGuestCustomerSync implements Migration
+class AddCustomerIndices implements Migration
 {
     /**
      * {@inheritdoc}
