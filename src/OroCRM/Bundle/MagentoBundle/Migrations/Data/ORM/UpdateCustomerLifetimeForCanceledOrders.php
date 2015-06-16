aa2f03de1df1c6ccf8a712fbfc2303c03916662c
Alexander Kuzmenko <akuzmenko@magecore.com>
2015-06-16 15:15:05 +0300

    CRM-3276: Slow migration UpdateCustomerLifetimeForCanceledOrders

--- src/OroCRM/Bundle/MagentoBundle/Migrations/Data/ORM/UpdateCustomerLifetimeForCanceledOrders.php
+++ src/OroCRM/Bundle/MagentoBundle/Migrations/Data/ORM/UpdateCustomerLifetimeForCanceledOrders.php
@@ -2,64 +2,112 @@
 
 namespace OroCRM\Bundle\MagentoBundle\Migrations\Data\ORM;
 
-use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
 
 use Oro\Bundle\BatchBundle\ORM\Query\BufferedQueryResultIterator;
 use OroCRM\Bundle\MagentoBundle\Entity\Order;
-use OroCRM\Bundle\MagentoBundle\Entity\Repository\CustomerRepository;
+use OroCRM\Bundle\ChannelBundle\Migrations\Data\ORM\AbstractDefaultChannelDataFixture;
 
 /**
- * Recalculate lifetime value for customers who has canceled orders.
+ * Recalculate lifetime value for customers that have canceled orders.
  */
-class UpdateCustomerLifetimeForCanceledOrders extends AbstractFixture
+class UpdateCustomerLifetimeForCanceledOrders extends AbstractDefaultChannelDataFixture
 {
-    const BUFFER_SIZE = 25;
+    const BUFFER_SIZE = 10000;
 
-    /**
-     * {@inheritDoc}
-     */
+    /** @var EntityRepository */
+    protected $customerRepository;
+
     public function load(ObjectManager $manager)
     {
         /** @var EntityManager $manager */
-        /** @var CustomerRepository $customerRepo */
-        $customerRepo = $manager->getRepository('OroCRMMagentoBundle:Customer');
-        $queryBuilder = $manager->createQueryBuilder('c');
-        $queryBuilder->select('c')
-            ->from('OroCRMMagentoBundle:Customer', 'c')
-            ->join('OroCRMMagentoBundle:Order', 'o', 'WITH', 'o.customer = c')
-            ->where(
-                $queryBuilder->expr()->andX(
-                    $queryBuilder->expr()->eq('o.status', ':status')
-                )
+        $this->customerRepository = $manager->getRepository('OroCRMMagentoBundle:Customer');
+
+        // Calculate lifetime value for all customers
+        $queryBuilder = $this->customerRepository->createQueryBuilder('customer');
+        $queryBuilder
+            ->select(
+                'SUM(
+                    CASE WHEN customerOrder.subtotalAmount IS NOT NULL THEN customerOrder.subtotalAmount ELSE 0 END -
+                    CASE WHEN customerOrder.discountAmount IS NOT NULL THEN ABS(customerOrder.discountAmount) ELSE 0 END
+                ) AS lifetime',
+                'customer.id as customerId',
+                'customer.lifetime AS oldLifetime',
+                'IDENTITY(customer.dataChannel) as dataChannelId'
             )
-            ->setParameter('status', Order::STATUS_CANCELED)
-            ->groupBy('c');
+            ->leftJoin(
+                'customer.orders',
+                'customerOrder',
+                'WITH',
+                $queryBuilder->expr()->neq($queryBuilder->expr()->lower('customerOrder.status'), ':status')
+            )
+            ->groupBy('customer.account, customer.id')
+            ->orderBy('customer.account')
+            ->setParameter('status', Order::STATUS_CANCELED);
+
+        // Get lifetime value only for customers that have canceled orders
+        $this->addFilterByOrderStatus($queryBuilder, Order::STATUS_CANCELED);
 
         $iterator = new BufferedQueryResultIterator($queryBuilder);
         $iterator->setBufferSize(self::BUFFER_SIZE);
 
-        $processed = 0;
-        foreach ($iterator as $customer) {
-            $oldLifetime = (float)$customer->getLifetime();
-            $newLifetime = $customerRepo->calculateLifetimeValue($customer);
-            if ($newLifetime !== $oldLifetime) {
-                $customer = $manager->merge($customer);
-                $customer->setLifetime($newLifetime);
-                $processed++;
+        $channels = [];
+        foreach ($iterator as $row) {
+            if ($row['lifetime'] == $row['oldLifetime']) {
+                continue;
             }
-
-            if ($processed === self::BUFFER_SIZE) {
-                $manager->flush();
-                $manager->clear();
-                $processed = 0;
+            $customerId = $row['customerId'];
+            $lifetime = $row['lifetime'] ?: 0;
+            $this->updateCustomerLifetimeValue($customerId, $lifetime);
+            if (!isset($channels[$row['dataChannelId']])) {
+                $channels[$row['dataChannelId']] = $row['dataChannelId'];
             }
         }
-
-        if ($processed > 0) {
-            $manager->flush();
-            $manager->clear();
+        foreach ($channels as $channelId) {
+            $channel = $manager->getReference('OroCRMChannelBundle:Channel', $channelId);
+            $this->updateLifetimeForAccounts($channel);
         }
+    }
+
+    /**
+     * @param int    $customerId
+     * @param string $value
+     */
+    protected function updateCustomerLifetimeValue($customerId, $value)
+    {
+        $qb = $this->customerRepository
+            ->createQueryBuilder('c')
+            ->update('OroCRMMagentoBundle:Customer', 'c')
+            ->set('c.lifetime', $value)
+            ->where('c.id = :id')
+            ->setParameter('id', $customerId);
+
+        $qb->getQuery()->execute();
+    }
+
+    /*
+     * @param QueryBuilder $qb
+     * @param string       $orderStatus
+     */
+    protected function addFilterByOrderStatus(QueryBuilder $qb, $orderStatus)
+    {
+        $aliases = $qb->getRootAliases();
+        $subQueryBuilder = $this->customerRepository->createQueryBuilder('c');
+        $subQueryBuilder
+            ->select('IDENTITY(c.account)')
+            ->join(
+                'c.orders',
+                'o',
+                'WITH',
+                $subQueryBuilder->expr()->eq($subQueryBuilder->expr()->lower('o.status'), ':filteredOrderStatus')
+            );
+
+        $qb->andWhere(
+            $qb->expr()->in($aliases[0] . '.account', $subQueryBuilder->getDQL())
+        )
+            ->setParameter('filteredOrderStatus', $orderStatus);
     }
 }
