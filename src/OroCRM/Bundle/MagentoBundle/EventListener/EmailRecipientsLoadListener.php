37550371f242afa06adb2d96a3c82cc5498a70b2
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-06-24 11:55:58 +0200

    CRM-3533: Add autocomplete for to, cc, bcc fields with possibility to type new email address
    - add context autocomplete for accounts customers

---
+++ src/OroCRM/Bundle/MagentoBundle/EventListener/EmailRecipientsLoadListener.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace OroCRM\Bundle\MagentoBundle\EventListener;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityRepository;
+
+use Symfony\Component\Translation\TranslatorInterface;
+
+use Oro\Bundle\EmailBundle\Event\EmailRecipientsLoadEvent;
+use Oro\Bundle\EmailBundle\Provider\RelatedEmailsProvider;
+use OroCRM\Bundle\AccountBundle\Entity\Account;
+
+class EmailRecipientsLoadListener
+{
+    /** @var Registry */
+    protected $registry;
+
+    /** @var RelatedEmailsProvider */
+    protected $relatedEmailsProvider;
+
+    /** @var TranslatorInterface */
+    protected $translator;
+
+    /**
+     * @param Registry $registry
+     * @param RelatedEmailsProvider $relatedEmailsProvider
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(
+        Registry $registry,
+        RelatedEmailsProvider $relatedEmailsProvider,
+        TranslatorInterface $translator
+    ) {
+        $this->registry = $registry;
+        $this->relatedEmailsProvider = $relatedEmailsProvider;
+        $this->translator = $translator;
+    }
+
+    /**
+     * @param EmailRecipientsLoadEvent $event
+     */
+    public function onLoad(EmailRecipientsLoadEvent $event)
+    {
+        $query = $event->getQuery();
+        $limit = $event->getLimit() - count($event->getResults());
+
+        if (!$limit || !$event->getRelatedEntity() instanceof Account) {
+            return;
+        }
+
+        $customers = $this->getCustomerRepository()->findByAccount($event->getRelatedEntity());
+        $emails = [];
+        foreach ($customers as $customer) {
+            $emails = array_merge($emails, $this->relatedEmailsProvider->getEmails($customer, 2));
+        }
+
+        $filteredEmails = array_filter($emails, function ($email) use ($query) {
+            return stripos($email, $query) !== false;
+        });
+
+        $id = $this->translator->trans('oro.email.autocomplete.contexts');
+        $resultsId = null;
+        $results = $event->getResults();
+        foreach ($results as $recordId => $record) {
+             if ($record['text'] === $id) {
+                 $resultsId = $recordId;
+
+                 break;
+             }
+        }
+
+        $children = $this->createResultFromEmails(array_splice($filteredEmails, 0, $limit));
+        if ($resultsId !== null) {
+            $results[$resultsId]['children'] = array_merge($results[$resultsId]['children'], $children);
+        } else {
+            $results = array_merge(
+                $results,
+                [
+                    [
+                        'text'     => $id,
+                        'children' => $children,
+                    ],
+                ]
+            );
+        }
+
+        $event->setResults($results);
+    }
+
+    /**
+     * @param array $emails
+     *
+     * @return array
+     */
+    protected function createResultFromEmails(array $emails)
+    {
+        $result = [];
+        foreach ($emails as $email => $name) {
+            $result[] = [
+                'id'   => $email,
+                'text' => $name,
+            ];
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return EntityRepository
+     */
+    protected function getCustomerRepository()
+    {
+        return $this->registry->getRepository('OroCRMMagentoBundle:Customer');
+    }
+}
