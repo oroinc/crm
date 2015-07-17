12c620919e09575add105c352c14dfbf702a407f
jakubbabiak <jakub@babiak.cz>
2015-07-17 16:34:11 +0200

    CRM-3537: Create convert to case process  - CaseMailboxProcessor initial implementation

--- src/OroCRM/Bundle/CaseBundle/Provider/CaseMailboxProcessor.php
+++ src/OroCRM/Bundle/CaseBundle/Provider/CaseMailboxProcessor.php
@@ -2,21 +2,40 @@
 
 namespace OroCRM\Bundle\CaseBundle\Provider;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
+use Symfony\Component\HttpFoundation\ParameterBag;
+
 use Oro\Bundle\EmailBundle\Entity\Email;
 use Oro\Bundle\EmailBundle\Entity\EmailUser;
 use Oro\Bundle\EmailBundle\Entity\MailboxProcessorSettings;
 use Oro\Bundle\EmailBundle\Provider\MailboxProcessorInterface;
 
+use OroCRM\Bundle\CaseBundle\Entity\CaseEntity;
 use OroCRM\Bundle\CaseBundle\Entity\CaseMailboxProcessorSettings as ProcessorEntity;
+use OroCRM\Bundle\ContactBundle\Entity\Contact;
 
 class CaseMailboxProcessor implements MailboxProcessorInterface
 {
+    /** @var ParameterBag */
+    protected $settings;
+    /** @var Registry */
+    protected $doctrine;
+
+    /**
+     * @param Registry $doctrine
+     */
+    public function __construct(Registry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function configureFromEntity(MailboxProcessorSettings $processor)
     {
-        //
+        $this->settings = $processor->getSettings();
     }
 
     /**
@@ -24,7 +43,24 @@ class CaseMailboxProcessor implements MailboxProcessorInterface
      */
     public function process(EmailUser $emailUser)
     {
-        //
+        $case = new CaseEntity();
+
+        $case->setOwner($this->settings->get('owner'));
+        $case->setAssignedTo($this->settings->get('assignTo'));
+        $case->setStatus($this->settings->get('status'));
+        $case->setPriority($this->settings->get('priority'));
+        $case->setTags($this->settings->get('tags'));
+        $case->setSource(
+            $this->doctrine->getManager()->getReference('OroCRMCaseBundle:CaseSource', 'email')
+        );
+
+        $email = $emailUser->getEmail();
+
+        $case->setSubject($email->getSubject());
+
+        $this->fillDescription($case, $email);
+        $contact = $this->fillRelatedContact($case, $email);
+        $this->fillRelatedAccount($case, $contact);
     }
 
     /**
@@ -63,5 +99,51 @@ class CaseMailboxProcessor implements MailboxProcessorInterface
     public function getSettingsFormType()
     {
         return 'orocrm_case_mailbox_processor';
+    }
+
+    /**
+     * @param CaseEntity $case
+     * @param Email      $email
+     */
+    private function fillDescription(CaseEntity $case, Email $email)
+    {
+        if ($email->getEmailBody()->getBodyIsText()) {
+            $case->setDescription($email->getEmailBody()->getBodyContent());
+        }
+    }
+
+    /**
+     * @param CaseEntity $case
+     * @param Email      $email
+     *
+     * @return null|Contact
+     */
+    private function fillRelatedContact(CaseEntity $case, Email $email)
+    {
+        if (!$email->getFromEmailAddress()->hasOwner()) {
+            return null;
+        }
+
+        $emailOwner = $email->getFromEmailAddress()->getOwner();
+
+        if ($emailOwner instanceof Contact) {
+            $case->setRelatedContact($emailOwner);
+            return $emailOwner;
+        }
+
+        return null;
+    }
+
+    /**
+     * @param CaseEntity   $case
+     * @param Contact|null $owner
+     */
+    private function fillRelatedAccount(CaseEntity $case, $owner)
+    {
+        $accounts = $owner->getAccounts();
+
+        if (count($accounts) == 1) {
+            $case->setRelatedAccount($accounts->first());
+        }
     }
 }
