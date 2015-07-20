b5942cfb7a247a6ed7bfdda77d6a19b07ac357d9
jakubbabiak <jakub@babiak.cz>
2015-07-20 14:45:31 +0200

    CRM-3537: Create convert to case process  - Modification to case mailbox processor

--- src/OroCRM/Bundle/CaseBundle/Provider/CaseMailboxProcessor.php
+++ src/OroCRM/Bundle/CaseBundle/Provider/CaseMailboxProcessor.php
@@ -45,22 +45,30 @@ class CaseMailboxProcessor implements MailboxProcessorInterface
     {
         $case = new CaseEntity();
 
+        // Set properties from processor settings
         $case->setOwner($this->settings->get('owner'));
         $case->setAssignedTo($this->settings->get('assignTo'));
         $case->setStatus($this->settings->get('status'));
         $case->setPriority($this->settings->get('priority'));
         $case->setTags($this->settings->get('tags'));
+
+        // Set source type to email
         $case->setSource(
             $this->doctrine->getManager()->getReference('OroCRMCaseBundle:CaseSource', 'email')
         );
 
         $email = $emailUser->getEmail();
 
+        // Same subject as email
         $case->setSubject($email->getSubject());
 
+        // Fill properties using email
         $this->fillDescription($case, $email);
         $contact = $this->fillRelatedContact($case, $email);
         $this->fillRelatedAccount($case, $contact);
+
+        // Set email as seen
+        $emailUser->setSeen(true);
     }
 
     /**
@@ -102,6 +110,8 @@ class CaseMailboxProcessor implements MailboxProcessorInterface
     }
 
     /**
+     * Fills description of case with email body, if that body is in text form.
+     *
      * @param CaseEntity $case
      * @param Email      $email
      */
@@ -135,11 +145,18 @@ class CaseMailboxProcessor implements MailboxProcessorInterface
     }
 
     /**
+     * Fills related account of case with account from owner contact, provided that contact has only one account.
+     * In cas of multiple accounts, no account is assigned.
+     *
      * @param CaseEntity   $case
-     * @param Contact|null $owner
+     * @param null|Contact $owner
      */
     private function fillRelatedAccount(CaseEntity $case, $owner)
     {
+        if ($owner === null) {
+            return;
+        }
+
         $accounts = $owner->getAccounts();
 
         if (count($accounts) == 1) {
