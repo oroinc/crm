e6308953b18764406746ad3c685925912c94ed32
jakubbabiak <jakub@babiak.cz>
2015-07-22 14:51:17 +0200

    CRM-3536: Create convert to lead process  - Migration corrections

--- src/OroCRM/Bundle/CaseBundle/Migrations/Schema/OroCRMCaseBundleInstaller.php
+++ src/OroCRM/Bundle/CaseBundle/Migrations/Schema/OroCRMCaseBundleInstaller.php
@@ -12,9 +12,6 @@ use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 use OroCRM\Bundle\CaseBundle\Migrations\Schema\v1_0\OroCRMCaseBundle;
 use OroCRM\Bundle\CaseBundle\Migrations\Schema\v1_1\OroCRMCaseBundle as OroCRMCaseBundle11;
 use OroCRM\Bundle\CaseBundle\Migrations\Schema\v1_2\OroCRMCaseBundle as OroCRMCaseBundle12;
-use OroCRM\Bundle\CaseBundle\Migrations\Schema\v1_3\OroCRMCaseBundle as OroCRMCaseBundle13;
-use OroCRM\Bundle\CaseBundle\Migrations\Schema\v1_4\OroCRMCaseBundle as OroCRMCaseBundle14;
-use OroCRM\Bundle\CaseBundle\Migrations\Schema\v1_5\OroCRMCaseBundle as OroCRMCaseBundle15;
 
 class OroCRMCaseBundleInstaller implements
     Installation,
@@ -36,7 +33,7 @@ class OroCRMCaseBundleInstaller implements
      */
     public function getMigrationVersion()
     {
-        return 'v1_5';
+        return 'v1_3';
     }
 
     /**
@@ -52,14 +49,10 @@ class OroCRMCaseBundleInstaller implements
 
         OroCRMCaseBundle12::addOrganization($schema);
 
-        $migration13 = new OroCRMCaseBundle13();
-        $migration13->setAttachmentExtension($this->attachmentExtension);
-        $migration13->up($schema, $queries);
-
-        $migration14 = new OroCRMCaseBundle14();
-        $migration14->up($schema, $queries);
-
-        OroCRMCaseBundle15::addOroEmailMailboxProcessorColumns($schema);
-        OroCRMCaseBundle15::addOroEmailMailboxProcessorForeignKeys($schema);
+        $this->attachmentExtension->addImageRelation(
+            $schema,
+            'orocrm_case_comment',
+            'attachment'
+        );
     }
 }
