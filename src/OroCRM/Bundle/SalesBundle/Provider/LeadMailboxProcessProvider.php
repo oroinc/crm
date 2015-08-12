90228c5f2fc98a726f23c2cf123768572c0ba9a6
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-08-12 12:44:04 +0200

    CRM-2267: Mailbox auto-response rule management
    - update lead provider

--- src/OroCRM/Bundle/SalesBundle/Provider/LeadMailboxProcessProvider.php
+++ src/OroCRM/Bundle/SalesBundle/Provider/LeadMailboxProcessProvider.php
@@ -2,10 +2,28 @@
 
 namespace OroCRM\Bundle\SalesBundle\Provider;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityRepository;
+
 use Oro\Bundle\EmailBundle\Mailbox\MailboxProcessProviderInterface;
+
+use OroCRM\Bundle\ChannelBundle\Entity\Channel;
 
 class LeadMailboxProcessProvider implements MailboxProcessProviderInterface
 {
+    const LEAD_CLASS = 'OroCRM\Bundle\SalesBundle\Entity\Lead';
+
+    /** @var Registry */
+    protected $registry;
+
+    /**
+     * @param Registry $registry
+     */
+    public function __construct(Registry $registry)
+    {
+        $this->registry = $registry;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -28,5 +46,31 @@ class LeadMailboxProcessProvider implements MailboxProcessProviderInterface
     public function getLabel()
     {
         return 'orocrm.sales.mailbox.process.lead.label';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isEnabled()
+    {
+        $qb = $this->getChannelRepository()->createQueryBuilder('c');
+
+        return (bool) $qb
+            ->select('COUNT(c.id)')
+            ->join('c.entities', 'e')
+            ->andWhere('e.name = :name')
+            ->andWhere('c.status = :status')
+            ->setParameter('name', static::LEAD_CLASS)
+            ->setParameter('status', Channel::STATUS_ACTIVE)
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
+    /**
+     * @return EntityRepository
+     */
+    protected function getChannelRepository()
+    {
+        return $this->registry->getRepository('OroCRMChannelBundle:Channel');
     }
 }
