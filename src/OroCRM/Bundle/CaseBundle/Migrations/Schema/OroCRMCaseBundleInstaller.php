1fe7c78df97a858e0fec2e7a7fb2c352cdb899bb
Olga Syzova <osizova@magecore.com>
2015-08-19 19:25:39 +0300

    CRM-3944: Schema installer is not up today  - fixed migrations OroCRMSalesBundle, OroCRMCaseBundle

--- src/OroCRM/Bundle/CaseBundle/Migrations/Schema/OroCRMCaseBundleInstaller.php
+++ src/OroCRM/Bundle/CaseBundle/Migrations/Schema/OroCRMCaseBundleInstaller.php
@@ -21,7 +21,7 @@ class OroCRMCaseBundleInstaller implements
      */
     public function getMigrationVersion()
     {
-        return 'v1_4';
+        return 'v1_5';
     }
 
     /**
@@ -47,9 +47,13 @@ class OroCRMCaseBundleInstaller implements
         $this->createOrocrmCasePriorityTranslationTable($schema);
         $this->createOrocrmCaseCommentTranslationTable($schema);
 
+        /** Tables update */
+        $this->addOroEmailMailboxProcessSettingsColumns($schema);
+
         /** Foreign keys generation **/
         $this->addOrocrmCaseForeignKeys($schema);
         $this->addOrocrmCaseCommentForeignKeys($schema);
+        $this->addOroEmailMailboxProcessSettingsForeignKeys($schema);
     }
 
     /**
@@ -320,6 +324,60 @@ class OroCRMCaseBundleInstaller implements
             $schema,
             'orocrm_case_comment',
             'attachment'
+        );
+    }
+
+    /**
+     * Adds required columns to oro_email_mailbox_process table.
+     *
+     * @param Schema $schema
+     */
+    public static function addOroEmailMailboxProcessSettingsColumns(Schema $schema)
+    {
+        $table = $schema->getTable('oro_email_mailbox_process');
+
+        $table->addColumn('case_assign_to_id', 'integer', ['notnull' => false]);
+        $table->addColumn('case_status_name', 'string', ['notnull' => false, 'length' => 16]);
+        $table->addColumn('case_owner_id', 'integer', ['notnull' => false]);
+        $table->addColumn('case_priority_name', 'string', ['notnull' => false, 'length' => 16]);
+
+        $table->addIndex(['case_owner_id'], 'IDX_CE8602A3E9411B84', []);
+        $table->addIndex(['case_assign_to_id'], 'IDX_CE8602A37CFDD645', []);
+        $table->addIndex(['case_priority_name'], 'IDX_CE8602A3F1B25087', []);
+        $table->addIndex(['case_status_name'], 'IDX_CE8602A3C168B4FB', []);
+    }
+
+    /**
+     * Adds foreign keys to new columns in oro_email_mailbox_process table.
+     *
+     * @param Schema $schema
+     */
+    public static function addOroEmailMailboxProcessSettingsForeignKeys(Schema $schema)
+    {
+        $table = $schema->getTable('oro_email_mailbox_process');
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_user'),
+            ['case_assign_to_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('orocrm_case_status'),
+            ['case_status_name'],
+            ['name'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_user'),
+            ['case_owner_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('orocrm_case_priority'),
+            ['case_priority_name'],
+            ['name'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
         );
     }
 }
