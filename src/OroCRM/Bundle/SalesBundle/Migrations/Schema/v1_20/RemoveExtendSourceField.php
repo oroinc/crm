a7f4ebaca4f59dc48bd9e409f5b3d2dc5e91e805
Vova Soroka <vsoroka@magecore.com>
2015-09-15 14:34:11 +0300

    BAP-8857: Clear-up existing migrations related to OptionSets. Remove outdated Lead::extend_source field

--- src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_20/RemoveExtendSourceField.php
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_20/RemoveExtendSourceField.php
@@ -4,16 +4,35 @@ namespace OroCRM\Bundle\SalesBundle\Migrations\Schema\v1_20;
 
 use Doctrine\DBAL\Schema\Schema;
 
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Oro\Bundle\EntityExtendBundle\Migration\ExtendOptionsManager;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-class RemoveExtendSourceField implements Migration
+class RemoveExtendSourceField implements Migration, ContainerAwareInterface
 {
+    /** @var ContainerInterface */
+    protected $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function up(Schema $schema, QueryBag $queries)
     {
         $queries->addQuery(new RemoveExtendSourceFieldQuery());
+
+        /** @var ExtendOptionsManager $extendOptionsManager */
+        $extendOptionsManager = $this->container->get('oro_entity_extend.migration.options_manager');
+        $extendOptionsManager->removeColumnOptions('orocrm_sales_lead', 'extend_source');
     }
 }
