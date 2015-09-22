10def1f1f9af126ec1b7054e798e5b42e16b55b3
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2015-09-23 00:39:24 +0300

    BAP-9051: 'oro:cron:import-tracking' performance - use transaction

--- src/OroCRM/Bundle/ContactBundle/Tests/Functional/ImportExportTest.php
+++ src/OroCRM/Bundle/ContactBundle/Tests/Functional/ImportExportTest.php
@@ -25,18 +25,35 @@ class ImportExportTest extends WebTestCase
     protected function setUp()
     {
         $this->initClient(array(), $this->generateBasicAuthHeader());
-
-        $this->getContainer()->get('akeneo_batch.job_repository')->getJobManager()->beginTransaction();
     }
 
+    /**
+     * Delete data required because there is commit to job repository in import/export controller action
+     * Please use
+     *   $this->getContainer()->get('akeneo_batch.job_repository')->getJobManager()->beginTransaction();
+     *   $this->getContainer()->get('akeneo_batch.job_repository')->getJobManager()->rollback();
+     *   $this->getContainer()->get('akeneo_batch.job_repository')->getJobManager()->getConnection()->clear();
+     * if you don't use controller
+     */
     protected function tearDown()
     {
         // clear DB from separate connection
-        $manager = $this->getContainer()->get('akeneo_batch.job_repository')->getJobManager();
-        $manager->rollback();
-        $manager->getConnection()->close();
+        $batchJobManager = $this->getBatchJobManager();
+        $batchJobManager->createQuery('DELETE AkeneoBatchBundle:JobInstance')->execute();
+        $batchJobManager->createQuery('DELETE AkeneoBatchBundle:JobExecution')->execute();
+        $batchJobManager->createQuery('DELETE AkeneoBatchBundle:StepExecution')->execute();
 
         parent::tearDown();
+    }
+
+    /**
+     * @return \Doctrine\ORM\EntityManager
+     */
+    protected function getBatchJobManager()
+    {
+        /** @var BatchJobRepository $batchJobRepository */
+        $batchJobRepository = $this->getContainer()->get('akeneo_batch.job_repository');
+        return $batchJobRepository->getJobManager();
     }
 
     public function strategyDataProvider()
