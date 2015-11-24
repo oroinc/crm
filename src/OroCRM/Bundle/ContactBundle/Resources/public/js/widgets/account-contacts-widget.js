eff627b1fdbfa92602c0e9198d5ffd6a2a201fb9
Sergii Zhdanyuk <szhdanyuk@magecore.com>
2015-11-24 20:03:22 +0200

    CRM-4562 Fixed account-contacts-widget boxClickHandler function.

--- src/OroCRM/Bundle/ContactBundle/Resources/public/js/widgets/account-contacts-widget.js
+++ src/OroCRM/Bundle/ContactBundle/Resources/public/js/widgets/account-contacts-widget.js
@@ -10,12 +10,12 @@ define(['jquery'], function($) {
          * @desc Fire name link click
          * @callback
          */
-        boxClickHandler: function(even) {
+        boxClickHandler: function(event) {
             /**
              * @desc if target item has class contact-box-link
              * we does not click redirection link(name link)
              */
-            if ($(even.target).hasClass('contact-box-link')) {
+            if (!$(event.target).hasClass('contact-box-row')) {
                 return;
             }
             $(this).find('.contact-box-name-link').click();
