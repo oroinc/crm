8d85b84a3d269ae998009a954070d2b839ba4612
Ievgenii Myshusta <imyshusta@magecore.com>
2015-11-26 19:02:38 +0200

    CRM-4649: Incorrect exist customers mapping - fixed using channel in find Existing entity logic

--- src/OroCRM/Bundle/MagentoBundle/ImportExport/Strategy/DefaultMagentoImportStrategy.php
+++ src/OroCRM/Bundle/MagentoBundle/ImportExport/Strategy/DefaultMagentoImportStrategy.php
@@ -3,6 +3,7 @@
 namespace OroCRM\Bundle\MagentoBundle\ImportExport\Strategy;
 
 use Oro\Bundle\ImportExportBundle\Strategy\Import\ConfigurableAddOrReplaceStrategy;
+use Doctrine\Common\Util\ClassUtils;
 
 class DefaultMagentoImportStrategy extends ConfigurableAddOrReplaceStrategy
 {
@@ -20,5 +21,46 @@ class DefaultMagentoImportStrategy extends ConfigurableAddOrReplaceStrategy
         }
 
         return $entity;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function findExistingEntity($entity, array $searchContext = [])
+    {
+        $entityName = ClassUtils::getClass($entity);
+        $identifier = $this->databaseHelper->getIdentifier($entity);
+        $existingEntity = null;
+
+        // find by identifier
+        if ($identifier) {
+            $existingEntity = $this->databaseHelper->find($entityName, $identifier);
+        }
+
+        // find by identity fields
+        if (!$existingEntity
+            && (!$searchContext || $this->databaseHelper->getIdentifier(current($searchContext)))
+        ) {
+            $identityValues = $searchContext;
+
+            if ($this->context->hasOption('channel')) {
+                $channel = $this->databaseHelper->findOneBy(
+                    'Oro\Bundle\IntegrationBundle\Entity\Channel',
+                    ['id' => $this->context->getOption('channel')]
+                );
+                $identityValues['channel'] = $channel;
+            }
+
+            $identityValues += $this->fieldHelper->getIdentityValues($entity);
+            $existingEntity = $this->findEntityByIdentityValues($entityName, $identityValues);
+        }
+
+        if ($existingEntity && !$identifier) {
+            $identifier = $this->databaseHelper->getIdentifier($existingEntity);
+            $identifierName = $this->databaseHelper->getIdentifierFieldName($entity);
+            $this->fieldHelper->setObjectValue($entity, $identifierName, $identifier);
+        }
+
+        return $existingEntity;
     }
 }
