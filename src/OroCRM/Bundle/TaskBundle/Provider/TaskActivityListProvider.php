15d3f026197acdd900f0d9d4470d6f60f14ad02c
Vova Soroka <vsoroka@magecore.com>
2016-01-21 17:38:27 +0200

    BAP-9639: 500 error is occurred on Business Units - Fix disappear activity tab for any entity that has only notes association - Refactoring - Small performance fixes

--- src/OroCRM/Bundle/TaskBundle/Provider/TaskActivityListProvider.php
+++ src/OroCRM/Bundle/TaskBundle/Provider/TaskActivityListProvider.php
@@ -2,13 +2,13 @@
 
 namespace OroCRM\Bundle\TaskBundle\Provider;
 
+use Oro\Bundle\ActivityBundle\Tools\ActivityAssociationHelper;
 use Oro\Bundle\ActivityListBundle\Entity\ActivityList;
 use Oro\Bundle\ActivityListBundle\Model\ActivityListProviderInterface;
 use Oro\Bundle\ActivityListBundle\Entity\ActivityOwner;
 use Oro\Bundle\CommentBundle\Model\CommentProviderInterface;
+use Oro\Bundle\CommentBundle\Tools\CommentAssociationHelper;
 use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
-use Oro\Bundle\EntityConfigBundle\Config\ConfigManager;
-use Oro\Bundle\EntityConfigBundle\Config\Id\ConfigIdInterface;
 use Oro\Bundle\EntityConfigBundle\DependencyInjection\Utils\ServiceLink;
 
 use OroCRM\Bundle\TaskBundle\Entity\Task;
@@ -24,25 +24,40 @@ class TaskActivityListProvider implements ActivityListProviderInterface, Comment
     /** @var ServiceLink */
     protected $entityOwnerAccessorLink;
 
+    /** @var ActivityAssociationHelper */
+    protected $activityAssociationHelper;
+
+    /** @var CommentAssociationHelper */
+    protected $commentAssociationHelper;
+
     /**
-     * @param DoctrineHelper $doctrineHelper
-     * @param ServiceLink    $entityOwnerAccessorLink
+     * @param DoctrineHelper            $doctrineHelper
+     * @param ServiceLink               $entityOwnerAccessorLink
+     * @param ActivityAssociationHelper $activityAssociationHelper
+     * @param CommentAssociationHelper  $commentAssociationHelper
      */
-    public function __construct(DoctrineHelper $doctrineHelper, ServiceLink $entityOwnerAccessorLink)
-    {
-        $this->doctrineHelper = $doctrineHelper;
-        $this->entityOwnerAccessorLink = $entityOwnerAccessorLink;
+    public function __construct(
+        DoctrineHelper $doctrineHelper,
+        ServiceLink $entityOwnerAccessorLink,
+        ActivityAssociationHelper $activityAssociationHelper,
+        CommentAssociationHelper $commentAssociationHelper
+    ) {
+        $this->doctrineHelper            = $doctrineHelper;
+        $this->entityOwnerAccessorLink   = $entityOwnerAccessorLink;
+        $this->activityAssociationHelper = $activityAssociationHelper;
+        $this->commentAssociationHelper  = $commentAssociationHelper;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function isApplicableTarget(ConfigIdInterface $configId, ConfigManager $configManager)
+    public function isApplicableTarget($entityClass, $accessible = true)
     {
-        $provider = $configManager->getProvider('activity');
-        return $provider->hasConfigById($configId)
-            && $provider->getConfigById($configId)->has('activities')
-            && in_array(self::ACTIVITY_CLASS, $provider->getConfigById($configId)->get('activities'));
+        return $this->activityAssociationHelper->isActivityAssociationEnabled(
+            $entityClass,
+            self::ACTIVITY_CLASS,
+            $accessible
+        );
     }
 
     /**
@@ -147,11 +162,9 @@ class TaskActivityListProvider implements ActivityListProviderInterface, Comment
     /**
      * {@inheritdoc}
      */
-    public function hasComments(ConfigManager $configManager, $entity)
+    public function isCommentsEnabled($entityClass)
     {
-        $config = $configManager->getProvider('comment')->getConfig($entity);
-
-        return $config->is('enabled');
+        return $this->commentAssociationHelper->isCommentAssociationEnabled($entityClass);
     }
 
     /**
