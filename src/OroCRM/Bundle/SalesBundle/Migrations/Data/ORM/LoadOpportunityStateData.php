fbde16c8a263bde2cd02718ead96cf12fecd532e
Mykhailo Kudelia <mkudelia@magecore.com>
2016-04-15 10:56:47 +0300

    CRM-5293: Migrate old status data

--- src/OroCRM/Bundle/SalesBundle/Migrations/Data/ORM/LoadOpportunityStateData.php
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Data/ORM/LoadOpportunityStateData.php
@@ -2,18 +2,34 @@
 
 namespace OroCRM\Bundle\SalesBundle\Migrations\Data\ORM;
 
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
 use Oro\Bundle\EntityExtendBundle\Migration\Fixture\AbstractEnumFixture;
 
 use OroCRM\Bundle\SalesBundle\Entity\Opportunity;
 
-class LoadOpportunityStateData extends AbstractEnumFixture
+class LoadOpportunityStateData extends AbstractEnumFixture implements DependentFixtureInterface
 {
+    /**
+     * @var ObjectManager
+     */
+    protected $manager;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDependencies()
+    {
+        return ['OroCRM\Bundle\SalesBundle\Migrations\Data\ORM\LoadOpportunityStatusData'];
+    }
+
     /**
      * @return array
      */
     protected function getData()
     {
-        return [
+        $statuses = [
             'identification_alignment' => 'Identification & Alignment',
             'needs_analysis' => 'Needs Analysis',
             'solution_development' => 'Solution Development',
@@ -21,6 +37,21 @@ class LoadOpportunityStateData extends AbstractEnumFixture
             'won' => 'Closed Won',
             'lost' => 'Closed Lost'
         ];
+
+        $oldStatuses = $this->manager
+            ->getRepository('OroCRMSalesBundle:OpportunityStatus')
+            ->findAll();
+
+        foreach ($oldStatuses as $oldStatus) {
+            $oldName = $oldStatus->getName();
+            $oldLabel = $oldStatus->getLabel();
+
+            if (!array_key_exists($oldName, $statuses)) {
+                $statuses[$oldName] = $oldLabel;
+            }
+        }
+
+        return $statuses;
     }
 
     /**
@@ -29,5 +60,14 @@ class LoadOpportunityStateData extends AbstractEnumFixture
     protected function getEnumCode()
     {
         return Opportunity::INTERNAL_STATE_CODE;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+        parent::load($manager);
     }
 }
