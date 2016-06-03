a56cd0d839fd408cff9599b0274b99a72658ba4e
Bogdan Tsyganovskiy <btsyganovskiy@magecore.com>
2016-06-03 23:23:43 +0300

    CRM-5597: Create CrmTaskBundleBridge  - add CrmTaskBundleBridge bundle  - move crm related demo data and migrations to new bundle

--- src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_40/CreateActivityAssociation.php
+++ src/OroCRM/Bundle/MagentoBundle/Migrations/Schema/v1_40/CreateActivityAssociation.php
@@ -52,11 +52,6 @@ class CreateActivityAssociation implements Migration, ActivityExtensionAwareInte
      */
     public static function addActivityAssociations(Schema $schema, ActivityExtension $activityExtension)
     {
-        $associationTableName = $activityExtension->getAssociationTableName('orocrm_task', 'orocrm_magento_order');
-        if ($schema->hasTable('orocrm_task') && !$schema->hasTable($associationTableName)) {
-            $activityExtension->addActivityAssociation($schema, 'orocrm_task', 'orocrm_magento_order');
-        }
-
         $associationTableName = $activityExtension
             ->getAssociationTableName('oro_calendar_event', 'orocrm_magento_order');
         if (!$schema->hasTable($associationTableName)) {
