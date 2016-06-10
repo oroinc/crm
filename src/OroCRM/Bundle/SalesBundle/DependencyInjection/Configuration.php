d4943a2941a1f7ae316c0a67324c7e976d55a229
Ivaylo Badinov <venimus@users.noreply.github.com>
2016-06-10 11:39:37 +0300

    CRM-5084: Bundle config with default probabilities

--- src/OroCRM/Bundle/SalesBundle/DependencyInjection/Configuration.php
+++ src/OroCRM/Bundle/SalesBundle/DependencyInjection/Configuration.php
@@ -5,6 +5,8 @@ namespace OroCRM\Bundle\SalesBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
+use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
+
 class Configuration implements ConfigurationInterface
 {
     /**
@@ -18,6 +20,26 @@ class Configuration implements ConfigurationInterface
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
+
+        $defaults = [
+            'lost' => 0,
+            'in_progress' => .1,
+            'needs_analysis' => .2,
+            'solution_development' => .5,
+            'negotiation' => .8,
+            'won' => 1,
+        ];
+
+        SettingsBuilder::append(
+            $rootNode,
+            [
+                'default_opportunity_probabilities' => [
+                    'value' => $defaults,
+                    'type' => 'array',
+                ],
+            ]
+        );
+
         return $treeBuilder;
     }
 }
--- src/OroCRM/Bundle/SalesBundle/DependencyInjection/OroCRMSalesExtension.php
+++ src/OroCRM/Bundle/SalesBundle/DependencyInjection/OroCRMSalesExtension.php
@@ -15,11 +15,13 @@ class OroCRMSalesExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
+        $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('form.yml');
         $loader->load('importexport.yml');
+
+        $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip(['settings'])));
     }
 }
