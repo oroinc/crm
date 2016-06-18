9186577a737bd17c02c9fa66dc99f38900ed2158
Denis Voronin <dvoronin@magecore.com>
2016-06-18 13:40:16 +0300

    CRM-5627: Configure open opportunities widget CRM-5628: Add filter to exclude opportunities by status

---
+++ src/OroCRM/Bundle/SalesBundle/Form/Type/OpportunityStatusSelectType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace OroCRM\Bundle\SalesBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Oro\Bundle\EntityExtendBundle\Provider\EnumValueProvider;
+
+class OpportunityStatusSelectType extends AbstractType
+{
+    const NAME = 'orocrm_type_widget_opportunity_status_select';
+
+    /** @var EnumValueProvider */
+    private $enumValueProvider;
+
+    /**
+     * @param EnumValueProvider $enumValueProvider
+     */
+    public function __construct(EnumValueProvider $enumValueProvider)
+    {
+        $this->enumValueProvider = $enumValueProvider;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+
+        $resolver->setDefaults(
+            [
+                'choices'  => $this->enumValueProvider->getEnumChoicesByCode('opportunity_status'),
+                'multiple' => true,
+                'configs'  => [
+                    'width'      => '400px',
+                    'allowClear' => true,
+                ]
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'genemu_jqueryselect2_choice';
+    }
+}
