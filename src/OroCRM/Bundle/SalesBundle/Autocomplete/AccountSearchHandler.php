bf9c50f1d1f4524b2b444535a71dbf7afd4e8be1
Pavel Usachev <pusachev@magecore.com>
2016-06-24 15:45:10 +0300

    CRM-5473: Customer selector control at Opportunity form should be named Account - Added custom search handler for account autocomplite search - Added tooltip in account select input with autocomplite - Added translation message for account tooltip

---
+++ src/OroCRM/Bundle/SalesBundle/Autocomplete/AccountSearchHandler.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace OroCRM\Bundle\SalesBundle\Autocomplete;
+
+use OroCRM\Bundle\ChannelBundle\Autocomplete\ChannelLimitationHandler;
+use OroCRM\Bundle\SalesBundle\Entity\B2bCustomer;
+
+class AccountSearchHandler extends ChannelLimitationHandler
+{
+
+    const CUSTOMER_NAME_PROPERTY = 'name';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertItem($item)
+    {
+        $result = [];
+
+        if ($this->idFieldName) {
+            $result[$this->idFieldName] = $this->getPropertyValue($this->idFieldName, $item);
+        }
+
+        foreach ($this->properties as $property) {
+            if ($property === self::CUSTOMER_NAME_PROPERTY) {
+                $result[$property] = $this->checkCustomerName($property, $item);
+            } else {
+                $result[$property] = $this->getPropertyValue($property, $item);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Check if customer name is equal with account name
+     * and return customer name with account if not equal
+     *
+     * @param string        $propertyPath
+     * @param B2bCustomer   $entity
+     * @return string
+     */
+    private function checkCustomerName($propertyPath, B2bCustomer $entity)
+    {
+        $accountName  = $entity->getAccount()->getName();
+        $customerName = $entity->getName();
+
+        if($accountName === $customerName) {
+            return $this->getPropertyValue($propertyPath, $entity);
+        }
+
+        return $customerName . '('.$accountName.')';
+    }
+}
