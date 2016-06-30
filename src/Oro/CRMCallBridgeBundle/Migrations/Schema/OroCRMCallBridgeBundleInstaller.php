422c806db01f1fa39cf21ae73a2022753b7c33f6
Pavel Usachev <pusachev@magecore.com>
2016-06-30 19:14:40 +0300

    CRM-5500: Move CallsBundle to separate package - Move part of logic of install in migration - fixed roles load - fixes fir code review (cherry picked from commit 8486462)

--- src/Oro/CRMCallBridgeBundle/Migrations/Schema/OroCRMCallBridgeBundleInstaller.php
+++ src/Oro/CRMCallBridgeBundle/Migrations/Schema/OroCRMCallBridgeBundleInstaller.php
@@ -4,9 +4,6 @@ namespace Oro\CRMCallBridgeBundle\Migrations\Schema;
 
 use Doctrine\DBAL\Schema\Schema;
 
-use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtension;
-use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtensionAwareInterface;
-
 use Oro\Bundle\MigrationBundle\Migration\Installation;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
@@ -15,19 +12,8 @@ use Oro\Bundle\MigrationBundle\Migration\QueryBag;
  * @SuppressWarnings(PHPMD.ExcessiveClassLength)
  */
 class OroCRMCallBridgeBundleInstaller implements
-    Installation,
-    ActivityExtensionAwareInterface
+    Installation
 {
-    /** @var ActivityExtension */
-    protected $activityExtension;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setActivityExtension(ActivityExtension $activityExtension)
-    {
-        $this->activityExtension = $activityExtension;
-    }
 
     /**
      * {@inheritdoc}
@@ -37,26 +23,10 @@ class OroCRMCallBridgeBundleInstaller implements
         return 'v1_0';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function up(Schema $schema, QueryBag $queries)
     {
-        /** if CallBundle is installed  do nothing **/
-        if ($schema->hasTable('orocrm_call')) {
-            return;
-        }
-
-        $this->activityExtension->addActivityAssociation($schema, 'orocrm_call', 'orocrm_sales_lead');
-        $this->activityExtension->addActivityAssociation($schema, 'orocrm_call', 'orocrm_sales_opportunity');
-        $this->activityExtension->addActivityAssociation($schema, 'orocrm_call', 'orocrm_sales_b2bcustomer');
-        $this->activityExtension->addActivityAssociation($schema, 'orocrm_call', 'orocrm_contactus_request');
-        $this->activityExtension->addActivityAssociation($schema, 'orocrm_call', 'orocrm_magento_customer');
-        $this->activityExtension->addActivityAssociation($schema, 'orocrm_call', 'orocrm_magento_order');
-        $this->activityExtension->addActivityAssociation($schema, 'orocrm_call', 'orocrm_magento_cart');
-
-        $associationTableName = $this->activityExtension->getAssociationTableName('orocrm_call', 'orocrm_case');
-        if (!$schema->hasTable($associationTableName)) {
-            $this->activityExtension->addActivityAssociation($schema, 'orocrm_call', 'orocrm_case');
-        }
     }
-
-   
 }
