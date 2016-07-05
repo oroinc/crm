6b714ba71b432f7acef6829a53c5e678e0708431
Alexandr Parkhomenko <alexandr.parhomenk@gmail.com>
2016-07-05 21:23:49 +0300

    CRM-5240: Qualify Lead to Opportunity - Added logic that attach created contact to the lead entity

--- src/OroCRM/Bundle/SalesBundle/Form/Handler/LeadToOpportunityHandler.php
+++ src/OroCRM/Bundle/SalesBundle/Form/Handler/LeadToOpportunityHandler.php
@@ -5,6 +5,8 @@ namespace OroCRM\Bundle\SalesBundle\Form\Handler;
 use Doctrine\Common\Persistence\ObjectManager;
 
 use OroCRM\Bundle\SalesBundle\Entity\Opportunity;
+use OroCRM\Bundle\SalesBundle\Entity\Lead;
+use OroCRM\Bundle\ContactBundle\Entity\Contact;
 use OroCRM\Bundle\ChannelBundle\Provider\RequestChannelProvider;
 use OroCRM\Bundle\SalesBundle\Model\ChangeLeadStatus;
 
@@ -34,9 +36,21 @@ class LeadToOpportunityHandler extends OpportunityHandler
     {
         $processResult = parent::process($entity);
         if ($processResult && in_array($this->request->getMethod(), array('POST', 'PUT'))) {
+            $this->attachContactToLead($entity->getLead(), $entity);
             return $this->changeLeadStatusModel->qualify($entity->getLead());
         }
         return $processResult;
+    }
+
+    /**
+     * @param Lead        $lead
+     * @param Opportunity $opportunity
+     */
+    protected function attachContactToLead(Lead $lead, Opportunity $opportunity)
+    {
+        if (!$lead->getContact() instanceof Contact) {
+            $lead->setContact($opportunity->getContact());
+        }
     }
 
     /**
