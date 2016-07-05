bc88e299eda432c4b28f49426194b20f77ba7f79
Ivan Shakuta <ishakuta@gmail.com>
2016-07-05 14:04:36 +0300

    Fix CaseEntity notice
    set updateClosedAt to null instead of unsetting it, which leads to notice or warning when it used afterwards.

--- src/OroCRM/Bundle/CaseBundle/Entity/CaseEntity.php
+++ src/OroCRM/Bundle/CaseBundle/Entity/CaseEntity.php
@@ -608,7 +608,7 @@ class CaseEntity extends ExtendCaseEntity implements EmailHolderInterface
         $this->closedAt = $closedAt;
 
         if ($this->closedAt) {
-            unset($this->updateClosedAt);
+            $this->updateClosedAt = null;
         }
 
         return $this;
