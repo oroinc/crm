10832024c678362beb9755037e3ffca38d4b37bb
Alex Solonenko <asolonenko@magecore.com>
2016-07-08 17:32:24 +0300

    CRM-5820: Create migration for new fields  -  fix indexes

--- src/OroCRM/Bundle/SalesBundle/Migrations/Schema/OroCRMSalesBundleInstaller.php
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/OroCRMSalesBundleInstaller.php
@@ -425,7 +425,7 @@ class OroCRMSalesBundleInstaller implements
         $table->addColumn('phone', 'string', ['length' => 255]);
         $table->addColumn('is_primary', 'boolean', ['notnull' => false]);
         $table->setPrimaryKey(['id']);
-        $table->addIndex(['owner_id'], 'IDX_8475907F7E3C61F9', []);
+        $table->addIndex(['owner_id'], 'IDX_F0D0BDFA7E3C61F9', []);
         $table->addIndex(['phone', 'is_primary'], 'primary_phone_idx', []);
         $table->addIndex(['phone'], 'phone_idx');
     }
@@ -443,7 +443,7 @@ class OroCRMSalesBundleInstaller implements
         $table->addColumn('email', 'string', ['length' => 255]);
         $table->addColumn('is_primary', 'boolean', ['notnull' => false]);
         $table->setPrimaryKey(['id']);
-        $table->addIndex(['owner_id'], 'IDX_335A28C37E3C61F9', []);
+        $table->addIndex(['owner_id'], 'IDX_D564AB17E3C61F9', []);
         $table->addIndex(['email', 'is_primary'], 'primary_email_idx', []);
     }
 
--- src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_24/CreateB2bCustomerPhone.php
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_24/CreateB2bCustomerPhone.php
@@ -32,7 +32,7 @@ class CreateB2bCustomerPhone implements Migration
         $table->addColumn('phone', 'string', ['length' => 255]);
         $table->addColumn('is_primary', 'boolean', ['notnull' => false]);
         $table->setPrimaryKey(['id']);
-        $table->addIndex(['owner_id'], 'IDX_8475907F7E3C61F9', []);
+        $table->addIndex(['owner_id'], 'IDX_F0D0BDFA7E3C61F9', []);
         $table->addIndex(['phone', 'is_primary'], 'primary_phone_idx', []);
         $table->addIndex(['phone'], 'phone_idx');
     }
