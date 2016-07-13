496b1846a834693e07be3a034d37aa718a571105
Bogdan Tsyganovskiy <btsyganovskiy@magecore.com>
2016-07-13 16:05:57 +0300

    CRM-5636: Make field phones  - fix import fixture

--- src/OroCRM/Bundle/SalesBundle/ImportExport/TemplateFixture/LeadFixture.php
+++ src/OroCRM/Bundle/SalesBundle/ImportExport/TemplateFixture/LeadFixture.php
@@ -2,6 +2,7 @@
 
 namespace OroCRM\Bundle\SalesBundle\ImportExport\TemplateFixture;
 
+use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
 use Oro\Bundle\ImportExportBundle\TemplateFixture\AbstractTemplateRepository;
 use Oro\Bundle\ImportExportBundle\TemplateFixture\TemplateFixtureInterface;
 use OroCRM\Bundle\SalesBundle\Entity\Lead;
@@ -65,9 +66,15 @@ class LeadFixture extends AbstractTemplateRepository implements TemplateFixtureI
                 $entity->setFirstName('Jerry');
                 $entity->setLastName('Coleman');
                 $entity->setNameSuffix('Jr.');
-                $entity->setStatus(new LeadStatus('New'));
+                
+                $statusName = 'New';
+                $className = ExtendHelper::buildEnumValueClassName(Lead::INTERNAL_STATUS_CODE);
+                $id = ExtendHelper::buildEnumValueId($statusName);
+                $entity->setStatus(new $className($id, $statusName));
+                
                 $entity->setJobTitle('Manager');
                 $entity->addPhone(new LeadPhone('585-255-1127'));
+                $entity->addPhone(new LeadPhone('978-242-1314'));
                 $entity->setWebsite('http://orocrm.com');
                 $entity->setNumberOfEmployees(100);
                 $entity->setIndustry('Internet');
