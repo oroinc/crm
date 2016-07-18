70ccf4ae3d81c9850f678ed5f79c9353bd3d7488
Olga Syzova <osizova@magecore.com>
2016-07-18 19:58:38 +0300

    CRM-5719: Fatal is occurred on Magento Customers page with restricted Inegration entity     - fixed fatal error for Magento Customers grid - fixed fatal error for Magento Newsletter subscribers grid     - added ability to create Magento Customer with all ACL permission for Integration = NONE - added ability to edit Magento Customer with ASSIGN ACL permission for Integration - fixed select store grid (CRM-5892)

--- src/OroCRM/Bundle/MagentoBundle/Form/Type/CustomerGroupSelectType.php
+++ src/OroCRM/Bundle/MagentoBundle/Form/Type/CustomerGroupSelectType.php
@@ -4,10 +4,32 @@ namespace OroCRM\Bundle\MagentoBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\Options;
+
+use Oro\Bundle\SecurityBundle\SecurityFacade;
 
 class CustomerGroupSelectType extends AbstractType
 {
     const NAME = 'orocrm_magento_customer_group_select';
+
+    /**
+     * @var SecurityFacade
+     */
+    protected $securityFacade;
+
+    /**
+     * @var boolean
+     */
+    protected $canAssignChannel;
+
+    /**
+     * @param SecurityFacade $securityFacade
+     */
+    public function __construct(SecurityFacade $securityFacade)
+    {
+        $this->securityFacade = $securityFacade;
+        $this->canAssignChannel = $this->securityFacade->isGranted('oro_integration_assign');
+    }
 
     /**
      * {@inheritdoc}
@@ -21,6 +43,18 @@ class CustomerGroupSelectType extends AbstractType
                 'configs' => [
                     'placeholder' => 'orocrm.magento.customergroup.placeholder'
                 ]
+            ]
+        );
+
+        // Set store form type readonly if ASSIGN permissions for integration not set
+        $resolver->setNormalizers(
+            [
+                'disabled' => function (Options $options, $value) {
+                    return $this->isReadOnly($options) ? true : $value;
+                },
+                'validation_groups' => function (Options $options, $value) {
+                    return $options['disabled'] ? false : $value;
+                },
             ]
         );
     }
@@ -39,5 +73,20 @@ class CustomerGroupSelectType extends AbstractType
     public function getParent()
     {
         return 'oro_entity_create_or_select_inline_channel_aware';
+    }
+
+    /**
+     * Checks if the form type should be read-only or not
+     *
+     * @param array $options
+     *
+     * @return bool
+     */
+    protected function isReadOnly($options)
+    {
+        if (!$this->canAssignChannel) {
+            return true;
+        }
+        return false;
     }
 }
