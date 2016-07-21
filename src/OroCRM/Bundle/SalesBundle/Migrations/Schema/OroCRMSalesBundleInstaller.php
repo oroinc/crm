871b1ba6bae5992c5dfa6e1e13e016a9fed836fd
Ivaylo Badinov <venimus@users.noreply.github.com>
2016-07-21 18:14:33 +0300

    CRM-5084: Refactor migration

--- src/OroCRM/Bundle/SalesBundle/Migrations/Schema/OroCRMSalesBundleInstaller.php
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/OroCRMSalesBundleInstaller.php
@@ -635,6 +635,10 @@ class OroCRMSalesBundleInstaller implements
      */
     protected function addOrocrmSalesOpportunityStatusField(Schema $schema, QueryBag $queries)
     {
+        $immutableCodes = ['in_progress', 'won', 'lost'];
+
+        AddOpportunityStatus::addStatusField($schema, $this->extendExtension, $immutableCodes);
+
         $statuses = [
             'identification_alignment' => 'Identification & Alignment',
             'needs_analysis' => 'Needs Analysis',
@@ -645,22 +649,7 @@ class OroCRMSalesBundleInstaller implements
             'lost' => 'Closed Lost',
         ];
 
-        $immutable = [
-            'in_progress',
-            'won',
-            'lost',
-        ];
-
-        $defaultValue = 'in_progress';
-
-        AddOpportunityStatus::addStatusField(
-            $schema,
-            $this->extendExtension,
-            $queries,
-            $statuses,
-            $immutable,
-            $defaultValue
-        );
+        AddOpportunityStatus::addEnumValues($queries, $statuses);
     }
 
     /**
