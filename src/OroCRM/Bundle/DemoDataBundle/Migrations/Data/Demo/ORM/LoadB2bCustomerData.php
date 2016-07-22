spolischook/travis-nginx-test

CRM-5819: Add collection fields to entity (emails, phones)  -  create collection entities, change phone data provider, fix fixtures

Migrations/Data/B2B/ORM/B2B/LoadB2bCustomerData.php
@@ -10,6 +10,8 @@ use Oro\Bundle\AddressBundle\Entity\Address;
 use OroCRM\Bundle\ContactBundle\Entity\Contact;
 use OroCRM\Bundle\ContactBundle\Entity\ContactAddress;
 use OroCRM\Bundle\SalesBundle\Entity\B2bCustomer;
+use OroCRM\Bundle\SalesBundle\Entity\B2bCustomerEmail;
+use OroCRM\Bundle\SalesBundle\Entity\B2bCustomerPhone;
 use OroCRMPro\Bundle\DemoDataBundle\Migrations\Data\B2C\ORM\AbstractFixture;
 
 class LoadB2bCustomerData extends AbstractFixture implements OrderedFixtureInterface
@@ -67,6 +69,14 @@ class LoadB2bCustomerData extends AbstractFixture implements OrderedFixtureInter
         }
 
         $this->addCustomerAddress($customer, $contact);
+
+        $customerPhone = new B2bCustomerPhone($customerData['TelephoneNumber']);
+        $customerPhone->setPrimary(true);
+        $customer->addPhone($customerPhone);
+
+        $email = new B2bCustomerEmail($customerData['EmailAddress']);
+        $email->setPrimary(true);
+        $customer->addEmail($email);
 
         return $customer;
     }

oroinc/crm

src/OroCRM/Bundle/DemoDataBundle/Migrations/Data/Demo/ORM/LoadB2bCustomerData.php
@@ -11,6 +11,7 @@ use Oro\Bundle\OrganizationBundle\Entity\Organization;
 use OroCRM\Bundle\AccountBundle\Entity\Account;
 use OroCRM\Bundle\ChannelBundle\Entity\Channel;
 use OroCRM\Bundle\SalesBundle\Entity\B2bCustomer;
+use OroCRM\Bundle\SalesBundle\Entity\B2bCustomerPhone;
 
 class LoadB2bCustomerData extends AbstractDemoFixture implements DependentFixtureInterface
 {
@@ -82,6 +83,10 @@ class LoadB2bCustomerData extends AbstractDemoFixture implements DependentFixtur
         $customer->setOwner($this->getRandomUserReference());
         $customer->setAccount($this->getAccount());
         $customer->setOrganization($organization);
+
+        $phone = new B2bCustomerPhone($data['TelephoneNumber']);
+        $phone->setPrimary(true);
+        $customer->addPhone($phone);
 
         $address->setCity($data['City']);
         $address->setStreet($data['StreetAddress']);