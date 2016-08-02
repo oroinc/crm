7db5fe5ca9559a5d02079d05f6a6d8036cdece09
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2016-08-02 10:32:09 +0200

    OEE-880: User cannot see Magento customers - 500 error appears

---
+++ src/OroCRM/Bundle/DemoDataBundle/Migrations/Data/ORM/UpdateIntegrationAccessLevels.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace OroCRM\Bundle\DemoDataBundle\Migrations\Data\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Oro\Bundle\SecurityBundle\Acl\Persistence\AclManager;
+use Oro\Bundle\UserBundle\Entity\Role;
+
+class UpdateIntegrationAccessLevels extends AbstractFixture implements
+    ContainerAwareInterface,
+    DependentFixtureInterface
+{
+    /** @var ContainerInterface */
+    protected $container;
+
+    /** @var ObjectManager */
+    protected $objectManager;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDependencies()
+    {
+        return ['OroCRM\Bundle\DemoDataBundle\Migrations\Data\ORM\LoadRolesData'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->objectManager = $manager;
+
+        /** @var AclManager $aclManager */
+        $aclManager = $this->container->get('oro_security.acl.manager');
+
+        if ($aclManager->isAclEnabled()) {
+            $this->addIntegrationPermissions($aclManager);
+            $aclManager->flush();
+        }
+    }
+
+    /**
+     * @param AclManager $manager
+     */
+    protected function addIntegrationPermissions(AclManager $manager)
+    {
+        $roles = [
+            'ROLE_SALES_REP',
+            'ROLE_ONLINE_SALES_REP',
+            'ROLE_LEADS_DEVELOPMENT_REP',
+        ];
+
+        $oid = $manager->getOid('entity:Oro\Bundle\IntegrationBundle\Entity\Channel');
+        foreach ($roles as $roleName) {
+            $role = $this->getRole($roleName);
+            if ($role) {
+                $sid = $manager->getSid($role);
+
+                $maskBuilder = $manager->getMaskBuilder($oid)
+                    ->add('VIEW_SYSTEM');
+                $manager->setPermission($sid, $oid, $maskBuilder->get());
+            }
+        }
+    }
+
+    /**
+     * @param string $roleName
+     * @return Role|null
+     */
+    protected function getRole($roleName)
+    {
+        return $this->objectManager->getRepository('OroUserBundle:Role')->findOneBy(['role' => $roleName]);
+    }
+}
