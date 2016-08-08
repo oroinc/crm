2bade08131db64b13d0167f861283bd4f2a974aa
Maksym Perepelytsya <mperepelytsya@magecore.com>
2016-08-08 12:50:46 +0300

    CRM-5995: Pipeline filter in dashboard widgets - Forecast Provider fixes  - add migration to fill closedAt in audit
    Conflicts:
    	package/crm/src/OroCRM/Bundle/SalesBundle/Migrations/Schema/OroCRMSalesBundleInstaller.php

---
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_25_1/AddClosedAtField.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace OroCRM\Bundle\SalesBundle\Migrations\Schema\v1_28;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+use OroCRM\Bundle\SalesBundle\Migration\FillClosedAtField;
+
+class AddClosedAtField implements Migration
+{
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $table = $schema->getTable('orocrm_sales_opportunity');
+        if (!$table->hasColumn('closed_at')) {
+            $table->addColumn('closed_at', 'datetime', ['notnull' => false, 'comment' => '(DC2Type:datetime)']);
+            $queries->addPostQuery(new FillClosedAtField());
+        }
+    }
+}
