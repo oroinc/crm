4ea6557fb33b3f472db64fdff0ba1afe912639a3
Maksym Perepelytsya <mperepelytsya@magecore.com>
2016-08-08 17:22:59 +0300

    CRM-5995: Pipeline filter in dashboard widgets - Forecast Provider fixes  - fix indent

--- src/OroCRM/Bundle/SalesBundle/Migration/FillClosedAtField.php
+++ src/OroCRM/Bundle/SalesBundle/Migration/FillClosedAtField.php
@@ -37,29 +37,29 @@ class FillClosedAtField extends ParametrizedMigrationQuery
 INSERT INTO oro_audit_field
 (audit_id, FIELD, data_type, new_datetime)
 SELECT
-	a.id AS audit_id,
-	'closedAt' AS field,
-	'datetime' AS data_type,
-	(
-		CASE
-			WHEN af.new_text IN (:statuses) THEN a.logged_at
-			ELSE NULL
-		END
-	) AS new_datetime
+    a.id AS audit_id,
+    'closedAt' AS field,
+    'datetime' AS data_type,
+    (
+        CASE
+            WHEN af.new_text IN (:statuses) THEN a.logged_at
+            ELSE NULL
+        END
+    ) AS new_datetime
 FROM oro_audit_field af
 JOIN oro_audit a ON a.id = af.audit_id
 INNER JOIN
 (
-	SELECT MAX(af1.id) id FROM oro_audit_field af1 JOIN oro_audit a1 ON a1.id = af1.audit_id WHERE
-		a1.object_class = :objectClass
-		AND af1.field = :field
-		AND
-			(
-				af1.old_text IN (:statuses)
-				OR
-				af1.new_text IN (:statuses)
-			)
-		GROUP BY a1.object_id
+    SELECT MAX(af1.id) id FROM oro_audit_field af1 JOIN oro_audit a1 ON a1.id = af1.audit_id WHERE
+        a1.object_class = :objectClass
+        AND af1.field = :field
+                AND
+            (
+                af1.old_text IN (:statuses)
+                OR
+                af1.new_text IN (:statuses)
+            )
+        GROUP BY a1.object_id
 ) af1 ON af1.id = af.id
 GROUP BY object_id
 SQL;
