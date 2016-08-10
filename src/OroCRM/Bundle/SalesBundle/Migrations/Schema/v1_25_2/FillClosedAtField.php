9c942c2b3fd4cbd7a99ffa19b5188b5da24922aa
Maxym Perepelytsya <mperepelytsya@magecore.com>
2016-08-11 02:51:02 +0300

    CRM-5995: Pipeline filter in dashboard widgets - Forecast Provider fixes  - fix postgre sql in migration

--- src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_25_2/FillClosedAtField.php
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_25_2/FillClosedAtField.php
@@ -4,11 +4,14 @@ namespace OroCRM\Bundle\SalesBundle\Migrations\Schema\v1_25_2;
 
 use Psr\Log\LoggerInterface;
 
+use Doctrine\DBAL\Platforms\MySqlPlatform;
+use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Types\Type;
 
 use Oro\Bundle\MigrationBundle\Migration\ArrayLogger;
 use Oro\Bundle\MigrationBundle\Migration\ParametrizedMigrationQuery;
+use Oro\Bundle\MigrationBundle\Exception\UnsupportedDatabasePlatformException;
 
 use OroCRM\Bundle\SalesBundle\Entity\Opportunity;
 
@@ -77,7 +80,7 @@ INNER JOIN
             )
         GROUP BY a1.object_id
 ) af1 ON af1.id = af.id
-GROUP BY object_id
+GROUP BY object_id, a.id, af.new_text
 SQL;
         $params         = [
             'field'       => 'status',
@@ -100,10 +103,36 @@ SQL;
     /**
      * @param LoggerInterface $logger
      * @param bool            $dryRun
+     *
+     * @throws UnsupportedDatabasePlatformException
      */
     protected function updateOpportunityClosedAtValue(LoggerInterface $logger, $dryRun)
     {
-        $updateSql = <<<SQL
+        $platform = $this->connection->getDatabasePlatform();
+
+        if ($platform instanceof PostgreSqlPlatform) {
+            $updateSql = <<<SQL
+UPDATE orocrm_sales_opportunity o
+SET closed_at = afm.logged_at
+FROM 
+oro_audit a
+INNER JOIN
+(
+    SELECT
+    MAX(af.audit_id) AS max_audit_id,
+    MAX(am.logged_at) AS logged_at
+    FROM oro_audit_field af
+    INNER JOIN oro_audit am ON am.id = af.audit_id AND am.object_class = :objectClass
+    WHERE af.field = :field AND af.new_text IN (:statuses)
+    GROUP BY am.object_id
+) afm
+ON afm.max_audit_id = a.id
+
+WHERE o.status_id IN (:status_ids) and 
+a.object_id = o.id AND a.object_class = :objectClass
+SQL;
+        } elseif ($platform instanceof MySqlPlatform) {
+            $updateSql = <<<SQL
 UPDATE orocrm_sales_opportunity o
 INNER JOIN oro_audit a ON a.object_id = o.id AND a.object_class = :objectClass
 INNER JOIN
@@ -120,13 +149,18 @@ ON afm.max_audit_id = a.id
 SET o.closed_at = afm.logged_at
 WHERE o.status_id IN (:status_ids)
 SQL;
-        $params    = [
+        } else {
+            throw new UnsupportedDatabasePlatformException(
+                sprintf('Platform %s is not supported', $platform->getName())
+            );
+        }
+        $params = [
             'field'       => 'status',
             'statuses'    => ['Closed Lost', 'Closed Won', 'Lost', 'Won'],
             'objectClass' => 'OroCRM\Bundle\SalesBundle\Entity\Opportunity',
             'status_ids'  => [Opportunity::STATUS_WON, Opportunity::STATUS_LOST]
         ];
-        $types     = [
+        $types  = [
             'field'       => Type::STRING,
             'statuses'    => Connection::PARAM_STR_ARRAY,
             'objectClass' => Type::STRING,
