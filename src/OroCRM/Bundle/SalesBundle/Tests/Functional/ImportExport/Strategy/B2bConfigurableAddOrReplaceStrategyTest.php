725dedaf5bcef10ed727586c4e67515ab2ce2485
Dmytro Krushelnytskyi <dkrushelnytskyi@magecore.com>
2016-08-23 18:22:24 +0300

    CRM-6051: B2B customer, address removal by CSV import update - fix functional test

--- src/OroCRM/Bundle/SalesBundle/Tests/Functional/ImportExport/Strategy/B2bConfigurableAddOrReplaceStrategyTest.php
+++ src/OroCRM/Bundle/SalesBundle/Tests/Functional/ImportExport/Strategy/B2bConfigurableAddOrReplaceStrategyTest.php
@@ -55,8 +55,6 @@ class B2bConfigurableAddOrReplaceStrategyTest extends WebTestCase
             $container->get('oro_importexport.strategy.import.helper'),
             $container->get('oro_importexport.field.field_helper'),
             $container->get('oro_importexport.field.database_helper'),
-            $container->get('oro_entity.entity_class_name_provider'),
-            $container->get('translator'),
             $container->get('oro_entity.doctrine_helper')
         );
 
