7e70b1d6a1136e1c3335bbec2dc62a0b5a76669e
Dmytro Krushelnytskyi <dkrushelnytskyi@magecore.com>
2016-08-23 17:51:24 +0300

    CRM-6051: B2B customer, address removal by CSV import update - fix functional test

--- src/OroCRM/Bundle/SalesBundle/Tests/Functional/ImportExport/Strategy/B2bConfigurableAddOrReplaceStrategyTest.php
+++ src/OroCRM/Bundle/SalesBundle/Tests/Functional/ImportExport/Strategy/B2bConfigurableAddOrReplaceStrategyTest.php
@@ -57,7 +57,6 @@ class B2bConfigurableAddOrReplaceStrategyTest extends WebTestCase
             $container->get('oro_importexport.field.database_helper'),
             $container->get('oro_entity.entity_class_name_provider'),
             $container->get('translator'),
-            $container->get('oro_importexport.strategy.new_entities_helper'),
             $container->get('oro_entity.doctrine_helper')
         );
 
@@ -98,8 +97,8 @@ class B2bConfigurableAddOrReplaceStrategyTest extends WebTestCase
         /** @var B2bCustomer $existedCustomer */
         $existedCustomer = $this->getReference('default_b2bcustomer');
         self::assertEquals('1215 Caldwell Road', $existedCustomer->getShippingAddress()->getStreet());
-        $this->strategy->process($newB2bCustomer);
-        self::assertEquals('Test1', $existedCustomer->getShippingAddress()->getStreet());
+        $entity = $this->strategy->process($newB2bCustomer);
+        self::assertEquals('Test1', $entity->getShippingAddress()->getStreet());
     }
 
     public function testUpdateCustomerByEmptyAddress()
@@ -118,8 +117,8 @@ class B2bConfigurableAddOrReplaceStrategyTest extends WebTestCase
         /** @var B2bCustomer $existedCustomer */
         $existedCustomer = $this->getReference('default_b2bcustomer');
         self::assertEquals('1215 Caldwell Road', $existedCustomer->getShippingAddress()->getStreet());
-        $this->strategy->process($newB2bCustomer);
-        self::assertNull($existedCustomer->getShippingAddress());
+        $entity = $this->strategy->process($newB2bCustomer);
+        self::assertNull($entity->getShippingAddress());
     }
 
     public function testUpdateRegionText()
@@ -153,9 +152,9 @@ class B2bConfigurableAddOrReplaceStrategyTest extends WebTestCase
         /** @var B2bCustomer $existedCustomer */
         $existedCustomer = $this->getReference('default_b2bcustomer');
         self::assertEquals('Arizona1', $existedCustomer->getShippingAddress()->getRegionText());
-        $this->strategy->process($newB2bCustomer);
-        self::assertEquals('test', $existedCustomer->getShippingAddress()->getRegionText());
-        self::assertEquals('test', $existedCustomer->getBillingAddress()->getRegionText());
+        $entity = $this->strategy->process($newB2bCustomer);
+        self::assertEquals('test', $entity->getShippingAddress()->getRegionText());
+        self::assertEquals('test', $entity->getBillingAddress()->getRegionText());
     }
 
     public function testConvertRegionTextToRegion()
@@ -191,9 +190,9 @@ class B2bConfigurableAddOrReplaceStrategyTest extends WebTestCase
         $existedCustomer = $this->getReference('default_b2bcustomer');
         self::assertEquals('Arizona1', $existedCustomer->getShippingAddress()->getRegionText());
         self::assertNull($existedCustomer->getShippingAddress()->getRegion());
-        $this->strategy->process($newB2bCustomer);
-        self::assertNull($existedCustomer->getShippingAddress()->getRegionText());
-        self::assertNull($existedCustomer->getBillingAddress()->getRegionText());
+        $entity = $this->strategy->process($newB2bCustomer);
+        self::assertNull($entity->getShippingAddress()->getRegionText());
+        self::assertNull($entity->getBillingAddress()->getRegionText());
 
         $exceptedRegion = $this->getContainer()->get('doctrine')->getRepository('OroAddressBundle:Region')
             ->findOneBy(
@@ -203,7 +202,7 @@ class B2bConfigurableAddOrReplaceStrategyTest extends WebTestCase
                 ]
             );
 
-        self::assertEquals($exceptedRegion, $existedCustomer->getShippingAddress()->getRegion());
+        self::assertEquals($exceptedRegion, $entity->getShippingAddress()->getRegion());
     }
 
     public function testNewB2bCustomerWithRegionText()
@@ -218,7 +217,7 @@ class B2bConfigurableAddOrReplaceStrategyTest extends WebTestCase
         $address->setRegionText('Arizona');
 
         $account = new Account();
-        $account->setName('some account name 1');
+        $account->setName('some account name');
 
         $channel = new Channel();
         $channel->setName('b2b Channel');
