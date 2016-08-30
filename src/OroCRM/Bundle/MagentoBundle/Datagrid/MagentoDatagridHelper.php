352759556cd57c255ddc10e8d6c9cfaee645a778
Ivaylo Badinov <venimus@users.noreply.github.com>
2016-08-30 18:04:02 +0300

    CRM-4994: InclydeAdd channel name in group labels

--- src/OroCRM/Bundle/MagentoBundle/Datagrid/MagentoDatagridHelper.php
+++ src/OroCRM/Bundle/MagentoBundle/Datagrid/MagentoDatagridHelper.php
@@ -2,12 +2,22 @@
 
 namespace OroCRM\Bundle\MagentoBundle\Datagrid;
 
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 
+use OroCRM\Bundle\MagentoBundle\Entity\CustomerGroup;
 use OroCRM\Bundle\MagentoBundle\Provider\ChannelType;
 
 class MagentoDatagridHelper
 {
+    /** @var EntityManager */
+    protected $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * Returns query builder callback for magento channels
      *
@@ -20,5 +30,25 @@ class MagentoDatagridHelper
                 ->where('c.type = :type')
                 ->setParameter('type', ChannelType::TYPE);
         };
+    }
+
+    /**
+     * Returns choices for Magento Customer Group filter
+     * Labels include integration channel name if any
+     *
+     * @return array
+     */
+    public function getMagentoGroupsChoices()
+    {
+        $er = $this->em->getRepository(CustomerGroup::class);
+
+        $qb = $er->createQueryBuilder('g')
+            ->select('g.id as id')
+            ->addSelect('CONCAT(g.name, COALESCE(CONCAT(\' (\', gc.name, \')\'), \'\')) as name')
+            ->leftJoin('g.channel', 'gc');
+        $groups = $qb->getQuery()->getArrayResult();
+        $choices = array_combine(array_column($groups, 'id'), array_column($groups, 'name'));
+
+        return $choices;
     }
 }
