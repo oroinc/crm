da95426e08182f060bd76ae4ab62a120725f6205
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2016-09-13 17:11:12 +0200

    BAP-11849: Error is occurred during tracking events processing
    - exclude events without website from summary

--- src/OroCRM/Bundle/CampaignBundle/Command/CalculateTrackingEventSummaryCommand.php
+++ src/OroCRM/Bundle/CampaignBundle/Command/CalculateTrackingEventSummaryCommand.php
@@ -109,7 +109,7 @@ class CalculateTrackingEventSummaryCommand extends ContainerAwareCommand impleme
 
         $em = $this->getEntityManager($this->getTrackingEventSummaryEntityClass());
         foreach ($events as $event) {
-            $website = !$event['websiteId'] ? null : $this->getDoctrineHelper()
+            $website = $this->getDoctrineHelper()
                 ->getEntityReference(
                     $this->getTrackingWebsiteEntityClass(),
                     $event['websiteId']
