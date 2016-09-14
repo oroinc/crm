832d4a534f709f64c784795c5295fe03e07b86f1
Olga Syzova <osizova@magecore.com>
2016-09-14 12:06:41 +0300

    CRM-6045: Not found entity Magento Customer during Magento sync - refactored import strategies to avoid complexity

--- src/OroCRM/Bundle/MagentoBundle/ImportExport/Strategy/CustomerStrategy.php
+++ src/OroCRM/Bundle/MagentoBundle/ImportExport/Strategy/CustomerStrategy.php
@@ -136,39 +136,11 @@ class CustomerStrategy extends AbstractImportStrategy
                 }
             }
         } elseif ($entity instanceof Region) {
+            /** @var Region $existingEntity */
             $existingEntity = $this->findRegionEntity($entity);
-
         } else {
             /** @var Customer $existingEntity */
             $existingEntity = parent::findExistingEntity($entity, $searchContext);
-        }
-
-        return $existingEntity;
-    }
-
-    /**
-     * @param Region $entity
-     *
-     * @return null|object
-     */
-    protected function findRegionEntity($entity)
-    {
-        $existingEntity = null;
-
-        /** @var \OroCRM\Bundle\MagentoBundle\Entity\Region $magentoRegion */
-        $magentoRegion = $this->databaseHelper->findOneBy(
-            'OroCRM\Bundle\MagentoBundle\Entity\Region',
-            [
-                'regionId' => $entity->getCombinedCode()
-            ]
-        );
-        if ($magentoRegion) {
-            $existingEntity = $this->databaseHelper->findOneBy(
-                'Oro\Bundle\AddressBundle\Entity\Region',
-                [
-                    'combinedCode' => $magentoRegion->getCombinedCode()
-                ]
-            );
         }
 
         return $existingEntity;
