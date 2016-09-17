58c9f34047b7628e00021f1c08c268460dc2e51e
Vova Soroka <vsoroka@magecore.com>
2016-09-17 19:33:50 +0300

    BAP-11780: Move phones and emails from relationships to attributes

--- src/OroCRM/Bundle/SalesBundle/DependencyInjection/OroCRMSalesExtension.php
+++ src/OroCRM/Bundle/SalesBundle/DependencyInjection/OroCRMSalesExtension.php
@@ -19,6 +19,7 @@ class OroCRMSalesExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('services_api.yml');
         $loader->load('form.yml');
         $loader->load('importexport.yml');
 
