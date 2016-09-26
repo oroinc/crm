7c13a66e11436655ac29eb2e8d1dc0ab6bbf987a
Makar Sichevoy <sichevoy@gmail.com>
2016-09-26 11:11:35 +0300

    BAP-11808: Move CRM bundles to Oro namespace

--- src/Oro/Bundle/CRMBundle/OroCRMBundle.php
+++ src/Oro/Bundle/CRMBundle/OroCRMBundle.php
@@ -4,7 +4,7 @@ namespace Oro\Bundle\CRMBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class OroBundle extends Bundle
+class OroCRMBundle extends Bundle
 {
     const PACKAGE_NAME = 'oro/crm';
 }
