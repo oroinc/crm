313173b8d0d1efbc8e12bc7a999065b8186b5318
Dmytro Krushelnytskyi <dkrushelnytskyi@magecore.com>
2016-10-06 20:01:22 +0300

    BAP-11824: Migration for activity - rename OroCRMCallBrindgeBundle to OroCallCRMBrindgeBundle - rename OroCRMTaskBrindgeBundle to OroTaskCRMBrindgeBundle - update migration to rename oro_rel_* tables

---
+++ src/Oro/Bridge/TaskCRM/Migrations/Schema/OroTaskCRMBundleInstaller.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Oro\Bridge\TaskCRM\Migrations\Schema;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtension;
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtensionAwareInterface;
+use Oro\Bundle\MigrationBundle\Migration\Installation;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bridge\TaskCRM\Migrations\Schema\v1_0\OroTaskCRMBundle as TaskCRMBundle_v1_0;
+
+class OroTaskCRMBundleInstaller implements
+    Installation,
+    ActivityExtensionAwareInterface
+{
+    /** @var ActivityExtension */
+    protected $activityExtension;
+
+    /**
+     * @param ActivityExtension $activityExtension
+     */
+    public function setActivityExtension(ActivityExtension $activityExtension)
+    {
+        $this->activityExtension = $activityExtension;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMigrationVersion()
+    {
+        return 'v1_0';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        TaskCRMBundle_v1_0::addTaskActivityRelations($schema, $this->activityExtension);
+    }
+}
