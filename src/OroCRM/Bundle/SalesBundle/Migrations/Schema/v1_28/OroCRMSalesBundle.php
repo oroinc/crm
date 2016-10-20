5d554d19c6f4c2db6c1ab7faee36fc938b6f9385
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2016-10-20 13:29:36 +0200

    CRM-6391: Adding Leads & Opportunities to Magento channels should be disabled Disappear Sales and Custom channels

--- src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_28/OroCRMSalesBundle.php
+++ src/OroCRM/Bundle/SalesBundle/Migrations/Schema/v1_28/OroCRMSalesBundle.php
@@ -2,9 +2,11 @@
 
 namespace OroCRM\Bundle\SalesBundle\Migrations\Schema\v1_28;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Schema\Schema;
 
 use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\ParametrizedSqlMigrationQuery;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
 class OroCRMSalesBundle implements Migration
@@ -15,5 +17,20 @@ class OroCRMSalesBundle implements Migration
     public function up(Schema $schema, QueryBag $queries)
     {
         $queries->addQuery(new UpdateFeatureConfig());
+
+        $queries->addQuery(
+            new ParametrizedSqlMigrationQuery(
+                'DELETE FROM orocrm_channel_entity_name WHERE name IN (:entities)',
+                [
+                    'entities' => [
+                        'OroCRM\Bundle\SalesBundle\Entity\Lead',
+                        'OroCRM\Bundle\SalesBundle\Entity\Opportunity',
+                    ],
+                ],
+                [
+                    'entities' => Connection::PARAM_STR_ARRAY,
+                ]
+            )
+        );
     }
 }
