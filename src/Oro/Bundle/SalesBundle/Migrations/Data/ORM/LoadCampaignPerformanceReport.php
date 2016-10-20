8c248f8f9d598d9f157a071b2d4386d728eca173
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2016-10-20 17:33:39 +0200

    CRM-6416: Add configs "segments", "reports" into features.yml
    - update fixtures with internal ids
    
    Conflicts:
    	package/crm/src/Oro/Bundle/SalesBundle/Migrations/Data/ORM/LoadCampaignPerformanceReport.php
    	package/demo-data/Migrations/Data/B2B/ORM/data/reports/reports.csv
    	package/demo-data/Migrations/Data/B2C/ORM/data/reports/reports.csv
    	package/demo-data/Migrations/Data/Multi/ORM/data/reports/reports.csv

--- src/Oro/Bundle/SalesBundle/Migrations/Data/ORM/LoadCampaignPerformanceReport.php
+++ src/Oro/Bundle/SalesBundle/Migrations/Data/ORM/LoadCampaignPerformanceReport.php
@@ -51,6 +51,7 @@ class LoadCampaignPerformanceReport extends AbstractFixture implements
 
         $report = new Report();
         $report->setName('Campaign Performance');
+        $report->setInternalId('campaign_performance');
         $report->setEntity('Oro\Bundle\CampaignBundle\Entity\Campaign');
         $type = $em->getReference('OroReportBundle:ReportType', ReportType::TYPE_TABLE);
         $report->setType($type);
