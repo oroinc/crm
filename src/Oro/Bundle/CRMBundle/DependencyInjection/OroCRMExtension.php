c7d4fc9e91af4b3b7dd8fadbc4932524b5a81980
Maxym Perepelytsya <mperepelytsya@magecore.com>
2016-10-21 03:58:53 +0300

    CRM-6332: Enable Leads & Opportunities as a feature  - after namespace changing fixes

--- src/Oro/Bundle/CRMBundle/DependencyInjection/OroCRMExtension.php
+++ src/Oro/Bundle/CRMBundle/DependencyInjection/OroCRMExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class OroExtension extends Extension
+class OroCRMExtension extends Extension
 {
     /**
      * {@inheritDoc}
