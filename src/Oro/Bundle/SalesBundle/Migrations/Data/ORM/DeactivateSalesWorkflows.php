bbdef7f0b1c6c352d6c27993b606de736b701b67
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2016-10-27 15:05:52 +0200

    CRM-6415: Review, testing, demo sessions

--- src/Oro/Bundle/SalesBundle/Migrations/Data/ORM/DeactivateSalesWorkflows.php
+++ src/Oro/Bundle/SalesBundle/Migrations/Data/ORM/DeactivateSalesWorkflows.php
@@ -8,7 +8,6 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-use Oro\Bundle\WorkflowBundle\Exception\WorkflowNotFoundException;
 use Oro\Bundle\WorkflowBundle\Model\WorkflowManager;
 
 class DeactivateSalesWorkflows extends AbstractFixture implements ContainerAwareInterface
@@ -28,20 +27,36 @@ class DeactivateSalesWorkflows extends AbstractFixture implements ContainerAware
 
     /**
      * {@inheritDoc}
+     *
+     * @todo remove enabling features once it's possible to use workflow in code if features are disabled
      */
     public function load(ObjectManager $manager)
     {
+        $requiredFeatureToggles = [
+            'oro_sales.lead_feature_enabled',
+            'oro_sales.opportunity_feature_enabled',
+        ];
+
+        $originalFeatureTogglesSetting = [];
+
+        $cm = $this->container->get('oro_config.global');
+        foreach ($requiredFeatureToggles as $toggle) {
+            $originalFeatureTogglesSetting[$toggle] = $cm->get($toggle, false, true);
+            $cm->set($toggle, true);
+        }
+
         /** @var WorkflowManager $workflowManager */
         $workflowManager = $this->container->get('oro_workflow.manager');
 
-        try {
-            $workflowManager->deactivateWorkflow('b2b_flow_lead');
-        } catch (WorkflowNotFoundException $e) {
-        }
+        $workflowManager->deactivateWorkflow('b2b_flow_lead');
+        $workflowManager->deactivateWorkflow('opportunity_flow');
 
-        try {
-            $workflowManager->deactivateWorkflow('opportunity_flow');
-        } catch (WorkflowNotFoundException $e) {
+        foreach ($originalFeatureTogglesSetting as $toggle => $setting) {
+            if (!isset($setting['use_parent_scope_value']) || $setting['use_parent_scope_value']) {
+                $cm->reset($toggle);
+            } else {
+                $cm->set($toggle, $setting['value']);
+            }
         }
     }
 }
