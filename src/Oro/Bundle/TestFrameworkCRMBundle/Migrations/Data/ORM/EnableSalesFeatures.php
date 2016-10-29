2153ac4d40bfdf054db66ce72fff203f50c4d49b
Denis Voronin <hurricane.voronin@gmail.com>
2016-10-29 17:37:05 +0300

    CRM-6332: Enable Leads & Opportunities as a feature  - Enable Lead and Opportunity feature for test environment for tests after upgrade

--- src/Oro/Bundle/TestFrameworkCRMBundle/Migrations/Data/ORM/EnableSalesFeatures.php
+++ src/Oro/Bundle/TestFrameworkCRMBundle/Migrations/Data/ORM/EnableSalesFeatures.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
 use Oro\Bundle\ConfigBundle\Config\GlobalScopeManager;
 
-class UpdateFeaturesConfigs extends AbstractFixture implements ContainerAwareInterface
+class EnableSalesFeatures extends AbstractFixture implements ContainerAwareInterface
 {
     use ContainerAwareTrait;
 
@@ -20,8 +20,14 @@ class UpdateFeaturesConfigs extends AbstractFixture implements ContainerAwareInt
     public function load(ObjectManager $manager)
     {
         $configManager = $this->getConfigManager();
+
+        // Enable Lead and Opportunity features, for tests after upgrade
+        $configManager->set('oro_sales.lead_feature_enabled', true);
+        $configManager->set('oro_sales.opportunity_feature_enabled', true);
+
         // Enable Sales Funnel feature for REST API tests
         $configManager->set('oro_sales.salesfunnel_feature_enabled', true);
+
         $configManager->flush();
     }
 
