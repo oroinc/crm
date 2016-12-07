eff3cb7db378c63277b1a0f8c9aae137c5d65779
Miloslav Nenadál <nenadalm@gmail.com>
2016-12-07 15:29:34 +0100

    CRM-6383: Cannot edit opportunity after related account has been deleted (#5105)

--- src/Oro/Bundle/MagentoBundle/Migrations/Schema/OroMagentoBundleInstaller.php
+++ src/Oro/Bundle/MagentoBundle/Migrations/Schema/OroMagentoBundleInstaller.php
@@ -118,7 +118,7 @@ class OroMagentoBundleInstaller implements
      */
     public function getMigrationVersion()
     {
-        return 'v2_2';
+        return 'v2_3';
     }
 
     /**
@@ -1017,7 +1017,7 @@ class OroMagentoBundleInstaller implements
             $schema->getTable('orocrm_account'),
             ['account_id'],
             ['id'],
-            ['onDelete' => 'SET NULL']
+            ['onDelete' => 'CASCADE']
         );
         $table->addForeignKeyConstraint(
             $schema->getTable('oro_user'),
