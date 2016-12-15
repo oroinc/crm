227795476ed3d242d1a6c4729aee89548d2cbd82
Alexandr Parkhomenko <alexandr-parkhomenko@users.noreply.github.com>
2016-12-15 16:14:59 +0200

    BB-6045: Update handler and helper logic to support multi-currency field (#5269)
    - Added guesser for options in section 'multicurrency_config'
    - Chaged translation keys for base values
    - Change priority bundle of 'MultiCurrencyBundle'
    - Use method `updateMultiCurrencyFields` on event PreFlush instead of PrePersist and PreUpdate
    - Update method that do update order total or subtotal
    - Fixed MultiCurrencyTransformer

--- src/Oro/Bundle/SalesBundle/Entity/Opportunity.php
+++ src/Oro/Bundle/SalesBundle/Entity/Opportunity.php
@@ -63,6 +63,8 @@ use Oro\Bundle\ChannelBundle\Model\ChannelAwareInterface;
  * )
  *
  * @SuppressWarnings(PHPMD.ExcessivePublicCount)
+ * @SuppressWarnings(PHPMD.TooManyFields)
+ * @SuppressWarnings(PHPMD.ExcessiveClassLength)
  * @SuppressWarnings(PHPMD.ExcessiveClassComplexity)
  */
 class Opportunity extends ExtendOpportunity implements
@@ -542,47 +544,25 @@ class Opportunity extends ExtendOpportunity implements
     }
 
     /**
-     * @param MultiCurrency $budgetAmount|null
+     * @param MultiCurrency $budgetAmount
      * @return Opportunity
      */
-    public function setBudgetAmount(MultiCurrency $budgetAmount = null)
+    public function setBudgetAmount(MultiCurrency $budgetAmount)
     {
         $this->budgetAmount = $budgetAmount;
-        $this->updateMultiCurrencyFields();
 
         return $this;
     }
 
     /**
-     * @ORM\PrePersist
-     * @ORM\PreUpdate
+     * @ORM\PreFlush
      *
      * @return void
      */
     public function updateMultiCurrencyFields()
     {
-        if ($this->budgetAmount) {
-            $this->budgetAmountValue = $this->budgetAmount->getValue();
-
-            if (null !== $this->budgetAmountValue && '' !== $this->closeRevenueValue) {
-                $this->setBudgetAmountCurrency($this->budgetAmount->getCurrency());
-                $this->setBaseBudgetAmountValue($this->budgetAmount->getBaseCurrencyValue());
-            } else {
-                $this->setBudgetAmountCurrency(null);
-                $this->setBaseBudgetAmountValue(null);
-            }
-        }
-        if ($this->closeRevenue) {
-            $this->closeRevenueValue = $this->closeRevenue->getValue();
-
-            if (null !== $this->closeRevenueValue && '' !== $this->closeRevenueValue) {
-                $this->setCloseRevenueCurrency($this->closeRevenue->getCurrency());
-                $this->setBaseCloseRevenueValue($this->closeRevenue->getBaseCurrencyValue());
-            } else {
-                $this->setCloseRevenueCurrency(null);
-                $this->setBaseCloseRevenueValue(null);
-            }
-        }
+        $this->updateBudgetAmount();
+        $this->updateCloseRevenue();
     }
 
     /**
@@ -592,10 +572,7 @@ class Opportunity extends ExtendOpportunity implements
     public function setBaseBudgetAmountValue($value)
     {
         $this->baseBudgetAmountValue = $value;
-
-        if ($this->budgetAmount instanceof MultiCurrency) {
-            $this->budgetAmount->setBaseCurrencyValue($value);
-        }
+        $this->budgetAmount->setBaseCurrencyValue($value);
 
         return $this;
     }
@@ -615,10 +592,7 @@ class Opportunity extends ExtendOpportunity implements
     public function setBaseCloseRevenueValue($value)
     {
         $this->baseCloseRevenueValue = $value;
-
-        if ($this->closeRevenue instanceof MultiCurrency) {
-            $this->closeRevenue->setBaseCurrencyValue($value);
-        }
+        $this->closeRevenue->setBaseCurrencyValue($value);
 
         return $this;
     }
@@ -638,10 +612,7 @@ class Opportunity extends ExtendOpportunity implements
     public function setBudgetAmountCurrency($currency)
     {
         $this->budgetAmountCurrency = $currency;
-
-        if ($this->budgetAmount instanceof MultiCurrency) {
-            $this->budgetAmount->setCurrency($currency);
-        }
+        $this->budgetAmount->setCurrency($currency);
 
         return $this;
     }
@@ -653,10 +624,7 @@ class Opportunity extends ExtendOpportunity implements
     public function setBudgetAmountValue($budgetAmountValue)
     {
         $this->budgetAmountValue = $budgetAmountValue;
-
-        if ($this->budgetAmount instanceof MultiCurrency) {
-            $this->budgetAmount->setValue($budgetAmountValue);
-        }
+        $this->budgetAmount->setValue($budgetAmountValue);
 
         return $this;
     }
@@ -684,10 +652,7 @@ class Opportunity extends ExtendOpportunity implements
     public function setCloseRevenueCurrency($currency)
     {
         $this->closeRevenueCurrency = $currency;
-
-        if ($this->closeRevenue instanceof MultiCurrency) {
-            $this->closeRevenue->setCurrency($currency);
-        }
+        $this->closeRevenue->setCurrency($currency);
 
         return $this;
     }
@@ -699,10 +664,7 @@ class Opportunity extends ExtendOpportunity implements
     public function setCloseRevenueValue($closeRevenueValue)
     {
         $this->closeRevenueValue = $closeRevenueValue;
-
-        if ($this->closeRevenue instanceof MultiCurrency) {
-            $this->closeRevenue->setValue($closeRevenueValue);
-        }
+        $this->closeRevenue->setValue($closeRevenueValue);
 
         return $this;
     }
@@ -857,15 +819,12 @@ class Opportunity extends ExtendOpportunity implements
     }
 
     /**
-     * @param MultiCurrency $closeRevenue|null
+     * @param MultiCurrency $closeRevenue
      * @return Opportunity
      */
-    public function setCloseRevenue(MultiCurrency $closeRevenue = null)
+    public function setCloseRevenue(MultiCurrency $closeRevenue)
     {
-        if ($closeRevenue) {
-            $this->closeRevenue = $closeRevenue;
-            $this->updateMultiCurrencyFields();
-        }
+        $this->closeRevenue = $closeRevenue;
 
         return $this;
     }
@@ -1060,5 +1019,31 @@ class Opportunity extends ExtendOpportunity implements
     public static function getClosedStatuses()
     {
         return [self::STATUS_WON, self::STATUS_LOST];
+    }
+
+    protected function updateBudgetAmount()
+    {
+        $this->budgetAmountValue = $this->budgetAmount->getValue();
+        if (null !== $this->budgetAmountValue) {
+            $this->setBudgetAmountCurrency($this->budgetAmount->getCurrency());
+            $this->setBaseBudgetAmountValue($this->budgetAmount->getBaseCurrencyValue());
+            return;
+        }
+
+        $this->setBudgetAmountCurrency(null);
+        $this->setBaseBudgetAmountValue(null);
+    }
+
+    protected function updateCloseRevenue()
+    {
+        $this->closeRevenueValue = $this->closeRevenue->getValue();
+        if (null !== $this->closeRevenueValue) {
+            $this->setCloseRevenueCurrency($this->closeRevenue->getCurrency());
+            $this->setBaseCloseRevenueValue($this->closeRevenue->getBaseCurrencyValue());
+            return;
+        }
+
+        $this->setCloseRevenueCurrency(null);
+        $this->setBaseCloseRevenueValue(null);
     }
 }
