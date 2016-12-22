144cf79226eb08888c3ea5b595f0dcb3012faa65
Dincho Todorov <dincho@users.noreply.github.com>
2016-12-22 13:53:16 +0200

    CRM-6723: Adapt Business customer to the new Customer association
    - fix LTSV recalculation

--- src/Oro/Bundle/SalesBundle/Entity/Repository/B2bCustomerRepository.php
+++ src/Oro/Bundle/SalesBundle/Entity/Repository/B2bCustomerRepository.php
@@ -5,6 +5,8 @@ namespace Oro\Bundle\SalesBundle\Entity\Repository;
 use Doctrine\ORM\EntityRepository;
 
 use Oro\Bundle\CurrencyBundle\Query\CurrencyQueryBuilderTransformerInterface;
+use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
+use Oro\Bundle\SalesBundle\EntityConfig\CustomerScope;
 use Oro\Bundle\SalesBundle\Entity\B2bCustomer;
 
 class B2bCustomerRepository extends EntityRepository
@@ -23,13 +25,18 @@ class B2bCustomerRepository extends EntityRepository
         B2bCustomer $customer,
         CurrencyQueryBuilderTransformerInterface $qbTransformer
     ) {
+        $associationName = ExtendHelper::buildAssociationName(
+            B2bCustomer::class,
+            CustomerScope::ASSOCIATION_KIND
+        );
+
         $qb = $this->getEntityManager()->getRepository('OroSalesBundle:Opportunity')
             ->createQueryBuilder('o');
         $closeRevenueQuery = $qbTransformer->getTransformSelectQuery('closeRevenue', $qb);
         $qb->select(sprintf('SUM(%s)', $closeRevenueQuery));
-        $qb->innerJoin('o.customer', 'c');
+        $qb->innerJoin('o.customerAssociation', 'c');
         $qb->innerJoin('o.status', 's');
-        $qb->andWhere('c = :customer');
+        $qb->andWhere(sprintf('c.%s = :customer', $associationName));
         $qb->andWhere('s.id = :status');
         $qb->setParameter('customer', $customer);
         $qb->setParameter('status', self::VALUABLE_STATUS);
