7b951a2e815f91ac6a46b6d21fa7d6f4eb6becf1
Julian Atanasov <atanasov@vmandco.com>
2017-02-24 16:55:24 +0200

    CRM-6546: Break Business Sales Channel statistics widget into two
    - func tests for Lead Statistics widget

--- src/Oro/Bundle/SalesBundle/Tests/Functional/Widget/LeadStatistics.php
+++ src/Oro/Bundle/SalesBundle/Tests/Functional/Widget/LeadStatistics.php
@@ -2,9 +2,10 @@
 
 namespace Oro\Bundle\SalesBundle\Tests\Functional\Widget;
 
+use Symfony\Component\DomCrawler\Form;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\DomCrawler\Field\InputFormField;
-use Symfony\Component\DomCrawler\Form;
+use Symfony\Component\DomCrawler\Field\ChoiceFormField;
 
 use Oro\Bundle\DashboardBundle\Entity\Widget;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
@@ -49,10 +50,8 @@ class LeadStatistics extends WebTestCase
          */
         $crawler = $this->client->getCrawler();
         $form = $crawler->selectButton('Save')->form();
-        $this->createAdditionalDateRandgeFormElements($form);
+        $this->createAndSetDateRangeFormElements($form, ['type' => AbstractDateFilterType::TYPE_ALL_TIME]);
         $this->createMetricsElements($form);
-        $form['lead_statistics[dateRange][part]'] = 'value';
-        $form['lead_statistics[dateRange][type]'] = AbstractDateFilterType::TYPE_ALL_TIME;
         $form['lead_statistics[usePreviousInterval]'] = 1;
         $this->client->submit($form);
 
@@ -82,43 +81,176 @@ class LeadStatistics extends WebTestCase
             )
         );
         $this->assertEquals(
-            $openLeadsMetric->getNode(0)->nodeValue,
-            0,
+            $openLeadsMetric->text(),
+            1,
             '"Open Leads" metric doesn\'t much expected value !'
         );
-        $onewLeadsMetric = $crawler->filterXPath(
+        $newLeadsMetric = $crawler->filterXPath(
             $this->getMetricValueByLabel(
                 $this->metrics['new_leads_count']
             )
         );
         $this->assertEquals(
-            $onewLeadsMetric->getNode(0)->nodeValue,
-            0,
+            $newLeadsMetric->getNode(0)->nodeValue,
+            1,
             '"New Leads" metric doesn\'t much expected value !'
         );
     }
 
-    public function wudgetProvider()
+    /**
+     * @dataProvider widgetProvider
+     */
+    public function testCustomConfiguration(
+        $owners,
+        $dateRange,
+        $comparePrevious,
+        $advancedFilters,
+        $result,
+        $previousResult
+    ) {
+        $this->getConfigureDialog();
+
+        /**
+         * @var $crawler Crawler
+         * @var $form Form
+         */
+        $crawler = $this->client->getCrawler();
+        $form = $crawler->selectButton('Save')->form();
+        $form['lead_statistics[owners][users]'] = $owners;
+        $this->createMetricsElements($form);
+        $this->setComparePrevious($form, $comparePrevious);
+        $this->setAdvancedFilters($form, $advancedFilters);
+        $this->createAndSetDateRangeFormElements($form, $dateRange);
+
+        $this->client->submit($form);
+
+        $this->inspectResult($result, $previousResult);
+    }
+
+    /**
+     * @return array
+     */
+    public function widgetProvider()
     {
         return [
             'Apply owner filter with date range filter on today' => [
-                'owners' => '',
-                'dateRange' => '',
-                'advancedFilters' => '',
-                'widgetItemResult' => [
-                    'open_leads_count' => '',
-                    'new_leads_count' => ''
+                'owners' => '1',
+                'dateRange' => ['type' => AbstractDateFilterType::TYPE_TODAY],
+                'comparePrevious' => true,
+                'advancedFilters' => [
+                    'entity' => 'Oro\Bundle\SalesBundle\Entity\Lead',
+                    'filters' => [
+                        'columnName' => 'updatedAt',
+                        'criterion' => [
+                            'filter' => 'datetime',
+                            'data' => [
+                                'type' => AbstractDateFilterType::TYPE_MORE_THAN,
+                                'part' => 'value',
+                                'value' => [
+                                    'start' => '2016-01-01 00:00',
+                                    'end' => ''
+                                ]
+                            ]
+                        ]
+                    ]
+
                 ],
-                'widgetItemPreviousResult' => [
-                    'open_leads_count' => '',
-                    'new_leads_count' => ''
-                ]
+                'widgetItemResult' => [
+                    'open_leads_count' => 0,
+                    'new_leads_count' => 0
+                ],
+                'widgetItemPreviousResult' => ['new_leads_count' => 'No changes']
             ],
-            'Apply "All time" date range filter' => [],
-            'Apply "Today" date range filter' => [],
-            'Apply "Custom" date range filter' => [],
-            'Apply advanced filters with owner filter' => [],
-            'Apply advanced filters without owner filter' => [],
+            'Apply "All time" date range filter' => [
+                'owners' => '',
+                'dateRange' => ['type' => AbstractDateFilterType::TYPE_ALL_TIME],
+                'comparePrevious' => true,
+                'advancedFilters' => [],
+                'widgetItemResult' => [
+                    'open_leads_count' => 1,
+                    'new_leads_count' => 1
+                ],
+                'widgetItemPreviousResult' => []
+            ],
+            'Apply "Today" date range filter' => [
+                'owners' => '',
+                'dateRange' => ['type' => AbstractDateFilterType::TYPE_TODAY],
+                'comparePrevious' => true,
+                'advancedFilters' => [],
+                'widgetItemResult' => [
+                    'open_leads_count' => 1,
+                    'new_leads_count' => 1
+                ],
+                'widgetItemPreviousResult' => ['new_leads_count' => '+1']
+            ],
+            'Apply "Custom" date range filter' => [
+                'owners' => '',
+                'dateRange' => [
+                    'type' => AbstractDateFilterType::TYPE_MORE_THAN,
+                    'start' => '2017-01-01',
+                    'end' => ''
+                ],
+                'comparePrevious' => true,
+                'advancedFilters' => [],
+                'widgetItemResult' => ['open_leads_count' => 1, 'new_leads_count' => 1],
+                'widgetItemPreviousResult' => ['new_leads_count' => '+1']
+            ],
+            'Apply advanced filters with owner filter' => [
+                'owners' => '1',
+                'dateRange' => ['type' => AbstractDateFilterType::TYPE_TODAY],
+                'comparePrevious' => false,
+                'advancedFilters' => [
+                    'entity' => 'Oro\Bundle\SalesBundle\Entity\Lead',
+                    'filters' => [
+                        'columnName' => 'updatedAt',
+                        'criterion' => [
+                            'filter' => 'datetime',
+                            'data' => [
+                                'type' => strval(AbstractDateFilterType::TYPE_BETWEEN),
+                                'part' => 'value',
+                                'value' => [
+                                    'start' => date_format($this->createDateTime('-1 day'), 'Y-m-d H:i:s'),
+                                    'end' => date_format($this->createDateTime('+1 day'), 'Y-m-d H:i:s')
+                                ]
+                            ]
+                        ]
+                    ]
+
+                ],
+                'widgetItemResult' => [
+                    'open_leads_count' => 0,
+                    'new_leads_count' => 0
+                ],
+                'widgetItemPreviousResult' => []
+            ],
+            'Apply advanced filters without owner filter' => [
+                'owners' => '',
+                'dateRange' => ['type' => AbstractDateFilterType::TYPE_TODAY],
+                'comparePrevious' => true,
+                'advancedFilters' => [
+                    'entity' => 'Oro\Bundle\SalesBundle\Entity\Lead',
+                    'filters' => [
+                        'columnName' => 'createdAt',
+                        'criterion' => [
+                            'filter' => 'datetime',
+                            'data' => [
+                                'type' => strval(AbstractDateFilterType::TYPE_BETWEEN),
+                                'part' => 'value',
+                                'value' => [
+                                    'start' => date_format($this->createDateTime('-1 day'), 'Y-m-d H:i:s'),
+                                    'end' => date_format($this->createDateTime('+1 day'), 'Y-m-d H:i:s')
+                                ]
+                            ]
+                        ]
+                    ]
+
+                ],
+                'widgetItemResult' => [
+                    'open_leads_count' => 1,
+                    'new_leads_count' => 1
+                ],
+                'widgetItemPreviousResult' => ['new_leads_count' => '+1']
+            ],
         ];
     }
 
@@ -143,25 +275,36 @@ class LeadStatistics extends WebTestCase
     }
 
     /**
-     * Create fields of 'WidgetConfigDateRangeFilter' component
+     * Create and set fields of 'WidgetConfigDateRangeFilter' component
      *
      * @param Form $form
+     * @param array|null $data
      */
-    protected function createAdditionalDateRandgeFormElements(Form $form)
+    protected function createAndSetDateRangeFormElements(Form $form, $data = null)
     {
         $doc = new \DOMDocument("1.0");
-        $doc->loadHTML(
-            '<input type="text" name="lead_statistics[dateRange][type]" value="" />' .
-            '<input type="text" name="lead_statistics[dateRange][value][start]" value="" />' .
-            '<input type="text" name="lead_statistics[dateRange][value][end]" value="" />'
-        );
+        $inputs = '<input type="text" name="lead_statistics[dateRange][type]" value="" />';
 
-        $dateRangeTypeField = new InputFormField($doc->getElementsByTagName('input')->item(0));
-        $form->set($dateRangeTypeField);
-        $dateRangeTypeField = new InputFormField($doc->getElementsByTagName('input')->item(1));
-        $form->set($dateRangeTypeField);
-        $dateRangeTypeField = new InputFormField($doc->getElementsByTagName('input')->item(2));
-        $form->set($dateRangeTypeField);
+        if (!empty($data) && count($data) > 1) {
+            $inputs .= '<input type="text" name="lead_statistics[dateRange][value][start]" value="" />' .
+                       '<input type="text" name="lead_statistics[dateRange][value][end]" value="" />';
+        }
+        $doc->loadHTML($inputs);
+
+        $index = 0;
+        foreach ($data as $key => $value) {
+            $dateRangeTypeField = new InputFormField($doc->getElementsByTagName('input')->item($index));
+            $form->set($dateRangeTypeField);
+
+            $fieldName = $index > 0
+                ? 'lead_statistics[dateRange][value]['.$key.']'
+                : 'lead_statistics[dateRange]['.$key.']' ;
+
+            $form[$fieldName] = $value;
+            $index++;
+        }
+
+        $form['lead_statistics[dateRange][part]'] = 'value';
     }
 
     /**
@@ -179,7 +322,7 @@ class LeadStatistics extends WebTestCase
             $html .= sprintf(
                 '<input type="text" name="lead_statistics[subWidgets][items][%1$s][id]" value="%2$s" />' .
                 '<input type="text" name="lead_statistics[subWidgets][items][%1$s][order]" value="%1$s" />' .
-                '<input type="text" name="lead_statistics[subWidgets][items][%1$s][show]" value="on" />',
+                '<input type="checkbox" name="lead_statistics[subWidgets][items][%1$s][show]" checked />',
                 $index,
                 $metricsKeys[$index]
             );
@@ -188,12 +331,12 @@ class LeadStatistics extends WebTestCase
         $doc->loadHTML($html);
 
         for ($index=0; $index < $metricsCount; $index++) {
-            $dateRangeTypeField = new InputFormField($doc->getElementsByTagName('input')->item(0 + $index * 3));
-            $form->set($dateRangeTypeField);
-            $dateRangeTypeField = new InputFormField($doc->getElementsByTagName('input')->item(1 + $index * 3));
-            $form->set($dateRangeTypeField);
-            $dateRangeTypeField = new InputFormField($doc->getElementsByTagName('input')->item(2 + $index * 3));
-            $form->set($dateRangeTypeField);
+            $subItemTypeField = new InputFormField($doc->getElementsByTagName('input')->item(0 + $index * 3));
+            $form->set($subItemTypeField);
+            $subItemTypeField = new InputFormField($doc->getElementsByTagName('input')->item(1 + $index * 3));
+            $form->set($subItemTypeField);
+            $subItemTypeField = new ChoiceFormField($doc->getElementsByTagName('input')->item(2 + $index * 3));
+            $form->set($subItemTypeField);
         }
     }
 
@@ -205,6 +348,9 @@ class LeadStatistics extends WebTestCase
         return $this->getReference('widget_lead_statistics');
     }
 
+    /**
+     * Request widget configuration form
+     */
     protected function getConfigureDialog()
     {
         $this->client->request(
@@ -217,5 +363,109 @@ class LeadStatistics extends WebTestCase
 
         $response = $this->client->getResponse();
         $this->assertEquals($response->getStatusCode(), 200, 'Failed in getting configure widget dialog window !');
+    }
+
+    /**
+     * @param Form $form
+     * @param bool $comparePrevious
+     */
+    protected function setComparePrevious(Form $form, $comparePrevious)
+    {
+        $form->remove('lead_statistics[usePreviousInterval]');
+        if ($comparePrevious) {
+
+            $doc = new \DOMDocument("1.0");
+            $doc->loadHTML('<input type="text" name="lead_statistics[usePreviousInterval]" value="1" checked="checked"/>');
+            $compareToPreviousField = new InputFormField($doc->getElementsByTagName('input')->item(0));
+            $form->set($compareToPreviousField);
+            $form['lead_statistics[usePreviousInterval]'] = 1;
+        }
+    }
+
+    protected function setAdvancedFilters(Form $form, array $advancedFilters)
+    {
+        if (!empty($advancedFilters)) {
+            $filters = json_encode($advancedFilters['filters']);
+            $form['lead_statistics[queryFilter][entity]'] = $advancedFilters['entity'];
+            $form['lead_statistics[queryFilter][definition]'] = '{"filters":['.$filters.']}';
+        }
+    }
+
+    /**
+     * @param array $result
+     * @param array $previousResult
+     */
+    protected function inspectResult(array $result, array $previousResult)
+    {
+        $response = $this->client->getResponse();
+        $this->assertEquals($response->getStatusCode(), 200, "Failed in submit widget configuration options !");
+
+        $crawler = $this->client->request(
+            'GET',
+            $this->getUrl(
+                'oro_dashboard_itemized_data_widget',
+                [
+                    'widget' => 'lead_statistics',
+                    'bundle' => 'OroDashboardBundle',
+                    'name' => 'bigNumbers',
+                    '_widgetId' => $this->getWidget()->getId()
+                ]
+            )
+        );
+
+        $response = $this->client->getResponse();
+
+        $this->assertEquals($response->getStatusCode(), 200, "Failed in gettting widget view !");
+        $this->assertNotEmpty($crawler->html());
+
+        $openLeadsMetric = $crawler->filterXPath(
+            $this->getMetricValueByLabel(
+                $this->metrics['open_leads_count']
+            )
+        );
+        $this->assertEquals(
+            $openLeadsMetric->text(),
+            $result['open_leads_count'],
+            '"Open Leads" metric doesn\'t much expected value !'
+        );
+
+        $newLeadsMetric = $crawler->filterXPath(
+            $this->getMetricValueByLabel(
+                $this->metrics['new_leads_count']
+            )
+        );
+        $this->assertEquals(
+            $newLeadsMetric->text(),
+            $result['new_leads_count'],
+            '"New Leads" metric doesn\'t much expected value !'
+        );
+
+        $deviationMetric = $crawler->filterXPath(
+            $this->getMetricPreviousIntervalValueByLabel(
+                $this->metrics['new_leads_count']
+            )
+        );
+
+        if (!empty($previousResult)) {
+            $this->assertEquals(
+                trim($deviationMetric->text()),
+                $previousResult['new_leads_count'],
+                '"New Leads" previous period metric doesn\'t much expected value !'
+            );
+        } else {
+            $this->assertEquals(0, $deviationMetric->count());
+        }
+    }
+
+    /**
+     * @param string $modifyStr
+     * @return \DateTime
+     */
+    protected function createDateTime($modifyStr)
+    {
+        $date = new \DateTime('now', new \DateTimeZone('UTC'));
+        $date->modify($modifyStr);
+
+        return $date;
     }
 }
