32272e0e1fb6c404e281a5344aeeab0cbe8ba762
Makar <sichevoy@gmail.com>
2017-02-28 19:20:35 +0200

    CRM-7823: Incorrect entity names in Channel view after migration to 2.0 (#7933)
    entities Oro\Bundle\ContactUsBundle\Entity\ContactRequest
    Oro\Bundle\SalesBundle\Entity\Opportunity
    Oro\Bundle\SalesBundle\Entity\Lead are not available for selection in channel

---
+++ src/Oro/Bundle/SalesBundle/Migrations/Schema/v1_35/RemoveOldData.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Oro\Bundle\SalesBundle\Migrations\Schema\v1_35;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\ParametrizedSqlMigrationQuery;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class RemoveOldData implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $sql = <<<EOF
+DELETE FROM orocrm_channel_entity_name
+WHERE name = :name
+EOF;
+
+        $removeDataQuery = new ParametrizedSqlMigrationQuery();
+        $removeDataQuery->addSql(
+            $sql,
+            ['name' => 'Oro\\Bundle\\SalesBundle\\Entity\\Lead'],
+            ['name' => Type::STRING]
+        );
+        $queries->addPostQuery($removeDataQuery);
+
+        $removeDataQuery = new ParametrizedSqlMigrationQuery();
+        $removeDataQuery->addSql(
+            $sql,
+            ['name' => 'Oro\\Bundle\\SalesBundle\\Entity\\Opportunity'],
+            ['name' => Type::STRING]
+        );
+        $queries->addPostQuery($removeDataQuery);
+    }
+}
