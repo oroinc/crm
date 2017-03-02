f67c17113a128f96ef50fbb78ddd9c03df37961a
Ivan Shirko <Ivan.Shirko@effective-soft.com>
2017-03-02 19:49:01 +0300

    CRM-7880: Incorrect workflow items migration from 1.12 to 2.0 (#8110)
    * CRM-7880: Incorrect workflow items migration from 1.12 to 2.0
    - add migration for replace OroCRM to Oro in Workflow Item
    
    * CRM-7880: Incorrect workflow items migration from 1.12 to 2.0
    - fix issue when no CRMBundle before

---
+++ src/Oro/Bundle/CRMBundle/Migrations/Schema/v1_3_1/WorkflowItemEntityClass.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Oro\Bundle\CRMBundle\Migrations\Schema\v1_3_1;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\InstallerBundle\Migration\UpdateTableFieldQuery;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class WorkflowItemEntityClass implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        self::updateWorkflowItemEntityClass($queries);
+    }
+
+    /**
+     * @param QueryBag $queries
+     */
+    public static function updateWorkflowItemEntityClass(QueryBag $queries)
+    {
+        $queries->addQuery(new UpdateTableFieldQuery(
+            'oro_workflow_item',
+            'entity_class',
+            'OroCRM',
+            'Oro'
+        ));
+    }
+}
