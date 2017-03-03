b8e99e32f9f8f0022009c03300e28fb382ce19d2
Makar <sichevoy@gmail.com>
2017-03-03 09:42:56 +0200

    BAP-13991: Notification rules are lost after migration to 2.0 version (#8010)

---
+++ src/Oro/Bundle/CRMBundle/Migrations/Schema/v1_4/NotificationEntityName.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Oro\Bundle\CRMBundle\Migrations\Schema\v1_4;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\InstallerBundle\Migration\UpdateTableFieldQuery;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class NotificationEntityName implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        self::updateTaggingEntityName($queries);
+    }
+
+    /**
+     * @param QueryBag $queries
+     */
+    public static function updateTaggingEntityName(QueryBag $queries)
+    {
+        $queries->addQuery(new UpdateTableFieldQuery(
+            'oro_notification_email_notif',
+            'entity_name',
+            'OroCRM',
+            'Oro'
+        ));
+    }
+}
