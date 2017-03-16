4373aee3fa454a4e519d72950a325abaee28d8e7
Maksym Perepelytsya <mperepelytsya@magecore.com>
2017-03-16 14:54:55 +0200

    CRM-7904: Cannot delete all accounts by mass action (#8546)

---
+++ src/Oro/Bundle/MagentoBundle/Migrations/Schema/v1_46/SetCascadeDeleteToOrderCartFK.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Oro\Bundle\MagentoBundle\Migrations\Schema\v1_46;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class SetCascadeDeleteToOrderCartFK implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $table  = $schema->getTable('orocrm_magento_order');
+        $fkName = 'FK_4D09F3051AD5CDBF';
+        $fk     = $table->getForeignKey($fkName);
+        if ($fk->getOption('onDelete') !== 'CASCADE') {
+            $table->removeForeignKey($fkName);
+            $table->addForeignKeyConstraint(
+                $schema->getTable('orocrm_magento_cart'),
+                ['cart_id'],
+                ['id'],
+                ['onDelete' => 'CASCADE']
+            );
+        }
+    }
+}
