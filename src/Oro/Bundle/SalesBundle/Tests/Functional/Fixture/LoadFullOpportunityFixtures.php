5b97833eab3c583120e0931d47c1c3a63963566b
Alexandr Parkhomenko <alexandr-parkhomenko@users.noreply.github.com>
2017-04-03 16:53:36 +0300

    CRM-7694: Opportunity changes when submit form without new data (#9076)
    * CRM-7694: Opportunity changes when submit form without new data
    - Added ModelTransformer to fix the issue
    - Added functional test to cover functionality in `MoneyValueTransformerTest`

---
+++ src/Oro/Bundle/SalesBundle/Tests/Functional/Fixture/LoadFullOpportunityFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Oro\Bundle\SalesBundle\Tests\Functional\Fixture;
+
+use Oro\Bundle\CurrencyBundle\Entity\MultiCurrency;
+use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
+use Oro\Bundle\SalesBundle\Entity\Opportunity;
+
+class LoadFullOpportunityFixtures extends AbstractOpportunityFixtures
+{
+    /**
+     * @return void
+     */
+    protected function createOpportunity()
+    {
+        $opportunity = new Opportunity();
+
+        $classMetaData = $this->em->getClassMetadata(Opportunity::class);
+        /**
+         * Fix case when we test upgrade from 1.12
+         */
+        if ($classMetaData->hasAssociation('data_channel')) {
+            $opportunity->setDataChannel($this->getReference('default_channel'));
+        }
+
+        $opportunity->setName('Full data opportunity');
+
+        $opportunity->setCustomerAssociation($this->getReference('default_account_customer'));
+        $opportunity->setOrganization($this->getOrganization());
+        $opportunity->setOwner($this->getUser());
+
+        $budgetAmount = MultiCurrency::create(50, 'USD');
+        $opportunity->setBudgetAmount($budgetAmount);
+
+        $closeRevenue = MultiCurrency::create(0, 'USD');
+        $opportunity->setCloseRevenue($closeRevenue);
+
+        $opportunity->setProbability(40);
+
+        $enumClass = ExtendHelper::buildEnumValueClassName(Opportunity::INTERNAL_STATUS_CODE);
+        $opportunity->setStatus($this->em->getReference($enumClass, 'in_progress'));
+
+        $this->em->persist($opportunity);
+        $this->em->flush();
+
+        $this->setReference('full_opportunity', $opportunity);
+    }
+}
