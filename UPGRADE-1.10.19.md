c8a18bf53e21ba276fc2dcc180c9027a88b040e8
Viacheslav Bemza <vbemza@magecore.com>
2017-04-04 12:52:50 +0300

    CRM-8049: Incorrect Country mapping during Magento synchronization  (#9188)
    * CRM-8049: Incorrect Country mapping during Magento synchronization
    - find iso2code in AbstractAddressDataConverter
    - add tests
    
    * CRM-8049: Incorrect Country mapping during Magento synchronization
    - cs fix
    
    * CRM-8049: Incorrect Country mapping during Magento synchronization
    - remove BC break, change constructor argument in AbstractAddressDataConverter to setter
    
    * CRM-8049: Incorrect Country mapping during Magento synchronization
    - update UPGRADE-1.10.19.md
    
    * CRM-8049: Incorrect Country mapping during Magento synchronization
    - update UPGRADE-1.10.19.md
    
    * CRM-8049: Incorrect Country mapping during Magento synchronization
    - fix AbstractAddressDataConverter logic for country_id
    - add tests
    
    # Conflicts:
    #	package/crm/src/Oro/Bundle/MagentoBundle/ImportExport/Converter/AbstractAddressDataConverter.php
    
    * CRM-8049: Incorrect Country mapping during Magento synchronization
    - fix AbstractAddressDataConverter logic for country_id
    - add tests
    
    * CRM-8049: Incorrect Country mapping during Magento synchronization
    - remove abstract class for address data converters tests
    
    * CRM-8049: Incorrect Country mapping during Magento synchronization
    - remove abstract class for address data converters tests

--- UPGRADE-1.10.19.md
+++ UPGRADE-1.10.19.md
@@ -6,3 +6,7 @@ UPGRADE FROM 1.10.18 to 1.10.19
 * Search index fields `description`, `resolution` and `message` for `CaseEntity` now contain no more than 255 characters
   each. Please, run reindexation for this entity using command
   `php app/console oro:search:reindex OroCRMCaseBundle:CaseEntity --env=prod`
+
+#### OroCRMMagentoBundle
+
+* Added setter `setIso2CodeProvider` for `OroCRM\Bundle\MagentoBundle\ImportExport\Converter\AbstractAddressDataConverter`
---
+++ src/OroCRM/Bundle/MagentoBundle/Tests/Unit/ImportExport/Converter/OrderAddressDataConverterTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace OroCRM\Bundle\MagentoBundle\Tests\Unit\Importexport\Converter;
+
+use OroCRM\Bundle\MagentoBundle\ImportExport\Converter\OrderAddressDataConverter;
+use OroCRM\Bundle\MagentoBundle\Provider\Iso2CodeProvider;
+
+class OrderAddressDataConverterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Iso2CodeProvider|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $iso2CodeProvider;
+
+    /**
+     * @var OrderAddressDataConverter
+     */
+    protected $dataConverter;
+
+    protected function setUp()
+    {
+        $this->iso2CodeProvider = $this->getMockBuilder(Iso2CodeProvider::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->dataConverter = new OrderAddressDataConverter();
+    }
+
+    /**
+     * @dataProvider importDataProviderWith2CodeProvider
+     * @param array $import
+     * @param array $expected
+     * @param string $foundCode
+     */
+    public function testConvertToImportFormatWithCodeProvider(array $import, array $expected, $foundCode)
+    {
+        $this->dataConverter->setIso2CodeProvider($this->iso2CodeProvider);
+        $this->iso2CodeProvider->expects($this->any())
+            ->method('getIso2CodeByCountryId')
+            ->with($import['country_id'])
+            ->willReturn($foundCode);
+        $result = $this->dataConverter->convertToImportFormat($import);
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @return array
+     */
+    public function importDataProviderWithCodeProvider()
+    {
+        return [
+            'customer address without country_id' => [
+                [
+                    'country_id' => null,
+                ],
+                [
+                    'region' => null,
+                ],
+                'NewFoundCode',
+            ],
+            'customer address without foundCode' => [
+                [
+                    'country_id' => 'US',
+                ],
+                [
+                    'country' => null,
+                    'region' => null,
+                ],
+                null,
+            ],
+            'customer address with foundCode' => [
+                [
+                    'country_id' => 'US',
+                ],
+                [
+                    'country' => [
+                        'iso2Code' => 'NewFoundCode'
+                    ],
+                    'region' => null,
+                ],
+                'NewFoundCode',
+            ]
+        ];
+    }
+}
