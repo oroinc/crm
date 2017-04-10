25a4c952c4e3a708cdc6a8434344122efeb620ed
Dmitriy Krushelnitskiy <dkrushelnytskyi@oroinc.com>
2017-04-10 10:39:46 +0300

    CRM-7340: Converted Magento Card status should be corrected (#9401)

--- src/Oro/Bundle/MagentoBundle/Migrations/Schema/OroMagentoBundleInstaller.php
+++ src/Oro/Bundle/MagentoBundle/Migrations/Schema/OroMagentoBundleInstaller.php
@@ -72,7 +72,7 @@ class OroMagentoBundleInstaller implements
      */
     public function getMigrationVersion()
     {
-        return 'v1_47_1';
+        return 'v1_48';
     }
 
     /**
---
+++ src/Oro/Bundle/MagentoBundle/Migrations/Schema/v1_48/RenameMagentoCartStatusLabel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Oro\Bundle\MagentoBundle\Migrations\Schema\v1_48;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\ParametrizedSqlMigrationQuery;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class RenameMagentoCartStatusLabel implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $sql = 'UPDATE orocrm_magento_cart_status' .
+            ' SET label = :label' .
+            ' WHERE name = :name';
+
+        $queries->addQuery(
+            new ParametrizedSqlMigrationQuery(
+                $sql,
+                ['label' => 'Converted to Opportunity', 'name' => 'converted_to_opportunity']
+            )
+        );
+    }
+}
