d2ba17a7909b6de0dec6fb0384c6ef83aaa2d536
Andrew Fetisov <afetisov@magecore.com>
2017-05-16 15:14:20 +0300

    BB-9060: CRUD for Contact Reasons - migration for deletedAt field

---
+++ src/Oro/Bundle/ContactUsBundle/Migrations/Schema/v1_16/OroContactUsBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Oro\Bundle\ContactUsBundle\Migrations\Schema\v1_16;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class OroContactUsBundle implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $table = $schema->getTable('orocrm_contactus_contact_rsn');
+
+        $table->addColumn('deletedAt', 'datetime', ['notnull' => false]);
+    }
+}
