55d646c1a456596fd087024dadc96ae861d012fb
Yurii Muratov <ymuratov@oroinc.com>
2017-06-07 20:41:24 +0300

    CRM-8305: Lost account to contact relation for Magento customers (#10731)
     - Fix process definition. Make Contact as default for this Account
     - Add migrations

---
+++ src/Oro/Bundle/MagentoBundle/Migrations/Schema/v1_46_3/UpdateRelationsAccountsAndContacts.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Oro\Bundle\MagentoBundle\Migrations\Schema\v1_46_3;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class UpdateRelationsAccountsAndContacts implements Migration, ContainerAwareInterface
+{
+    /** @var ContainerInterface */
+    protected $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        // send migration message to queue. we should process this migration asynchronous because instances
+        // could have a lot of magento customers in system.
+        $this->container->get('oro_message_queue.message_producer')
+            ->send(UpdateRelationsAccountsAndContactsProcessor::TOPIC_NAME, '');
+    }
+}
