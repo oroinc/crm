c0b74a5d3427486e8df8bd9c622567326f1ad320
Pavel Usachev <pusachev@oroinc.com>
2017-10-27 16:33:00 +0300

    CRM-8582: Functional test for "Recent Magento Purchases" Grid (#14333)

---
+++ src/Oro/Bundle/MagentoBundle/Tests/Functional/Datagrid/RecentPurchasesGridTest.php
@@ -0,0 +1,302 @@
+<?php
+
+namespace Oro\Bundle\MagentoBundle\Tests\Functional\Datagrid;
+
+use Oro\Bundle\ChannelBundle\Entity\Channel;
+use Oro\Bundle\MagentoBundle\Entity\Customer;
+use Oro\Bundle\MagentoBundle\Tests\Functional\Fixture\LoadRecentPurchasesData;
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Oro\Bundle\UserBundle\Tests\Functional\DataFixtures\LoadUserACLData;
+
+/**
+ * @dbIsolationPerTest
+ *
+ * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+ */
+class RecentPurchasesGridTest extends WebTestCase
+{
+    const CUSTOMER_REFERENCE = 'customer';
+
+    public function setUp()
+    {
+        $this->initClient(['debug' => false], $this->generateBasicAuthHeader());
+        $this->client->useHashNavigation(true);
+
+        $this->loadFixtures([LoadRecentPurchasesData::class, LoadUserACLData::class]);
+    }
+
+    /**
+     * @dataProvider gridAclDataProvider
+     *
+     * @param string    $gridName
+     */
+    public function testGridIfUserAclNotAllowed($gridName)
+    {
+        $this->loginUser(LoadUserACLData::SIMPLE_USER_2_ROLE_LOCAL);
+
+        $channelId = $this->getChannel()->getId();
+        $customerId = $this->getCustomer()->getId();
+
+        $gridParameters = $this->createGridParameters(
+            $gridName,
+            $channelId,
+            $customerId
+        );
+
+        $response = $this->client->requestGrid(
+            $gridParameters,
+            [],
+            true
+        );
+
+        $this->getJsonResponseContent($response, 403);
+    }
+
+    /**
+     * @dataProvider gridDataProvider
+     *
+     * @param string    $gridName
+     * @param bool      $hasChannel,
+     * @param bool      $hasCustomer,
+     * @param array     $gridFilters,
+     * @param array     $asserts,
+     * @param int       $expectedResultCount
+     */
+    public function testGrid(
+        $gridName,
+        $hasChannel,
+        $hasCustomer,
+        array $asserts,
+        $expectedResultCount
+    ) {
+        $channelId  = !$hasChannel ? null : $this->getChannel()->getId();
+        $customerId = !$hasCustomer ? null : $this->getCustomer()->getId();
+
+        $gridParameters = $this->createGridParameters(
+            $gridName,
+            $channelId,
+            $customerId
+        );
+
+        $response = $this->client->requestGrid(
+            $gridParameters,
+            [],
+            true
+        );
+        $result = $this->getJsonResponseContent($response, 200);
+
+        $this->assertCount($expectedResultCount, $result['data']);
+
+        foreach ($asserts as $assertKey => $assert) {
+            $gridData = $result['data'][$assertKey];
+            foreach ($assert as $key => $value) {
+                $this->assertEquals($gridData[$key], $value);
+            }
+        }
+    }
+
+    public function testCustomerRecentPurchasesAction()
+    {
+        /** @var Customer $customer */
+        $customer = $this->getReference(self::CUSTOMER_REFERENCE);
+
+        $this->client->request(
+            'GET',
+            $this->getUrl(
+                'oro_magento_widget_customer_recent_purchases',
+                [
+                    'customerId' => $customer->getId(),
+                    'channelId'  => $customer->getChannel()->getId(),
+                    '_widgetContainer' => 'block'
+                ]
+            )
+        );
+
+        $result = $this->client->getResponse();
+
+        $this->assertResponseStatusCodeEquals($result, 200);
+    }
+
+    public function testCustomerRecentPurchasesWidgetAction()
+    {
+        /** @var Customer $customer */
+        $customer = $this->getReference(self::CUSTOMER_REFERENCE);
+
+        $this->client->request(
+            'GET',
+            $this->getUrl(
+                'oro_magento_customer_recent_purchases_widget',
+                [
+                    'customerId' => $customer->getId(),
+                    'channelId'  => $customer->getChannel()->getId(),
+                    '_widgetContainer' => 'block'
+                ]
+            )
+        );
+
+        $result = $this->client->getResponse();
+
+        $this->assertResponseStatusCodeEquals($result, 200);
+    }
+
+    /**
+     *
+     * @return array
+     */
+    public function gridDataProvider()
+    {
+        return [
+            'Magento recent purchases grid' => [
+                'gridName'  => 'magento-customer-recent-purchases-grid',
+                'channelId' => true,
+                'customerId' => true,
+                'asserts' => [
+                    [
+                        'orderOriginId' => '100000307',
+                        'sku' => 'some sku',
+                        'name' => 'some order item',
+                        'qty' => 1,
+                        'price' => '$75.00',
+                        'originalPrice' => '$0.00',
+                        'discountPercent' => 4,
+                        'discountAmount' => '$0.00',
+                        'taxPercent' => 2,
+                        'taxAmount' => '$1.50',
+                        'rowTotal' => '$234.00',
+                    ],
+                    [
+                        'orderOriginId' => '100000505',
+                        'sku' => 'some sku2',
+                        'name' => 'some order item',
+                        'qty' => 1,
+                        'price' => '$75.00',
+                        'originalPrice' => '$0.00',
+                        'discountPercent' => 4,
+                        'discountAmount' => '$0.00',
+                        'taxPercent' => 2,
+                        'taxAmount' => '$1.50',
+                        'rowTotal' => '$234.00',
+                    ]
+                ],
+                'expectedResultCount' => 2
+            ],
+            'Magento recent purchases grid without channel' => [
+                'gridName'  => 'magento-customer-recent-purchases-grid',
+                'channelId' => false,
+                'customerId' => true,
+                'asserts' => [],
+                'expectedResultCount' => 0
+            ],
+            'Magento recent purchases grid without customer' => [
+                'gridName'  => 'magento-customer-recent-purchases-grid',
+                'channelId' => true,
+                'customerId' => false,
+                'asserts' => [],
+                'expectedResultCount' => 0
+            ],
+            'Magento recent purchases grid without customer and channel' => [
+                'gridName'  => 'magento-customer-recent-purchases-grid',
+                'channelId' => false,
+                'customerId' => false,
+                'asserts' => [],
+                'expectedResultCount' => 0
+            ],
+            'Account recent purchases grid' => [
+                'gridName'  => 'magento-customer-recent-purchases-widget-grid',
+                'channelId' => true,
+                'customerId' => true,
+                'asserts' => [
+                    [
+                        'orderOriginId' => '100000307',
+                        'sku' => 'some sku',
+                        'name' => 'some order item',
+                        'qty' => 1,
+                        'price' => '$75.00',
+                        'originalPrice' => '$0.00',
+                        'discountPercent' => 4,
+                        'discountAmount' => '$0.00',
+                        'taxPercent' => 2,
+                        'taxAmount' => '$1.50',
+                        'rowTotal' => '$234.00',
+                    ],
+                    [
+                        'orderOriginId' => '100000505',
+                        'sku' => 'some sku2',
+                        'name' => 'some order item',
+                        'qty' => 1,
+                        'price' => '$75.00',
+                        'originalPrice' => '$0.00',
+                        'discountPercent' => 4,
+                        'discountAmount' => '$0.00',
+                        'taxPercent' => 2,
+                        'taxAmount' => '$1.50',
+                        'rowTotal' => '$234.00',
+                    ]
+                ],
+                'expectedResultCount' => 2
+            ],
+            'Account recent purchases grid without channel' => [
+                'gridName'  => 'magento-customer-recent-purchases-widget-grid',
+                'channelId' => false,
+                'customerId' => true,
+                'asserts' => [],
+                'expectedResultCount' => 0
+            ],
+            'Account recent purchases grid without customer' => [
+                'gridName'  => 'magento-customer-recent-purchases-widget-grid',
+                'channelId' => true,
+                'customerId' => false,
+                'asserts' => [],
+                'expectedResultCount' => 0
+            ],
+            'Account recent purchases grid without customer and channel' => [
+                'gridName'  => 'magento-customer-recent-purchases-widget-grid',
+                'channelId' => true,
+                'customerId' => false,
+                'asserts' => [],
+                'expectedResultCount' => 0
+            ],
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function gridAclDataProvider()
+    {
+        return [
+            ['gridName' => 'magento-customer-recent-purchases-widget-grid'],
+            ['gridName' => 'magento-customer-recent-purchases-grid']
+        ];
+    }
+
+    /**
+     * @param string    $gridName
+     * @param int|null  $channelId
+     * @param int|null  $customerId
+     * @return array
+     */
+    protected function createGridParameters($gridName, $channelId, $customerId)
+    {
+        return [
+            'gridName' => $gridName,
+            $gridName => [
+                'channelId' => $channelId,
+                'customerId' => $customerId
+            ]
+        ];
+    }
+
+
+    /** @return Channel */
+    protected function getChannel()
+    {
+        return $this->getCustomer()->getChannel();
+    }
+
+    /** @return Customer */
+    protected function getCustomer()
+    {
+        return $this->getReference(self::CUSTOMER_REFERENCE);
+    }
+}
---
+++ src/Oro/Bundle/MagentoBundle/Tests/Functional/Fixture/LoadRecentPurchasesData.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace Oro\Bundle\MagentoBundle\Tests\Functional\Fixture;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Oro\Bundle\MagentoBundle\Entity\Cart;
+use Oro\Bundle\MagentoBundle\Entity\Customer;
+use Oro\Bundle\MagentoBundle\Entity\Order;
+use Oro\Bundle\MagentoBundle\Entity\OrderItem;
+
+class LoadRecentPurchasesData extends LoadMagentoChannel
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->em        = $manager;
+        $this->countries = $this->loadStructure('OroAddressBundle:Country', 'getIso2Code');
+        $this->regions   = $this->loadStructure('OroAddressBundle:Region', 'getCombinedCode');
+        $this->organization = $manager->getRepository('OroOrganizationBundle:Organization')->getFirst();
+
+        $this
+            ->createTransport()
+            ->createIntegration()
+            ->createChannel()
+            ->createWebSite()
+            ->createCustomerGroup()
+            ->createGuestCustomerGroup()
+            ->createStore();
+
+        $magentoAddress = $this->createMagentoAddress($this->regions['US-AZ'], $this->countries['US']);
+        $account        = $this->createAccount();
+        $this->setReference('account', $account);
+
+        $customer       = $this->createCustomer(1, $account, $magentoAddress);
+        $customer2      = $this->createCustomer(2, $account, $magentoAddress);
+        $cartAddress1   = $this->createCartAddress($this->regions['US-AZ'], $this->countries['US'], 1);
+        $cartAddress2   = $this->createCartAddress($this->regions['US-AZ'], $this->countries['US'], 2);
+        $cartItem       = $this->createCartItem();
+        $status         = $this->getStatus();
+        $items          = new ArrayCollection();
+        $items->add($cartItem);
+
+        $cart = $this->createCart($cartAddress1, $cartAddress2, $customer, $items, $status);
+        $this->updateCartItem($cartItem, $cart);
+
+        $date = new \DateTime('now', new \DateTimeZone('UTC'));
+        $date->modify('-3 day');
+
+        $order = $this->createOrder($cart, $customer);
+        $order2 = $this->createOrder($cart, $customer, '100000505', $date, 2);
+        $order3 = $this->createOrder($cart, $customer2, '100000602', $date, 3);
+
+        $creditMemo = $this->createCreditMemo($order);
+
+        $this->setReference('customer', $customer);
+        $this->setReference('integration', $this->integration);
+        $this->setReference('store', $this->store);
+        $this->setReference('organization', $this->organization);
+        $this->setReference('user', $this->getUser());
+        $this->setReference('cart', $cart);
+        $this->setReference('order', $order);
+        $this->setReference('order2', $order2);
+        $this->setReference('creditMemo', $creditMemo);
+
+        $baseOrderItem = $this->createBaseOrderItem($order);
+        $order->setItems([$baseOrderItem]);
+        $this->em->persist($order);
+
+        $baseOrderItem2 = $this->createBaseOrderItem($order2, 'some sku2');
+        $order2->setItems([$baseOrderItem2]);
+        $this->em->persist($order2);
+
+        $baseOrderItem3 = $this->createBaseOrderItem($order3, 'some sku3');
+        $order3->setItems([$baseOrderItem3]);
+        $this->em->persist($order3);
+
+        $cartAddress3 = $this->createGuestCartAddress($this->regions['US-AZ'], $this->countries['US'], null);
+        $cartAddress4 = $this->createGuestCartAddress($this->regions['US-AZ'], $this->countries['US'], null);
+
+        $cartItem = $this->createCartItem();
+        $status   = $this->getStatus();
+        $items    = new ArrayCollection();
+        $items->add($cartItem);
+        $guestCart = $this->createGuestCart($cartAddress3, $cartAddress4, $items, $status);
+        $this->updateCartItem($cartItem, $guestCart);
+        $guestOrder = $this->createGuestOrder($guestCart);
+
+        $this->setReference('guestCart', $guestCart);
+        $this->setReference('guestOrder', $guestOrder);
+
+        $baseOrderItem = $this->createBaseOrderItem($guestOrder);
+        $order->setItems([$baseOrderItem]);
+        $this->em->persist($guestOrder);
+
+        $this->em->flush();
+    }
+
+    /**
+     * @param Cart              $cart
+     * @param Customer          $customer
+     * @param string            $incrementId
+     * @param \DateTime|null    $createdAt
+     * @param int               $originId
+     *
+     * @return Order
+     */
+    protected function createOrder(
+        Cart $cart,
+        Customer $customer,
+        $incrementId = '100000307',
+        \DateTime $createdAt = null,
+        $originId = 1
+    ) {
+        $createdAt = $createdAt ? $createdAt : new \DateTime('now', new \DateTimeZone('UTC'));
+
+        $order = new Order();
+        $order->setChannel($this->integration);
+        $order->setDataChannel($this->channel);
+        $order->setStatus('open');
+        $order->setOriginId($originId);
+        $order->setIncrementId($incrementId);
+        $order->setCreatedAt($createdAt);
+        $order->setUpdatedAt(new \DateTime('now', new \DateTimeZone('UTC')));
+        $order->setCart($cart);
+        $order->setStore($this->store);
+        $order->setCustomer($customer);
+        $order->setCustomerEmail('customer@email.com');
+        $order->setDiscountAmount(4.40);
+        $order->setTaxAmount(12.47);
+        $order->setShippingAmount(5);
+        $order->setTotalPaidAmount(17.85);
+        $order->setTotalInvoicedAmount(11);
+        $order->setTotalRefundedAmount(4);
+        $order->setTotalCanceledAmount(0);
+        $order->setShippingMethod('some unique shipping method');
+        $order->setRemoteIp('127.0.0.1');
+        $order->setGiftMessage('some very unique gift message');
+        $order->setOwner($this->getUser());
+        $order->setOrganization($this->organization);
+
+        $this->em->persist($order);
+
+        return $order;
+    }
+
+    /**
+     * @param Cart      $cart
+     * @param string    $incrementId
+     *
+     * @return Order
+     */
+    protected function createGuestOrder(Cart $cart, $incrementId = '100000308')
+    {
+        $order = new Order();
+        $order->setChannel($this->integration);
+        $order->setDataChannel($this->channel);
+        $order->setStatus('open');
+        $order->setIncrementId($incrementId);
+        $order->setCreatedAt(new \DateTime('now'));
+        $order->setUpdatedAt(new \DateTime('now'));
+        $order->setCart($cart);
+        $order->setStore($this->store);
+        $order->setCustomer(null);
+        $order->setIsGuest(1);
+        $order->setCustomerEmail('guest@email.com');
+        $order->setDiscountAmount(4.40);
+        $order->setTaxAmount(12.47);
+        $order->setShippingAmount(5);
+        $order->setTotalPaidAmount(17.85);
+        $order->setTotalInvoicedAmount(11);
+        $order->setTotalRefundedAmount(4);
+        $order->setTotalCanceledAmount(0);
+        $order->setShippingMethod('some unique shipping method');
+        $order->setRemoteIp('127.0.0.1');
+        $order->setGiftMessage('some very unique gift message');
+        $order->setOwner($this->getUser());
+        $order->setOrganization($this->organization);
+
+        $this->em->persist($order);
+
+        return $order;
+    }
+
+    /**
+     * @param Order     $order
+     * @param string    $sku
+     *
+     * @return OrderItem
+     */
+    protected function createBaseOrderItem(Order $order, $sku = 'some sku')
+    {
+        $orderItem = new OrderItem();
+        $orderItem->setId(mt_rand(0, 9999));
+        $orderItem->setName('some order item');
+        $orderItem->setSku($sku);
+        $orderItem->setQty(1);
+        $orderItem->setOrder($order);
+        $orderItem->setCost(51.00);
+        $orderItem->setPrice(75.00);
+        $orderItem->setWeight(6.12);
+        $orderItem->setTaxPercent(2);
+        $orderItem->setTaxAmount(1.5);
+        $orderItem->setDiscountPercent(4);
+        $orderItem->setDiscountAmount(0);
+        $orderItem->setRowTotal(234);
+        $orderItem->setOwner($this->organization);
+
+        $this->em->persist($orderItem);
+
+        return $orderItem;
+    }
+}
