35f56a45078e9fedd911c28adf629acbc3b581ff
Dmitry Khrysev <dkhrysev@oroinc.com>
2018-01-22 20:04:03 +0200

    OIS-112: Provide fix for insecure query usages - improved inspections - fixes

--- src/Oro/Bundle/ChannelBundle/Datagrid/ChannelLimitationExtension.php
+++ src/Oro/Bundle/ChannelBundle/Datagrid/ChannelLimitationExtension.php
@@ -96,6 +96,7 @@ class ChannelLimitationExtension extends AbstractExtension
 
         if (!$joinAlias) {
             $joinAlias = uniqid($relationPath);
+            QueryBuilderUtil::checkIdentifier($joinAlias);
             $queryBuilder->leftJoin($fullJoinPath, $joinAlias);
         }
 
