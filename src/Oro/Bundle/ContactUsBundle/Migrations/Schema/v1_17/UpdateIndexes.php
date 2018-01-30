798b8c2cf654048ebacba1277efbfed1ff1018f8
Galaev Igor <igalayev@oroinc.com>
2018-01-30 18:35:51 +0200

    BB-13568: Fix migrations for upgrade from 1.0 to 1.6 (#16434)
    * BB-13568: Fix migrations for upgrade from 1.0 to 1.6
    - added duplicated migrations with patch versions format for broken migrations
    - fix OroSalesBundleInstaller migration version
    - revert regression builds
    - move comment from v1_46 to v1_46_2 migration for UpdateIndexes.php
    - reuse original migrations
    - use migration inheritance
    - move update index migrations to higher migrations versions to guarantee their execution from any patch version
    - move update index migrations to higher migrations versions to guarantee their execution from any patch version

---
+++ src/Oro/Bundle/ContactUsBundle/Migrations/Schema/v1_17/UpdateIndexes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Oro\Bundle\ContactUsBundle\Migrations\Schema\v1_17;
+
+use Oro\Bundle\ContactUsBundle\Migrations\Schema\v1_14\UpdateIndexes as BaseMigration;
+
+/**
+ * This migration duplicates v1_14 migration because of confused migration versions in 1.0.0 (see v1_14 migration, it
+ * has v1_15 version for now).
+ */
+class UpdateIndexes extends BaseMigration
+{
+}
