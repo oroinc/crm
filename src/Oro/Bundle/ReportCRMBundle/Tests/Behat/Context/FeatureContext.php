878b194005d17c654c015b588f6f27e79d73239b
Mykhailo Sulyma <msulima@oroinc.com>
2018-03-02 13:24:21 +0200

    CRM-8757: "Created Date" filter at "Leads By Date" report get 500 error (#16984)
    CRM-8757: "Created Date" filter at "Leads By Date" report get 500 error
     - fixed total extension for datagrids
     - added type hints for setters of date fields in Lead entity
     - updated unit and behat tests

---
+++ src/Oro/Bundle/ReportCRMBundle/Tests/Behat/Context/FeatureContext.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Oro\Bundle\ReportCRMBundle\Tests\Behat\Context;
+
+use Behat\Symfony2Extension\Context\KernelAwareContext;
+use Behat\Symfony2Extension\Context\KernelDictionary;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use Oro\Bundle\SalesBundle\Entity\Lead;
+use Oro\Bundle\TestFrameworkBundle\Behat\Context\OroFeatureContext;
+use Oro\Bundle\TestFrameworkBundle\Behat\Fixtures\FixtureLoaderAwareInterface;
+use Oro\Bundle\TestFrameworkBundle\Behat\Fixtures\FixtureLoaderDictionary;
+
+class FeatureContext extends OroFeatureContext implements KernelAwareContext, FixtureLoaderAwareInterface
+{
+    use KernelDictionary;
+    use FixtureLoaderDictionary;
+
+    /**
+     * Load "LeadsByDateReportFixture.yml" alice fixture from ReportCRMBundle suite
+     *
+     * PrePersist lifecycleCallback will override createdAt and updatedAt fields passed from fixture.
+     * So, we should disable this callback to save original values.
+     *
+     * @Given /^leads by date report fixture loaded$/
+     */
+    public function bestSellingFixtureLoaded()
+    {
+        $metadata = $this->getMetadata();
+
+        $events = $metadata->lifecycleCallbacks;
+        $metadata->setLifecycleCallbacks([]);
+
+        $this->fixtureLoader->loadFixtureFile('OroReportCRMBundle:LeadsByDateReportFixture.yml');
+
+        $metadata->setLifecycleCallbacks($events);
+    }
+
+    /**
+     * @return ClassMetadataInfo
+     */
+    private function getMetadata()
+    {
+        $manager = $this->getContainer()->get('doctrine')->getManagerForClass(Lead::class);
+
+        return $manager->getClassMetadata(Lead::class);
+    }
+}
