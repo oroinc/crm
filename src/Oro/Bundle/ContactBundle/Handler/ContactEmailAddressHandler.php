031a8e00851d35b9edea3f087466a918c3d0b4e8
Dmitry Khrysev <x86demon@users.noreply.github.com>
2018-03-13 16:42:51 +0200

    CRM-8752: Deadlock detected in import due to EmailBundle:EntityListener (#17194)
    * CRM-8752: Deadlock detected in import due to EmailBundle:EntityListener
     - phpcs fix
     - fixed error caused by referenced entities
    - removed not existing method call

---
+++ src/Oro/Bundle/ContactBundle/Handler/ContactEmailAddressHandler.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace Oro\Bundle\ContactBundle\Handler;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query\Expr\Join;
+use Oro\Bundle\ContactBundle\Entity\Contact;
+use Oro\Bundle\ContactBundle\Entity\ContactEmail;
+use Oro\Bundle\EmailBundle\Entity\Email;
+use Oro\Bundle\EmailBundle\Entity\EmailRecipient;
+use Oro\Bundle\EmailBundle\Entity\Manager\EmailAddressManager;
+use Oro\Bundle\EntityBundle\ORM\InsertFromSelectQueryExecutor;
+
+/**
+ * ContactEmailAddressHandler is responsible for actualization of EmailAddress records for ContactEmails.
+ * First it removes records from EmailAddress for Contact emails that were removed for contact
+ * Then it adds new Contact emails that are not present in EmailAddress
+ */
+class ContactEmailAddressHandler
+{
+    /**
+     * @var InsertFromSelectQueryExecutor
+     */
+    private $insertFromSelectQueryExecutor;
+
+    /**
+     * @var EmailAddressManager
+     */
+    private $emailAddressManager;
+
+    /**
+     * @var ManagerRegistry
+     */
+    private $registry;
+
+    /**
+     * @param InsertFromSelectQueryExecutor $insertFromSelectQueryExecutor
+     * @param EmailAddressManager $emailAddressManager
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(
+        InsertFromSelectQueryExecutor $insertFromSelectQueryExecutor,
+        EmailAddressManager $emailAddressManager,
+        ManagerRegistry $registry
+    ) {
+        $this->insertFromSelectQueryExecutor = $insertFromSelectQueryExecutor;
+        $this->emailAddressManager = $emailAddressManager;
+        $this->registry = $registry;
+    }
+
+    /**
+     * @return string
+     */
+    private function getEmailAddressClass()
+    {
+        return $this->emailAddressManager->getEmailAddressProxyClass();
+    }
+
+    /**
+     * Actualize EmailAddress records based on ContactEmail entities
+     */
+    public function actualizeContactEmailAssociations()
+    {
+        $this->deleteRemovedEmailAssociations();
+        $this->addContactEmailAssociations();
+    }
+
+    /**
+     * Add EmailAddress records based on ContactEmail entities
+     */
+    private function addContactEmailAssociations()
+    {
+        $contactAssociationFieldName = $this->getContactAssociationFieldName();
+        if ($contactAssociationFieldName) {
+            $qb = $this->getEntityManager()->createQueryBuilder();
+            $nowString = $this->getCurrentTimestampString();
+
+            $qb->from(ContactEmail::class, 'ce')
+                ->select(
+                    'ce.email',
+                    'MAX(IDENTITY(ce.owner))',
+                    (string)$qb->expr()->literal($nowString),
+                    (string)$qb->expr()->literal($nowString),
+                    'CASE WHEN ce.email IS NOT NULL THEN TRUE ELSE FALSE END'
+                )
+                ->leftJoin($this->getEmailAddressClass(), 'ea', Join::WITH, $qb->expr()->eq('ce.email', 'ea.email'))
+                ->where($qb->expr()->isNull('ea.id'))
+                ->groupBy('ce.email');
+
+            $this->insertFromSelectQueryExecutor->execute(
+                $this->getEmailAddressClass(),
+                ['email', $contactAssociationFieldName, 'created', 'updated', 'hasOwner'],
+                $qb
+            );
+        }
+    }
+
+    /**
+     * Delete EmailAddress records which contains emails that are not present in ContactEmail
+     */
+    private function deleteRemovedEmailAssociations()
+    {
+        $idsToRemove = $this->getNonExistingEmailAssociationIds();
+        if ($idsToRemove) {
+            $deleteQb = $this->getEntityManager()
+                ->createQueryBuilder()
+                ->delete($this->getEmailAddressClass(), 'ea');
+            $deleteQb->where($deleteQb->expr()->in('ea.id', ':deleteIds'))
+                ->setParameter('deleteIds', $idsToRemove)
+                ->getQuery()
+                ->execute();
+        }
+    }
+
+    /**
+     * @return array
+     */
+    private function getNonExistingEmailAssociationIds(): array
+    {
+        $contactAssociationFieldName = $this->getContactAssociationFieldName();
+        if ($contactAssociationFieldName) {
+            // Skip email addresses that was already synced
+            $emailDQL = $this->getEntityManager()->createQueryBuilder()
+                ->select('e.id')
+                ->from(Email::class, 'e')
+                ->where('e.fromEmailAddress = ea.id')
+                ->getDQL();
+            $emailRecipientDQL = $this->getEntityManager()->createQueryBuilder()
+                ->select('er.id')
+                ->from(EmailRecipient::class, 'er')
+                ->where('er.emailAddress = ea.id')
+                ->getDQL();
+
+            $toDeleteQb = $this->getEntityManager()->createQueryBuilder();
+            $toDeleteQb->select('ea.id')
+                ->from($this->getEmailAddressClass(), 'ea')
+                ->leftJoin(
+                    ContactEmail::class,
+                    'ce',
+                    Join::WITH,
+                    'ea.email = ce.email AND ce.owner = ea.' . $contactAssociationFieldName
+                )
+                ->where($toDeleteQb->expr()->isNull('ce.id'))
+                ->andWhere($toDeleteQb->expr()->isNotNull('ea.' . $contactAssociationFieldName))
+                ->andWhere(
+                    $toDeleteQb->expr()->not(
+                        $toDeleteQb->expr()->exists($emailDQL)
+                    )
+                )
+                ->andWhere(
+                    $toDeleteQb->expr()->not(
+                        $toDeleteQb->expr()->exists($emailRecipientDQL)
+                    )
+                );
+
+            $result = $toDeleteQb->getQuery()->getScalarResult();
+            return array_map('current', $result);
+        }
+
+        return [];
+    }
+
+    /**
+     * @return null|string
+     */
+    private function getContactAssociationFieldName()
+    {
+        $em = $this->getEntityManager();
+        $metadata = $em->getClassMetadata($this->getEmailAddressClass());
+        $contactAssociations = $metadata->getAssociationsByTargetClass(Contact::class);
+        $contactAssociation = reset($contactAssociations);
+
+        if ($contactAssociation && !empty($contactAssociation['fieldName'])) {
+            return $contactAssociation['fieldName'];
+        }
+
+        return null;
+    }
+
+    /**
+     * @return EntityManager|ObjectManager
+     */
+    private function getEntityManager()
+    {
+        return $this->registry->getManagerForClass(ContactEmail::class);
+    }
+
+    /**
+     * @return string
+     */
+    private function getCurrentTimestampString(): string
+    {
+        $dateFormat = $this->getEntityManager()
+            ->getConnection()
+            ->getDatabasePlatform()
+            ->getDateTimeFormatString();
+        $now = new \DateTime('now', new \DateTimeZone('UTC'));
+
+        return $now->format($dateFormat);
+    }
+}
