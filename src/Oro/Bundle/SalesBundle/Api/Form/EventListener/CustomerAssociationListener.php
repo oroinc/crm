703ef7d6b98e815374b5a835ad68b8b95a596652
Marcin Łukaszewicz <mlukaszewicz@oroinc.com>
2018-05-15 10:59:49 +0200

    CRM-8996: Impossible to create a lead via JSON.API due to excessive mandatory fields - added listener to set default status if it is not given - made customerAssociation optional for Lead in API

--- src/Oro/Bundle/SalesBundle/Api/Form/EventListener/CustomerAssociationListener.php
+++ src/Oro/Bundle/SalesBundle/Api/Form/EventListener/CustomerAssociationListener.php
@@ -4,6 +4,7 @@ namespace Oro\Bundle\SalesBundle\Api\Form\EventListener;
 
 use Doctrine\Common\Util\ClassUtils;
 
+use Oro\Bundle\SalesBundle\Entity\Lead;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -23,6 +24,9 @@ class CustomerAssociationListener implements EventSubscriberInterface
 {
     const ACCOUNT_FIELD_NAME  = 'account';
     const CUSTOMER_FIELD_NAME = 'customer';
+
+    /** @var array */
+    protected $relationOptionalForClasses = [Lead::class];
 
     /** @var AccountCustomerManager */
     protected $accountCustomerManager;
@@ -245,7 +249,9 @@ class CustomerAssociationListener implements EventSubscriberInterface
         $submittedCustomer = null
     ) {
         if (null === $submittedAccount && null === $submittedCustomer) {
-            FormUtil::addFormError($form, 'Either an account or a customer should be set.');
+            if (!$this->isRelationOptionalForClass(get_class($form->getData()))) {
+                FormUtil::addFormError($form, 'Either an account or a customer should be set.');
+            }
         } else {
             $entity = $form->getData();
 
@@ -294,5 +300,32 @@ class CustomerAssociationListener implements EventSubscriberInterface
     protected function addFieldModificationDeniedFormError(FormInterface $form)
     {
         $this->fieldAclHelper->addFieldModificationDeniedFormError($form);
+    }
+
+    /**
+     * @param string $class
+     */
+    protected function addClassWhereRelationIsOptional(string $class)
+    {
+        $this->relationOptionalForClasses[] = $class;
+    }
+
+    /**
+     * @param string $class
+     */
+    protected function removeClassWhereRelationIsOptional(string $class)
+    {
+        if (($key = array_search($class, $this->relationOptionalForClasses)) !== false) {
+            unset($this->relationOptionalForClasses[$key]);
+        }
+    }
+
+    /**
+     * @param string $class
+     * @return bool
+     */
+    protected function isRelationOptionalForClass(string $class)
+    {
+        return in_array($class, $this->relationOptionalForClasses);
     }
 }
---
+++ src/Oro/Bundle/SalesBundle/Api/Form/EventListener/LeadStatusListener.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Oro\Bundle\SalesBundle\Api\Form\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+use Oro\Bundle\EntityExtendBundle\Provider\EnumValueProvider;
+
+class LeadStatusListener implements EventSubscriberInterface
+{
+    const STATUS_FIELD_NAME = 'status';
+
+    /** @var EnumValueProvider */
+    protected $enumProvider;
+
+    /**
+     * @param EnumValueProvider $enumProvider
+     */
+    public function __construct(EnumValueProvider $enumProvider)
+    {
+        $this->enumProvider = $enumProvider;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FormEvents::SUBMIT   => 'onSubmit',
+        );
+    }
+
+    /**
+     * @param FormEvent $event
+     */
+    public function onSubmit(FormEvent $event)
+    {
+        $lead = $event->getData();
+
+        if (!$lead->getStatus()) {
+            $lead->setStatus($this->enumProvider->getDefaultEnumValuesByCode('lead_status')[0]);
+        }
+        $event->setData($lead);
+    }
+}
