22fbb3cc1efb8559d9ef4838d544b52ddf518c72
Vadim Bova <folour@gmail.com>
2021-07-30 16:29:13 +0300

    BAP-15117: Associated account name cannot be used in opportunity email template [4.2] (#30308)

---
+++ src/Oro/Bundle/SalesBundle/Tests/Unit/Provider/EntityVariablesProviderTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Oro\Bundle\SalesBundle\Tests\Unit\Provider;
+
+use Oro\Bundle\AccountBundle\Entity\Account;
+use Oro\Bundle\EntityExtendBundle\Extend\RelationType;
+use Oro\Bundle\SalesBundle\Entity\Customer;
+use Oro\Bundle\SalesBundle\Provider\EntityVariablesProvider;
+
+class EntityVariablesProviderTest extends \PHPUnit\Framework\TestCase
+{
+    /** @var EntityVariablesProvider */
+    private $variableProvider;
+
+    protected function setUp(): void
+    {
+        $this->variableProvider = new EntityVariablesProvider();
+    }
+
+    public function testGetVariableGetters(): void
+    {
+        $this->assertEquals(
+            [Customer::class => ['account' => 'getAccount']],
+            $this->variableProvider->getVariableGetters()
+        );
+    }
+
+    public function testGetVariableDefinitions(): void
+    {
+        $expected = [
+            Customer::class => [
+                'account' => [
+                    'type' => RelationType::TO_ONE,
+                    'label' => 'Account',
+                    'related_entity_name' => Account::class
+                ]
+            ]
+        ];
+
+        $this->assertEquals($expected, $this->variableProvider->getVariableDefinitions());
+    }
+
+    /**
+     * @inheridoc
+     */
+    public function testGetVariableProcessors(): void
+    {
+        $this->assertEmpty($this->variableProvider->getVariableProcessors('acme'));
+    }
+}
