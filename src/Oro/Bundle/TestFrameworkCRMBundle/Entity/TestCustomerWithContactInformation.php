4031c6bc1cbb206b74751747031c6819da0c0c4a
Dmitry Khrysev <x86demon@users.noreply.github.com>
2022-01-24 16:39:58 +0200

    CRM-9332: Mailchimp sync fails if custom entity field is used as email contact information (#31189)
    - made First/Last Name fields of Customer entity not required for Mailchimp integration

---
+++ src/Oro/Bundle/TestFrameworkCRMBundle/Entity/TestCustomerWithContactInformation.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Oro\Bundle\TestFrameworkCRMBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
+use Oro\Bundle\TestFrameworkBundle\Entity\TestFrameworkEntityInterface;
+use Oro\Bundle\TestFrameworkCRMBundle\Model\ExtendTestCustomerWithContactInformation;
+
+/**
+ * TestCustomerWithContactInformation ORM entity.
+ *
+ * @ORM\Table(name="test_customer_with_contact_info")
+ * @ORM\Entity
+ * @Config(
+ *     defaultValues={
+ *          "entity"={
+ *              "contact_information"={
+ *                  "email"={
+ *                      {"fieldName"="email"}
+ *                  },
+ *                  "phone"={
+ *                      {"fieldName"="phone"}
+ *                  }
+ *              }
+ *          }
+ *     }
+ * )
+ */
+class TestCustomerWithContactInformation extends ExtendTestCustomerWithContactInformation implements
+    TestFrameworkEntityInterface
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $phone;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return TestCustomerWithContactInformation
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     * @return TestCustomerWithContactInformation
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+}
