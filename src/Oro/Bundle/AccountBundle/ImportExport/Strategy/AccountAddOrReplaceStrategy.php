c5c57df5b200338c25bf67e42f6b6b499be35666
Aleksandr Parkhomenko <alexandr-parkhomenko@users.noreply.github.com>
2022-10-07 17:06:12 +0300

    BB-21810: Duplicated accounts are created by import (4.2) (#33919)

--- src/Oro/Bundle/AccountBundle/ImportExport/Strategy/AccountAddOrReplaceStrategy.php
+++ src/Oro/Bundle/AccountBundle/ImportExport/Strategy/AccountAddOrReplaceStrategy.php
@@ -2,12 +2,35 @@
 
 namespace Oro\Bundle\AccountBundle\ImportExport\Strategy;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Util\ClassUtils;
 use Oro\Bundle\AccountBundle\Entity\Account;
+use Oro\Bundle\ContactBundle\Entity\Contact;
 use Oro\Bundle\ImportExportBundle\Strategy\Import\ConfigurableAddOrReplaceStrategy;
 
+/**
+ * Handles logic of changing values in fields "contacts" and "defaultContact" in case updating an existing entity
+ */
 class AccountAddOrReplaceStrategy extends ConfigurableAddOrReplaceStrategy
 {
+    protected function importEntityFields($entity, $existingEntity, $isFullData, $entityIsRelation, $itemData)
+    {
+        $entity = parent::importEntityFields($entity, $existingEntity, $isFullData, $entityIsRelation, $itemData);
+
+        /**
+         * Remove duplicate contacts that caused issues with a duplicate unique key.
+         */
+        if ($entity instanceof Account && !$existingEntity instanceof Account) {
+            $contacts = $entity->getContacts()->toArray();
+            $entity->setContacts(new ArrayCollection());
+            foreach ($contacts as $contact) {
+                $entity->addContact($contact);
+            }
+        }
+
+        return $entity;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -19,25 +42,54 @@ class AccountAddOrReplaceStrategy extends ConfigurableAddOrReplaceStrategy
     ) {
         // manually handle recursive relation to contacts
         $entityName = ClassUtils::getClass($entity);
-        $fieldName = 'contacts';
 
-        if ($entity instanceof Account
-            && $existingEntity instanceof Account
-            && !$this->isFieldExcluded($entityName, $fieldName, $itemData)
-            && !in_array($fieldName, $excludedFields)
-        ) {
-            foreach ($existingEntity->getContacts() as $contact) {
-                $existingEntity->removeContact($contact);
+        if ($entity instanceof Account && $existingEntity instanceof Account) {
+            if (!$this->isFieldExcluded($entityName, 'defaultContact', $itemData)) {
+                $this->processChangesInFieldDefaultContact($entity, $existingEntity);
+
+                $excludedFields[] = 'defaultContact';
             }
 
-            foreach ($entity->getContacts() as $contact) {
-                $contact->removeAccount($entity);
-                $existingEntity->addContact($contact);
-            }
+            if (!$this->isFieldExcluded($entityName, 'contacts', $itemData) &&
+                !in_array('contacts', $excludedFields, true)) {
+                $this->processChangesInFieldContacts($entity, $existingEntity);
 
-            $excludedFields[] = $fieldName;
+                $excludedFields[] = 'contacts';
+            }
         }
 
         parent::importExistingEntity($entity, $existingEntity, $itemData, $excludedFields);
+    }
+
+    private function processChangesInFieldDefaultContact(Account $entity, Account $existingEntity): void
+    {
+        $defaultContact = $entity->getDefaultContact();
+        $existingDefaultContact = $existingEntity->getDefaultContact();
+        if ($existingDefaultContact instanceof Contact &&
+            $entity->getDefaultContact() !== $existingEntity->getDefaultContact()) {
+            $existingDefaultContact->removeDefaultInAccount($existingEntity);
+            $existingDefaultContact->removeAccount($existingEntity);
+        }
+
+        if ($defaultContact instanceof Contact) {
+            $defaultContact->removeDefaultInAccount($entity);
+            $defaultContact->removeAccount($entity);
+            $existingEntity->setDefaultContact($defaultContact);
+        }
+    }
+
+    private function processChangesInFieldContacts(Account $entity, Account $existingEntity): void
+    {
+        foreach ($existingEntity->getContacts() as $contact) {
+            if ($existingEntity->getDefaultContact() === $contact) {
+                continue;
+            }
+            $existingEntity->removeContact($contact);
+        }
+
+        foreach ($entity->getContacts() as $contact) {
+            $contact->removeAccount($entity);
+            $existingEntity->addContact($contact);
+        }
     }
 }
