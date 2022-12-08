426afb1ee7f42d901b6e55d85552b9d8390806c2
Stevenson Kuo <42029775+StevensonKuo@users.noreply.github.com>
2022-12-08 21:34:56 +0800

    BB-21871: Contact import skips duplicated data in some fields - 5.0-2 (#34309)

--- src/Oro/Bundle/ContactBundle/ImportExport/Strategy/ContactAddOrReplaceStrategy.php
+++ src/Oro/Bundle/ContactBundle/ImportExport/Strategy/ContactAddOrReplaceStrategy.php
@@ -3,6 +3,8 @@
 namespace Oro\Bundle\ContactBundle\ImportExport\Strategy;
 
 use Oro\Bundle\ContactBundle\Entity\Contact;
+use Oro\Bundle\ContactBundle\Entity\ContactAddress;
+use Oro\Bundle\ContactBundle\Entity\ContactPhone;
 use Oro\Bundle\ImportExportBundle\Strategy\Import\ConfigurableAddOrReplaceStrategy;
 
 /**
@@ -10,12 +12,9 @@ use Oro\Bundle\ImportExportBundle\Strategy\Import\ConfigurableAddOrReplaceStrate
  */
 class ContactAddOrReplaceStrategy extends ConfigurableAddOrReplaceStrategy
 {
-    /**
-     * @var ContactImportHelper
-     */
-    protected $contactImportHelper;
+    protected ContactImportHelper $contactImportHelper;
 
-    public function setContactImportHelper(ContactImportHelper $contactImportHelper)
+    public function setContactImportHelper(ContactImportHelper $contactImportHelper): void
     {
         $this->contactImportHelper = $contactImportHelper;
     }
@@ -23,7 +22,7 @@ class ContactAddOrReplaceStrategy extends ConfigurableAddOrReplaceStrategy
     /**
      * {@inheritdoc}
      */
-    protected function beforeProcessEntity($entity)
+    protected function beforeProcessEntity($entity): Contact
     {
         /** @var Contact $entity */
         $entity = parent::beforeProcessEntity($entity);
@@ -47,7 +46,7 @@ class ContactAddOrReplaceStrategy extends ConfigurableAddOrReplaceStrategy
     /**
      * {@inheritdoc}
      */
-    protected function afterProcessEntity($entity)
+    protected function afterProcessEntity($entity): Contact
     {
         /** @var Contact $entity */
         $entity = parent::afterProcessEntity($entity);
@@ -61,17 +60,25 @@ class ContactAddOrReplaceStrategy extends ConfigurableAddOrReplaceStrategy
     }
 
     /**
+     * Since addresses and phones are always unique for a specific contact and do not have unique identifiers,
+     * there is no point in using them again. Also see method: storeNewEntity
+     *
      * {@inheritdoc}
      */
-    protected function findEntityByIdentityValues($entityName, array $identityValues)
+    protected function findEntityByIdentityValues($entityName, array $identityValues): ?object
     {
+        if (is_a($entityName, ContactPhone::class, true)
+            || is_a($entityName, ContactAddress::class, true)
+        ) {
+            return null;
+        }
+
         // contact last name and first name must be in this order to support compound index
         if (is_a($entityName, Contact::class, true)) {
             if (array_key_exists('firstName', $identityValues) && array_key_exists('lastName', $identityValues)) {
                 $firstName = $identityValues['firstName'];
                 $lastName = $identityValues['lastName'];
-                unset($identityValues['firstName']);
-                unset($identityValues['lastName']);
+                unset($identityValues['firstName'], $identityValues['lastName']);
                 $identityValues = array_merge(
                     array('lastName' => $lastName, 'firstName' => $firstName),
                     $identityValues
@@ -79,6 +86,16 @@ class ContactAddOrReplaceStrategy extends ConfigurableAddOrReplaceStrategy
             }
         }
 
+
         return parent::findEntityByIdentityValues($entityName, $identityValues);
+    }
+
+    protected function storeNewEntity(object $entity, array $identityValues = null): ?object
+    {
+        if (is_a($entity, ContactPhone::class, true) || is_a($entity, ContactAddress::class, true)) {
+            return null;
+        }
+
+        return parent::storeNewEntity($entity, $identityValues);
     }
 }
