55364a78ac4da6fcd9b28cbe3d23bdd6acf3e6b7
Vova Soroka <vsoroka@oroinc.com>
2023-08-29 20:42:43 +0300

    CRM-9412: "oro:activity-contact:recalculate" command failed after installation (#36268)

--- src/Oro/Bundle/ActivityContactBundle/Migrations/Data/ORM/ActivityContactRecalculate.php
+++ src/Oro/Bundle/ActivityContactBundle/Migrations/Data/ORM/ActivityContactRecalculate.php
@@ -8,34 +8,25 @@ use Oro\Bundle\ActivityContactBundle\Command\ActivityContactRecalculateCommand;
 use Oro\Bundle\CronBundle\Async\Topic\RunCommandTopic;
 use Oro\Component\MessageQueue\Client\MessageProducerInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
 /**
  * Produces message to recalculate contacting activities.
  */
 class ActivityContactRecalculate extends AbstractFixture implements ContainerAwareInterface
 {
-    /** @var ContainerInterface */
-    protected $container;
+    use ContainerAwareTrait;
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         /** @var MessageProducerInterface $producer */
         $producer = $this->container->get('oro_message_queue.client.message_producer');
         $producer->send(RunCommandTopic::getName(), [
             'command' => ActivityContactRecalculateCommand::getDefaultName(),
-            'arguments' => ['-v' => 1, '--disabled-listeners' => ['all']]
+            'arguments' => ['-v' => true, '--disabled-listeners' => ['all']]
         ]);
     }
 }
