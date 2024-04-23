39c2d7b11c28df2c5424622c9d32f8fdd0ab6bd4
timofiyprisyazhnyuk <33844707+timofiyprisyazhnyuk@users.noreply.github.com>
2024-04-23 12:38:10 +0300

    BAP-22515: Entity cannot have more than one Multi-Target Extended Associations (#37902)
    BAP-22515: Entity cannot have more than one Multi-Target Extended Associations
    - Fixed bug with multi-target extend entity associations

--- src/Oro/Bundle/SalesBundle/EntityExtend/CustomerEntityFieldExtension.php
+++ src/Oro/Bundle/SalesBundle/EntityExtend/CustomerEntityFieldExtension.php
@@ -7,6 +7,7 @@ namespace Oro\Bundle\SalesBundle\EntityExtend;
 use Oro\Bundle\EntityExtendBundle\EntityExtend\AbstractAssociationEntityFieldExtension;
 use Oro\Bundle\EntityExtendBundle\EntityExtend\EntityFieldProcessTransport;
 use Oro\Bundle\EntityExtendBundle\Extend\RelationType;
+use Oro\Bundle\EntityExtendBundle\Tools\AssociationNameGenerator;
 use Oro\Bundle\SalesBundle\Entity\Customer;
 use Oro\Bundle\SalesBundle\EntityConfig\CustomerScope;
 
@@ -15,17 +16,18 @@ use Oro\Bundle\SalesBundle\EntityConfig\CustomerScope;
  */
 class CustomerEntityFieldExtension extends AbstractAssociationEntityFieldExtension
 {
-    protected function isApplicable(EntityFieldProcessTransport $transport): bool
+    public function isApplicable(EntityFieldProcessTransport $transport): bool
     {
-        return $transport->getClass() === Customer::class;
+        return $transport->getClass() === Customer::class
+            && AssociationNameGenerator::extractAssociationKind($transport->getName()) === $this->getRelationKind();
     }
 
-    protected function getRelationKind(): ?string
+    public function getRelationKind(): ?string
     {
         return CustomerScope::ASSOCIATION_KIND;
     }
 
-    protected function getRelationType(): string
+    public function getRelationType(): string
     {
         return RelationType::MANY_TO_ONE;
     }
