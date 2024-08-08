636de57890890a20f7caedb0e48aa36ed7a5a011
Vova Soroka <vsoroka@oroinc.com>
2024-08-08 09:12:18 +0300

    BB-24385: Failed functional test /package/customer-portal/src/Oro/Bundle/CustomerBundle/Tests/Functional/Api/RestJsonApi/CustomerUserAddressUpdateListTest.php (#39067)

--- src/Oro/Bundle/SalesBundle/Tests/Functional/Api/RestJsonApi/LeadAddressUpdateListTest.php
+++ src/Oro/Bundle/SalesBundle/Tests/Functional/Api/RestJsonApi/LeadAddressUpdateListTest.php
@@ -20,6 +20,28 @@ class LeadAddressUpdateListTest extends RestJsonApiUpdateListTestCase
         ]);
     }
 
+    private function getLeadAddressId(string $street): int
+    {
+        /** @var LeadAddress|null $address */
+        $address = $this->getEntityManager()->getRepository(LeadAddress::class)
+            ->findOneBy(['street' => $street]);
+        if (null === $address) {
+            throw new \RuntimeException(sprintf('The address "%s" not found.', $street));
+        }
+
+        return $address->getId();
+    }
+
+    private function getLeadAddressIndex(array $data, string $street): int
+    {
+        foreach ($data as $i => $item) {
+            if ($item['attributes']['street'] === $street) {
+                return $i;
+            }
+        }
+        throw new \RuntimeException(sprintf('The address "%s" not found.', $street));
+    }
+
     public function testCreateEntities(): void
     {
         $data = [
@@ -72,6 +94,8 @@ class LeadAddressUpdateListTest extends RestJsonApiUpdateListTestCase
             ['entity' => 'leads', 'id' => '<toString(@lead1->id)>'],
             ['include' => 'addresses']
         );
+        $address1Id = $this->getLeadAddressId('15a Lewis Circle');
+        $address2Id = $this->getLeadAddressId('16 Lewis Circle');
         $responseContent = [
             'data'     => [
                 'type'          => 'leads',
@@ -79,19 +103,20 @@ class LeadAddressUpdateListTest extends RestJsonApiUpdateListTestCase
                 'relationships' => [
                     'addresses' => [
                         'data' => [
-                            ['type' => 'leadaddresses', 'id' => 'new'],
-                            ['type' => 'leadaddresses', 'id' => 'new']
+                            ['type' => 'leadaddresses', 'id' => (string)$address1Id],
+                            ['type' => 'leadaddresses', 'id' => (string)$address2Id]
                         ]
                     ]
                 ]
             ],
             'included' => $data['data']
         ];
-        $responseContent['included'][0]['id'] = 'new';
-        $responseContent['included'][0]['attributes']['primary'] = true;
-        $responseContent['included'][1]['id'] = 'new';
-        $responseContent['included'][1]['attributes']['primary'] = false;
-        $responseContent = $this->updateResponseContent($responseContent, $response);
+        $address1Index = $this->getLeadAddressIndex($responseContent['included'], '15a Lewis Circle');
+        $address2Index = $this->getLeadAddressIndex($responseContent['included'], '16 Lewis Circle');
+        $responseContent['included'][$address1Index]['id'] = (string)$address1Id;
+        $responseContent['included'][$address1Index]['attributes']['primary'] = true;
+        $responseContent['included'][$address2Index]['id'] = (string)$address2Id;
+        $responseContent['included'][$address2Index]['attributes']['primary'] = false;
         $this->assertResponseContains($responseContent, $response);
     }
 }
